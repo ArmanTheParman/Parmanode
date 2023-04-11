11e4596f4f086d970457f9e9fa257b83938dee63
Parman <armantheparman@gmail.com>
2023-04-11 20:43:00 +1000

    syntax corrections

--- src/general_functions/exit_choice.sh
+++ src/general_functions/exit_choice.sh
@@ -1,21 +1,19 @@
+
 #USAGE:
 #exit_choice ; if [[ $? == 1 ]]; then return 1 ; fi 
+#if a case for $choice exists immediately after the function, must include empty string as an option
 
 function exit_choice {
 read choice
-
 case $choice in
 
-q|QUIT|Q|quit)
-exit 0 ;;
+    q|QUIT|Q|quit)
+    exit 0 ;;
 
-p|P)
-return 1 ;;
+    p|P)
+    return 1 ;;
 
-"")
-return 2 ;;
+    esac
 
-esac
-
-return 3
+return 2 
 }
