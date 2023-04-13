9246840adaefdd9c01c36dc26eedc1a674261099
Parman <armantheparman@gmail.com>
2023-04-14 02:10:29 +1000

    Fixed dependency issues and wget issues

---
+++ src/Mac/brew_check.sh
@@ -0,0 +1,50 @@
+function brew_check {
+
+if command -v brew >/dev/null 2>&1i 
+    then
+	return 0
+    else
+while true ; do
+set_terminal
+echo "
+########################################################################################
+
+
+                                     Homebrew
+
+
+   In order to install certain dependencies (these are fragments of code that Bitcoin
+   needs to function) Homebrew is needed. Homebrew is a package manager (a program
+   that manages certain other programs' installation and maintenance on your system
+   via web servers where the software is delievered to you). 
+
+   Parmanode has detected that Homebrew has not been installed on your system before.
+   It will do this for you now, with your approval. It doesn't harm or slow down
+   your system, but it is a real pain to download - it's going to take a while. Make
+   sure your computer doesn't go into hybernation during the process to save power. It
+   may take around 30 minutes, speaking from experience (I have an old Mac).
+   
+				i)    Install Homebrew 
+				
+				s)    Skip
+
+########################################################################################
+"
+choose "epq" ; read choice
+if [[ $choice == "p" ]] ; then return 1 ; fi
+if [[ $choice == "q" ]] ; then exit 0 ; fi
+if [[ $choice == "s" ]] ; then return 0 ; fi
+if [[ $choice == "" ]] ; then break ; fi
+invalid
+done
+
+# User chose <enter>, while breaks to here:
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && return 0
+
+set_terminal
+echo "Download using curl failed. Unknown error. You should try again. 
+Proceed with caution."
+return 0
+}
+        
+
