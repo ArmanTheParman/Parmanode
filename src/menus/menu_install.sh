9c34a37d0d34b9e1824de5881994dae3115dffe6
Parman <armantheparman@gmail.com>
2023-04-14 02:50:11 +1000

    added IP address check for user

--- src/menus/menu_install.sh
+++ src/menus/menu_install.sh
@@ -6,15 +6,20 @@ set_terminal
 echo "
 ########################################################################################
 
+                                   P A R M A N O D E
+
+
                     i)          Install Parmanode ......... (Must install first)
 
                     u)          Uninstall Parmanode
 
                     pp)         Parmanode Premium
 
-                    a)          Add more program .......... (Can install Bitcoin here)
+                    a)          Add more programs ......... (Can install Bitcoin here)
 
                     r)          Remove programs
+
+                    ip)         What's my computer's IP address?
 
 ########################################################################################
 "
@@ -44,6 +49,10 @@ a)
     return 0;;
 r|R)
     remove_programs
+    continue
+    ;;
+ip|IP|iP|Ip)
+    IP_address
     continue
     ;;
 quit | QUIT | q | Q)
