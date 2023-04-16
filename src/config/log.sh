0b015acd1e37a887ec192a00c1b17fcdc4757233
Parman <armantheparman@gmail.com>
2023-04-17 00:52:46 +1000

    working on fulcrum install

---
+++ src/config/log.sh
@@ -0,0 +1,14 @@
+# Usage: Argument 1 should be the name of the program. Argument 2 is the message.
+#        Argument 2 can be "delte" which will remove the file name "argument1".log
+
+function install_log {
+
+if [[ $2 == "delete" ]] ; then
+rm $HOME/.parmanode/"$1".log
+fi
+
+
+echo date "$2" >> $HOME/.parmanode/"$1".log
+
+
+}
---
+++ src/fulcrum/uninstall_fulcrum.sh
@@ -0,0 +1,10 @@
+function uninstall_fulcrum {
+
+
+
+
+
+
+delete_line "$HOME/.parmanode/parmanode.conf" "drive_fulcrum="
+
+}
