de2cb71266874bf43b0756d8dc5cec704f725112
Parman <armantheparman@gmail.com>
2023-04-17 02:04:26 +1000

    more fulcrum work. config file done.

--- src/fulcrum/fulcrum_make_directories.sh
+++ src/fulcrum/fulcrum_make_directories.sh
@@ -1,10 +1,15 @@
 function fulcrum_make_directories {
 
 # Make parmanode/fulcrum directory on internal drive
-mkdir $HOME/parmanode/fulcrum >/dev/null 2>&1 && \
-installed_config_add "fulcrum-start"
+    mkdir $HOME/parmanode/fulcrum >/dev/null 2>&1 && \
+    installed_config_add "fulcrum-start"
 
-if [[ $drive_fulcrim == "external" ]] ; then
+
+
+
+# Make fulcrum_db on the internal or external drive
+
+if [[ $drive_fulcrum == "external" ]] ; then
 
 set_terminal ; echo "
 ########################################################################################
@@ -15,8 +20,23 @@ set_terminal ; echo "
 "
 enter_continue ; mount_drive && log "fulcrum" "Drive mounted."
 
-mkdir /media/$(whoami)/parmanode/fulcrum_db >/dev/null 2>&1 || log "fulcrum" "mkdir fulcrum_db on ext drive failed."
+case $OS in
+    Linux) mkdir /media/$(whoami)/parmanode/fulcrum_db >/dev/null 2>&1 && log "fulcrum" "fulcrum_db made - external" && return 0 ;;
+    Mac ) mkdir /Volumes/parmanode/fulcrum_db >/dev/null 2>&1 && log "fulcrum" "fulcrum_db made - external" && return 0 ;;
+    *) log "fulcrum" "mkdir fulcrum_db on ext drive failed."    
+       debug "Failed to make fulcrum_db. Continue with caution." 
+       return 1 ;;
+    esac
+fi
 
-return 0
+
+
+
+if [[ $drive_fulcrum == "internal" ]] ; then
+    
+    { mkdir $HOME/parmanode/fulcrum_db >/dev/null 2>&1 && log "fulcrum" "fulcrum_db made - internal" ; } || \
+    { debug "mkdir fulcrum_db on internal drive failed. Continue with caution." && return 1 ; } 
+    return 0
+    fi
 }
 
