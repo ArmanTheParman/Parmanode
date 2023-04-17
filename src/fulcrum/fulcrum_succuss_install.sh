677d8e6c023fd86d84f230eb145e1dec29fb0de6
Parman <armantheparman@gmail.com>
2023-04-18 02:10:32 +1000

    added fulcrum files

---
+++ src/fulcrum/fulcrum_succuss_install.sh
@@ -0,0 +1,15 @@
+function fulcrum_success_install {
+    
+set_terminal ; echo "
+########################################################################################
+    
+                                    SUCCESS !!!
+
+    Fulcrum will begin syncing after a reboot, but only if Bitcoin Core has finished
+    syncing. You can also start Fulcrum from the Parmanode menu.
+
+########################################################################################
+" && installed_config_add "fulcrum-end"
+enter_continue
+return 0 
+}
