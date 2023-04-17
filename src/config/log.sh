2e38a78dc40120bd7ad1b7c954b339018685d0db
Parman <armantheparman@gmail.com>
2023-04-17 18:21:13 +1000

    log function naming error fixed

--- src/config/log.sh
+++ src/config/log.sh
@@ -1,7 +1,7 @@
 # Usage: Argument 1 should be the name of the program. Argument 2 is the message.
 #        Argument 2 can be "delte" which will remove the file name "argument1".log
 
-function install_log {
+function log {
 
 if [[ $2 == "delete" ]] ; then
 rm $HOME/.parmanode/"$1".log
