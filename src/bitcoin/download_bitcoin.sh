79e2eb0bde886461da5f811e77a8fd86e601fd56
Parman <armantheparman@gmail.com>
2023-04-17 23:46:18 +1000

    more curl, less wget

--- src/bitcoin/download_bitcoin.sh
+++ src/bitcoin/download_bitcoin.sh
@@ -68,20 +68,20 @@ read #using custom function "enter_continue" here produced a strange error I don
      #line) without any user input to continue."
 
 set_terminal ; echo "Downloading Bitcoin files to $HOME/parmanode/bitcoin ..."
-wget https://bitcoincore.org/bin/bitcoin-core-24.0.1/SHA256SUMS 
-wget https://bitcoincore.org/bin/bitcoin-core-24.0.1/SHA256SUMS.asc 
+curl -LO https://bitcoincore.org/bin/bitcoin-core-24.0.1/SHA256SUMS 
+crul -LO https://bitcoincore.org/bin/bitcoin-core-24.0.1/SHA256SUMS.asc 
 
 # ARM Pi4 support. If not, checks for 64 bit x86.
 
 	chip="$(uname -m)" >/dev/null 2>&1
 	    if [[ $chip == "armv7l" || $chip == "armv8l" ]] ; then 		#32 bit Pi4
-		wget https://bitcoincore.org/bin/bitcoin-core-24.0.1/bitcoin-24.0.1-arm-linux-gnueabihf.tar.gz ; fi
+		curl -LO https://bitcoincore.org/bin/bitcoin-core-24.0.1/bitcoin-24.0.1-arm-linux-gnueabihf.tar.gz ; fi
 
 	    if [[ $chip == "aarch64" ]] ; then 				#64 bit Pi4 
-		wget https://bitcoincore.org/bin/bitcoin-core-24.0.1/bitcoin-24.0.1-aarch64-linux-gnu.tar.gz ; fi
+		curl -LO https://bitcoincore.org/bin/bitcoin-core-24.0.1/bitcoin-24.0.1-aarch64-linux-gnu.tar.gz ; fi
 		
 	    if [[ $chip == "x86_64" ]] ; then 
-		wget https://bitcoincore.org/bin/bitcoin-core-24.0.1/bitcoin-24.0.1-x86_64-linux-gnu.tar.gz ; fi
+		curl -LO https://bitcoincore.org/bin/bitcoin-core-24.0.1/bitcoin-24.0.1-x86_64-linux-gnu.tar.gz ; fi
 
 sha256sum --ignore-missing --check SHA256SUMS
 
