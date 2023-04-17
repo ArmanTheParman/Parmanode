4b906b59b3e164f0e7d54f0b979784e053a3761c
Parman <armantheparman@gmail.com>
2023-04-17 16:17:25 +1000

    syntax errors fixed

--- src/fulcrum/download_fulcrum.sh
+++ src/fulcrum/download_fulcrum.sh
@@ -7,14 +7,14 @@ if [[OS == "Linux" ]] ; then
         if [[ $chip == "x86_64" ]] ; then
             curl -LO https://github.com/cculianu/Fulcrum/releases/download/v1.9.1/Fulcrum-1.9.1-x86_64-linux.tar.gz && \
             curl -LO https://github.com/cculianu/Fulcrum/releases/download/v1.9.1/Fulcrum-1.9.1-x86_64-linux.tar.gz.asc && \
-            log "fulcrum" "files downloaded" && return 0 
+            log "fulcrum" "files downloaded" && return 0 \
             || { debug "Failed to download Fulcrum" ; return 1 ; }
             fi
 
 	    if [[ $chip == "aarch64" ]] ; then 				#64 bit Pi4 
             curl -LO https://github.com/cculianu/Fulcrum/releases/download/v1.9.1/Fulcrum-1.9.1-arm64-linux.tar.gz && \
             curl -LO https://github.com/cculianu/Fulcrum/releases/download/v1.9.1/Fulcrum-1.9.1-arm64-linux.tar.gz.asc && \
-            log "fulcrum" "files downloaded" && return 0 
+            log "fulcrum" "files downloaded" && return 0 \
             || { debug "Failed to download Fulcrum" ; return 1 ; }
             fi
 fi
