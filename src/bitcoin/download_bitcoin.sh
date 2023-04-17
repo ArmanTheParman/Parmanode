fb054272ac9cc14423ddb45a179c5efa9f5b8135
Parman <armantheparman@gmail.com>
2023-04-17 23:07:03 +1000

    stray accidental unnecessary command pauses bitcoin download - fixed

--- src/bitcoin/download_bitcoin.sh
+++ src/bitcoin/download_bitcoin.sh
@@ -74,7 +74,6 @@ wget https://bitcoincore.org/bin/bitcoin-core-24.0.1/SHA256SUMS.asc
 # ARM Pi4 support. If not, checks for 64 bit x86.
 
 	chip="$(uname -m)" >/dev/null 2>&1
-	    read choice 
 	    if [[ $chip == "armv7l" || $chip == "armv8l" ]] ; then 		#32 bit Pi4
 		wget https://bitcoincore.org/bin/bitcoin-core-24.0.1/bitcoin-24.0.1-arm-linux-gnueabihf.tar.gz ; fi
 
