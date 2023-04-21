439a6959b49eeab7f08b61f0266681cb062cd5a1
Parman <armantheparman@gmail.com>
2023-04-21 17:47:13 +1000

    more work

--- src/docker/install_docker.sh
+++ src/docker/install_docker.sh
@@ -1,19 +1,44 @@
 function install_docker {
-docker_install_check 
-    if [ $? = 1 ] ; then log "docker" "docker already installed" ; return 1 ; fi
 
+#Docker Installation
 set_terminal ; echo "
 ########################################################################################
 
-                               Installing Docker...
+                                Docker Insallation
+
+    There is no Fulcrum executable available yet for Mac computers. To get around
+    this, Parmanode will install a Docker on your system, and run a Linux container
+    on your Mac which you can manage through the Parmanode program. Fulcrum will be
+    intstalled on the Linux container which your computer can access.
+    
+    If the automated Parmanode installation of Docker fails, you could download and
+    install Docker yourself, and still be able to access its functionality through 
+    Parmanode's menus.
+
+                            i)      Install Docker 
 
 ########################################################################################
-
 "
-enter_continue
+choose "xpq" ; read choice
+while true ; do
+case $choice in q|Q|Quit|QUIT) exit 0 ;; p|P) return 0 ;; 
+i|I)
+    break
+    ;; 
+*) invalid ;; 
+esac
 
+done
 
-echo "
+docker_install_check  
+    if [ $? = 2 ] ; then 
+        log "docker" "Docker is already installed. Returning to menu." 
+        echo ""
+        echo "Docker is already instaled. Returning to menu."
+        return 0 
+        fi
+
+set_terminal ; echo "
 ########################################################################################
 
                                Downloading Docker...
@@ -26,8 +51,8 @@ download_docker
     if [ $? = 1 ] ; then return 1 ; fi
 
 first_start_docker
+    if [ $? = 1 ] ; then return 1 ; fi
 
 return 0
-
 }
 
--- src/docker/install_fulcrum_mac.sh
+++ src/docker/install_fulcrum_mac.sh
@@ -7,7 +7,7 @@ install_check "fulcrum-start"
   if [[ $return_value = "1" ]] ; then return 1 ; fi       #Fulcrum already installed
   log "fulcrum" "install check passed."
 
- fulcrum_drive_selection
+fulcrum_drive_selection
   if [[ $? == 1 ]] ; then return 1 ; fi
   log "fulcrum" "drive seletected as $drive_fulcrum"
 
@@ -15,42 +15,14 @@ fulcrum_make_directories
   if [[ $? == 1 ]] ; then return 1 ; fi
   log "fulcrum" "make directories function exited."
 
+install_docker
+  if [[ $? == 1 ]] ; then return 1 ; fi
 
-set_terminal ; echo "
-########################################################################################
+build_fulcrum_docker
+  if [[ $? == 1 ]] ; then return 1 ; fi
 
-                          Fulcrum Insallation : Mac (Docker)
+run_fulcrum_docker
 
-    There is no Fulcrum executable available yet for Mac computers. To get around
-    this, Parmanode will install a Docker on your system, and run a Linux container
-    on your Mac which you can manage through the Parmanode program. Fulcrum will be
-    intstalled on the Linux container which your computer can access.
-    
-    If the automated Parmanode installation of Docker fails, you could download and
-    install Docker yourself, and still be able to access its functionality through 
-    Parmanode's menus.
-
-                            i)      Install Docker 
-
-########################################################################################
-"
-choose "xpq" ; read choice
-while true ; do
-case $choice in q|Q|Quit|QUIT) exit 0 ;; p|P) return 0 ;; 
-
-i|I)
-    install_docker_mac
-    build_fulcrum_docker
-    drive_choice_fulcrum_docker
-    run_fulcrum_docker
     edit_user_pass_fulcrum_docker 
-    return 0
-    ;; 
-
-*) invalid ;; 
-esac
-
-done
-
 return 0
 }
