040ebe19c2c5882c6de06e18ed4c072e8c3df136
Parman <armantheparman@gmail.com>
2023-04-23 03:57:14 +1000

    sourcing issue fixed

--- src/docker/edit_user_pass_fulcrum_docker.sh
+++ src/docker/edit_user_pass_fulcrum_docker.sh
@@ -6,7 +6,8 @@ rpcpassword=$(grep -w "rpcpassword" $HOME/.bitcoin/bitcoin.conf | awk -F '=' '{p
 
 if docker ps | grep fulcrum >/dev/null 2>&1 ; then
     { docker exec -d -u parman fulcrum /bin/bash -c \
-    "source ~/.bashrc && edit_user_pass_fulcrum_conf_indocker $rpcuser $rpcpassword" \
+    "source /home/parman/parmanode/src/edit_user_pass_fulcrum_conf_indocker.sh ; \
+    edit_user_pass_fulcrum_conf_indocker $rpcuser $rpcpassword" \
     && log "fulcrum" "docker exec edit user pass fulcrum indocker has run" ; } \
     || { log "fulcrum" "Failed to run edit_user_pass_fulcrum_conf_indocker" && return 1 ; }
     fi
--- src/functions/set_rpc_authentication.sh
+++ src/functions/set_rpc_authentication.sh
@@ -1,6 +1,6 @@
 function set_rpc_authentication {
 while true ; do
-set_terminal_bit_higher ; echo "
+set_terminal ; echo "
 ########################################################################################
 
                            Bitcoin Core RPC Authentication
