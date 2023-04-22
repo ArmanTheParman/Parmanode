a5d626d01ca66dcab13b0f251f8570911f760818
Parman <armantheparman@gmail.com>
2023-04-23 03:29:42 +1000

    user pass edits

--- src/docker/edit_user_pass_fulcrum_docker.sh
+++ src/docker/edit_user_pass_fulcrum_docker.sh
@@ -5,7 +5,9 @@ rpcuser=$(grep -w "rpcuser" $HOME/.bitcoin/bitcoin.conf | awk -F '=' '{print $2}
 rpcpassword=$(grep -w "rpcpassword" $HOME/.bitcoin/bitcoin.conf | awk -F '=' '{print $2}')
 
 if docker ps | grep fulcrum >/dev/null 2>&1 ; then
-    { docker exec -d -u parman fulcrum /bin/bash -c "edit_user_pass_fulcrum_conf_indocker $rpcuser $rpcpassword" \
+    { docker exec -d -u parman fulcrum /bin/bash -c \
+    "source /home/parman/parmanode/src/edit_user_pass_fulcrum_conf_indocker.sh ; \
+    edit_user_pass_fulcrum_conf_indocker $rpcuser $rpcpassword" \
     && log "fulcrum" "docker exec edit user pass fulcrum indocker has run" ; } \
     || { log "fulcrum" "Failed to run edit_user_pass_fulcrum_conf_indocker" && return 1 ; }
     fi
