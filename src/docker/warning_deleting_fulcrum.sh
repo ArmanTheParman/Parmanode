ebbe9b69e18c0436d488db11f36dbb46f8606fa0
Parman <armantheparman@gmail.com>
2023-04-22 21:19:45 +1000

    smol edits

--- src/docker/warning_deleting_fulcrum.sh
+++ src/docker/warning_deleting_fulcrum.sh
@@ -3,15 +3,16 @@ function warning_deleting_fulcrum {
 set_terminal ; echo "
 ########################################################################################
 
-WARNING: If you have a previous Fulcrum container, this installation will delete it,
-and any Fulcrum images. We're starting fresh, too bad. If you want to preserve any old
-data, you should quit now, and back them up. If you don't know how... internet  ;p
+    WARNING: If you have a previous Fulcrum Docker container, this installation will 
+    delete it, and any Fulcrum images. We're starting fresh, too bad. If you want to 
+    preserve any old data, you should quit now, and back them up. If you don't know 
+    how... see internet  ;p
 
-                               <enter> to continue
+                                <enter> to continue
 
 ########################################################################################
 "
 choose "xpq" ; read choice
- while true ; do case $choice in q|Q|QUIT|Quit) exit 0 ;; p|P) return 1 ;; "") break ;; *) invalid ;; esac ; done
-
+while true ; do case $choice in q|Q|QUIT|Quit) exit 0 ;; p|P) return 1 ;; "") break ;; *) invalid ;; esac ; done
+return 0
 }
