3e52b8d12ba76dd8589773ec6752d2ae3a5f2d84
Parman <armantheparman@gmail.com>
2023-04-23 14:24:44 +1000

    uninstall fulcrum touch up

--- src/docker/uninstall_fulcrum_docker.sh
+++ src/docker/uninstall_fulcrum_docker.sh
@@ -23,18 +23,33 @@ if ! grep -q "fulcrum" $HOME/.parmanode/installed.conf >/dev/null 2>&1 ; then
     return 1 
     fi
 
+log "fulcrum" "uninstall commenced"
+
+if [[ $drive_fulcrum == "external" ]] ; then
+    mount_drive || { set_terminal ; echo "drive needs to be mounted to remove fulcrum_db from drive. Proceed with caution." ; \
+    enter_continue ; log "fulcrum" "drive not mounted, fulcrum_db  not deleted during uninstall." ; }
+    rm -rf /media/$(whoami)/parmanode/fulcrum_db || debug "failed to delete fulcrum_db."
+    fi
+rm -rf /Volumes/parmanode/fulcrum_db >/dev/null 2>&1 && log "fulcrum" "fulcrum_db removed from ext drive."
+
+if [[ $drive_fulcrum == "internal" ]] ; then
+    rm -rf $HOME/parmanode/fulcrum_db
+    fi
+
 rm -rf $HOME/parmanode/fulcrum >/dev/null 2>&1 
-rm -rf $HOME/parmmanode/fulcrum_db >/dev/null 2>&1 
-rm -rf /Volumes/parmanode/fulcrum_db >/dev/null 2>&1
 
 stop_and_remove_docker_containers_and_images_fulcrum
 
-delete_line "$HOME/.parmanode/parmanode.conf" "fulcrum"
-delete_line "$HOME/.parmanode/installed.conf" "fulcrum"
+parmanode_conf_remove "fulcrum"
+installed_config_remove "fulcrum"
+
+log "fulcrum" "uninstall completed." && { set_terminal ; echo "Fulcrum has been uninstalled." ; enter_continue ; return 0 ; }
 
 return 0
 
 }
+
+
 
 function stop_and_remove_docker_containers_and_images_fulcrum {
 
