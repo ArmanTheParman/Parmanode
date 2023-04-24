3b2eb415092afef17f4ea203d97c5edee022872a
Parman <armantheparman@gmail.com>
2023-04-24 20:27:45 +1000

    logs. Starting to work on Tor

---
+++ src/tor/install_tor.sh
@@ -0,0 +1,13 @@
+function install_tor {
+
+set_terminal ; echo "
+########################################################################################
+
+                                   Install Tor
+    
+    Parmanode will install Tor daemon (not browser). It runs in the background allowing
+    you to access the Tor network.
+
+########################################################################################    
+"
+}
