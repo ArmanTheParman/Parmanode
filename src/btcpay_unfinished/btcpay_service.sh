b8a5809b9a23b99eeec50ec8e06dbb9de46ebf0f
Parman <armantheparman@gmail.com>
2023-04-25 19:03:13 +1000

    btcpay

--- src/btcpay_unfinished/btcpay_service.sh
+++ src/btcpay_unfinished/btcpay_service.sh
@@ -1,22 +1,25 @@
+function btcpay_service {
+echo "
 [Unit]
 Description=BtcPayServer daemon
 Requires=btcpayserver.service
 After=nbxplorer.service
 
 [Service]
-ExecStart=/usr/bin/dotnet run --no-launch-profile --no-build -c Release -p "/home/satoshi/source/btcpayserver/BTCPayServer/BTCPayServer.csproj" -- $@
-User=satoshi
-Group=satoshi
+ExecStart=/usr/bin/dotnet run --no-launch-profile --no-build -c Release -p "$HOME/source/btcpayserver/BTCPayServer/BTCPayServer.csproj" -- $@
+User=$(whoami)
+Group=$(whoami)
 Type=simple
 PIDFile=/run/btcpayserver/btcpayserver.pid
 Restart=on-failure
 
 [Install]
-WantedBy=multi-user.target
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/btcpayserver.service
 
 
 
+# sudo systemctl enable btcpayserver.service
+# sudo service btcpayserver start
+# sudo service btcpayserver status
 
-sudo systemctl enable btcpayserver.service
-sudo service btcpayserver start
-sudo service btcpayserver status
+}
