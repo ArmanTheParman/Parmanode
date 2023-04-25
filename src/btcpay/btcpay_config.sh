fc35db58ec6805c55c9f795140fe9307ccc04005
Parman <armantheparman@gmail.com>
2023-04-26 01:57:53 +1000

    Changes to be committed: 	new file:   src/btcpay/btcpay_config.sh 	modified:   src/btcpay/install_btcpay.sh 	new file:   src/btcpay/nbxplorer_config.sh 	new file:   src/btcpay/run_btcpay_docker.sh 	new file:   todo.txt

---
+++ src/btcpay/btcpay_config.sh
@@ -0,0 +1,16 @@
+function btcpay_config {
+
+echo "
+network=mainnet
+port=23001
+bind=0.0.0.0
+chains=btc
+BTC.explorer.url=http://127.0.0.1:24445
+#BTC.lightning=... (do later)
+postgres=User ID=parman;Password=NietShitcoin;Host=localhost;Port=5432;Database=btcpayserver;
+" | tee $HOME/.btcpayserver/Main/settings.config || \
+    {log "btcpayserver" "failed to make settings.config" && \
+    log "btcpayserver" "failed to make settings.config" && errormessage && return 1 ; }
+
+log "btcpayserver" "end btcpayserver_config" && return 0
+}
---
+++ src/btcpay/nbxplorer_config.sh
@@ -0,0 +1,16 @@
+function nbxplorer_config {
+source <(cat $HOME/.bitcoin/bitcoin.conf | grep "rpcuser=")
+source <(cat $HOME/.bitcoin/bitcoin.conf | grep "rpcpassword=")
+source $HOME/.parmanode/parmanode.conf  #get postgres user and password
+
+echo "
+btc.rpc.auth=${rpcuser}:${rpcpassword}
+port=24445
+mainnet=1
+postgres=User ID=parman;Password=NietShitcoin;Host=localhost;Port=5432;Database=nbxplorer;
+" | tee $HOME/.nbxplorer/Main/settings.config || \
+    {log "nbxplorer" "failed to make settings.config" && \
+    log "nbxplorer" "failed to make settings.config" && errormessage && return 1 ; }
+
+log "nbxplorer" "end nbxplorer_config" && return 0
+}
