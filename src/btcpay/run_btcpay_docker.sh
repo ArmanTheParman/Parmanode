6bb0f9f1679a99dcbd447aa2897cc6127feb2844
Parman <armantheparman@gmail.com>
2023-04-28 17:23:28 +1000

    modified:   src/btcpay/run_btcpay_docker.sh 	modified:   src/tor/install_tor.sh

--- src/btcpay/run_btcpay_docker.sh
+++ src/btcpay/run_btcpay_docker.sh
@@ -5,7 +5,7 @@ docker run -d \
            -v $HOME/.btcpayserver:/home/parman/.btcpayserver \
            -v $HOME/.nbxplorer:/home/parman/.nbxplorer \
            -p 8080:8080 \
-           -p 23001:80 \
+           -p 23001:23001 \
            -p 49392:49392 \
            -p 5432:5432 \
            -p 9735:9735 \
