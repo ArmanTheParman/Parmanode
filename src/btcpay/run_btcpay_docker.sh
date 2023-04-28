81e21583a06fba83066ca6fd5bb4f32e038e7401
Parman <armantheparman@gmail.com>
2023-04-29 00:57:24 +1000

    modified:   src/btcpay/run_btcpay_docker.sh 	modified:   src/btcpay/uninstall_btcpay.sh

--- src/btcpay/run_btcpay_docker.sh
+++ src/btcpay/run_btcpay_docker.sh
@@ -11,7 +11,7 @@ docker run -d \
            -p 23001:23001 \
            -p 49392:49392 \
            -p 9735:9735 \
-           -p 8332:8332 \
+           -p 8336:8332 \
            btcpay && \
 log "btcpay" "docker run btcpay executed" && return 0 \
 || log "btcpay" "docker run btcpay failed" && return 1
