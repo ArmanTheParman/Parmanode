477ab815c2e048a6ffe66e55fecc641b1e89f6f0
Parman <armantheparman@gmail.com>
2023-04-28 21:27:16 +1000

    modified:   src/btcpay/postgres_functions.sh

--- src/btcpay/postgres_functions.sh
+++ src/btcpay/postgres_functions.sh
@@ -1,6 +1,6 @@
 # start postgress, create parman database user with script, create 2 databases.
 
-function #startup_postgres {
+function startup_postgres {
 docker exec -d -u postgres btcpay /bin/bash -c "/usr/bin/pg_ctlcluster 13 main start ; \
 /usr/local/bin/postgres_script.sh ; \
 createdb -O parman btcpayserver ; \
