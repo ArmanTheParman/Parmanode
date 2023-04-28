ad0e69e2d272bce0cd2041cd331cd4e132b21ff9
Parman <armantheparman@gmail.com>
2023-04-28 21:14:19 +1000

    modified:   src/btcpay/Dockerfile 	modified:   src/btcpay/install_btcpay_linux.sh 	modified:   src/btcpay/postgres_functions.sh

--- src/btcpay/postgres_functions.sh
+++ src/btcpay/postgres_functions.sh
@@ -5,6 +5,13 @@ docker exec -d -u postgres btcpay /bin/bash -c "/usr/bin/pg_ctlcluster 13 main s
 || log "btcpay" "failed to start postgress"
 }
 
+function make_postgres_user {
+
+docker exec -d -u postgres btcpay /bin/bash -c "/usr/local/bin/postgres_script.sh" \
+|| log "btcpay" "failed to make parman postgres database user"
+
+}
+
 function create_pg_databases {
 
 docker exec -d -u postgres btcpay /bin/bash -c "createdb -O parman btcpayserver && \
@@ -12,3 +19,4 @@ docker exec -d -u postgres btcpay /bin/bash -c "createdb -O parman btcpayserver 
 log "btcpay" "2 databases created" || \
 log "btcpay" "2 databases failed to be created" 
 }
+
