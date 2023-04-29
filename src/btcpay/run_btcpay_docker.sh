948c3dfa6975d197764d74a1f91b1f9867f1a58b
Parman <armantheparman@gmail.com>
2023-04-29 20:57:51 +1000

    modified:   src/btcpay/run_btcpay_docker.sh

--- src/btcpay/run_btcpay_docker.sh
+++ src/btcpay/run_btcpay_docker.sh
@@ -4,14 +4,7 @@ docker run -d \
            --name btcpay \
            -v $HOME/.btcpayserver:/home/parman/.btcpayserver \
            -v $HOME/.nbxplorer:/home/parman/.nbxplorer \
-           -p 8080:8080 \
-           -p 8070:8070 \
-           -p 8060:8060 \
-           -p 8050:8050 \
-           -p 23001:23001 \
-           -p 49392:49392 \
-           -p 9735:9735 \
-           -p 8332:8332 \
+           --network="host" \
            btcpay && \
 log "btcpay" "docker run btcpay executed" && return 0 \
 || log "btcpay" "docker run btcpay failed" && return 1
@@ -26,4 +19,4 @@ log "btcpay" "docker run btcpay executed" && return 0 \
     # 8070, 8060 and 8050 are spare ports so users wont have to re-run (remake) the container for
     # extra features later.
 
-#           --network="host" \
+\
