19157f2ca61930e975623d2304631b0a04e014cf
Parman <armantheparman@gmail.com>
2023-04-29 15:59:15 +1000

    modified:   src/btcpay/postgres_functions.sh

--- src/btcpay/postgres_functions.sh
+++ src/btcpay/postgres_functions.sh
@@ -2,8 +2,11 @@
 
 function startup_postgres {
 docker exec -d -u postgres btcpay /bin/bash -c "/usr/bin/pg_ctlcluster 13 main start ; \
-/usr/local/bin/postgres_script.sh ; \
-createdb -O parman btcpayserver ; \
-createdb -O parman nbxplorer" \
+createdb -O postgres btcpayserver ; \
+createdb -O postgres nbxplorer" \
 || log "btcpay" "failed to do startup postgress"
 }
+
+# /usr/local/bin/postgres_script.sh ; \
+#createdb -O parman btcpayserver ; \
+#createdb -O parman nbxplorer" \
