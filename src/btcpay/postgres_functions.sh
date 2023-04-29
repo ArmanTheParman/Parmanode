e5dfc23e61312cb396c952d00587526a9be15f8d
Parman <armantheparman@gmail.com>
2023-04-29 18:13:09 +1000

    modified:   src/btcpay/btcpay_config.sh 	modified:   src/btcpay/nbxplorer_config.sh 	modified:   src/btcpay/postgres_functions.sh

--- src/btcpay/postgres_functions.sh
+++ src/btcpay/postgres_functions.sh
@@ -1,15 +1,15 @@
 # start postgress, create parman database user with script, create 2 databases.
 
 function startup_postgres {
+docker exec -d -u root btcpay /bin/bash -c \
+"echo \"host all  postgres  trust\" | tee -a /etc/postgresql/*/main/pg_hba.conf" 
+
 docker exec -d -u postgres btcpay /bin/bash -c \
    "/usr/bin/pg_ctlcluster 13 main start" 
    
 docker exec -d -u postgres btcpay /bin/bash -c \
    "createdb -O postgres btcpayserver ; \
     createdb -O postgres nbxplorer"
-
-docker exec -d -u root btcpay /bin/bash -c \
-"echo \"host all  postgres  172.17.0.1/32 trust\" | tee -a /etc/postgresql/*/main/pg_hba.conf" 
 }
 
 # /usr/local/bin/postgres_script.sh ; \
