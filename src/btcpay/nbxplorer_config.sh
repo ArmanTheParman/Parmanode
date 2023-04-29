9d4dffc7c6597a37e36597dec05e99b9692c78b9
Parman <armantheparman@gmail.com>
2023-04-29 17:53:48 +1000

    modified:   src/btcpay/btcpay_config.sh 	modified:   src/btcpay/nbxplorer_config.sh 	modified:   src/btcpay/postgres_functions.sh

--- src/btcpay/nbxplorer_config.sh
+++ src/btcpay/nbxplorer_config.sh
@@ -7,7 +7,7 @@ btc.rpc.auth=${rpcuser}:${rpcpassword}
 btc.rpc.url=http://172.17.0.1:8332
 port=24445
 mainnet=1
-postgres=User ID=parman;Password=NietShitcoin;Host=172.17.0.1;Port=5432;Database=nbxplorer;
+postgres=User ID=postgres;Host=172.17.0.1;Port=5432;Database=nbxplorer;
 " | tee $HOME/.nbxplorer/Main/settings.config >/dev/null 2>&1 || \
     { log "nbxplorer" "failed to make settings.config" && \
     log "nbxplorer" "failed to make settings.config" && errormessage && return 1 ; }
