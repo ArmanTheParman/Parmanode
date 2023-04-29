ebfe40356a75df75f369e0d85c30b8f7b31d09fc
Parman <armantheparman@gmail.com>
2023-04-29 18:44:40 +1000

    modified:   src/btcpay/postgres_functions.sh

--- src/btcpay/postgres_functions.sh
+++ src/btcpay/postgres_functions.sh
@@ -4,17 +4,6 @@ function startup_postgres {
 docker exec -d -u root btcpay /bin/bash -c \
 "echo \"host all  postgres  trust\" | tee -a /etc/postgresql/*/main/pg_hba.conf" 
 
-docker exec -d -u root btcpay /bin/bash -c \
-"echo \"
-local   all             postgres                                peer
-local   all             all                                     peer
-host    all             all             127.0.0.1/32            trust
-host    all             all             ::1/128                 trust
-local   replication     all                                     peer
-host    replication     all             127.0.0.1/32            md5
-host    replication     all             ::1/128                 md5
-host all  postgres  trust\" | tee /etc/postgresql/*/main/pg_hba.conf >/dev/null"
-
 docker exec -d -u postgres btcpay /bin/bash -c \
    "/usr/bin/pg_ctlcluster 13 main start" 
    
@@ -27,3 +16,14 @@ docker exec -d -u postgres btcpay /bin/bash -c \
 #createdb -O parman nbxplorer" \
 
 # config file: /etc/postgresql/13/main/pg_hba.conf 
+
+#docker exec -d -u root btcpay /bin/bash -c \
+#"echo \"
+#local   all             postgres                                peer
+#local   all             all                                     peer
+#host    all             all             127.0.0.1/32            trust
+#host    all             all             ::1/128                 trust
+#local   replication     all                                     peer
+#host    replication     all             127.0.0.1/32            md5
+#host    replication     all             ::1/128                 md5
+#host all  postgres  trust\" | tee /etc/postgresql/*/main/pg_hba.conf >/dev/null"
