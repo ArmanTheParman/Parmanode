996edc22341c6cddf006e9f8faae4fd4656929b0
Parman <armantheparman@gmail.com>
2023-04-29 17:31:22 +1000

    modified:   src/btcpay/btcpay_config.sh 	modified:   src/btcpay/nbxplorer_config.sh 	modified:   src/btcpay/postgres_functions.sh

--- src/btcpay/btcpay_config.sh
+++ src/btcpay/btcpay_config.sh
@@ -6,7 +6,7 @@ port=23001
 bind=0.0.0.0
 chains=btc
 BTC.explorer.url=http://172.17.0.1:24445
-postgres=User ID=parman;Password=NietShitcoin;Host=192.168.0.206;Port=5432;Database=btcpayserver;
+postgres=User ID=parman;Password=NietShitcoin;Host=172.17.0.1;Port=5432;Database=btcpayserver;
 " | tee $HOME/.btcpayserver/Main/settings.config >/dev/null 2>&1 || \
     { log "btcpay" "failed to make settings.config" && \
     log "btcpay" "failed to make settings.config" && errormessage && return 1 ; }
--- src/btcpay/postgres_functions.sh
+++ src/btcpay/postgres_functions.sh
@@ -3,10 +3,14 @@
 function startup_postgres {
 docker exec -d -u postgres btcpay /bin/bash -c "/usr/bin/pg_ctlcluster 13 main start ; \
 createdb -O postgres btcpayserver ; \
-createdb -O postgres nbxplorer" \
+createdb -O postgres nbxplorer" ; \
+echo "local   nbxplorer  postgres  trust" | sudo tee -a /etc/postgresql*/*/main/pg_hba.conf ; \ 
+echo "local   btcpayserver postgres  trust" | sudo tee -a /etc/postgresql*/*/main/pg_hba.conf ; \
 || log "btcpay" "failed to do startup postgress"
 }
 
 # /usr/local/bin/postgres_script.sh ; \
 #createdb -O parman btcpayserver ; \
 #createdb -O parman nbxplorer" \
+
+# config file: /etc/postgresql/13/main/pg_hba.conf 
