f67ac43e7998416d0e8a8795163d01f4997f9bed
Parman <armantheparman@gmail.com>
2023-04-30 23:09:42 +1000

    run btcpay mixed up with nbxplorer; fixed

--- src/btcpay/run_btcpay.sh
+++ src/btcpay/run_btcpay.sh
@@ -5,7 +5,7 @@ while [ $count -le 1 ] ; do
 
 if docker ps | grep btcpay ; then   
 docker exec -d -u root btcpay /bin/bash -c \
-"/usr/bin/dotnet \"/home/parman/parmanode/NBXplorer/NBXplorer/bin/Release/netcoreapp2.1/NBXplorer.dll\" -c /home/parman/.nbxplorer/Main/settings.config" \
+"/usr/bin/dotnet run --no-launch-profile --no-build -c Release -p \"$HOME/source/btcpayserver/BTCPayServer/BTCPayServer.csproj\" -- $@" \
 && log "btcpay" "btcpay started" && return 0 || log "btcpay" "failed to start btcpay" && return 1    
 #docker exec -d -u parman btcpay /bin/bash -c "$HOME/parmanode/btcpayserver/run.sh" && \
 
