1c542851bf571abfbcc5ab3569c1e40d5a303a39
Parman <armantheparman@gmail.com>
2023-04-30 19:17:38 +1000

    stray space after \

--- src/btcpay/run_nbxplorer.sh
+++ src/btcpay/run_nbxplorer.sh
@@ -3,7 +3,7 @@ count=0
 while [ $count -le 1 ] ; do
 
 if docker ps | grep btcpay ; then   
-docker exec -d -u root btcpay /bin/bash -c \ 
+docker exec -d -u root btcpay /bin/bash -c \
 "/usr/bin/dotnet \"/home/parman/NBXplorer/NBXplorer/bin/Release/netcoreapp2.1/NBXplorer.dll\" -c /home/parman/.nbxplorer/Main/settings.config" \
 && log "nbxplorer" "nbxplorer started" && return 0 || log "nbxplorer" "failed to start nbxplorer" && return 1    
 #docker exec -d -u parman btcpay /bin/bash -c "$HOME/parmanode/NBXplorer/run.sh" && \
