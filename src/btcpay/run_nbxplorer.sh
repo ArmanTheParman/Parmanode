a00cd57f6c50bdd048580f7199006e83f9af6cb8
Parman <armantheparman@gmail.com>
2023-05-01 01:34:19 +1000

    forgot ./ before run.sh, FFS.

--- src/btcpay/run_nbxplorer.sh
+++ src/btcpay/run_nbxplorer.sh
@@ -1,6 +1,6 @@
 function run_nbxplorer {
 
-docker exec -d -u parman btcpay /bin/bash -c "cd /home/parman/parmanode/NBXplorer ; run.sh >/home/parman/parmanode/nbx.log" 
+docker exec -d -u parman btcpay /bin/bash -c "cd /home/parman/parmanode/NBXplorer ; ./run.sh >/home/parman/parmanode/nbx.log" 
 
 #"/usr/bin/dotnet \"/home/parman/parmanode/NBXplorer/NBXplorer/bin/Release/net6.0/NBXplorer.dll >/home/parman/parmanode/nbx.log\" -c \\
 #/home/parman/.nbxplorer/Main/settings.config" \
