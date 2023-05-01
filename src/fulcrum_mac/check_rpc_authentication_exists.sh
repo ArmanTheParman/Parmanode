7edcb74ffcc9fb46114ee03131b28d9695be6500
Parman <armantheparman@gmail.com>
2023-05-01 23:23:17 +1000

    new debug1 custom function

--- src/fulcrum_mac/check_rpc_authentication_exists.sh
+++ src/fulcrum_mac/check_rpc_authentication_exists.sh
@@ -3,6 +3,7 @@ function check_rpc_authentication_exists {
 source $HOME/.bitcoin/bitcoin.conf
 
 if [ -z $rpcuser || -z $rpcpassword ] ; then
+debug1 "user/pass string empty. calling set rpc authentication"
 set_rpc_authentication
 fi
 return 0
