8b069f1c2ea660076fd1d51b1550cdca7bb8779a
Parman <armantheparman@gmail.com>
2023-05-01 19:25:19 +1000

    v2.3.1 beta

--- src/lnd_unfinished/zmq.sh
+++
@@ -1,9 +0,0 @@
-# added this to bitcoin.conf
-
-#zmqpubrawblock=tcp://127.0.0.1:28332
-#zmqpubrawtx=tcp://127.0.0.1:28333
-#
-#as these are enabled in lnd.conf
-#
-#bitcoind.zmqpubrawblock=tcp://127.0.0.1:28332
-#bitcoind.zmqpubrawtx=tcp://127.0.0.1:28333
