1fc2060eab6a24f5148eeb85bb29671110aa1742
Parman <armantheparman@gmail.com>
2023-05-02 05:03:21 +1000

    docker ports switched, unknown reason:wq

--- src/fulcrum_mac/run_fulcrum_docker.sh
+++ src/fulcrum_mac/run_fulcrum_docker.sh
@@ -27,8 +27,8 @@ log "fulcrum" "Docker Volume Mount set at $docker_volume_mount"
 
 please_wait
 docker run -d --name fulcrum \
-                -p 50002:50001 \
-                -p 50001:50002 \
+                -p 50001:50001 \
+                -p 50002:50002 \
                 -p 50003:50003 \
                 -v ${docker_volume_mount}:/home/parman/parmanode/fulcrum_db \
                 -v $HOME/parmanode/fulcrum/config:/home/parman/parmanode/fulcrum/config \
