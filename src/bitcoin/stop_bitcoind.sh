d4d43348090e66cfd5170de5eef7aff72c7ae741
Parman <armantheparman@gmail.com>
2023-05-02 02:17:16 +1000

    wow, found stray "continue" commands from incomplete code change much earlier on.

--- src/bitcoin/stop_bitcoind.sh
+++ src/bitcoin/stop_bitcoind.sh
@@ -3,13 +3,11 @@ function stop_bitcoind {
 if [[ $OS == "Linux" ]] ; then 
 set_terminal 
 sudo systemctl stop bitcoind.service >/dev/null
-continue 
 fi
 
 if [[ $OS == "Mac" ]] ; then
 set_terminal 
 /usr/local/bin/bitcoin-cli stop >/dev/null
-continue
 fi
 
 return 0
