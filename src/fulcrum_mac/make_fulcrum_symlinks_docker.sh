6258359385d9b014cc1c7748dd68eb5940031b63
Arman The Parman <77603167+ArmanTheParman@users.noreply.github.com>
2023-05-02 12:59:41 +1000

    Update make_fulcrum_symlinks_docker.sh

--- src/fulcrum_mac/make_fulcrum_symlinks_docker.sh
+++ src/fulcrum_mac/make_fulcrum_symlinks_docker.sh
@@ -2,12 +2,11 @@ function make_fulcrum_symlinks_docker {
 
 #On host
 
-cd $HOME/parmanode/fulcrum/config/
-ln -s ../fulcrum.conf fulcrum.conf
+cd $HOME/parmanode/fulcrum/ && ln -s ./config/fulcrum.conf fulcrum.conf
 
 #In Docker
 
 docker exec -d -u parman fulcrum /bin/bash -c \
-"cd /home/parman/parmanode/fulcrum/config/ && ln -s ../fulcrum.conf fulcrum.conf"
+"cd /home/parman/parmanode/fulcrum/ && ln -s ./config/fulcrum.conf fulcrum.conf"
 
 }
