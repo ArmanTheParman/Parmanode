139323d0333326872e669829748c9877e5c701fc
Parman <armantheparman@gmail.com>
2023-05-02 16:44:23 +1000

    reversed --network="host", not available on Mac

--- src/fulcrum_mac/run_fulcrum_docker.sh
+++ src/fulcrum_mac/run_fulcrum_docker.sh
@@ -27,7 +27,9 @@ log "fulcrum" "Docker Volume Mount set at $docker_volume_mount"
 
 please_wait
 docker run -d --name fulcrum \
-                --network="host"				
+                -p 50001:50001 \
+                -p 50002:50002 \
+                -p 50003:50003 \
                 -v ${docker_volume_mount}:/home/parman/parmanode/fulcrum_db \
                 -v $HOME/parmanode/fulcrum/config:/home/parman/parmanode/fulcrum/config \
                 fulcrum >/$HOME/parmanode/fulcrum.log 2>&1 \
