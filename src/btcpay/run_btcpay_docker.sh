ec54ab28325fd6d4cf3793a92c67eee657329e4b
Parman <armantheparman@gmail.com>
2023-05-02 22:39:54 +1000

    need "sleep" not "wait"

--- src/btcpay/run_btcpay_docker.sh
+++ src/btcpay/run_btcpay_docker.sh
@@ -17,10 +17,8 @@ docker run -d -u parman \
            -p 49393:49392 \
            -p 23001:23001 \
            -p 8080:8080 \
-           -p 8070:8070 \
-           -p 8060:8060 \
-           -p 8050:8050 \
-           -p 9735:9735 \
+           -p 8090:8090 \
+           
            btcpay 
 
 debug1 "after docker run"
@@ -33,6 +31,5 @@ fi
     # make sure the 8080 port is not duplicated when other programs, eg RTL, are added.
     # 49392 is for REST API
     # 9735 is not needed by BTCpay; LND uses that for LN protocol communication
-    # 8070, 8060 and 8050 are spare ports so users wont have to re-run (remake) the container for
-    # extra features later.
+   
 
