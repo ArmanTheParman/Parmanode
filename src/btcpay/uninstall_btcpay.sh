8a5846b7b26ff4c0a50f72123b7d7b4a7bffd069
Parman <armantheparman@gmail.com>
2023-05-02 20:43:15 +1000

    debug option for btcpay uninstall - saves time

--- src/btcpay/uninstall_btcpay.sh
+++ src/btcpay/uninstall_btcpay.sh
@@ -27,7 +27,10 @@ done
 please_wait
 echo "Stopping containers..." && sudo docker stop btcpay || log "btcpay" "failed to stop btcpay. May not be running."
 echo "Removing containers..." && sleep 0.5 && sudo docker rm btcpay || log "btcpay" "failed to remove btcpay docker container. May not exist."
-echo "Removing Docker images..." && sleep 0.5 && sudo docker rmi btcpay || log "btcpay" "failed to remove btcpay image. May not exist"
+if [[ $debug != 1 ]]  
+    then echo "Removing Docker images..." && sleep 0.5 && sudo docker rmi btcpay \
+    || log "btcpay" "failed to remove btcpay image. May not exist" 
+    fi
 
 #remove directories
 echo "Removing BTCpay and NBXplorer directories..." && sleep 1 && rm -rf $HOME/.btcpayserver $HOME/.nbxplorer \
