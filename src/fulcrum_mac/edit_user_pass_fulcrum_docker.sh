2d69f7ea3e92184d21c1c5c67405a8b1ae35feef
Parman <armantheparman@gmail.com>
2023-05-02 16:12:34 +1000

    fixed user/pass issue with Fulcrum Mac

--- src/fulcrum_mac/edit_user_pass_fulcrum_docker.sh
+++ src/fulcrum_mac/edit_user_pass_fulcrum_docker.sh
@@ -1,9 +1,6 @@
 function edit_user_pass_fulcrum_docker {
 #from the host machine
 source $HOME/.bitcoin/bitcoin.conf
-rpcuser="$1"
-
-rpcpassword="$2"
 
 delete_line "$HOME/parmanode/fulcrum/fulcrum.conf" "rpcuser"
 
