976f50c3816f90d8748fc1c59a3eaede2de1a8f5
Parman <armantheparman@gmail.com>
2023-05-04 02:47:59 +1000

    v2.3.4

--- run_parmanode.sh
+++ run_parmanode.sh
@@ -37,9 +37,9 @@ IP=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | grep -v 172.1 | awk '{print $
 # set "trap" conditions; currently makes sure user's terminal reverts to default colours.
 
 	clean_exit 
-	
+
 # Config directory needs to be made
-mkdir $HOME/.parmanode >/dev/null 2>&1 #NEW
+mkdir $HOME/.parmanode >/dev/null 
 debug1 "Pause here to check for error output before clear screen." 
 
 # Load config 
