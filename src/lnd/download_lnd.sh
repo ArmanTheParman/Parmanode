b7e1a4c58f6022facb9c63335208ff2e175ca0fa
Parman <armantheparman@gmail.com>
2023-05-05 02:27:27 +1000

    curl -LO , not -Lo

--- src/lnd/download_lnd.sh
+++ src/lnd/download_lnd.sh
@@ -4,27 +4,27 @@ if [[ $OS != "Linux" ]] ; then return 1 ; fi
 
 if [[ $chip == "x86_64" ]] ; then
 cd $HOME/parmanode
-curl -Lo https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-amd64-v0.16.2-beta.tar.gz
+curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-amd64-v0.16.2-beta.tar.gz
 fi
 
 if [[ $chip == "arm64" ]] ; then
 cd $HOME/parmanode
-curl -Lo https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-arm64-v0.16.2-beta.tar.gz 
+curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-arm64-v0.16.2-beta.tar.gz 
 fi
 
 if [[ $chip == "arm64" ]] ; then
 cd $HOME/parmanode
-curl -Lo https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-arm64-v0.16.2-beta.tar.gz
+curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-arm64-v0.16.2-beta.tar.gz
 fi
 
 if [[ $chip == "armv6l" ]] ; then
 cd $HOME/parmanode
-curl -Lo https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-armv6-v0.16.2-beta.tar.gz
+curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-armv6-v0.16.2-beta.tar.gz
 fi
 
 if [[ $chip == "armv7l" ]] ; then
 cd $HOME/parmanode
-curl -Lo https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-armv7-v0.16.2-beta.tar.gz
+curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-armv7-v0.16.2-beta.tar.gz
 fi
 
 }
