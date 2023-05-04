9361736ccd7d02e4a7ef9f3ee80e4ae9d90e9931
Parman <armantheparman@gmail.com>
2023-05-05 03:30:28 +1000

    new file:   src/lnd/lnd.conf 	new file:   src/lnd/make_dot_lnd.sh 	new file:   src/lnd/make_lnd_conf.sh 	new file:   src/lnd/set_lnd_alias.sh

--- src/lnd/set_lnd_alias.sh
+++ src/lnd/set_lnd_alias.sh
@@ -1,18 +1,3 @@
-function install_lnd {
-download_lnd
-verify_lnd || return 1
-unpack_lnd
-
-sudo install -m 0755 -o root -g root -t /usr/local/bin $HOME/parmanode/lnd/* >/dev/null 2>&1
-
-make_dot_lnd
-
-
-set_lnd_alias
-make_lnd_conf
-
-}
-
 function set_lnd_alias {
 while true ; do
 set_terminal ; echo "
