99bd6fe9ed0ec3ff60cbbbedc58a5de9f72b91ca
Parman <armantheparman@gmail.com>
2023-05-05 03:31:20 +1000

    modified:   src/lnd/install_lnd.sh

--- src/lnd/install_lnd.sh
+++ src/lnd/install_lnd.sh
@@ -1,4 +1,4 @@
-function install_lnd {
+unction install_lnd {
 download_lnd
 verify_lnd || return 1
 unpack_lnd
@@ -11,38 +11,4 @@ make_dot_lnd
 set_lnd_alias
 make_lnd_conf
 
-}
-
-function set_lnd_alias {
-while true ; do
-set_terminal ; echo "
-########################################################################################
-
-    Please type in an alias for your LND node. It's a good idea not to give away
-    personal information about yourself on this public network. For example, don't
-    use your real name or Twitter handle. If you leave it blank, the node will be
-    called Parmanode_LND followed by a random number.
-
-    Please type in your choice...
-
-########################################################################################
-"
-read alias_choice
-
-if [[ $alias_choice == "" ]] ; then 
-echo "Empty string"
-# random_alias_number, RAN
-RAN=$((10000000+ RANDOM % 90000000)) >/dev/null 
-alias="Parmanode_LND_$RAN"
-echo "Your alias will be $alias"
-else
-alias=$alias_choice
-echo "Your alias will be $alias"
-fi
-
-echo "
-Hit <enter> to accept or (x) to try again."
-read choice
-if [[ $choice == "" ]] ; then break ; else continue ; fi
-done
 }
