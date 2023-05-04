8c50a124f27da8200b3be202020ad03845dc851a
Parman <armantheparman@gmail.com>
2023-05-05 02:55:44 +1000

    modified:   src/lnd/verify_lnd.sh

--- src/lnd/verify_lnd.sh
+++ src/lnd/verify_lnd.sh
@@ -1,9 +1,9 @@
 function verify_lnd {
-
+#import Roasbeef's public key
 gpg --import $original_dir/src/lnd/roasbeef.pgp >/dev/null 2>&1
 
 cd $HOME/parmanode
-
+#verify SHA256 output list file.
 if ! gpg verify *.sig manifest*.txt ; then
     set_terminal
     echo "GPG verification fail. Unknown reason. Please report to Parman. Aborting."
@@ -14,6 +14,10 @@ else
     echo "GPG verification passed."
     sleep 2
 fi
-
-
+#Perform SHA256 to verify
+if ! sha256sum --ignore-missing --check man*.txt ; then
+set_terminal
+echo "SHA256 check failed. Unknown reason. Please report to Parman. Aborting."
+return 1
+fi
 }   
