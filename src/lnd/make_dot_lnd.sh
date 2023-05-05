51c6eec0808261ef552a15873839260c45cc71d9
Parman <armantheparman@gmail.com>
2023-05-05 19:14:34 +1000

    make lnd directories, better.

--- src/lnd/make_dot_lnd.sh
+++ src/lnd/make_dot_lnd.sh
@@ -1,4 +1,4 @@
-function make_dot_lnd {
+function make_lnd_directories {
 if [[ -d $HOME/.lnd ]] ; then
 set_terminal
 echo "The .lnd directory exists. It will be deleted. If that's ok, "
@@ -7,6 +7,7 @@ enter_continue
 fi
 
 rm -rf $HOME/.lnd && mkdir $HOME/.lnd >/dev/null 2>&1
+rm -rf $HOME/parmanode/lnd && mkdir $HOME/parmanode/lnd >/dev/null 2>&1
 }
 
 
