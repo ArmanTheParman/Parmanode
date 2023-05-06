7137c3e13c626fe529aec09938b3efd7eb54a539
Parman <armantheparman@gmail.com>
2023-05-06 16:34:02 +1000

    create wallet message

--- src/lnd/create_wallet.sh
+++ src/lnd/create_wallet.sh
@@ -4,6 +4,8 @@ if ! sudo systemctl status lnd.service >/dev/null ; then
     set_terminal ; echo "LND is not running. Start LND first."
     enter_continue ; return 1 ; 
 fi
-
+set_terminal
+echo "You will be asked for a password - this is you LND password, not passphrase.
+"
 lncli create
 }
