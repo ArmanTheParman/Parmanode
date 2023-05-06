d6855433c673f8ebfa83a04bd301d3aefa9e4a7b
Parman <armantheparman@gmail.com>
2023-05-07 09:43:56 +1000

    modified:   src/mempool/install_mempool.sh 	new file:   src/mempool/make_docker_compose.sh 	new file:   src/mempool/set_mempool_userpass.sh 	new file:   src/text_functions/swap_string.sh

--- src/mempool/install_mempool.sh
+++ src/mempool/install_mempool.sh
@@ -1,33 +1,32 @@
 function install_mempool {
 
+install_check "mempool" || return 1
+
 cd $HOME/parmanode
 
 git clone http://github.com/mempool/mempool.git
 
 cd mempool/docker
 
-docker-compose.yml
-    -change port from 80 to 4080
-    change core_RPC_Host, and username and password
-    change on failure to always (three locations)
-    change mempool backend to "electrum"
+make_docker_compose
 
-    api:
-        environment:
-            ELECTRUM_HOST: "own IP"
-            ELECTRUM_PORT: "50002"
-            ELECTRUM_TLS_ENABLED: "true"
+installed_conf_add "mempool"
+docker compose up -d
 
-    add at the end:
-    
-    networks:
-        default:
-            driver: bridge
-            ipam:
-                config:
-                    -subnet: 172.16.57.0/24
-    
-    docker compose up -d
+set_terminal ; echo "
+########################################################################################
 
+                                   S U C C E S S ! 
 
+    Mempool Space has finished being installed. It may day a few days for the backend
+    to synchronise. You can access the webpage at $IP:4080
+
+    You can take a look before the synchronisation is finished; more info will appear
+    once it's done.
+
+########################################################################################
+"
+enter_continue
+return 0
 }
+
