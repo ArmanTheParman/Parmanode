3b0a7ef6d293e1282940a3e8aaf124e95abdeec8
Parman <armantheparman@gmail.com>
2023-05-07 10:30:43 +1000

    menu mempool

--- src/menus/menu_mempool.sh
+++ src/menus/menu_mempool.sh
@@ -19,3 +19,9 @@ choose "xpq" ; read choice ; set_terminal
 case $choice in 
 q|Q|QUIT|Quit) exit 0 ;;
 p|P) return 1 ;;
+
+start|START|Start) docker compose start api web db ; enter_continue ; return 1 ;;
+stop|STOP|Stop) docker compose stop api web db ; enter_continue ; return 1 ;; 
+*) invalid ;;
+esac ; done 
+}
