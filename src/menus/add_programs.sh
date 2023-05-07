f3c06c9a52c8164f0781008def374adabd354071
Parman <armantheparman@gmail.com>
2023-05-08 05:06:47 +1000

    conflict after merge fixed

--- src/menus/add_programs.sh
+++ src/menus/add_programs.sh
@@ -90,11 +90,7 @@ case $choice in
        if [[ $OS == "Linux" ]] ; then 
        install_btcpay_linux ; return 0 ; fi
        if [[ $OS == "Mac" ]] ; then 
-<<<<<<< HEAD
        install_btcpay_mac || return 1  ; fi
-=======
-       no_mac ; return 0  ; fi
->>>>>>> master
        ;;
     
     t|T|tor|Tor)
