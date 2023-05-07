53cfac2e7c177ed1dc0fd5630b621748b39fe7f9
Parman <armantheparman@gmail.com>
2023-05-08 06:55:23 +1000

    run bitcoind docker

--- src/btcpay/run_bitcoind_docker.sh
+++ src/btcpay/run_bitcoind_docker.sh
@@ -1,6 +1,6 @@
 function run_bitcoind_docker {
 
-doecker exec -itu parman btcpay /home/parman/parmanode/bitcoin/bin/bitcoind || return 1
+docker exec -itu parman btcpay /home/parman/parmanode/bitcoin/bin/bitcoind || return 1
 
 return 0
 
