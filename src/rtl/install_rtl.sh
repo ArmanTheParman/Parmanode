f9a55d49c5ead3451d6b214cda86d5978973bd45
Parman <armantheparman@gmail.com>
2023-05-15 05:06:37 +1000

    removed rtl_db

--- src/rtl/install_rtl.sh
+++ src/rtl/install_rtl.sh
@@ -11,14 +11,13 @@ install_check "rtl"
 #install_rtl
 #fi
 
-mkdir $HOME/parmanode/rtl $HOME/parmanode/rtl_db
+mkdir $HOME/parmanode/rtl 
 installed_config_add "rtl-start"
 make_rtl_config
 
 docker build -t rtl ./src/rtl || { debug1 "failed to build rtl image" && return 1 ; }
 docker run -d --name rtl -p 3000:3000 \
                          -v $HOME/parmanode/rtl:/home/parman/RTL2 \
-                         -v $HOME/parmanode/rtl_db:/home/parman/rtl_db \
 			 -v $HOME/.lnd:/home/parman/.lnd \
                          -v $HOME/.parmanode/:/home/parman/.parmanode \
                          rtl \
