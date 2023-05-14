1c000a1be6834ce3fd265ee4fb254eb151108499
Parman <armantheparman@gmail.com>
2023-05-15 02:09:30 +1000

    smol bug

--- src/rtl/install_rtl.sh
+++ src/rtl/install_rtl.sh
@@ -22,7 +22,7 @@ docker build --build-arg USERNAME="$(whoami)" \
 docker run -d --name rtl -p 3000:3000 \
                          -v $HOME/parmanode/rtl:/home/parman/RTL \
 			 -v $HOME/.lnd:/home/$(whoami)/.lnd
-                         --restart unless-stopped
+                         --restart unless-stopped \
         || { debug1 "failed to run rtl image" && return 1 ; }
 
 rtl_password_changer
