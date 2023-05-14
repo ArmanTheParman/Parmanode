1e483a2d40bad659f6c177d75e5d7f924ea3d4d8
Parman <armantheparman@gmail.com>
2023-05-15 04:09:19 +1000

    lost files from mounting, fixed

--- src/rtl/install_rtl.sh
+++ src/rtl/install_rtl.sh
@@ -25,6 +25,7 @@ docker run -d --name rtl -p 3000:3000 \
                          rtl \
         || { debug1 "failed to run rtl image" && return 1 ; }
 
+docker exec -d -e source="/home/$(whoami)/RTL2/" -e dest="/home/$(whoami)/RTL/" rtl sh -c "mv ${source}* $dest && rm -rf $source"
 mv $original_dir/src/rtl/RTL-Config.json $HOME/parmanode/rtl
 rtl_password_changer
 
