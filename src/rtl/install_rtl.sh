ad7601da6e7365b6f71e6177ecf98f2b1af548d5
Parman <armantheparman@gmail.com>
2023-05-15 02:56:32 +1000

    smol bug

--- src/rtl/install_rtl.sh
+++ src/rtl/install_rtl.sh
@@ -21,7 +21,7 @@ docker build --build-arg USERNAME="$(whoami)" \
 
 docker run -d --name rtl -p 3000:3000 \
                          -v $HOME/parmanode/rtl:/home/parman/RTL \
-			 -v $HOME/.lnd:/home/$(whoami)/.lnd
+			 -v $HOME/.lnd:/home/$(whoami)/.lnd \
                          --restart unless-stopped \
         || { debug1 "failed to run rtl image" && return 1 ; }
 
