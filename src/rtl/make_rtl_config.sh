7369fddfa041797c5aa9ed96ebb96d59e50e50ae
Parman <armantheparman@gmail.com>
2023-05-15 01:33:25 +1000

    modified:   src/bitcoin/password_changer.sh 	modified:   src/rtl/install_rtl.sh 	new file:   src/rtl/make_rtl_config.sh 	deleted:    src/rtl/rtl_config.txt 	new file:   src/rtl/rtl_functions.sh 	new file:   src/rtl/rtl_password_changer.sh 	modified:   src/rtl_docker/Dockerfile

---
+++ src/rtl/make_rtl_config.sh
@@ -0,0 +1,40 @@
+function make_rtl_config {
+
+echo "  
+{
+  \"multiPass\": \"$rlt_password\",
+  \"port\": \"3000\",
+  \"defaultNodeIndex\": 1,
+  \"dbDirectoryPath\": \"home/parman/RTL\",
+  \"SSO\": {
+    \"rtlSSO\": 0,
+    \"rtlCookiePath\": \"\",
+    \"logoutRedirectLink\": \"\"
+  },
+  \"nodes\": [
+    {
+      \"index\": 1,
+      \"lnNode\": \"Node 1\",
+      \"lnImplementation\": \"LND\",
+      \"Authentication\": {
+       \"macaroonPath\": \"/home/$(whoami)/.lnd/data/chain/bitcoin/mainnet\",
+        \"configPath\": \"/home/$(whoami)/.lnd/lnd.conf\",
+        \"swapMacaroonPath\": \"C:\\Users\\xyz\\AppData\\Local\\Loop\\mainnet\",
+        \"boltzMacaroonPath\": \"C:\\Users\\xyz\\AppData\\Boltz\\mainnet\"
+      },
+      \"Settings\": {
+        \"userPersona\": \"MERCHANT\",
+        \"themeMode\": \"DAY\",
+        \"themeColor\": \"PURPLE\",
+        \"channelBackupPath\": \"C:\\Users\\xyz\\backup\\node-1\",
+        \"logLevel\": \"ERROR\",
+        \"lnServerUrl\": \"https://127.0.0.1:8080\",
+        \"swapServerUrl\": \"https://127.0.0.1:8081\",
+          \"boltzServerUrl\": \"https://127.0.0.1:9003\",
+        \"fiatConversion\": false,
+        \"unannouncedChannels\": false
+      }
+    }
+  ]
+}" > ./src/rtl/RTL-Config.json 2>/dev/null
+}
