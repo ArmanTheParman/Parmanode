33c9272c66569854bf0e713420a0e7cd190ad5fb
Parman <armantheparman@gmail.com>
2023-05-15 05:13:58 +1000

    modified:   src/rtl/uninstall_rtl.sh

--- src/rtl/uninstall_rtl.sh
+++ src/rtl/uninstall_rtl.sh
@@ -7,8 +7,8 @@ please_wait
 docker stop rtl 2>/dev/null
 docker rm rtl 2>/dev/null
 docker rmi rtl 2>/dev/null
-rm -rf $HOME/parmanode/rtl >/dev/null 2>&1
-rm ./src/rtl/RTL-Config.json >/dev/null 2>&1
+sudo rm -rf $HOME/parmanode/rtl >/dev/null 2>&1
+sudo rm ./src/rtl/RTL-Config.json >/dev/null 2>&1
 
 installed_config_remove "rtl"
 success "RTL" "being uninstalled."
