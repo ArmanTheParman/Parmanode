89605f4ce05e2d466e32fe97616925d1575359c5
Parman <armantheparman@gmail.com>
2023-05-17 23:22:27 +1000

    modified:   src/rtl/make_rtl_startup_script.sh

--- src/rtl/make_rtl_startup_script.sh
+++ src/rtl/make_rtl_startup_script.sh
@@ -2,12 +2,12 @@ function make_rtl_startup_script {
 
 echo "#!/bin/bash
 docker start rtl
-docker exec -d rtl sh -c "cd /home/parman/RTL && node rtl" && exit 0 || sleep 1
-docker exec -d rtl sh -c "cd /home/parman/RTL && node rtl" && exit 0 || sleep 1
-docker exec -d rtl sh -c "cd /home/parman/RTL && node rtl" && exit 0 || sleep 1
-docker exec -d rtl sh -c "cd /home/parman/RTL && node rtl" && exit 0 || sleep 1
-docker exec -d rtl sh -c "cd /home/parman/RTL && node rtl" && exit 0 || sleep 1
-docker exec -d rtl sh -c "cd /home/parman/RTL && node rtl" && exit 0 || echo "failed to start rtl"
+docker exec -d rtl sh -c \"cd /home/parman/RTL && node rtl\" && exit 0 || sleep 1
+docker exec -d rtl sh -c \"cd /home/parman/RTL && node rtl\" && exit 0 || sleep 1
+docker exec -d rtl sh -c \"cd /home/parman/RTL && node rtl\" && exit 0 || sleep 1
+docker exec -d rtl sh -c \"cd /home/parman/RTL && node rtl\" && exit 0 || sleep 1
+docker exec -d rtl sh -c \"cd /home/parman/RTL && node rtl\" && exit 0 || sleep 1
+docker exec -d rtl sh -c \"cd /home/parman/RTL && node rtl\" && exit 0 || echo \"failed to start rtl\"
 sleep 2
 exit 1" > $HOME/parmanode/startup_scripts/rtl_startup.sh 2>&1
 sudo chmod +x $HOME/parmanode/startup_scripts/rtl_startup.sh >/dev/null
