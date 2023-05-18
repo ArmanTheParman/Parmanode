d3ffc06d394fc36b7f0b93a6da148b248071ac74
Parman <armantheparman@gmail.com>
2023-05-18 18:43:12 +1000

    v3.3.3 (fstab, nofail added)

--- src/text_functions/about.sh
+++ src/text_functions/about.sh
@@ -5,7 +5,7 @@ set_terminal_wide ; echo "
                                 
                                               About Parmanode
             
-            Version:                3.3.2          
+            Version:                3.3.3          
 
             Websites:               www.parmanode.com  (under construction) 
                                     github.com/armantheparman/parmanode
