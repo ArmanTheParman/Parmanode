075e5c09418613d162d85b302c58b2c7cde1e4ad
Parman <armantheparman@gmail.com>
2023-05-23 05:07:04 +1000

    modified:   src/drive_functions/format_ext_drive.sh new file:   src/drive_functions/write_to_fstab.sh

--- src/drive_functions/format_ext_drive.sh
+++ src/drive_functions/format_ext_drive.sh
@@ -42,10 +42,8 @@ if [[ $OS == "Linux" ]] ; then
 
         #Extract the *NEW* UUID of the disk and write to config file.
         get_UUID "$disk" && parmanode_conf_add "UUID=$UUID" && log "bitcoin" "new UUID $UUID"
-        #Write to fstab 
-        echo "UUID=$UUID /media/$(whoami)/parmanode ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab > /dev/null 2>&1
-        log "bitcoin" "fstab grep output for parmanode:" && \
-        grep "parmanode" /etc/fstab >> $HOME/.parmanode/bitcoin.log     
+
+        write_to_fstab "$UUID"
 
         #Mounting
         sudo mkdir /media/$(whoami)/parmanode >> $HOME/.parmanode/bitcoin.log 2>&1    
