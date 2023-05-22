8a5ddbca597152af161e3b8f5577be7cabe929f7
Parman <armantheparman@gmail.com>
2023-05-23 04:42:40 +1000

    test dir typo

--- src/general_functions/test_directory_placement.sh
+++ src/general_functions/test_directory_placement.sh
@@ -18,23 +18,24 @@ function test_directory_placement_message {
 clear ; echo "
 ########################################################################################
 
-    Oh dear, you've downloaded the Parmanode software directly to your home directory.
-	This will cause a conflict the the Parmanode installation. Here's what you do:
-	after exiting this program, from the command line, type exactly this (case
-	sensitive)...
+    Oh dear, you've downloaded the Parmanode software directly to your home directory. 
+    This will cause a conflict for the Parmanode installation. Here's what you do:
+
+    after exiting this program, from the command line, type exactly this (case
+    sensitive)...
 
 			cd .. && mv parmanode ./Desktop/
 	
-	Then, you will see the downloaded directory moved to your desktop. You can enter
+    Then, you will see the downloaded directory moved to your desktop. You can enter
     the directory from the command line with...
 
 	        cd $HOME/Desktop/parmanode
 
-	Then you can run the program from there with...
+    Then you can run the program from there with...
 
 			./run_parmanode.sh
 
-	Take a photo of these instructions or copy them to a file if it's hard to remember.
+    Take a photo of these instructions or copy them to a file if it's hard to remember.
 
 ########################################################################################
 "
