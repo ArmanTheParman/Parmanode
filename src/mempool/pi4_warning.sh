59e3b1837de9de3cec9875943a66e73ffb2da99e
Parman <armantheparman@gmail.com>
2023-05-23 20:15:06 +1000

    modified:   src/mempool/pi4_warning.sh 	modified:   src/menus/menu_mempool.sh

--- src/mempool/pi4_warning.sh
+++ src/mempool/pi4_warning.sh
@@ -5,9 +5,10 @@ set_terminal ; echo "
 
                          W A R N I N G: Pi4 will be SLOW
 
-    It is not recommended that you run Mempool on a Pi4. It will install, it will
-    work for a bit, but the device rapidly comes to a stall, and can become completely
-    unrepsonsive. So don't do it.
+    It is not recommended that you run Mempool on a Pi4. During testing, it installed, 
+    it worked for a bit, but the device rapidly came to a stall, and was completely
+    unrepsonsive, requiring a hard reboot. However, upon restarting, it was working
+    fine. Be warned. 
 
     To ignore my sage advice, type yolo, to proceed with the install. Otherwise, 
     hit <enter> to abort.
