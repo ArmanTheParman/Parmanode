ce25a2fa4916028d71998c30e55fa9356272c5d3
Parman <armantheparman@gmail.com>
2023-05-23 22:30:07 +1000

    modified:   src/drive_functions/write_to_fstab.sh

--- src/drive_functions/write_to_fstab.sh
+++ src/drive_functions/write_to_fstab.sh
@@ -12,8 +12,7 @@ source $HOME/.parmanode/var
 
 export $(sudo blkid -o export $disk) >/dev/null
 
-if grep -q "$UUID" /etc/fstab ; then echo "UUID already exists in fstab" ; sleep 2 ; return 1 ; fi
-
+delete_line "/etc/fstab" "$UUID"
 echo "UUID=$UUID /media/$(whoami)/parmanode $TYPE defaults,nofail 0 2" | sudo tee -a /etc/fstab >/dev/null 2>&1
 
 }
