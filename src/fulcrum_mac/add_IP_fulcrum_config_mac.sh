4d1b5ed2b39682f012d8d7641aeee9a34b87c15d
Parman <armantheparman@gmail.com>
2023-05-31 17:06:24 +1000

    fulcrum mac IP

--- src/fulcrum_mac/add_IP_fulcrum_config_mac.sh
+++ src/fulcrum_mac/add_IP_fulcrum_config_mac.sh
@@ -2,5 +2,5 @@ function add_IP_fulcrum_config_mac {
 
 delete_line "$HOME/parmanode/fulcrum/fulcrum.conf" "bitcoind"
 
-echo "bitcoind = $IP" >> $HOME/parmanode/fulcrum/fulcrum.conf 
+echo "bitcoind = $IP:8332" >> $HOME/parmanode/fulcrum/fulcrum.conf 
 }
