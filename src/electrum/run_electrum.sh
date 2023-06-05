6355e7fed4bafc05328546f44c01cc113b6a3588
Parman <armantheparman@gmail.com>
2023-06-05 14:41:50 +1000

    modified:   src/electrum/run_electrum.sh

--- src/electrum/run_electrum.sh
+++ src/electrum/run_electrum.sh
@@ -5,5 +5,13 @@ cd $HOME/.electrum && rm -rf certs cache daemon*
 
 please_wait
 
+
+if [[ $OS == "Linux" ]] ; then
 nohup $HOME/parmanode/electrum/electrum*AppImage >/dev/null 2>&1 &
+fi
+
+if [[ $OS == "Mac" ]] ; then
+open /Applications/Electrum.app
+fi
+
 }
