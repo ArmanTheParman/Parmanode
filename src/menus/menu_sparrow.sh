1848ce1b3109f44b9040419bcf2cf39478ceab05
Parman <armantheparman@gmail.com>
2023-06-07 14:40:18 +1000

    modified:   changelog 	modified:   src/lnd/make_lnd_conf.sh 	modified:   src/menus/menu_sparrow.sh 	modified:   src/sparrow/install_sparrow.sh 	modified:   src/sparrow/sparrow-udev.sh

--- src/menus/menu_sparrow.sh
+++ src/menus/menu_sparrow.sh
@@ -4,14 +4,7 @@ while true ; do set_terminal ; echo "
                                 Sparrow Menu                               
 ########################################################################################
 
-
       (start)          Start Sparrow 
-
-
-      (u)              Install \"udev\" rules for Hardware Wallets
-
-                           - usually necessary for HWWs to work
-                           - not required for Mac computers
 
 ########################################################################################
 "
@@ -30,12 +23,6 @@ return 0 ;;
 #echo "The sparrow configuration directory has been emptied, and a new configuration file has been creatd."
 #enter_continue
 #;;
-
-u|U)
-sparrow_udev
-return 0
-;;
-
 
 *)
 invalid
