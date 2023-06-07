49767b7705c8f9260b1b1a5c8272c1c3d4d78311
Parman <armantheparman@gmail.com>
2023-06-07 15:52:01 +1000

    modified:   src/menus/add_programs.sh 	renamed:    src/menus/main.sh -> src/menus/menu_main.sh

--- src/menus/add_programs.sh
+++ src/menus/add_programs.sh
@@ -5,72 +5,70 @@ do
 set_terminal_bit_higher
 echo "
 ########################################################################################
-
-
-                         P A R M A N O D E - Install Programs
-
-
+#                                                                                      #
+#    P A R M A N O D E -- Main Menu --> Install Menu                                   #
+#                                                                                      #
 ########################################################################################
-
-Not yet installed...          
-"
+#                                                                                      #
+# Not yet installed...                                                                 #
+#                                                                                      #"
 if ! grep -q "bitcoin-end" $HOME/.parmanode/installed.conf ; then
-echo "                             (b)       Bitcoin Core
-                            " ; fi
+echo "#                            (b)       Bitcoin Core                                    #
+#                                                                                      #" ; fi
 if ! grep -q "fulcrum-end" $HOME/.parmanode/installed.conf ; then
-echo "                             (f)       Fulcrum (an Electrum Server)
-                            " ; fi
+echo "#                            (f)       Fulcrum (an Electrum Server)                    #
+#                                                                                      #" ; fi
 if ! grep -q "btcpay-end" $HOME/.parmanode/installed.conf ; then
-echo "                             (btcp)    BTCPay Server
-                            " ; fi
+echo "#                            (btcp)    BTCPay Server                                   #
+#                                                                                      #" ; fi
 if ! grep -q "tor-end" $HOME/.parmanode/installed.conf ; then
-echo "                             (t)       Tor 
-                            " ; fi
+echo "#                            (t)       Tor                                             #
+#                                                                                      #" ; fi
 if ! grep -q "lnd-end" $HOME/.parmanode/installed.conf ; then
-echo "                             (lnd)     LND
-                            " ; fi
+echo "#                            (lnd)     LND                                             #
+#                                                                                      #" ; fi
 if ! grep -q "mempool-end" $HOME/.parmanode/installed.conf ; then
-echo "                             (mem)     Mempool Space 
-                            " ; fi
+echo "#                            (mem)     Mempool Space                                   #
+#                                                                                      #" ; fi
 if ! grep -q "sparrow-end" $HOME/.parmanode/installed.conf ; then
-echo "                             (s)       Sparrow Wallet 
-                            " ; fi
+echo "#                            (s)       Sparrow Wallet                                  #
+#                                                                                      #" ; fi
 if ! grep -q "rtl-end" $HOME/.parmanode/installed.conf ; then
-echo "                             (r)       RTL Wallet
-                            " ; fi
+echo "#                            (r)       RTL Wallet                                      #
+#                                                                                      #" ; fi
 if ! grep -q "electrum-end" $HOME/.parmanode/installed.conf ; then
-echo "                             (e)       Electrum Wallet 
-                            " ; fi
-echo "Already installed...
-"
+echo "#                            (e)       Electrum Wallet                                 #
+#                                                                                      #" ; fi
+echo "# Already installed...                                                                 #
+#                                                                                      #"
 if grep -q "bitcoin-end" $HOME/.parmanode/installed.conf ; then
-echo "                                       Bitcoin Core
-                            " ; fi
+echo "#                                      Bitcoin Core                                    #
+#                                                                                      #" ; fi
 if grep -q "fulcrum-end" $HOME/.parmanode/installed.conf ; then
-echo "                                       Fulcrum (an Electrum Server)
-                            " ; fi
+echo "#                                      Fulcrum (an Electrum Server)                    #
+#                                                                                      #" ; fi
 if grep -q "btcpay-end" $HOME/.parmanode/installed.conf ; then
-echo "                                       BTCPay Server
-                            " ; fi
+echo "#                                      BTCPay Server                                   #
+#                                                                                      #" ; fi
 if grep -q "tor-end" $HOME/.parmanode/installed.conf ; then
-echo "                                       Tor 
-                            " ; fi
+echo "#                                      Tor                                             #
+#                                                                                      #" ; fi
 if grep -q "lnd-end" $HOME/.parmanode/installed.conf ; then
-echo "                                       LND
-                            " ; fi
+echo "#                                      LND                                             #
+#                                                                                      #" ; fi
 if grep -q "mempool-end" $HOME/.parmanode/installed.conf ; then
-echo "                                       Mempool Space 
-                            " ; fi
+echo "#                                      Mempool Space                                   #
+#                                                                                      #" ; fi
 if grep -q "sparrow-end" $HOME/.parmanode/installed.conf ; then
-echo "                                       Sparrow Wallet 
-                            " ; fi
+echo "#                                      Sparrow Wallet                                  #
+#                                                                                      #" ; fi
 if grep -q "rtl-end" $HOME/.parmanode/installed.conf ; then
-echo "                                       RTL Wallet 
-                            " ; fi
+echo "#                                      RTL Wallet                                      #
+#                                                                                      #" ; fi
 if grep -q "electrum-end" $HOME/.parmanode/installed.conf ; then
-echo "                                       Electrum Wallet 
-                            " ; fi
-echo "
+echo "#                                      Electrum Wallet                                 #
+#                                                                                      #" ; fi
+echo "#                                                                                      #
 ########################################################################################
 "
 choose "xpq"
