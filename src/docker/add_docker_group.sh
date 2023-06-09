5a06ee0b1a88020277ef2370af60fec237186836
Parman <armantheparman@gmail.com>
2023-06-09 13:13:58 +1000

    modified:   src/docker/add_docker_group.sh

--- src/docker/add_docker_group.sh
+++ src/docker/add_docker_group.sh
@@ -6,7 +6,7 @@ if which docker ; then true ; else return 1 ; fi
 
 if grep docker /etc/group ; then true ; else return 1 ; fi
 
-if id $whoami | grep docker ; then return p ; else 
+if id $whoami | grep docker ; then return 0 ; else 
     sudo usermod -aG docker $whoami
     fi
 
