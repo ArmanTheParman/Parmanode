45ea287b10a0c76f80f4f61e420457cf2092090c
Parman <armantheparman@gmail.com>
2023-06-13 21:59:39 +1000

    smol ting

--- src/menus/tor.sh
+++ src/menus/tor.sh
@@ -21,18 +21,19 @@ set_terminal ; echo "
 "
 choose "xpq" ; read choice
 case $choice in 
-Q|q|QUIT|Quit|quit) exit 0 ;; 
+Q|q|QUIT|Quit|quit) 
+    exit 0 ;; 
 
-p|P) return 0 ;;
+p|P) 
+    return 0 ;;
 
 start|START) 
 if [[ $OS == "Linux" ]] ; then sudo systemctl start tor ; return 0 ; fi
-if [[ $OS == "Mac" ]] ; then brew services start tor ; return 0 ; fi
-;;
+if [[ $OS == "Mac" ]] ; then brew services start tor ; return 0 ; fi ;;
+
 stop|STOP) 
 if [[ $OS == "Linux" ]] ; then sudo systemctl stop tor ; return 0 ; fi
-if [[ $OS == "Mac" ]] ; then brew services stop tor ; return 0 ; fi
-;;
+if [[ $OS == "Mac" ]] ; then brew services stop tor ; return 0 ; fi ;;
 
 status|STATUS) 
 if [[ $OS == "Linux" ]] ; then sudo systemctl status tor ; return 0 ; fi
