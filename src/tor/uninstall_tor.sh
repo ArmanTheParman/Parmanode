50c66633b34eec9c4dbdb4dcae6ab94a6e98296d
Parman <armantheparman@gmail.com>
2023-06-15 21:45:47 +1000

    tor

--- src/tor/uninstall_tor.sh
+++ src/tor/uninstall_tor.sh
@@ -15,7 +15,20 @@ case $choice in Q|q|Quit|QUIT) exit 0 ;; p|P) return 1 ;; esac
 sudo systemctl stop tor
 sudo apt-get purge tor -y
 sudo rm -rf /etc/tor /var/lib/tor >/dev/null 2>&1
+
+delete_line "$HOME/.bitcoin/bitcoin.conf" "onion" 
+delete_line "$HOME/.bitcoin/bitcoin.conf" "bind=127.0.0.1" 
+delete_line "$HOME/.bitcoin/bitcoin.conf" "onlynet"
+
+if [[ $OS == "Linux" ]] ; then 
+    sudo rm -rf /var/lib/tor/bitcoin* >/dev/null 2>&1 
+    rm $HOME/.bitcoin/*onion* >/dev/null 2>&1
+fi
+
+rm -rf $HOME/.sparrow/tor >/dev/null 2>&1
+
 installed_config_remove "tor"
+
 enter_continue
 return 0
 }
