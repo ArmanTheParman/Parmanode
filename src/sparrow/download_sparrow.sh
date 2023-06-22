bb0ca19982f11c347b675efc5407a6d2d40566a0
Parman <armantheparman@gmail.com>
2023-06-22 21:12:26 +1000

    sparrow v1.7.7

--- src/sparrow/download_sparrow.sh
+++ src/sparrow/download_sparrow.sh
@@ -4,21 +4,21 @@ cd $HOME/parmanode
 if [[ $OS == "Linux" ]] ; then
 
     if [[ $chip == "x86_64" || $chip == "amd64" ]] ; then
-    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.6/sparrow-1.7.6-x86_64.tar.gz
+    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.7/sparrow-1.7.7-x86_64.tar.gz
     fi
     if [[ $chip == "aarch64" ]] ; then
-    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.6/sparrow-1.7.6-aarch64.tar.gz 
+    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.7/sparrow-1.7.7-aarch64.tar.gz 
     fi
 fi
 if [[ $OS == "Mac" ]] ; then
     if [[ $chip == "aarch64" ]] ; then
-    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.6/Sparrow-1.7.6-aarch64.dmg
+    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.7/Sparrow-1.7.7-aarch64.dmg
     fi
     if [[ $chip == "x86_64" ]] ; then
-    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.6/Sparrow-1.7.6-x86_64.dmg
+    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.7/Sparrow-1.7.7-x86_64.dmg
     fi
 fi
 
-curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.6/sparrow-1.7.6-manifest.txt
-curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.6/sparrow-1.7.6-manifest.txt.asc
+curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.7/sparrow-1.7.7-manifest.txt
+curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.7/sparrow-1.7.7-manifest.txt.asc
 }
