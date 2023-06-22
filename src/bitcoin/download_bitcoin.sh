0c0b90fb16adb5e255073a308c9a5778760c67e9
Parman <armantheparman@gmail.com>
2023-06-22 11:44:02 +1000

    smol typo 	modified:   src/bitcoin/download_bitcoin.sh

--- src/bitcoin/download_bitcoin.sh
+++ src/bitcoin/download_bitcoin.sh
@@ -90,7 +90,7 @@ if ! sha256sum --ignore-missing --check SHA256SUMS ; then debug "Checksum failed
 set_terminal
 
 #gpg check
-echo " Please wait a moment for gpg verificatoin..."
+echo " Please wait a moment for gpg verification..."
 gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys E777299FC265DD04793070EB944D35F9AC3DB76A
 
     if gpg --verify SHA256SUMS.asc 2>&1 | grep -q "Good" 
