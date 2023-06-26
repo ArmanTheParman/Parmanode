671b12efbf3e64c4cf6b4c1c778800661d2c4a25
Parman <armantheparman@gmail.com>
2023-06-26 16:41:37 +1000

    modified:   src/sparrow/config 	modified:   src/sparrow/make_sparrow_config.sh

--- src/sparrow/make_sparrow_config.sh
+++ src/sparrow/make_sparrow_config.sh
@@ -4,15 +4,24 @@ mkdir $HOME/.sparrow >/dev/null 2>&1
 cp $original_dir/src/sparrow/config $HOME/.sparrow/config
 swap_string "$HOME/.sparrow/config" "coreDataDir" "\"coreDataDir\": \"$HOME/.bitcoin\","
 swap_string "$HOME/.sparrow/config" "coreAuth" "\"coreAuth\": \"$rpcuser:$rpcpassword\","
+}
+
+
+
 
 #work in progress...
-#if [[ $1 == "core-tor" ]] ; then
-#
-#get_onion_address_variable
-#
-#swap_string "$HOME/.sparrow/config" "coreServer" "\"coreServer\": \"http:$ONION_ADDR\"," 
-#
-#swap_string "$HOME/.sparrow/config" "useProxy" "\"useProxy\": true,"
+function temp {
+return 0
+
+if [[ $1 == "fulcrum-tor" ]] ; then
+
+get_onion_address_variable "fulcrum"
 
 
+swap_string "$HOME/.sparrow/config" "serverType" "\"serverType\": \"ELECTRUM_SERVER\"," 
+
+swap_string "$HOME/.sparrow/config" "coreServer" "\"coreServer\": \"http:$ONION_ADDR\"," 
+
+swap_string "$HOME/.sparrow/config" "useProxy" "\"useProxy\": true,"
+fi
 }
