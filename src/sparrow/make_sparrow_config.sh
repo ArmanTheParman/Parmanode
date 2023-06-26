516039e07cdd2e4e3e531530d82813f045c76cbd
Parman <armantheparman@gmail.com>
2023-06-26 17:35:35 +1000

    modified:   src/sparrow/make_sparrow_config.sh

--- src/sparrow/make_sparrow_config.sh
+++ src/sparrow/make_sparrow_config.sh
@@ -2,17 +2,19 @@ function  make_sparrow_config {
 source $HOME/.bitcoin/bitcoin.conf >/dev/null 2>&1
 mkdir $HOME/.sparrow >/dev/null 2>&1
 cp $original_dir/src/sparrow/config $HOME/.sparrow/config
-swap_string "$HOME/.sparrow/config" "coreDataDir" "\"coreDataDir\": \"    $HOME/.bitcoin\","
-swap_string "$HOME/.sparrow/config" "coreAuth\":" "\"coreAuth\": \"    $rpcuser:$rpcpassword\","
+swap_string "$HOME/.sparrow/config" "coreDataDir" "    \"coreDataDir\": \"$HOME/.bitcoin\","
+swap_string "$HOME/.sparrow/config" "coreAuth\":" "    \"coreAuth\": \"$rpcuser:$rpcpassword\","
 
 if [[ $1 == "fulcrum-tor" ]] ; then
 
 get_onion_address_variable "fulcrum" >/dev/null
 
-swap_string "$HOME/.sparrow/config" "serverType" "\"serverType\": \"ELECTRUM_SERVER\"," 
+swap_string "$HOME/.sparrow/config" "serverType" "    \"serverType\": \"ELECTRUM_SERVER\"," 
 
-swap_string "$HOME/.sparrow/config" "electrumServer" "\"coreServer\": \"tcp:$ONION_ADDR_FULCRUM:7002\"," 
+swap_string "$HOME/.sparrow/config" "useLegacyCoreWallet" "    \"useLegacyCoreWallet\": false,\n    \"electrumServer\": \"tcp://$ONION_ADDR_FULCRUM:7002\","
 
-swap_string "$HOME/.sparrow/config" "useProxy" "\"useProxy\": true,"
+#swap_string "$HOME/.sparrow/config" "electrumServer" "\"electrumServer\": \"tcp://$ONION_ADDR_FULCRUM:7002\"," 
+
+swap_string "$HOME/.sparrow/config" "useProxy" "    \"useProxy\": true,"
 fi
 }
