9290daca8e808c0369b0e6ac7180cc6ed3124697
Parman <armantheparman@gmail.com>
2023-06-26 16:58:00 +1000

    modified:   src/sparrow/make_sparrow_config.sh

--- src/sparrow/make_sparrow_config.sh
+++ src/sparrow/make_sparrow_config.sh
@@ -2,8 +2,8 @@ function  make_sparrow_config {
 source $HOME/.bitcoin/bitcoin.conf >/dev/null 2>&1
 mkdir $HOME/.sparrow >/dev/null 2>&1
 cp $original_dir/src/sparrow/config $HOME/.sparrow/config
-swap_string "$HOME/.sparrow/config" "coreDataDir" "\"coreDataDir\": \"$HOME/.bitcoin\","
-swap_string "$HOME/.sparrow/config" "coreAuth\":" "\"coreAuth\": \"$rpcuser:$rpcpassword\","
+swap_string "$HOME/.sparrow/config" "coreDataDir" "\"coreDataDir\": \"    $HOME/.bitcoin\","
+swap_string "$HOME/.sparrow/config" "coreAuth\":" "\"coreAuth\": \"    $rpcuser:$rpcpassword\","
 }
 
 
