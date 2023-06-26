e4a8a9d971cb3506c7929617d07f4227890fcaff
Parman <armantheparman@gmail.com>
2023-06-26 17:59:11 +1000

    modified:   src/menus/menu_sparrow.sh 	modified:   src/sparrow/install_sparrow.sh 	modified:   src/sparrow/make_sparrow_config.sh 	new file:   src/sparrow/sparrow_core.sh 	new file:   src/sparrow/sparrow_fulcrumssl.sh 	new file:   src/sparrow/sparrow_fulcrumtor.sh

--- src/menus/menu_sparrow.sh
+++ src/menus/menu_sparrow.sh
@@ -6,6 +6,12 @@ while true ; do set_terminal ; echo "
 
       (start)          Start Sparrow 
 
+      (d)         Connect Sparrow directly to Bitcoin Core via tcp (defualt)
+
+      (tor)       Connect Sparrow via Tor
+
+      (ssl)       Connect Sparrow to Fulcrum vis ssl
+
 ########################################################################################
 "
 choose "xpq" ; read choice ; set_terminal
@@ -16,6 +22,18 @@ p|P) return 1 ;;
 start|Start|START|S|s)
 run_sparrow
 return 0 ;;
+
+d|D)
+sparrow_core
+;;
+
+tor|Tor|TOR)
+sparrow_fulcrumtor
+;;
+
+ssl|Ssl|SSL)
+sparrow_fulcrumssl
+;;
 
 #rc|RC|Rc|rC)
 #rm -rf $HOME/.sparrow/*
--- src/sparrow/make_sparrow_config.sh
+++ src/sparrow/make_sparrow_config.sh
@@ -1,20 +1,36 @@
 function  make_sparrow_config {
 source $HOME/.bitcoin/bitcoin.conf >/dev/null 2>&1
 mkdir $HOME/.sparrow >/dev/null 2>&1
+rm $HOME/.sparrow/config >/dev/null 2>&1
 cp $original_dir/src/sparrow/config $HOME/.sparrow/config
 swap_string "$HOME/.sparrow/config" "coreDataDir" "    \"coreDataDir\": \"$HOME/.bitcoin\","
 swap_string "$HOME/.sparrow/config" "coreAuth\":" "    \"coreAuth\": \"$rpcuser:$rpcpassword\","
 
-if [[ $1 == "fulcrum-tor" ]] ; then
-
+if [[ $1 == "fulcrumtor" ]] ; then
+unset $ONION_ADDR_FULCRUM
 get_onion_address_variable "fulcrum" >/dev/null
+
+    if [[ -z $ONION_ADDR_FULCRUM ]] ; then
+    set_terminal
+    echo "Fulcrum Tor must first be enabled from the Fulcrum menu. Aborting."
+    enter_continue
+    make_sparrow_config
+    return 1
+    fi
 
 swap_string "$HOME/.sparrow/config" "serverType" "    \"serverType\": \"ELECTRUM_SERVER\"," 
 
+# the following odd bit of code is because electrumServer comes after useLegacyCoreWallet
+# and using a placehoder corrupts the file.
 swap_string "$HOME/.sparrow/config" "useLegacyCoreWallet" "    \"useLegacyCoreWallet\": false,\n    \"electrumServer\": \"tcp://$ONION_ADDR_FULCRUM:7002\","
-
-#swap_string "$HOME/.sparrow/config" "electrumServer" "\"electrumServer\": \"tcp://$ONION_ADDR_FULCRUM:7002\"," 
 
 swap_string "$HOME/.sparrow/config" "useProxy" "    \"useProxy\": true,"
 fi
 }
+
+if [[ $1 == "fulcrumssl" ]] ; then
+
+swap_string "$HOME/.sparrow/config" "serverType" "    \"serverType\": \"ELECTRUM_SERVER\"," 
+swap_string "$HOME/.sparrow/config" "useLegacyCoreWallet" "    \"useLegacyCoreWallet\": false,\n    \"electrumServer\": \"ssl://127.0.0.1:50002\","
+swap_string "$HOME/.sparrow/config" "useProxy" "    \"useProxy\": true,"
+fi
