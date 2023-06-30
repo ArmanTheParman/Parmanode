37433b18ca07ea39f97189e8cbc284952c354815
Parman <armantheparman@gmail.com>
2023-06-30 15:31:59 +1000

    v3.4.3

--- src/btcpay_tor/uninstall_btcpay_tor.sh
+++ src/btcpay_tor/uninstall_btcpay_tor.sh
@@ -4,6 +4,8 @@ curl http://parman.org/downloadable/counter/parmanode_btcpTOR_uninstall.html >/d
 
 delete_line "/etc/tor/torrc" "btcpayTOR"
 delete_line "/etc/tor/torrc" "7003"
+sudo rm -rf /var/lib/tor/btcpayTOR-server/ >/dev/null 2>&1
+
 sudo systemctl restart tor
 
 installed_config_remove "btcpTOR"
