c154246c74f708123f21859bb79c647e7d35ea3c
Parman <armantheparman@gmail.com>
2023-07-04 19:46:08 +1000

    modified:   src/bitcoin/set_rpc_authentication.sh 	modified:   src/fulcrum_linux/make_fulcrum_config.sh 	modified:   src/fulcrum_mac/check_rpc_authentication_exists.sh

--- src/fulcrum_linux/make_fulcrum_config.sh
+++ src/fulcrum_linux/make_fulcrum_config.sh
@@ -2,10 +2,12 @@ function make_fulcrum_config {
 source $HOME/.parmanode/parmanode.conf >/dev/null 2>&1
 
 # make config file
-if [[ -z $rpcuser ]] ; then #from parmanode.conf 
-    set_rpc_authentication 
-    set_terminal
-    if [ $? != 0 ] ; then set_terminal ; echo "Error setting rps user/pass. Aborting installation." ; enter_continue ; return 1 ; fi
+if which bitcoind >/dev/null 2>&1 ; then
+    if [[ -z $rpcuser ]] ; then #from parmanode.conf 
+        set_rpc_authentication 
+        set_terminal
+        if [ $? != 0 ] ; then set_terminal ; echo "Error setting rps user/pass. Aborting installation." ; enter_continue ; return 1 ; fi
+    fi
 fi
 
 # set datadir variable
@@ -33,7 +35,9 @@ peering = false " > $HOME/parmanode/fulcrum/fulcrum.conf || \
 { log "fulcrum" "make_fulcrum_config, echo redirect failed." ; \
 debug "echo redirect failed when making fulcrum.conf" ; return 1 ;}
 
+if which bitcoind >/dev/null 2>&1 ; then
 edit_user_pass_fulcrum_conf #gets user and pass from bitcoin.conf and adds to fulcrum.conf
+fi
 
 log "fulcrum" "fulcrum config file made"
 return 0
