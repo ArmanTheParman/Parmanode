535653574585c56aa631f2e7046cd9a09dfab311
Parman <armantheparman@gmail.com>
2023-07-05 02:22:59 +1000

    modified:   src/menus/remove_programs.sh 	modified:   todo.txt

--- src/menus/remove_programs.sh
+++ src/menus/remove_programs.sh
@@ -24,76 +24,76 @@ echo "
 if grep -q "parmanode-end" $HOME/.parmanode/installed.conf ; then
 echo "                                           (parmanode)        Parmanode
 " ; fi
-if grep -q "bitcoin-end" $HOME/.parmanode/installed.conf ; then
+if grep -q "bitcoin-end" $HOME/.parmanode/installed.conf ; then bitcoinmenu=1
 echo "                                           (bitcoin)          Bitcoin Core
 "
-elif grep -q "bitcoin-start" $HOME/.parmanode/installed.conf ; then
+elif grep -q "bitcoin-start" $HOME/.parmanode/installed.conf ; then bitcoinmenu=1
 echo "                                           (bitcoin)          Bitcoin (partial)
 " ; fi
-if grep -q "fulcrum-end" $HOME/.parmanode/installed.conf ; then                               
+if grep -q "fulcrum-end" $HOME/.parmanode/installed.conf ; then fulcrummenu=1 
 echo "                                           (fulcrum)          Fulcrum Server
 "
-elif grep -q "fulcrum-start" $HOME/.parmanode/installed.conf ; then                               
+elif grep -q "fulcrum-start" $HOME/.parmanode/installed.conf ; then fulcrummenu=1                                
 echo "                                           (fulcrum)          Fulcrum (partial)
 " ; fi
-if grep -q "docker-end" $HOME/.parmanode/installed.conf ; then                               
+if grep -q "docker-end" $HOME/.parmanode/installed.conf ; then dockermenu=1 
 echo "                                           (docker)           Docker 
 "
-elif grep -q "docker-start" $HOME/.parmanode/installed.conf ; then                               
+elif grep -q "docker-start" $HOME/.parmanode/installed.conf ; then dockermenu=1                       
 echo "                                           (docker)           Docker (partial) 
 " ; fi
-if grep -q "btcpay-end" $HOME/.parmanode/installed.conf ; then                               
+if grep -q "btcpay-end" $HOME/.parmanode/installed.conf ; then btcpaymenu=1              
 echo "                                           (btcp)             BTCPay 
 "
-elif grep -q "btcpay-start" $HOME/.parmanode/installed.conf ; then                               
+elif grep -q "btcpay-start" $HOME/.parmanode/installed.conf ; then btcpaymenu=1                               
 echo "                                           (btcp)             BTCPay (partial) 
 " ; fi
-if which tor >/dev/null 2>&1 ; then                               
+if which tor >/dev/null 2>&1 ; then tormenu=1
 echo "                                           (tor)              Tor 
 "
-elif grep -q "tor-start" $HOME/.parmanode/installed.conf ; then                               
+elif grep -q "tor-start" $HOME/.parmanode/installed.conf ; then tormenu=1
 echo "                                           (tor)              Tor (partial)
 " ; fi
-if grep -q "lnd-end" $HOME/.parmanode/installed.conf ; then                               
+if grep -q "lnd-end" $HOME/.parmanode/installed.conf ; then lndmenu=1
 echo "                                           (lnd)              LND 
 "
-elif grep -q "lnd-start" $HOME/.parmanode/installed.conf ; then                               
+elif grep -q "lnd-start" $HOME/.parmanode/installed.conf ; then lndmenu=1
 echo "                                           (lnd)              LND (partial) 
 " ; fi
-if grep -q "mempool-end" $HOME/.parmanode/installed.conf ; then                               
+if grep -q "mempool-end" $HOME/.parmanode/installed.conf ; then mempoomenu=1
 echo "                                           (mem)              Mempool Space 
 "
-elif grep -q "mempool-start" $HOME/.parmanode/installed.conf ; then                               
+elif grep -q "mempool-start" $HOME/.parmanode/installed.conf ; then mempoolmenu=1
 echo "                                           (mem)              Mempool (partial) 
 " ; fi
-if grep -q "sparrow-end" $HOME/.parmanode/installed.conf ; then                               
+if grep -q "sparrow-end" $HOME/.parmanode/installed.conf ; then sparrowmenu=1
 echo "                                           (s)                Sparrow Wallet 
 "  
-elif grep -q "sparrow-start" $HOME/.parmanode/installed.conf ; then                               
+elif grep -q "sparrow-start" $HOME/.parmanode/installed.conf ; then sparrowmenu=1
 echo "                                           (s)                Sparrow (partial) 
 " ; fi
-if grep -q "rtl-end" $HOME/.parmanode/installed.conf ; then                               
+if grep -q "rtl-end" $HOME/.parmanode/installed.conf ; then rtlmenu=1
 echo "                                           (r)                RTL Wallet 
 "  
-elif grep -q "rtl-start" $HOME/.parmanode/installed.conf ; then                               
+elif grep -q "rtl-start" $HOME/.parmanode/installed.conf ; then rtlmenu=1
 echo "                                           (r)                RTL (partial) 
 " ; fi
-if grep -q "electrum-end" $HOME/.parmanode/installed.conf ; then                               
+if grep -q "electrum-end" $HOME/.parmanode/installed.conf ; then electrummenu=1
 echo "                                           (e)                Electrum Wallet 
 "  
-elif grep -q "electrum-start" $HOME/.parmanode/installed.conf ; then                               
+elif grep -q "electrum-start" $HOME/.parmanode/installed.conf ; then electrummenu=1
 echo "                                           (e)                Electrum (partial) 
 " ; fi
-if grep -q "tor-server-end" $HOME/.parmanode/installed.conf ; then                               
+if grep -q "tor-server-end" $HOME/.parmanode/installed.conf ; then torservermenu=1
 echo "                                           (ts)               Tor Server 
 "
-elif grep -q "tor-server-start" $HOME/.parmanode/installed.conf ; then                               
+elif grep -q "tor-server-start" $HOME/.parmanode/installed.conf ; then torservermenu=1
 echo "                                           (ts)               Tor Server (partial) 
 " ; fi
-if grep -q "btcpTOR-end" $HOME/.parmanode/installed.conf ; then                               
+if grep -q "btcpTOR-end" $HOME/.parmanode/installed.conf ; then btcpTORmenu=1
 echo "                                           (btcpt)            Tor Server 
 "
-elif grep -q "btcpTOR-start" $HOME/.parmanode/installed.conf ; then                               
+elif grep -q "btcpTOR-start" $HOME/.parmanode/installed.conf ; then btcpTORmenu=1
 echo "                                           (btcpt)            Tor Server (partial) 
 " ; fi
 echo "
@@ -110,61 +110,85 @@ exit 0
 ;;
 
 bitcoin|Bitcoin|BITCOIN)
+if [[ $bitcoinmenu == 1]] ; then
 uninstall_bitcoin
 return 0
+fi
 ;;
 
 fulcrum|Fulcrum|FULCRUM)
+if [[ $fulcrummenu == 1]] ; then
 uninstall_fulcrum
 return 0
+fi
 ;;
 
 docker|Docker|DOCKER)
+if [[ $dockermenu == 1]] ; then
 if [[ $OS == "Mac" ]] ; then uninstall_docker_mac ; continue ; fi
 uninstall_docker_linux 
 return 0
+fi
 ;;
 
 btcp|BTCP|Btcp)
+if [[ $btcpaymenu == 1]] ; then
 uninstall_btcpay
 return 0
+fi
 ;;
 
 TOR|Tor|tor)
+if [[ $tormenu == 1]] ; then
 no_mac || return 1
 uninstall_tor
 return 0
+fi
 ;;
 
 lnd|LND|Lnd)
+if [[ $lndmenu == 1]] ; then
 if [[ $OS == "Linux" ]] ; then uninstall_lnd ; return 0 ; fi
+fi
 ;;
 
 mem|MEM|Mem)
+if [[ $mempoolmenu == 1]] ; then
 uninstall_mempool
+fi
 ;;
 
 s|S|SPARROW|Sparrow|sparrow)
+if [[ $sparrowmenu == 1]] ; then
     uninstall_sparrow
 	return 0 
+	fi
 	;;
 R|r|RTL|rtl|Rtl)
+if [[ $rtlmenu == 1]] ; then
 	uninstall_rtl
 	return 0
+	fi
 	;;
 
 e|E|Electrum|electrum|ELECTRUM)
+if [[ $electrummenu == 1]] ; then
     uninstall_electrum
 	return 0
+	fi
 	;;
 ts|TS|Ts)
+if [[ $torservermenu == 1]] ; then
 	no_mac || return 1
 	uninstall_tor_server
 	return 0
+	fi
 	;;
 btcpt|BTCPT)
+if [[ $btcpTORmenu == 1 ]] ; then
 	no_mac || return 1
 	uninstall_btcpay_tor
+	fi
     ;;
 
 p|P)
