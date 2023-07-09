276e7a4345ce4490d05920ff7bc9b81aacfa90c6
Parman <armantheparman@gmail.com>
2023-07-09 20:30:20 +1000

    modified:   src/lnd/download_lnd.sh

--- src/lnd/download_lnd.sh
+++ src/lnd/download_lnd.sh
@@ -13,12 +13,8 @@ if [[ $chip == "x86_64" ]] ; then
 curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-amd64-v0.16.2-beta.tar.gz
 fi
 
-if [[ $chip == "arm64" ]] ; then
+if [[ $chip == "arm64" || $chip == "aarch64" ]] ; then
 curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-arm64-v0.16.2-beta.tar.gz 
-fi
-
-if [[ $chip == "arm64" ]] ; then
-curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-arm64-v0.16.2-beta.tar.gz
 fi
 
 if [[ $chip == "armv6l" ]] ; then
