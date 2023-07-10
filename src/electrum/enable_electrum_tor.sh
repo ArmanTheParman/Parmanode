f7a579c1f761759e125f9df7123e6725215e83a2
Arman <armanandnina@Armans-MacBook-Pro.local>
2023-07-10 11:54:51 +1000

    swap string

--- src/electrum/enable_electrum_tor.sh
+++ src/electrum/enable_electrum_tor.sh
@@ -1,10 +1,11 @@
 function enable_electrum_tor {
-
+if [[ $OS == "Linux" ]] ; then
 if ! sudo cat /etc/tor/torrc | grep "fulcrum-service" ; then
 set_terminal
 echo "Please first enable Fulcrum to use Tor from the Parmanode menu."
 enter_continue
 return 0
+fi
 fi
 
 set_terminal
@@ -14,6 +15,8 @@ enter_continue
 
 
 get_onion_address_variable "fulcrum" >/dev/null 
+
+swap_string "$HOME/.electrum/config" "\"server" "    \"server\": \"server\": \"${ONION_ADDR_FULCRUM}:7002:t\","
 
 swap_string "$HOME/.electrum/config" "oneserver" "    \"oneserver\": false,\n     \"proxy\": \"socks5:127.0.0.1:9050::\","
 
