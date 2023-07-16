bffc884ea94d3b29fc5b0971f02386879b6a40bb
Parman <armantheparman@gmail.com>
2023-07-17 02:20:26 +1000

    enable electrum tor

--- src/electrum/enable_electrum_tor.sh
+++ src/electrum/enable_electrum_tor.sh
@@ -12,35 +12,9 @@ set_terminal
 echo "Make sure Electrum has been completely shut down before proceeding."
 enter_continue
 
-
-
 get_onion_address_variable "fulcrum" >/dev/null 
-
-#makes first part of file.
-cd $HOME/.electrum/
-grep -m1 "\"server" -B100 "$HOME/.electrum/config" > preconfig
-
-#deletes last line
-head -n $(($(wc -l < preconfig) - 1)) preconfig > file.tmp && mv file.tmp preconfig 
-
-#make second part of file
-echo "    \"server\": \"server\": \"${ONION_ADDR_FULCRUM}:7002:t\"," >> preconfig2
-
-grep -A 999 "\"server" preconfig > preconfig1.5
-
-cat preconfig preconfig1.5 preconfig2 > config
-rm preconf*
-
-#next part
-#makes first part of file
-grep -m1 "\"server\"" =B100 "$HOME/.electrum/config" > preconfig
-
-#delete last line
-head -n $(($(wc -l < preconfig) - 1)) preconfig > file.tmp && mv file.tmp preconfig 
-
-#make second part of file
-
-swap_string "$HOME/.electrum/config2" "oneserver" "    \"oneserver\": false,\n     \"proxy\": \"socks5:127.0.0.1:9050::\","
+change_string_mac "$HOME/.electrum/config" "\"server"  "    \"server\": \"server\": \"${ONION_ADDR_FULCRUM}:7002:t\"," "swap"
+change_string_mac "$HOME/.electrum/config" "oneserver" "    \"proxy\": \"socks5:127.0.0.1:9050::\"," "after"
 
 success "Electrum Wallet" "being setup to connect to Tor."
 
