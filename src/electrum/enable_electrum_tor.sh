d9f7cd2091a9b7e9b6f63d8d3f726748ed8aa7c3
Parman <armantheparman@gmail.com>
2023-07-17 02:36:25 +1000

    modified:   src/electrum/enable_electrum_tor.sh 	modified:   src/text_functions/change_string_mac.sh 	modified:   src/text_functions/instructions.sh

--- src/electrum/enable_electrum_tor.sh
+++ src/electrum/enable_electrum_tor.sh
@@ -11,10 +11,23 @@ fi
 set_terminal
 echo "Make sure Electrum has been completely shut down before proceeding."
 enter_continue
-
+if [[ $OS == "Linux" ]] ; then
 get_onion_address_variable "fulcrum" >/dev/null 
-change_string_mac "$HOME/.electrum/config" "\"server"  "    \"server\": \"server\": \"${ONION_ADDR_FULCRUM}:7002:t\"," "swap"
-change_string_mac "$HOME/.electrum/config" "oneserver" "    \"proxy\": \"socks5:127.0.0.1:9050::\"," "after"
+fi
+if [[ $OS == "Mac" ]] ; then
+set_terminal ; 
+echo "Please paste in the onion address for your Fulcrum/Electrum server you wish to use."
+echo "Note this is not the onion address for your Bitcoin Node. It's for Fulcrum/Electrum."
+echo "You need the long string plus the ending \".onion\", and nothing after that."
+echo "After pasting, hit <enter>."
+read ONION_ADDR_FULCRUM
+set_terminal
+echo "Do you use
+fi
+
+change_string_mac "$HOME/.electrum/config" "\"server"  "    \"server\": \"server\": \"${ONION_ADDR_FULCRUM}:7002:t\"," swap
+change_string_mac "$HOME/.electrum/config" proxy null delete
+change_string_mac "$HOME/.electrum/config" oneserver "    \"proxy\": \"socks5:127.0.0.1:9050::\"," after
 
 success "Electrum Wallet" "being setup to connect to Tor."
 
