838ada320282e8e2f3f8259187cb3aa9f8b43924
Parman <armantheparman@gmail.com>
2023-07-17 18:39:08 +1000

    change_string_mac

--- src/text_functions/change_string_mac.sh
+++ src/text_functions/change_string_mac.sh
@@ -9,41 +9,60 @@ searchstring="$2"
 newline="$3"
 positionnewline="$4" #("before" or "after" or "swap" or "delte")
 
+if [[ ! -f $inputfile ]] ; then echo "File doesn't exist. Change_string_mac function failed." 
+echo "
+Aborting."
+enter_continue
+exit 0
+fi
+
 if [[ $positionnewline == "after" ]] ; then
-    sudo grep -m1 -B 1000000 ${searchstring} ${inputfile} > /tmp/temp1.txt
+    sudo grep -m1 -B 100000 ${searchstring} ${inputfile} > /tmp/temp1.txt
     echo $newline | tee -a /tmp/temp1.txt >/dev/null
+    #new line is added after seaerchstring
 
     sudo grep -A 100000 ${searchstring} ${inputfile} > /tmp/temp2.txt 
-    if [[ $count == 1 || $count == 0 ]] ; then
-        cat /tmp/temp2.txt > /tmp/temp3.txt
-    else
     count=$(wc -l /tmp/temp2.txt | awk '{print $1}')
-    fi
-    tail -n $(($count -1)) /tmp/temp2.txt > /tmp/temp3.txt
 
-    sudo cat /tmp/temp1.txt /tmp/temp3.txt | sudo tee $inputfile >/dev/null
-    debug "hi after"
+    if [[ $count == 1 ]] ; then #condition where there's nothing to add
+        sudo cat /tmp/temp1.txt | sudo tee $inputfile >/dev/null
+        #file done
+    else
+        tail -n $(($count -1)) /tmp/temp2.txt >> /tmp/temp1.txt
+        sudo cat /tmp/temp1.txt | sudo tee $inputfile >/dev/null
+        #file done
+    fi
 fi
 
 
 if [[ $positionnewline == "before" ]] ; then
     sudo grep -m1 -B 1000000 ${searchstring} ${inputfile} > /tmp/temp1.txt
     count=$(wc -l /tmp/temp1.txt | awk '{print $1}')
-    if [[ $count == 1 || $count == 0 ]] ; then
-        cat /tmp/temp1.txt > /tmp/temp2.txt
+    if [[ $count == 1 ]] ; then
+        echo "$newline" > /tmp/temp2.txt
+        cat /tmp/temp2.txt /tmp/temp1.txt > $inputfile    
+        #file done 
     else
         head -n $(($count - 1 )) /tmp/temp1.txt > /tmp/temp2.txt
+        echo $newline | tee -a /tmp/temp2.txt >/dev/null
+        sudo grep -A 100000 ${searchstring} ${inputfile} | sudo tee -a /tmp/temp2.txt 
+        sudo cat /tmp/temp2.txt | sudo tee $inputfile >/dev/null
+        #file done
     fi
-    echo $newline | tee -a /tmp/temp2.txt >/dev/null
-    sudo grep -A 100000 ${searchstring} ${inputfile} >> /tmp/temp2.txt 
-    sudo cat /tmp/temp2.txt | sudo tee $inputfile >/dev/null
-    debug "hi before"
 fi
 
 if [[ $positionnewline == "swap" ]] ; then
     sudo grep -m1 -B 1000000 ${searchstring} ${inputfile} > /tmp/temp1.txt
     count=$(wc -l /tmp/temp1.txt | awk '{print $1}')
-    if [[ $count == 1 || $count == 0 ]] ; then
+    if [[ $count == 1 ]] ; then
+
+        inputfile_count=$(wc -l $inputfile | awk '{print $1}')
+        tail -n $(($inputfile_count - 1)) $inputfile > /tmp/temp2.txt
+
+#resume here
+
+        echo "$newline" | tee /tmp/temp1.txt
+        
         cat /tmp/temp1.txt > /tmp/temp2.txt
     else
         head -n $(($count - 1 )) /tmp/temp1.txt > /tmp/temp2.txt
