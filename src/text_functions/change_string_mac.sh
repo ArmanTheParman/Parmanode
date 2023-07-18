7b01863439ddbdaf24be56c950cc918410ed33a5
Parman <armantheparman@gmail.com>
2023-07-18 23:02:40 +1000

    modified:   src/text_functions/change_string_mac.sh

--- src/text_functions/change_string_mac.sh
+++ src/text_functions/change_string_mac.sh
@@ -1,6 +1,12 @@
 #need to carefully check this. There is a bug related to the if statements
 # the subsequent cat files need checking.
 
+#not taken into account multiple entries of a searchstring.
+#delete function will remove multiple entries, so be careful
+#to use it in other parts of this function.
+
+#need to check wc is no 1 or zero when subtraction. Review before finalising.
+
 
 function change_string_mac {
 #will replace entire line containing search string with the new line
@@ -16,6 +22,7 @@ enter_continue
 exit 0
 fi
 
+#working
 if [[ $positionnewline == "after" ]] ; then
     sudo grep -m1 -B 100000 ${searchstring} ${inputfile} > /tmp/temp1.txt
     echo $newline | tee -a /tmp/temp1.txt >/dev/null
@@ -24,74 +31,78 @@ if [[ $positionnewline == "after" ]] ; then
     sudo grep -A 100000 ${searchstring} ${inputfile} > /tmp/temp2.txt 
     count=$(wc -l /tmp/temp2.txt | awk '{print $1}')
 
-    if [[ $count == 1 ]] ; then #condition where there's nothing to add
-        sudo cat /tmp/temp1.txt | sudo tee $inputfile >/dev/null
+    if [[ $count == 1 ]] ; then 
+    #condition where there's nothing to add because nothing comes after the searchstring
+    read -p "count is 1, no lines after searchstring. Hit enter."
+        cat /tmp/temp1.txt | sudo tee $inputfile >/dev/null
         #file done
     else
         tail -n $(($count -1)) /tmp/temp2.txt >> /tmp/temp1.txt
-        sudo cat /tmp/temp1.txt | sudo tee $inputfile >/dev/null
+        cat /tmp/temp1.txt | sudo tee $inputfile >/dev/null
         #file done
     fi
 fi
 
 
+#working
 if [[ $positionnewline == "before" ]] ; then
     sudo grep -m1 -B 1000000 ${searchstring} ${inputfile} > /tmp/temp1.txt
     count=$(wc -l /tmp/temp1.txt | awk '{print $1}')
+
     if [[ $count == 1 ]] ; then
         echo "$newline" > /tmp/temp2.txt
         cat /tmp/temp2.txt /tmp/temp1.txt > $inputfile    
         #file done 
+
     else
         head -n $(($count - 1 )) /tmp/temp1.txt > /tmp/temp2.txt
         echo $newline | tee -a /tmp/temp2.txt >/dev/null
-        sudo grep -A 100000 ${searchstring} ${inputfile} | sudo tee -a /tmp/temp2.txt 
-        sudo cat /tmp/temp2.txt | sudo tee $inputfile >/dev/null
+        sudo grep -A 100000 ${searchstring} ${inputfile} | sudo tee -a /tmp/temp2.txt >/dev/null
+        cat /tmp/temp2.txt | sudo tee $inputfile >/dev/null
         #file done
     fi
 fi
 
+
+#working ok now
 if [[ $positionnewline == "swap" ]] ; then
-    sudo grep -m1 -B 1000000 ${searchstring} ${inputfile} > /tmp/temp1.txt
-    count=$(wc -l /tmp/temp1.txt | awk '{print $1}')
-    if [[ $count == 1 ]] ; then
-
-        inputfile_count=$(wc -l $inputfile | awk '{print $1}')
-        tail -n $(($inputfile_count - 1)) $inputfile > /tmp/temp2.txt
-
-#resume here
-
-        echo "$newline" | tee /tmp/temp1.txt
-        
-        cat /tmp/temp1.txt > /tmp/temp2.txt
-    else
-        head -n $(($count - 1 )) /tmp/temp1.txt > /tmp/temp2.txt
-    fi
-    echo $newline | tee -a /tmp/temp2.txt >/dev/null 
-
-    sudo grep -A 100000 ${searchstring} ${inputfile} > /tmp/temp3.txt 
-    count=$(wc -l /tmp/temp3.txt | awk '{print $1}')
-    tail -n $(($count -1 )) /tmp/temp3.txt > /tmp/temp4.txt
-
-    sudo cat /tmp/temp2.txt /tmp/temp4.txt | sudo tee $inputfile >/dev/null
-    debug "hi swap"
-fi
-
-if [[ $positionnewline == "delete" ]] ; then
     sudo grep -m1 -B 100000 ${searchstring} ${inputfile} > /tmp/temp1.txt
     count=$(wc -l /tmp/temp1.txt | awk '{print $1}')
-    if [[ $count == 1 || $count == 0 ]] ; then
-        cat /tmp/temp1.txt > /tmp/temp2.txt
+    cat /tmp/temp1.txt
+
+    if [ $count == 1 ] ; then # means searchstring is on line one of input file.
+        #need to delete first line of inputfile and replace with newline...
+        count_inputfile=$(wc -l $inputfile | awk '{print $1}')
+        sudo tail -n $(($count_inputfile -1)) $inputfile | tee /tmp/temp2.txt >/dev/null
+        #temp2 holds inputfile without first line now
+        echo "$newline" | tee /tmp/temp3.txt >/dev/null
+        cat /tmp/temp3.txt /tmp/temp2.txt | sudo tee $inputfile >/dev/null
+        #file done
     else
-        head -n $(( $count - 1 )) /tmp/temp1.txt > /tmp/temp2.txt
+        #temp1 has n number of other lines then searchstring last.
+        #need to delete last line of temp1
+        count_temp1=$(wc -l /tmp/temp1.txt | awk '{print $1}')
+        head -n $(( $count_temp1 - 1 )) /tmp/temp1.txt | tee /tmp/temp2.txt >/dev/null
+        #now append $newline to temp2
+        echo "$newline" | tee -a /tmp/temp2.txt >/dev/null
+        #now add bottom part of inputfile to temp2
+        sudo grep -A 100000 ${searchstring} ${inputfile} | tee /tmp/temp3.txt >/dev/null
+            #delete line one of temp3
+            count_temp3=$(wc -l /tmp/temp3.txt | awk '{print $1}')
+            tail -n $(( $count_temp3 - 1 )) /tmp/temp3.txt | tee /tmp/temp4.txt >/dev/null
+            #temp4 holds bottom part needed from inputfile
+
+        #now append temp4 to temp 2 to make inputfile final.
+        cat /tmp/temp2.txt /tmp/temp4.txt | sudo tee $inputfile >/dev/null
+        #file done
     fi
+fi
 
-    sudo grep -A 100000 ${searchstring} ${inputfile} > /tmp/temp3.txt 
-    count=$(wc -l /tmp/temp3.txt | awk '{print $1}')
-    tail -n $(($count - 1)) /tmp/temp3.txt > /tmp/temp4.txt
-
-    sudo cat /tmp/temp2.txt /tmp/temp4.txt | sudo tee $inputfile >/dev/null
-    debug "hi delete"
+#this part works
+if [[ $positionnewline == "delete" ]] ; then
+    
+    sudo grep -v $searchstring $inputfile | tee /tmp/temp1.txt >/dev/null
+    cat /tmp/temp1.txt | sudo tee $inputfile >/dev/null
 fi
 
 cd /tmp >/dev/null
