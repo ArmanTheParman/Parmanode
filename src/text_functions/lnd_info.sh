1a725c6bd52dea380f1262d5022194e2210389da
Parman <armantheparman@gmail.com>
2023-07-29 00:27:42 +1000

    channel backup info

--- src/text_functions/lnd_info.sh
+++ src/text_functions/lnd_info.sh
@@ -25,7 +25,44 @@ function lnd_info {
     You can buy the on line, and the give your computer some power during a powerout
     and allow you to safely shut the computer down, avoiding data corruption.
 
+
+    TYPE c TO LEARN ABOUT CHANNEL BACKUPS, THEN <ENTER> OR JUST <ENTER> TO RETURN  
+
 ########################################################################################
+"
+read choice
+if [[ $choice == c || $choice == C ]] ; then
+channel_info
+else
+return 0
+fi
+}
+
+function channel_info {
+
+set_terminal ; echo "
+########################################################################################
+
+    When backing up lightning channels, there are two possible avenues.
+
+   A) The best way is to back up the \"channel.backup\" file. This is a static channel
+      back up. When you restore your lightning wallet, you'll enter your seed, and 
+      this file (when prompted). Your on-chain funds will be restored immediately,
+      and the channels you had open will be closed. The funds will first move to a
+      sort of trustless \"escrow\" address, and after some time of holding, will be
+      sent to your lightning wallet (you will receive your share of the funds as per
+      the state of the channel, and your channel partner will receive theirs).
+
+   B) Another way (not recommended) is to rely of the channel.db file to back up. 
+      This method allows you to keep your channels open, however, if there is any
+      change to the channel state different to that recorded in the channel.db file,
+      then your channel partner is entitled to punish you and take all the funds in 
+      the channel. Sticking to option A is my suggestion, and that is the only option
+      supported by Parmanode.
+
+########################################################################################
+
 "
 enter_continue
 }
+
