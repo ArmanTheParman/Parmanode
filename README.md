dd3fa751ec5a3babb733d347456f5a97c66cee2a
Parman <armantheparman@gmail.com>
2023-07-29 12:37:29 +1000

    v3.4.6 - Sparrow v1.7.8

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Parmanode 3.4.5
+# Parmanode 3.4.6
 
 #### Instructions to install at the end
 
--- src/sparrow/download_sparrow.sh
+++ src/sparrow/download_sparrow.sh
@@ -1,24 +1,24 @@
 function download_sparrow {
 cd $HOME/parmanode
-
+sparrow_version="1.7.8"
 if [[ $OS == "Linux" ]] ; then
 
     if [[ $chip == "x86_64" || $chip == "amd64" ]] ; then
-    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.7/sparrow-1.7.7-x86_64.tar.gz
+    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/${sparrow_version}/sparrow-${sparrow_version}-x86_64.tar.gz
     fi
     if [[ $chip == "aarch64" ]] ; then
-    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.7/sparrow-1.7.7-aarch64.tar.gz 
+    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/${sparrow_version}/sparrow-${sparrow_version}-aarch64.tar.gz 
     fi
 fi
 if [[ $OS == "Mac" ]] ; then
     if [[ $chip == "aarch64" ]] ; then
-    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.7/Sparrow-1.7.7-aarch64.dmg
+    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/${sparrow_version}/Sparrow-${sparrow_version}-aarch64.dmg
     fi
     if [[ $chip == "x86_64" ]] ; then
-    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.7/Sparrow-1.7.7-x86_64.dmg
+    curl -LO https://github.com/sparrowwallet/sparrow/releases/download/${sparrow_version}/Sparrow-${sparrow_version}-x86_64.dmg
     fi
 fi
 
-curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.7/sparrow-1.7.7-manifest.txt
-curl -LO https://github.com/sparrowwallet/sparrow/releases/download/1.7.7/sparrow-1.7.7-manifest.txt.asc
+curl -LO https://github.com/sparrowwallet/sparrow/releases/download/${sparrow_version}/sparrow-${sparrow_version}-manifest.txt
+curl -LO https://github.com/sparrowwallet/sparrow/releases/download/${sparrow_version}/sparrow-${sparrow_version}-manifest.txt.asc
 }
