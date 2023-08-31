84d7d92572a7db877e06231eb026d6041475c62c
Parman <armantheparman@gmail.com>
2023-08-31 23:50:53 +1000

    modified:   src/sparrow/udev.sh

--- src/sparrow/udev.sh
+++ src/sparrow/udev.sh
@@ -18,7 +18,9 @@ if [[ $1 == "specter" ]] ; then return 0 ; fi
 
 
 cd /tmp
+debug "downloading udev rules..."
 curl -LO http://parman.org/downloadable/udev
 sudo ./udev installudevrules
+debug "finished downloading and installing udev rules; next rm udev"
 rm udev
 }
