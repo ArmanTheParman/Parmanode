4f306a1a51394be367e5191ff3ebf0124c04e384
Parman <armantheparman@gmail.com>
2023-08-31 16:23:52 +1000

    modified:   src/text_functions/debug.sh 	modified:   src/text_functions/swap_string.sh

--- src/text_functions/debug.sh
+++ src/text_functions/debug.sh
@@ -4,6 +4,7 @@
 # code to contine.
 
 function debug {
+if [ $debug == 1 ] ; then
 echo "Debug point. Message:
 "
 echo "$1
@@ -11,6 +12,7 @@ echo "$1
 log "debug" "$1"
 enter_continue
 return 0
+fi
 }
 	
 function debug1 {
