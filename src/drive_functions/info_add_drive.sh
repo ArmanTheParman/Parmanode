f81da164b1b8b8b55182714e4689b043b7b4a262
Parman <armantheparman@gmail.com>
2023-09-01 15:03:16 +1000

    modified:   src/drive_functions/info_add_drive.sh

--- src/drive_functions/info_add_drive.sh
+++ src/drive_functions/info_add_drive.sh
@@ -3,21 +3,28 @@ while true ; do
 set_terminal ; echo " 
 ########################################################################################
 
-    Using a second drive for Parmanode is possible, but there are many scenarios  
-    where things could go wrong. I have tried to anticipate what I can, and as things
-    pop up (from user feedback), I will make improvements.
+    Using a second drive for Parmanode is possible but only between operating systems.
+    For example, you can't import a Parmanode drive used on a Mac to a Linux computer.
 
-    Currently, you shouldn't attach two Parmanode external drives to the computer
+    Also, you shouldn't attach two Parmanode external drives to the computer
     simultaneously. Unpredictable things can happen.
 
     If you add your own drive, the label will be changed to "parmanode" (as Parmanode
     expects this label) - the poor thing gets a little confused if it's not labelled 
-    this way.
+    this way!
 
     The mount point must be /home/$(whoami)/parmanode for Linux, and
     /Volumes/parmanode for Mac. If the directory does not exist it will be created.
 
-    MAKE SURE THAT IF YOU HAVE AN OLD PARMANODE DRIVE, TO DISCONNECT IT NOW.
+    If you use an internal drive with Bitcoin Core and you want to switch to a new
+    or imported external drive, the easiest way to accomplish this is to uninstall
+    Bitcoin Core using Parmanode, then re-install it again, but choose external drive
+    when prompted. If an external drive was prepared earlier, make sue you select not
+    to format it during the Bitcoin Core installation.
+
+########################################################################################
+
+            IF YOU HAVE AN OLD PARMANODE DRIVE, TO DISCONNECT IT NOW.
 
 ########################################################################################
 "
