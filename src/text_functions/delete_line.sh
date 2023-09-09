bd567f126f19a95a9625e993ea643e3f532bae04
Parman <armantheparman@gmail.com>
2023-09-09 13:55:04 +1000

    modified:   changelog 	modified:   src/text_functions/delete_line.sh 	modified:   src/text_functions/swap_string.sh

--- src/text_functions/delete_line.sh
+++ src/text_functions/delete_line.sh
@@ -21,7 +21,7 @@ fi
 # For Linux...
 search_string="$2"
 input_file="$(readlink -f "$1")"	# to assign absolute path
-sudo sed -i "/${search_string}/d" "$input_file" 
+sudo sed -i "/${search_string}/d" "$input_file" >> $HOME/.parmanode/sed.log 2>&1
 # I will need to change the delimeter in the sed command to @ later instead of / to
 # avoid a bug if the search_strin contains a /.
 # Everything is working now, so not urgent.
