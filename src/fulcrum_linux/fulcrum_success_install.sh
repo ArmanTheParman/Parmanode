d52db2534df89a0088414e40392687b14591eb56
Parman <armantheparman@gmail.com>
2023-09-11 20:24:36 +1000

    modified:   src/fulcrum_linux/fulcrum_success_install.sh

--- src/fulcrum_linux/fulcrum_success_install.sh
+++ src/fulcrum_linux/fulcrum_success_install.sh
@@ -8,12 +8,6 @@ set_terminal ; echo "
     Fulcrum will begin running, but won't actually start syncing until Bitcoin has
     made some progress syncing itself.
 
-    Remember to reboot Bitcoin if you changed the RPC password during this install.
-
-    Note: Fulcrum will not sync unless Bitcoin Core has a username and password
-    set. Parmanode has attempted to copy these values into Fulcrum's configuration
-    file.
-
 ########################################################################################
 " && installed_config_add "fulcrum-end" && log "fulcrum" "install finished"
 enter_continue
