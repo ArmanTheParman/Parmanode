abf9ba036e3823782ebbe8876d6861cafa9d0b17
Parman <armantheparman@gmail.com>
2023-09-12 20:09:41 +1000

    modified:   src/electrs/install_electrs.sh

--- src/electrs/install_electrs.sh
+++ src/electrs/install_electrs.sh
@@ -1,5 +1,13 @@
 function install_electrs {
 
+    build_dependencies_electrs
+    download_electrs
+    compile_electrs
+    check_pruning_off || return 1
+
+}
+
+function build_dependencies_electrs {
 sudo apt update -y
 
 if ! which clang ; then sudo apt install -y clang ; fi
@@ -8,6 +16,8 @@ if ! which cmake ; then sudo apt install -y cmake ; fi
 
 if ! dpkg -l | grep build-essential ; then sudo apt install -y build-essential ; fi 
 
+# cargo install needed, but won't work if 64 bit system uses a 32 bit os,
+# like Raspian OS 32-bit (it supports 64 bit chips, but cargo won't work)
 if $(uname -m) == "aarch64" ; then 
     if [[ $(file /bin/bash | grep 64 | cut -d " " -f 3) != "64-bit" ]] then
     set_terminal ; echo "
@@ -23,22 +33,40 @@ enter_continue && return 1
 fi
 fi
 
+# if old version of cargo, won't work
 if ! which cargo ; then sudo install_cargo
 else
     if [[ $(cargo --version | cut -d . -f 2) -lt 63 ]] ; then
     sudo apt purge cargo rustc -y
     install_cargo
 fi
-
-cd $HOME/parmanode/ ; git clone --depth 1 https://github.com/romanz/electrs
-cd electrs
-
-#please follow prompts to insall cargo
-
-
 }
+########################################################################################
 
 function install_cargo {
+#please follow prompts to insall cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs/ | sh
 source $HOME/.cargo/env #or restart shell
+}
+
+function download_electrs {
+cd $HOME/parmanode/ && git clone --depth 1 https://github.com/romanz/electrs
+}
+
+function compile_electrs {
+cd $HOME/parmanode/electrs && cargo build --locked --release
+}
+
+function check_pruning_off {
+
+if ! cat $HOME/.bitcoin/bitcoin.conf | grep "prune=" ; then return 0 
+else
+    if cat $HOME/.bitcoin/bitcoin.conf | grep "prune=0" ; then return 0 
+    announce "Note that Electrs won't work if Bitcoin is pruned. You'll have to" \
+    "completely start bitcoin sync again without pruning to use Electrs. Sorry."
+    return 1
+    fi
+fi
+
+
 }
