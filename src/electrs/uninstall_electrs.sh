f0f2d9fd69ed7b386bb60a8f8ed8208eeb583e1d
Parman <armantheparman@gmail.com>
2023-09-13 13:56:33 +1000

    modified:   src/electrs/uninstall_electrs.sh

--- src/electrs/uninstall_electrs.sh
+++ src/electrs/uninstall_electrs.sh
@@ -1,10 +1,16 @@
 function uninstall_electrs {
+source $HOME/.parmanode/parmanode.conf
 
 parmanode_conf_remove "electrs"
 
 sudo systemctl stop electrs.service >/dev/null
 sudo systemctl disable electrs.service >/dev/null
 sudo rm /etc/systemd/system/electrs.servcie >/dev/null
+
+if [[ $drive_electrs == "external" ]] ; then
+sudo rm -rf /media/$USER/parmanode/electrs_db >/dev/null
+else
+rm -rf $HOME/parmanode/electrs
 
 installed_config_remove "electrs"
 
