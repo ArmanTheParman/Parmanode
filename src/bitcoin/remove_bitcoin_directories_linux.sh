be3f56d86edd78278d29efe1e2f83d745b83a0b6
Parman <armantheparman@gmail.com>
2023-09-13 15:21:56 +1000

    modified:   src/bitcoin/remove_bitcoin_directories_linux.sh 	modified:   src/drive_functions/format_warnings.sh

--- src/bitcoin/remove_bitcoin_directories_linux.sh
+++ src/bitcoin/remove_bitcoin_directories_linux.sh
@@ -10,7 +10,9 @@ source $HOME/.parmanode/parmanode.conf   # gets drive choice
 
 if [[ $drive == "external" && -d /media/$(whoami)/parmanode/.bitcoin ]] ; then #drive would have to be mounted to be true 
 while true ; do
+    if [[ $skip_formatting = "true" ]] ; then break ; fi
 set_terminal
+
 echo "
 ########################################################################################
 
@@ -88,5 +90,7 @@ fi #end checking internal drive for .bitcoin directory
 #Remove symlink to drive
 if [[ -L "$HOME/.bitcoin" ]] ; then rm $HOME/.bitcoin ; fi      #symlink deleted if it exists
 
+
+unset skip_formatting
 return 0
 }
