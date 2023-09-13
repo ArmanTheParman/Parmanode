db19016e67b344adc1bacd67b94d1a04678e1881
Parman <armantheparman@gmail.com>
2023-09-13 14:41:06 +1000

    modified:   src/electrs/install_electrs.sh 	new file:   src/electrs/make_electrs_config.sh 	new file:   src/electrs/prepare_drive_electrs.sh

---
+++ src/electrs/make_electrs_config.sh
@@ -0,0 +1,19 @@
+function make_electrs_config {
+mkdir -p $HOME/.electrs
+
+if [[ $drive_electrs == "external" ]] ; then 
+    db_dir="/media/$USER/parmanode/electrs_db"
+else
+    db_dir="$HOME/parmanode/electrs/electrs_db"
+fi
+
+echo "daemon_rpc_addr = \"127.0.0.1:8332\"
+daemon_p2p_addr = \"127.0.0.1:8333\"
+db_dir=\"$db_dir\"
+network=\"bitcoin\"
+electrum_rpc_addr = \"127.0.0.1:50001\"
+log_filters = \"INFO\"
+auth=\"$rpcuser:$rpcpassword\"
+" | tee $HOME/.electrs/config.toml >/dev/null
+
+}
