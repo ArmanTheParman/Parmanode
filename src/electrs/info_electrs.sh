238ce4d02af36c1ee06fc4ee0e540620c04615a3
Parman <armantheparman@gmail.com>
2023-09-13 23:11:14 +1000

    modified:   src/electrs/info_electrs.sh

--- src/electrs/info_electrs.sh
+++ src/electrs/info_electrs.sh
@@ -3,15 +3,17 @@ function info_electrs {
 set_terminal ; echo "
 ########################################################################################
 
-    It is possible, that when you inspect the log file, you'll see electrs starting
+    It is possible that when you inspect the log file, you'll see electrs starting
     and then terminating, and after some time starting again and failing again.
 
-    I have observed this while testing, and left electrs to run. After an hour or so
-    I was able to overcome this problem itself and begin syncing the chain.
+    I have observed this while testing on a Raspberry Pi 64-bit, but it works fine
+    on a Linux AMD 64 bit desktop computer.
 
-    Until some fix is released by the developer or electrs or Bitcoin, this will have
-    to do I'm afraid. My testing was done on a Raspberry Pi 64-bit, and it is 
-    unclear if this will happen on other systems.
+    The problem resolved on its own after an hour or so, and may do the same for you,
+    just a warning and hint.
+
+    Until some fix is released by the developer of electrs or Bitcoin, this will have
+    to do I'm afraid. 
 
 ########################################################################################
 "
