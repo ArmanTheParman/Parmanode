ef48f34e682027ce28e036f3bacebe968b81856c
Parman <armantheparman@gmail.com>
2023-09-13 23:50:48 +1000

    modified:   src/electrs/install_electrs.sh 	renamed:    src/fulcrum_linux/make_ssl_certificates.sh -> src/general_functions/make_ssl_certificates.sh

--- src/general_functions/make_ssl_certificates.sh
+++ src/general_functions/make_ssl_certificates.sh
@@ -4,7 +4,10 @@ set_terminal
 
 if ! openssl version >/dev/null 2>&1 ; then echo "Installing openssl..." ; sudo apt install openssl -y ; fi
 
-cd $HOME/parmanode/fulcrum/
+if [ -z $1 ] ; then cd $HOME/parmanode/fulcrum/ 
+elif [[ $1 == "electrs" ]] ; then cd $HOME/parmanode/electrs
+fi
+
 openssl genpkey -algorithm RSA -out key.pem -pkeyopt rsa_keygen_bits:2048
 openssl req -new -x509 -key key.pem -out cert.pem -days 36500 -subj "/C=/ST=/L=/O=/OU=/CN=/emailAddress=/" >/dev/null
 
