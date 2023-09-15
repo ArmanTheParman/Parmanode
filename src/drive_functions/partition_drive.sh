69b7766cae51b48bfa350ced24e4f8c80505dd2c
Parman <armantheparman@gmail.com>
2023-09-15 16:46:19 +1000

    modified:   src/drive_functions/partition_drive.sh

--- src/drive_functions/partition_drive.sh
+++ src/drive_functions/partition_drive.sh
@@ -1,6 +1,8 @@
 function partition_drive {
 
-if [ ! -e "$disk" ] ; then #eg if /dev/sda doesn't exist
+export disk_no_number="${disk%%[0-9]*}"
+
+if [ ! -e "$disk_no_number" ] ; then #eg if /dev/sda doesn't exist
     set_terminal
     echo "Drive $disk does not exist. Exiting."
     enter_continue
@@ -9,7 +11,7 @@ fi
 
 # Create a new GPT partition table and a single partition on the drive
 # interestingly, you can plonk a redirection in the middle of a heredoc like this:
-sudo fdisk "$disk" <<EOF >/dev/null 
+sudo fdisk "$disk_no_number" <<EOF >/dev/null 
 g
 n
 1
@@ -23,5 +25,5 @@ EOF
 # This will send g, enter, n, enter, 1, enter, enter, enter, & w to fdisk
 # automating the normally interactive job.
 
-echo "A new GPT partition table and a single partition have been created on $disk."
+echo "A new GPT partition table and a single partition have been created on $disk_no_number."
 }
