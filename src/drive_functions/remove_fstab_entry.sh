54bc20b09c31b3324d3094a9a814b9eb98cdb446
Parman <armantheparman@gmail.com>
2023-09-15 17:42:40 +1000

    modified:   src/drive_functions/remove_fstab_entry.sh

--- src/drive_functions/remove_fstab_entry.sh
+++ src/drive_functions/remove_fstab_entry.sh
@@ -2,29 +2,30 @@ function remove_parmanode_fstab {
 
 delete_line "/etc/fstab" "parmanode"
 }
+
 # function remove_fstab_entry {
 # #delete fstab entry of the disk immediately before wiping
 # remove_UUID_fstab "$disk" 
 # }
 
-# function remove_UUID_fstab {
+function remove_UUID_fstab {
 
-# drive_ID="$1"
+drive_ID="$1"
 
-# get_UUID "${drive_ID}"
-# if [ -z $UUID ] ; then debug "No UUID, 2" ; return 1 ; fi
+get_UUID "${drive_ID}"
+if [ -z $UUID ] ; then debug "No UUID, 2" ; return 1 ; fi
     
-# return 0
-# }
+return 0
+}
 
-# function get_UUID {
+function get_UUID {
 
-# drive_ID=$1
-# if [ -z $drive_ID ] ; then debug "no drive_ID" ; return 1 ; fi
+drive_ID=$1
+if [ -z $drive_ID ] ; then debug "no drive_ID" ; return 1 ; fi
 
-#         UUID=$(sudo blkid /dev/${drive_ID} | grep -o 'UUID="[^"]*"' | grep -o '"[^"]*"')
-#         UUID_temp=$(echo "$UUID" | sed 's/"//g')
-#         UUID=$UUID_temp
+        UUID=$(sudo blkid /dev/${drive_ID} | grep -o 'UUID="[^"]*"' | grep -o '"[^"]*"')
+        UUID_temp=$(echo "$UUID" | sed 's/"//g')
+        UUID=$UUID_temp
 
-# return 0 
-# }
+return 0 
+}
