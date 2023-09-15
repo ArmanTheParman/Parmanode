658385c52138cad28cb45f39fe651266dde57f16
Parman <armantheparman@gmail.com>
2023-09-15 19:07:27 +1000

    modified:   src/electrs/install_electrs.sh

--- src/electrs/install_electrs.sh
+++ src/electrs/install_electrs.sh
@@ -12,14 +12,14 @@ install_nginx #the function checks first before attempting install.
 electrs_nginx add
 build_dependencies_electrs && log "electrs" "build_dependencies success" ; debug "build dependencies done"
 
-log "electrs" "compile_electrs $compile_electrs"
-if [[ $compile_electrs == "true" ]] ; then
-download_electrs && log "electrs" "download_electrs success" ; debug "download electrs done"
-compile_electrs && log "electrs" "compile_electrs success" ; debug "build, download, compile... done"
-elif [[ $compile_electrs == "false" ]] ; then
-rm -rf $HOME/parmanode/electrs
-cp -r $HOME/.electrs_backup $HOME/parmanode/electrs/
-fi
+# log "electrs" "compile_electrs $compile_electrs"
+# if [[ $compile_electrs == "true" ]] ; then
+# download_electrs && log "electrs" "download_electrs success" ; debug "download electrs done"
+# compile_electrs && log "electrs" "compile_electrs success" ; debug "build, download, compile... done"
+# elif [[ $compile_electrs == "false" ]] ; then
+# rm -rf $HOME/parmanode/electrs
+# cp -r $HOME/.electrs_backup $HOME/parmanode/electrs/
+# fi
 
 debug "test"
 }
