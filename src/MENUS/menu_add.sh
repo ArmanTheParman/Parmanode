176c05d23e10caa3d60d51c659a5685ebb3ca431
Parman <armantheparman@gmail.com>
2023-09-15 18:42:44 +1000

    modified:   src/MENUS/menu_add.sh

--- src/MENUS/menu_add.sh
+++ src/MENUS/menu_add.sh
@@ -224,6 +224,7 @@ case $choice in
    ers|ERS|Ers|electrs)
       if [[ $electrsadd == 1 ]] ; then
          if [[ $OS != "Mac" ]] ; then
+         debug "before install_electrs"
          install_electrs ; debug "after install electrs" ; return 0
          else
          no_mac ; return 0
