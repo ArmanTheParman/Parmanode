ce7dcceae7e84a4bc8e60a94015e63fa48e8b552
Parman <armantheparman@gmail.com>
2023-09-15 19:20:00 +1000

    modified:   src/bitcoin/check_rpc_bitcoin.sh

--- src/bitcoin/check_rpc_bitcoin.sh
+++ src/bitcoin/check_rpc_bitcoin.sh
@@ -5,13 +5,12 @@ if [ -z $rpcuser ] ; then
 announce \
     "The program won't work unless Bitcoin Core has a username and password set." \
     "Would you like to set that now?    y   or    n"
-echo ""
-
+    
 read choice
 
     case $choice in 
     y|Y|YES|Yes|yes) 
-    set_rpc_authentication && return 0
+    please_wait ; set_rpc_authentication && return 0
     return 1
     ;;
     esac
