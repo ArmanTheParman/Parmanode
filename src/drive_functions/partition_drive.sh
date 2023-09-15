16e716f417ae48864ceb44ab45744209b9c33066
Parman <armantheparman@gmail.com>
2023-09-15 16:35:07 +1000

    modified:   src/drive_functions/partition_drive.sh

--- src/drive_functions/partition_drive.sh
+++ src/drive_functions/partition_drive.sh
@@ -1,19 +1,15 @@
 function partition_drive {
 
-
-sdrive="/dev/$disk"    #$disk chosen earlier and has the pattern sdX, where X is a letter
-
-# Check if the drive exists. I can't remember why I called it "sdrive"
-if [ ! -e "$sdrive" ] ; then #eg if /dev/sda doesn't exist
+if [ ! -e "$drive" ] ; then #eg if /dev/sda doesn't exist
     set_terminal
-    echo "Drive $sdrive does not exist. Exiting."
+    echo "Drive $drive does not exist. Exiting."
     enter_continue
     exit 1
 fi
 
 # Create a new GPT partition table and a single partition on the drive
 # interestingly, you can plonk a redirection in the middle of a heredoc like this:
-sudo fdisk "$sdrive" <<EOF >/dev/null 
+sudo fdisk "$drive" <<EOF >/dev/null 
 g
 n
 1
