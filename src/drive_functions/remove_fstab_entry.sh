231423df821e592bc3419b5e310e5d3c33b4cbe7
Parman <armantheparman@gmail.com>
2023-09-15 18:07:53 +1000

    modified:   src/drive_functions/remove_fstab_entry.sh

--- src/drive_functions/remove_fstab_entry.sh
+++ src/drive_functions/remove_fstab_entry.sh
@@ -26,7 +26,7 @@ if [ -z $drive_ID ] ; then debug "no drive_ID" ; return 1 ; fi
 
         UUID=$(sudo blkid $drive_ID} | grep -o 'UUID="[^"]*"' | grep -o '"[^"]*"')
         UUID_temp=$(echo "$UUID" | sed 's/"//g')
-        UUID=$UUID_temp
+        export UUID=$UUID_temp
 
 return 0 
 }
