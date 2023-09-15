25f628028592d86291f4ad9217d6dffdf8e80a97
Parman <armantheparman@gmail.com>
2023-09-15 16:55:29 +1000

    modified:   src/drive_functions/UUID.sh 	modified:   src/drive_functions/dd_wipe_drive.sh 	modified:   src/drive_functions/write_to_fstab.sh

--- src/drive_functions/write_to_fstab.sh
+++ src/drive_functions/write_to_fstab.sh
@@ -1,5 +1,6 @@
 function write_to_fstab {
-   UUID="$1"
+   UUID="$1"  
+   if [ -z $UUID ] ; then debug "no UUID" ; return 1 ; fi
    
         echo "UUID=$UUID /media/$(whoami)/parmanode ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab > /dev/null 2>&1
         log "bitcoin" "fstab grep output for parmanode:" && \
@@ -12,6 +13,7 @@ source $HOME/.parmanode/var
 
 export $(sudo blkid -o export $disk) >/dev/null
 
+if [ -z $UUID ] ; then debug "no UUID" ; return 1 ; fi
 delete_line "/etc/fstab" "$UUID"
 echo "UUID=$UUID /media/$(whoami)/parmanode $TYPE defaults,nofail 0 2" | sudo tee -a /etc/fstab >/dev/null 2>&1
 
