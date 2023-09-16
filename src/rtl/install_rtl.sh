40e23b379da31e24c2fa7a77777cd4edea1ac0a7
Parman <armantheparman@gmail.com>
2023-09-16 18:09:52 +1000

    modified:   src/rtl/install_rtl.sh

--- src/rtl/install_rtl.sh
+++ src/rtl/install_rtl.sh
@@ -3,26 +3,6 @@ if [[ $OS == "Mac" ]] ; then no_mac ; return 1 ; fi
 grep -q docker-end < $HOME/.parmanode/installed.conf || { announce "Must install Docker first. Aborting." && return 1 ; }
 
 
-#if working on installing on host...
-#	install_nodejs 
-#download_rtl
-#	verify_rtl || return 1
-#	extract_rtl
-#install_rtl
-#fi
-
-if ! which docker >/dev/null ; then 
-announce "Sorry, you need Docker first. This will be seemless soon, but for now," \
-"Please installed btcpay first to get Docker. Uninstall btcpay later if you want." 
-return 1
-else
-        if ! id | grep -q docker ; then
-        announce "Docker is installed but a reboot is required to get the USER into the Docker group." \
-        "Please reboot and try installing again."
-        return 1
-        fi
-fi
-
 lncli wallet accounts list >/dev/null 2>&1 || echo "
 ########################################################################################
 
@@ -35,7 +15,6 @@ lncli wallet accounts list >/dev/null 2>&1 || echo "
 
 ########################################################################################
 " && enter_continue && return 1
-
 
 mkdir $HOME/parmanode/rtl $HOME/parmanode/startup_scripts/ 2>/dev/null
 installed_config_add "rtl-start"
