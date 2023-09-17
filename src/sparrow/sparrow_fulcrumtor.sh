88af5d399db7913beba946ba8bd28773d16bb081
Parman <armantheparman@gmail.com>
2023-09-17 14:49:29 +1000

    modified:   src/MENUS/menu_sparrow.sh 	modified:   src/sparrow/make_sparrow_config.sh 	modified:   src/sparrow/sparrow_fulcrumtor.sh

--- src/sparrow/sparrow_fulcrumtor.sh
+++ src/sparrow/sparrow_fulcrumtor.sh
@@ -19,3 +19,25 @@ enter_continue
 rm $HOME/.sparrow/config
 make_sparrow_config "fulcrumtor"
 }
+
+function sparrow_electrstor {
+
+if ! which tor >/dev/null ; then
+announce \
+"Please install Tor first"
+return 1
+fi
+
+if ! grep -q "electrs-end" < $HOME/.parmanode/installed.conf ; then
+announce \
+"Please install electrs first."
+return 1
+fi
+
+
+set_terminal
+echo "Make sure Sparrow has been shut down before proceeding."
+enter_continue
+rm $HOME/.sparrow/config
+make_sparrow_config "electrstor"
+}
