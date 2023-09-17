e308101af0cd02dc3777d14d5296b056baf5a36b
Parman <armantheparman@gmail.com>
2023-09-18 02:27:54 +1000

    modified:   src/MENUS/menu_fulcrum.sh 	new file:   src/fulcrum_linux/database_corrupted.sh

---
+++ src/fulcrum_linux/database_corrupted.sh
@@ -0,0 +1,50 @@
+function database_corrupted {
+while true ; do
+set_terminal ; echo "
+########################################################################################
+
+    Have you noticed from the log menu that Fulcrum's database is corrupted? This
+    sometimes happens seemingly for no reason. It's sensitive; kind of ridiculous.
+
+    If this has happened, you need to stop Fulcrum, delete the database, and restart
+    Fulcrum - it's unfortunate, but it means starting over. An alternative is to use
+    electrs server instead.
+
+    Do you want Parmanode to clean it up and start Fulcrum over for you?
+
+                        y)      Yes, do it, I can't be bothered
+
+                        n)      No, leave it, I'll try to figure it out myself
+
+######################################################################################## 
+"
+choose "xpq" ; read choice ; set_terminal
+case $choice in
+q|Q) exit 0 ;;
+p|P) return 0 ;;
+n|N|NO|No|no) return 0 ;;
+y|Y|YES|Yes|yes) 
+
+if [[ $OS == Mac ]] ; then stop_fulcrum_docker 
+else
+stop_fulcrum_linux
+fi
+
+if [[ $drive_fulcrum == external ]] ; then 
+    if [[ $OS == Linux ]] ; then 
+         sudo rm -rf /media/$USER/parmanode/fulcrum_db  
+         sudo mkdir /media/$USER/parmanode/fulcrum_db
+    else
+    rm -rf /Volumes/parmanode/fulcrum_db
+    sudo mkdir /Volumes/parmanode/fulcrum_db
+    fi
+else
+rm -rf $HOME/parmanode/fulcrum_db
+mkdir $HOME/parmanode/fulcrum_db
+fi
+break
+;;
+*) invalid
+esac
+done
+}
