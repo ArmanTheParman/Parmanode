d8cea835990d120975a5a095ac004114be1e047e
Parman <armantheparman@gmail.com>
2023-09-18 01:36:53 +1000

    modified:   src/electrs/restore_electrs_drive.sh

--- src/electrs/restore_electrs_drive.sh
+++ src/electrs/restore_electrs_drive.sh
@@ -27,7 +27,10 @@ case $choice in q|Q) quit 0 ;; p|P) return 1 ;;
 #if "use it" selected, that's the only case where a new electrum_db directory won't be created.
 #"ignore it" is only available if the directory is not called electrum_db, so it's all good.
 #The variable electrum_db_restore is needed for the function prepare_drive_electrs
-u|U) export electrs_db_restore="true" ; break ;;
+u|U)
+export electrs_db_restore="true" 
+mv /media/$USER/parmanode/$dirname /media/$USER/parmanode/electrs_db
+break ;;
 del|Del|DEL) sudo rm -rf /media/$USER/parmanode/$dirname ; export electrs_db_restore="false" ; break ;;
 i|I) unset dirname ; electrs_db_restore="false" ; break ;;
 *) invalid
