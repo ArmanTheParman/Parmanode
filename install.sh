243ef1069c24b5b4a8745dbd507c0001731bacd8
Parman <armantheparman@gmail.com>
2023-09-20 03:46:28 +1000

    v3.7.0

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 # This is the install script kept at
 # https://parmanode.com/install.sh - the URL is easier to remember and shorter than if keeping it on Github.
 
+# IT IS NON FUNCTIONAL IN ITS CURRENT FORM HERE
 
+"
+#/bin/bash
 sudo apt update -y 
 sudo apt install git -y
 mkdir $HOME/parman_programs ; cd ; cd parman_programs
-git clone https://github.com/armantheparman.com/parmanode.git
+git clone https://github.com/armantheparman/parmanode.git
 
 echo '#!/bin/bash' > ~/Desktop/run_parmanode.sh
 echo "cd $HOME/parman_programs/parmanode ; ./run_parmanode.sh" >> $HOME/Desktop/run_parmanode.sh
 sudo chmod +x $HOME/Desktop/run_parmanode.sh
-exit
+"
+
+
