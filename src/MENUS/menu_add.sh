fc07340e96030116d4a2b2170c1af751b8bd3ce0
Parman <armantheparman@gmail.com>
2023-09-20 16:34:17 +1000

    modified:   src/MENUS/menu_add.sh 	modified:   src/electrum/verify_electrum.sh 	modified:   src/general_functions/system_info.sh

--- src/MENUS/menu_add.sh
+++ src/MENUS/menu_add.sh
@@ -371,7 +371,6 @@ case $choice in
       ;;
    e|E|electrum|Electrum|ELECTRUM)
       if [[ -n $electrum_n ]] ; then
-      debug "before install_electrum"
       install_electrum
       return 0
       fi
