217bb829137b9c2ebfcc210c6839f0b674768c7e
Parman <armantheparman@gmail.com>
2023-09-22 00:24:54 +1000

    modified:   src/text_functions/echo_choices.sh

--- src/text_functions/echo_choices.sh
+++ src/text_functions/echo_choices.sh
@@ -14,7 +14,7 @@ return 0
 }
 
 function enter_exit {
-echo -e "Hit <enter> to exit." ; read
+echo -e "${yellow}Hit <enter> to exit.$orange" ; read
 return 0
 }
 
@@ -22,25 +22,25 @@ function choose {
 
 if [[ $1 == "xpq" ]]
 then
-echo "Type your choice, or (p) for previous, (q) to quit, then <enter>: "
+echo -e "${yellow}Type your choice, or (p) for previous, (q) to quit, then <enter>: $orange"
 return 0
 fi
 
 if [[ $1 == "xq" ]]
 then
-echo "Type your choice, or (q) to quit, then <enter>: "
+echo -e "${yellow}Type your choice, or (q) to quit, then <enter>: $orange"
 return 0
 fi
 
 if [[ $1 == "x" ]]
 then
-echo "Type your choice, then <enter>: "
+echo -e "${yellow}Type your choice, then <enter>: $orange"
 return 0
 fi
 
 if [[ $1 == "epq" ]]
 then
-echo "Hit <enter> to continue, (p) for previous, (q) to quit, then <enter>: "
+echo -e "${yellow}Hit <enter> to continue, (p) for previous, (q) to quit, then <enter>: $orange"
 # while true ; do 
 # case $choice in q|Q|QUIT|Quit) exit 0 ;; p|P) return 1 ;; "") break ;; *) invalid ;; esac ; done
 return 0
@@ -48,7 +48,7 @@ fi
 
 if [[ $1 == "esq" ]]
 then
-echo "Hit <enter> to continue, (s) to skip, (q) to quit, then <enter>: "
+echo -e "${yellow}Hit <enter> to continue, (s) to skip, (q) to quit, then <enter>: $orange"
 # while true ; do 
 # case $choice in q|Q|QUIT|Quit) exit 0 ;; p|P) return 1 ;; "") break ;; *) invalid ;; esac ; done
 return 0
@@ -56,7 +56,7 @@ fi
 
 if [[ $1 == "qc" ]]
 then
-echo "Hit (q) then <enter> to quit, anything else to continue"
+echo -e "${yellow}Hit (q) then <enter> to quit, anything else to continue.$orange"
 return 0
 fi
 
@@ -67,19 +67,19 @@ function invalid {
 
 set_terminal
 
-echo "Invalid choice. Hit <enter> before trying again. " ; read
+echo -e "${yellow}Invalid choice. Hit <enter> before trying again. $orange" ; read
 return 0
 }
 
 function previous_menu { 
 
-echo "Hit <enter> to go back to the previous menu." ; read
+echo -e "${yellow}Hit <enter> to go back to the previous menu.$orange" ; read
 return 0
 }
 
 function please_wait { 
 set_terminal
-echo "
+echo -e "
 Please wait, this may take some time...
 "
 return 0
