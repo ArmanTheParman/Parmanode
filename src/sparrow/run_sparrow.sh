b59275e6f5a4d8a244b9d304ab05a31915504abb
Parman <armantheparman@gmail.com>
2023-09-22 16:20:39 +1000

    1

--- src/sparrow/run_sparrow.sh
+++ src/sparrow/run_sparrow.sh
@@ -1,10 +1,13 @@
 function run_sparrow {
 if [[ $OS == "Linux" ]] ; then
+
+debug "1"
 nohup $HOME/parmanode/Sparrow/bin/Sparrow >/dev/null 2>&1 & 
 please_wait
 sleep 2
 fi
 
+debug "1"
 if [[ $OS == "Mac" ]] ; then 
 open /Applications/Sparrow.app
 fi
