0481ba5ee4aba9d2cf96e29d57e3e8b3ea68f9ee
Parman <armantheparman@gmail.com>
2023-09-22 15:08:51 +1000

    modified:   src/drive_functions/info_add_drive.sh

--- src/drive_functions/info_add_drive.sh
+++ src/drive_functions/info_add_drive.sh
@@ -3,28 +3,28 @@ while true ; do
 set_terminal ; echo " 
 ########################################################################################
 
-    Using a second drive for Parmanode is possible but only between operating systems.
-    For example, you can't import a Parmanode drive used on a Mac to a Linux computer.
+    Using a second drive for Parmanode is possible but the file systems must be
+    compatible. For example, you can't import a Parmanode drive used on a Mac to a 
+    Linux computer.
 
-    Also, you shouldn't attach two Parmanode external drives to the computer
-    simultaneously. Unpredictable things can happen.
+   $cyan To bring in an external hard drive that contains Bitcoin blockchain data:$orange
+    
+      1. Make sure the path to the data is /.bitcoin on the drive - i.e. create a 
+         hidden directory called .bitoin at root directory of the drive, then move 
+         the data there.
 
-    If you add your own drive, the label will be changed to "parmanode" (as Parmanode
-    expects this label) - the poor thing gets a little confused if it's not labelled 
-    this way!
+      2. Continue with the this drive import function and follow the screen prompts
+         exactly.
 
-    The mount point must be /home/$(whoami)/parmanode for Linux, and
-    /Volumes/parmanode for Mac. If the directory does not exist it will be created.
+      3. Then install bitcoin (uninstall first if you have it already) using 
+         Parmanode. You will be asked if you want and internal or external drive.
+         Choose external drive, then select to skip formatting when prompted.
 
-    If you use an internal drive with Bitcoin Core and you want to switch to a new
-    or imported external drive, the easiest way to accomplish this is to uninstall
-    Bitcoin Core using Parmanode, then re-install it again, but choose external drive
-    when prompted. If an external drive was prepared earlier, make sue you select not
-    to format it during the Bitcoin Core installation.
+      4. Once the Bitcoin installation is finished, the program will be using
+         the newly imported drive to sync blocks. 
 
-########################################################################################
-
-            IF YOU HAVE AN OLD PARMANODE DRIVE, TO DISCONNECT IT NOW.
+    You shouldn't attach two Parmanode external drives to the computer simultaneously. 
+    Unpredictable things can happen.
 
 ########################################################################################
 "
