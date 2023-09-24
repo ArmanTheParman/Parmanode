1b9a3e9a0712903f4b24e96158d6a25bd1790d1d
Parman <armantheparman@gmail.com>
2023-09-24 16:09:30 +1000

    renamed:    src/parmanode/dot_desktop_file.sh -> src/parmanode/make_dot_desktop.sh 	modified:   src/parmanode/uninstall_parmanode.sh

--- src/parmanode/make_dot_desktop.sh
+++ src/parmanode/make_dot_desktop.sh
@@ -4,12 +4,12 @@ echo "[Desktop Entry]
 Type=Application
 Name=Parmanode
 Exec=$HOME/Desktop/run_parmanode.sh
-Icon=$HOME/parmanode/icon.png
+Icon=$HOME/.parmanode/icon.png
 Terminal=true" | sudo tee ~/.local/share/applications/parmanode.desktop >/dev/null
 
-debug "made make_dot_desktop"
-
-cp $original_dir/src/graphics/icon.png $HOME/parmanode/
+mkdir $HOME/.parmanode 
+cd $HOME/.parmanode 
+curl -LOs https://parmanode.com/icon.png
 
 sudo chmod +x ~/.local/share/applications/parmanode.desktop
 sudo update-icon-caches /usr/share/icons/* >/dev/null 2>&1
