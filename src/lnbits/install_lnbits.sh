4a7f1a22e0a49882404a3d59e175df22feea92e8
Parman <armantheparman@gmail.com>
2023-09-26 20:43:24 +1000

    modified:   src/MENUS/menu_add.sh 	new file:   src/lnbits/install_lnbits.sh 	new file:   src/lnbits/uninstall_lnbits.sh 	modified:   src/rtl/install_rtl.sh 	modified:   src/rtl/uninstall_rtl.sh

---
+++ src/lnbits/install_lnbits.sh
@@ -0,0 +1,20 @@
+function install_lnbits {
+if [[ $OS == "Mac" ]] ; then no_mac ; return 1 ; fi
+grep -q docker-end < $HOME/.parmanode/installed.conf || { announce "Must install Docker first. Aborting." && return 1 ; }
+grep -q lnd-end < $HOME/.parmanode/installed.conf || { announce "Please install LND first. Aborting." && return 1 ; }
+
+mkdir $HOME/parmanode/lnbits 2>/dev/null
+installed_config_add "lnbits-start"
+
+cd $HOME/parmanode/
+git clone --depth 1 https://github.com/lnbits/lnbits.git
+cd lnbits
+docker build -t lnbitsdocker/lnbits-legend .
+cp .env.example .env
+mkdir data
+docker run --detach --publish 5000:5000 --name lnbits --volume ${PWD}/.env:/app/.env --volume ${PWD}/data/:/app/data lnbitsdocker/lnbits-legend \
+|| { debug "failed to run lnbits image" && return 1 ; }
+
+success "LNbits" "being installed."
+installed_config_add "lnbits-end"
+}
