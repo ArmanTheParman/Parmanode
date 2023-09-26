c3ef544966820a958b51233de36b9de7b0a41024
Parman <armantheparman@gmail.com>
2023-09-26 16:05:31 +1000

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,29 @@
+return 0
 # This is the install script kept at
 # https://parmanode.com/install.sh - the URL is easier to remember and shorter than if keeping it on Github.
 
 # IT IS NON FUNCTIONAL IN ITS CURRENT FORM HERE
 
-"
-#/bin/bash
+#!/bin/sh
 sudo apt update -y 
 sudo apt install git -y
-mkdir $HOME/parman_programs ; cd ; cd parman_programs
+mkdir -p $HOME/parman_programs ; cd ; cd parman_programs
 git clone https://github.com/armantheparman/parmanode.git
 
-echo '#!/bin/bash' > ~/Desktop/run_parmanode.sh
-echo "cd $HOME/parman_programs/parmanode ; ./run_parmanode.sh" >> $HOME/Desktop/run_parmanode.sh
-sudo chmod +x $HOME/Desktop/run_parmanode.sh
-"
+mkdir -p ~/Desktop
+cp $HOME/parman_programs/parmanode/src/graphics/pn_icon.png $HOME/.icons/pn_icon.png
+echo "[Desktop Entry]
+Type=Application
+Exec=$HOME/parman_programs/parmanode/run_parmanode.sh
+Name=Parmanode
+Icon=$HOME/parman_programs/parmanode/src/graphics/pn_icon.png
+Terminal=true
+Path=$HOME/parman_programs/parmanode/
+Categories=Utility;Application;" | sudo tee $HOME/Desktop/parmanode.desktop 
+sudo chmod +x $HOME/Desktop/parmanode.desktop
+sudo chown $USER:$USER $HOME/Desktop/parmanode.desktop
+
+
+
 
 
