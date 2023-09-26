60221420f6299442ceb02f8277d06af637b18197
Parman <armantheparman@gmail.com>
2023-09-26 14:23:52 +1000

    modified:   src/graphics/testing.sh

--- src/graphics/testing.sh
+++ src/graphics/testing.sh
@@ -17,11 +17,26 @@ mkdir -p ~/.icons/
 sudo cp pn_icon.png /usr/share/icons/dummy_icon.png
 touch ~/Desktop/d.dummy #why does the icon association not work for this file?
 
+echo '[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Dummy File
+Comment=Open dummy files
+Icon=/home/usr/share/icons/dummy_icon.png
+Exec=/home/parman/Desktop/d.dummy %f
+Terminal=true
+Categories=Application;' | sudo tee /usr/share/applications/dummy.desktop
+
+sudo chmod +x /usr/share/applications/dummy.desktop
+
+sudo update-desktop-database /usr/share/applications/
+
+
 }
 function testing_delete {
 
 sudo rm /usr/share/mime/packages/*dummy* ~/.icons/*dummy* ~/Desktop/*dummy* /usr/share/icons/*dummy* /usr/share/mime/application/*dummy*
-
+sudo rm /usr/share/applications/*dummy*
 }
 
 function t2 {
