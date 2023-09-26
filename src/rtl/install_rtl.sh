958198273e52316ab578148fef2b2a184ddc18a0
Parman <armantheparman@gmail.com>
2023-09-26 22:08:56 +1000

    modified:   src/rtl/install_rtl.sh

--- src/rtl/install_rtl.sh
+++ src/rtl/install_rtl.sh
@@ -20,21 +20,21 @@ mkdir $HOME/parmanode/rtl $HOME/parmanode/startup_scripts/ 2>/dev/null
 installed_config_add "rtl-start"
 make_rtl_config
 
-docker build -t rtl ./src/rtl || { debug1 "failed to build rtl image" && return 1 ; }
+docker build -t rtl ./src/rtl || { log "rtl" "failed to build rtl image" && return 1 ; }
 docker run -d --name rtl \
                          --network="host" \
                          -v $HOME/parmanode/rtl:/home/parman/RTL2 \
 			 -v $HOME/.lnd:/home/parman/.lnd \
                          -v $HOME/.parmanode/:/home/parman/.parmanode \
                          rtl \
-        || { debug1 "failed to run rtl image" && return 1 ; }
+        || { log "rtl" "failed to run rtl image" && return 1 ; }
 
 mv $original_dir/src/rtl/RTL-Config.json $HOME/parmanode/rtl
 rtl_password_changer
 
-run_rtl
+run_rtl || { log "rtl" "rtl failed to run" && return 1 ; }
 
-make_rtl_service_file
+make_rtl_service_file && log "rtl" "rtl service file made"
 
 success "RTL" "being installed."
 installed_config_add "rtl-end"
