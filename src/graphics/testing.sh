7eb35d614af97d1cacf79db12ba6a2c81aa468eb
Parman <armantheparman@gmail.com>
2023-09-26 12:43:29 +1000

    modified:   src/graphics/testing.sh

--- src/graphics/testing.sh
+++ src/graphics/testing.sh
@@ -9,12 +9,17 @@ echo '<?xml version="1.0"?>
         <icon name="dummy_icon"/>
         <glob pattern="dummyfile.dummy"/>
     </mime-type>
-</mime-info>' | sudo tee /usr/share/mime/packages/dummy-mime.xml 
+</mime-info>' | sudo tee /usr/share/mime/packages/x-dummy.xml 
 
 sudo update-mime-database /usr/share/mime/
 
 mkdir -p ~/.icons/
 cp pn_icon.png ~/.icons/dummy_icon.png
 touch ~/Desktop/d.dummy
+
+}
+function testing_delete {
+
+sudo rm /usr/share/mime/packages/*dummy* ~/.icons/*dummy* ~/Desktop/*dummy*
 
 }
