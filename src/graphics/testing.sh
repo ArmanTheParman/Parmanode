a6a2e10d0b4e939941ea61ab4c7b3e51b35f0b7b
Parman <armantheparman@gmail.com>
2023-09-26 12:18:29 +1000

    i

--- src/graphics/testing.sh
+++ src/graphics/testing.sh
@@ -1,4 +1,4 @@
-function testing.sh {
+function testing {
 
 
 #make xml file
@@ -9,7 +9,7 @@ echo '<?xml version="1.0"?>
         <icon name="dummy_icon"/>
         <glob pattern="dummyfile.dummy"/>
     </mime-type>
-</mime-info>' | tee sudo /usr/share/mime/packages/dummy-mime.xml 
+</mime-info>' |sudo tee /usr/share/mime/packages/dummy-mime.xml 
 
 sudo update-mime-database /usr/share/mime/
 
