8ebe8426c1356b5671db82fbb963c7a591dfaa34
Parman <armantheparman@gmail.com>
2023-09-26 12:46:47 +1000

    modified:   src/graphics/testing.sh

--- src/graphics/testing.sh
+++ src/graphics/testing.sh
@@ -6,7 +6,7 @@ echo '<?xml version="1.0"?>
 <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
     <mime-type type="application/x-dummy">
         <comment>Dummy MIME Type Example</comment>
-        <icon name="dummy_icon"/>
+        <icon name="/usr/share/icons/dummy_icon.png"/>
         <glob pattern="dummyfile.dummy"/>
     </mime-type>
 </mime-info>' | sudo tee /usr/share/mime/packages/x-dummy.xml 
@@ -14,12 +14,12 @@ echo '<?xml version="1.0"?>
 sudo update-mime-database /usr/share/mime/
 
 mkdir -p ~/.icons/
-cp pn_icon.png ~/.icons/dummy_icon.png
+cp pn_icon.png /usr/share/icons/dummy_icon.png
 touch ~/Desktop/d.dummy
 
 }
 function testing_delete {
 
-sudo rm /usr/share/mime/packages/*dummy* ~/.icons/*dummy* ~/Desktop/*dummy*
+sudo rm /usr/share/mime/packages/*dummy* ~/.icons/*dummy* ~/Desktop/*dummy* /usr/share/icons/*dummy*
 
 }
