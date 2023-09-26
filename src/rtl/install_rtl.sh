48eaace328a7c1c642f5aaae4f8d9af8e6466b5a
Parman <armantheparman@gmail.com>
2023-09-27 01:49:11 +1000

    modified:   src/rtl/install_rtl.sh

--- src/rtl/install_rtl.sh
+++ src/rtl/install_rtl.sh
@@ -2,7 +2,8 @@ function install_rtl {
 if [[ $OS == "Mac" ]] ; then no_mac ; return 1 ; fi
 grep -q docker-end < $HOME/.parmanode/installed.conf || { announce "Must install Docker first. Aborting." && return 1 ; }
 
-
+set_terminal
+if [[ $debug != 1 ]] ; then
 lncli wallet accounts list >/dev/null 2>&1 || echo "
 ########################################################################################
 
@@ -15,6 +16,7 @@ lncli wallet accounts list >/dev/null 2>&1 || echo "
 
 ########################################################################################
 " && enter_continue && return 1
+fi
 
 mkdir $HOME/parmanode/rtl $HOME/parmanode/startup_scripts/ 2>/dev/null
 installed_config_add "rtl-start"
