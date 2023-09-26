4bc79fafb53188acf95708695f5420f770d1a4eb
Parman <armantheparman@gmail.com>
2023-09-26 21:29:58 +1000

    new file:   src/lnbits/start_stop_lnbits.sh

---
+++ src/lnbits/start_stop_lnbits.sh
@@ -0,0 +1,15 @@
+function start_lnbits {
+cd $HOME/parmanode/lnbits
+docker run --detach --publish 5000:5000 --name lnbits --volume ${PWD}/.env:/app/.env --volume ${PWD}/data/:/app/data lnbitsdocker/lnbits-legend
+cd -
+}
+
+function stop_lnbits {
+docker stop lnbits
+}
+
+function refresh_lnbits {
+stop_lnbits
+docker rm lnbits
+start_lnbits
+}
