12c6ab15ab379c2ede2de0bc5a0613c2d37c8a7e
Parman <armantheparman@gmail.com>
2023-09-28 19:37:22 +1000

    i

--- src/ParmanodL/install_parmanodL.sh
+++ src/ParmanodL/install_parmanodL.sh
@@ -56,7 +56,7 @@ for file in ~/parman_programs/parmanode/src/**/*.sh ; do
 
 	#Rasbperry Pi OS, 64 bit, with Desktop.
 	cd $HOME/parman_programs/ParmanodL
-	if [ ! -e *raspios-bullseye-arm64.img.xz ] ; then
+	if [ ! -e 2023-05-03-raspios-bullseye-arm64.img.xz ] ; then
 	curl -LO https://downloads.raspberrypi.org/raspios_arm64/images/raspios_arm64-2023-05-03/2023-05-03-raspios-bullseye-arm64.img.xz
 
 	# Check integrity.
@@ -68,9 +68,9 @@ for file in ~/parman_programs/parmanode/src/**/*.sh ; do
 		fi
 	fi
 
-	if [ ! -e *raspios-bullseye-arm64.img ] ; then
+	if [ ! -e 2023-05-03-raspios-bullseye-arm64.img ] ; then
 	# Unzip the image:
-	xz -vkd *.img.xz
+	xz -vkd 2023-05-03-raspios-bullseye-arm64.img.xz
 	fi
 
 ParmanodL_mount
