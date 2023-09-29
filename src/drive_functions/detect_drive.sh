be3e227d594911d974061169c6b4ad78b893c088
Parman <armantheparman@gmail.com>
2023-09-29 20:24:06 +1000

    modified:   src/drive_functions/add_drive.sh 	modified:   src/drive_functions/detect_drive.sh

--- src/drive_functions/detect_drive.sh
+++ src/drive_functions/detect_drive.sh
@@ -26,12 +26,12 @@ set_terminal "pink" ; echo -e "
 "
 read
 
-if [[ $OS == "Linux" ]] ; then 
+if [[ $(uname) == "Linux" ]] ; then 
     sudo blkid -g >/dev/null
     sudo blkid > $HOME/.parmanode/before
     fi
 
-if [[ $OS == "Mac" ]] ; then
+if [[ $(uname) == "Darwin" ]] ; then
     diskutil list > $HOME/.parmanode/before
     fi
 
@@ -49,24 +49,24 @@ enter_continue
 set_terminal
 sleep 2.5
 
-if [[ $OS == "Linux" ]] ; then
+if [[ $(uname) == "Linux" ]] ; then
     sudo blkid -g >/dev/null
     sudo blkid > $HOME/.parmanode/after
     fi
 
-if [[ $OS == "Mac" ]] ; then
+if [[ $(uname) == "Darwin" ]] ; then
     diskutil list > $HOME/.parmanode/after
     fi
 
-disk_after=$(cat $HOME/.parmanode/after | grep . $HOME/.parmanode/after | tail -n1 ) 
+disk_after=$(grep . $HOME/.parmanode/after | tail -n1 ) 
 # grep . filters out empty lines
-disk_before=$(cat $HOME/.parmanode/before | grep . $HOME/.parmanode/before | tail -n1 )
+disk_before=$(grep . $HOME/.parmanode/before | tail -n1 )
 
     if [[ "$disk_after" == "$disk_before" ]] ; then 
         echo "No new drive detected. Try again. Hit <enter>."
             read ; continue 
         else
-            if [[ $OS == "Linux" ]] ; then
+            if [[ $(uname) == "Linux" ]] ; then
                 sed -i s/://g $HOME/.parmanode/after
                 export disk=$(grep . $HOME/.parmanode/after | tail -n1 | awk '{print $1}')
                 echo "disk=\"$disk\"" > $HOME/.parmanode/var
@@ -74,7 +74,7 @@ disk_before=$(cat $HOME/.parmanode/before | grep . $HOME/.parmanode/before | tai
                 return 0
                 fi
             
-            if [[ $OS == "Mac" ]] ; then
+            if [[ $(uname) == "Darwin" ]] ; then
                 Ddiff=$(($(cat $HOME/.parmanode/after | wc -l)-$(cat $HOME/.parmanode/before |wc -l))) #visualstudo code shows last ) as an error but it's not.
                 export disk=$(grep . $HOME/.parmanode/after | tail -n $Ddiff | grep "dev" | awk '{print $1}')
                 echo "$(cat $HOME/.parmanode/after | tail -n $Ddiff)" > $HOME/.parmanode/difference
@@ -85,5 +85,6 @@ disk_before=$(cat $HOME/.parmanode/before | grep . $HOME/.parmanode/before | tai
 
             break
     fi
+    debug "disk is $disk"
 done
 }
