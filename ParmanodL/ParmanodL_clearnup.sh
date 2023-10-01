ccfd0182a5d274c391be4dfb9626fb5852b6932f
Parman <armantheparman@gmail.com>
2023-10-01 21:43:27 +1100

    parmanodl

--- ParmanodL/ParmanodL_clearnup.sh
+++ ParmanodL/ParmanodL_clearnup.sh
@@ -1,7 +1,8 @@
 function ParmanodL_cleanup {
 
 sudo rm -rf /tmp/mnt
-sudo rm -rf $HOME/.parmanodl $HOME/ParmanodL
+sudo rm -rf $HOME/.parmanodl  #was this needed?
+sudo umount -f ${disk}* >/dev/null 2>&1
 
 return 0
 }
