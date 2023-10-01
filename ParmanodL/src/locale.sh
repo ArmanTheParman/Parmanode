221423f086f7c4871f558820c260a4404f6074fa
Parman <armantheparman@gmail.com>
2023-10-01 23:04:12 +1100

    smol bug

--- ParmanodL/src/locale.sh
+++ ParmanodL/src/locale.sh
@@ -2,17 +2,17 @@ function set_locale {
 
 if [[ $OS == Linux ]] ; then
 sudo chroot /tmp/mnt/raspi /bin/bash -c 'raspi-config nonint do_change_locale en_US.UTF-8'
-sudo chroot /mnt/raspi /bin/bash -c 'raspi-config nonint do_wifi_country US'
-sudo chroot /mnt/raspi /bin/bash -c 'raspi-config nonint do_change_timezone Etc/UTC'
-sudo chroot /mnt/raspi /bin/bash -c 'raspi-config nonint do_configure_keyboard us'
+sudo chroot /tmp/mnt/raspi /bin/bash -c 'raspi-config nonint do_wifi_country US'
+sudo chroot /tmp/mnt/raspi /bin/bash -c 'raspi-config nonint do_change_timezone Etc/UTC'
+sudo chroot /tmp/mnt/raspi /bin/bash -c 'raspi-config nonint do_configure_keyboard us'
 
 fi
 
 if [[ $OS == Mac ]] ; then
 docker exec -it ParmanodL /bin/bash -c "chroot /tmp/mnt/raspi /bin/bash -c 'raspi-config nonint do_change_locale en_US.UTF-8'"
-docker exec -it ParmanodL /bin/bash -c "chroot /mnt/raspi /bin/bash -c 'raspi-config nonint do_wifi_country US'"
-docker exec -it ParmanodL /bin/bash -c "chroot /mnt/raspi /bin/bash -c 'raspi-config nonint do_change_timezone Etc/UTC'"
-docker exec -it ParmanodL /bin/bash -c "chroot /mnt/raspi /bin/bash -c 'raspi-config nonint do_configure_keyboard us'"
+docker exec -it ParmanodL /bin/bash -c "chroot /tmp/mnt/raspi /bin/bash -c 'raspi-config nonint do_wifi_country US'"
+docker exec -it ParmanodL /bin/bash -c "chroot /tmp/mnt/raspi /bin/bash -c 'raspi-config nonint do_change_timezone Etc/UTC'"
+docker exec -it ParmanodL /bin/bash -c "chroot /tmp/mnt/raspi /bin/bash -c 'raspi-config nonint do_configure_keyboard us'"
 fi
 
 }
