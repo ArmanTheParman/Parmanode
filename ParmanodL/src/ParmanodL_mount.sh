071885713fae009a4ed3dc6da6891019550c8ce2
Parman <armantheparman@gmail.com>
2023-10-01 17:52:11 +1100

    i

--- ParmanodL/src/ParmanodL_mount.sh
+++ ParmanodL/src/ParmanodL_mount.sh
@@ -3,9 +3,9 @@ function ParmanodL_mount {
 # Caculate offset for image, needed for mount command later.
 
    if [[ $OS == Linux ]] ; then
-      startSector=$(sudo fdisk -l $image | grep img2 | awk '{print $2}') >/dev/null
+      startSector=$(sudo fdisk -l $image_path | grep img2 | awk '{print $2}') >/dev/null
       byteOffset=$(($startSector*512)) >/dev/null
-      mount -v -o offset=$byteOffset -t ext4 $image /tmp/mnt/raspi || { announced "Failed to mount. Aborting." ; return 1 ; }
+      mount -v -o offset=$byteOffset -t ext4 $image_path /tmp/mnt/raspi || { announced "Failed to mount. Aborting." ; return 1 ; }
       sudo mount --bind /dev /tmp/mnt/raspi/dev >/dev/null 2>&1 
       sudo mount --bind /sys /tmp/mnt/raspi/sys >/dev/null 2>&1
       sudo mount --bind /proc /tmp/mnt/raspi/proc >/dev/null 2>&1
