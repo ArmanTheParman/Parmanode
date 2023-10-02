787960f1f2bfa87984c0eae40a17a7304ea5014a
Parman <armantheparman@gmail.com>
2023-10-02 11:25:23 +1100

    i

---
+++ ParmanodL/src/ParmanodL_systemd.sh
@@ -0,0 +1,11 @@
+function ParmanodL_make_systemd_script {
+
+source $HOME/parman_programs/parmanode/src/source_parmanode.sh && source_parmanode
+
+#check this is the first time ParmanodL is running this code.
+
+#check drive is appropriate, then format.
+#check the drive label is not Parmanode. If not, format.
+
+
+}
