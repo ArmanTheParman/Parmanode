702192b914f36d20be5f162547cb560593ad68a5
Parman <armantheparman@gmail.com>
2023-10-02 12:59:06 +1100

    modified:   ParmanodL/src/Installer/ParmanodL_chroot_Linux.sh 	modified:   ParmanodL/src/Installer/ParmanodL_chroot_Mac.sh 	new file:   ParmanodL/src/ParmanodL_start.sh 	deleted:    ParmanodL/src/ParmanodL_systemd.sh

---
+++ ParmanodL/src/ParmanodL_start.sh
@@ -0,0 +1,23 @@
+function ParmanodL_make_systemd_script {
+
+source $HOME/parman_programs/parmanode/src/source_parmanode.sh && source_parmanode
+
+
+function detct_format_or_mount {
+
+#Detect and Format SSD
+
+    if ! sudo blkid | grep parmanode ; then
+        if   sudo blkid | grep sda ; then
+            format_sda
+        fi
+    else
+    mount_sda
+    fi
+}
+
+function mount_sda {
+
+sudo mount
+
+
