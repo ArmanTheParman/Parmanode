fcec0be4a349239e514feea09bb1f96dec26e1f5
Parman <armantheparman@gmail.com>
2023-10-09 02:09:46 +1100

    i

--- src/ParmanodL/ParmanodL_docker_run.sh
+++ src/ParmanodL/ParmanodL_docker_run.sh
@@ -1,13 +1,19 @@
 function ParmanodL_docker_run {
-if [[ $uname == Linux ]] ; then return 0 ; fi
+if [[ $(uname) == Linux ]] ; then return 0 ; fi
 # Remove old ParmanodL containers in case of repeated installation
 
     if docker ps -a | grep -q ParmanodL ; then docker stop ParmanodL >/dev/null 2>&1 ; docker rm ParmanodL >/dev/null 2>&1 ; fi
 
 # Start a Linux docker container as a daemon process
 
+if [[ $1 == umbrelmac ]] ; then
+    docker run --privileged -d --device $disk:$disk -v $HOME/parman_programs:/volume --name umbrel arm64v8/debian tail -f /dev/null >/dev/null 2>&1 || \
+        { announce "Couldn't start Docker container. Aborting." ; return 1 ; }
+else
     docker run  --privileged -d -v $HOME/ParmanodL:/mnt/ParmanodL --name ParmanodL arm64v8/debian tail -f /dev/null >/dev/null 2>&1 || \
         { announce "Couldn't start Docker container. Aborting." ; return 1 ; }
+fi
+
 }
 
 function ParmanodL_docker_get_binaries {
