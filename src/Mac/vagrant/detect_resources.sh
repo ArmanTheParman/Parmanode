3891e6150d19adce09672e4a212c9b7b73d07edd
Parman <armantheparman@gmail.com>
2023-10-09 21:23:43 +1100

    i

--- src/Mac/vagrant/detect_resources.sh
+++ src/Mac/vagrant/detect_resources.sh
@@ -1,13 +1,14 @@
 function detect_resources {
 
-cpus=$(nproc)
 
 if [[ $(uname) == Linux ]] ; fi
 memory=$(free -m | awk '/^Mem:/{print $2/1024}') #in gigabytes
+cpus=$(nproc)
 fi
 
 if [[ $(uname) == Darwin ]] ; fi
 memory=$(sysctl -n hw.memsize | awk '{print $1/1024/1024/1024}') # in gigabytes
+cpus=$(sysctl -n hw.ncpu)
 fi
 
 }
