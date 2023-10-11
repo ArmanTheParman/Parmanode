e752142f2de0b3b6bb897a3d9f8fec33b2d9bee0
Parman <armantheparman@gmail.com>
2023-10-11 16:10:12 +1100

    i

--- src/MENUS/menu_main.sh
+++ src/MENUS/menu_main.sh
@@ -2,17 +2,6 @@ function menu_main {
 while true ; do
 set_terminal_bit_higher #custom function to resize the window
 
-unset uninstall
-if [[ -e $HOME/.parmanode/installed.conf ]] ; then
-if grep -q parmanode < $HOME/.parmanode/installed.conf ; then
-export uninstall="#    (uninstall)          Uninstall Parmanode ..........(Who'd do such a thing?)       #
-#                                                                                      #"
-
-fi
-else
-unset uninstall
-fi >/dev/null 2>&1
-
 # if statements in the menu printout makes the menu dynamic, ie changes accoring to the
 # tests performed. Variables are set to assit logic in the menu choice excution part
 # of the code at the bottom.
@@ -27,10 +16,9 @@ echo -e "
 #                                                                                      #
 #    (u)                  Use programs (Apps menu)........(Bitcoin, Sparrow, etc)      #
 #                                                                                      #
-
-#--------------------------------------------------------------------------------------#
-#                                                                                      #
 #    (remove)             Remove (uninstall) programs                                  #
+#                                                                                      #
+#--------------------------------------------------------------------------------------#
 #                                                                                      #
 #    (l)                  See logs and configuration files                             #
 #                                                                                      #
@@ -39,9 +27,10 @@ echo -e "
 #    (t)                  Tools                                                        #
 #                                                                                      #
 #    (d)                  Donate ;)                                                    #
-#                                                                                      #"
-if [[ -n $uninstall ]] ; then echo "$uninstall" ; fi
-echo "#    (update)             Update Parmanode                                             #
+#                                                                                      #
+#    (uninstall)          Uninstall Parmanode ..........(Who'd do such a thing?)       #
+#                                                                                      #
+#    (update)             Update Parmanode                                             #
 #                                                                                      #
 #    (ap)                 About Parmanode                                              #
 #                                                                                      #
@@ -57,12 +46,7 @@ case $choice in #the variable choice is tested through each of the case-choices 
 # these end in a closing bracked, have some code, and end with a ;;
 # once there is a match, the case block is exited (after the esac point below). Then
 # it repeats because case is inside a while loop.
-i|I)
-    if [[ $parmanodemain == 1 ]] ; then # this variable makes install_parmanode only available to execute if
-    # parmanodemain is set to 1, otherwise, the function can't run even if the user types "i".
-    install_parmanode 
-    fi
-    ;;
+
 add|Add| ADD)
     if [[ $parmanodemain == 0 ]] ; then # with parmanodemain set to 0, it's possible to add programs
     menu_add_programs
@@ -84,8 +68,8 @@ t|T)
 d|D)
     donations ;;
 uninstall|UNINSTALL)
-if [[ -n $uninstall ]] ; then uninstall_parmanode ; fi ;; #if $uninstall is "non zero length"
-
+uninstall_parmanode
+;;
 update|UPDATE|Update)
     update_parmanode ;;
 ap|AP|Ap|aP)
