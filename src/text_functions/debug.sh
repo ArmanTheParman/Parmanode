cd7fcfea7cb58644b8e821855dcfe1aa5f4306c1
Parman <armantheparman@gmail.com>
2023-10-12 08:22:11 +1100

    i

--- src/text_functions/debug.sh
+++ src/text_functions/debug.sh
@@ -3,6 +3,16 @@
 # without my knowledge. Only the character "d" will allow th
 # code to contine.
 
+function chuck {
+    if [[ $chuck ==1 ]] ; then
+    echo "Debugging for chuck."
+    echo "$1"
+    enter_continue
+    return 0
+    fi
+}
+
+
 function debug {
 if [[ $debug == 1 ]] ; then
 echo "Debug point. Message:
