c77461c54f616ec5aa58cdd51f2fc7bbfe5ff830
Parman <armantheparman@gmail.com>
2023-10-11 20:47:34 +1100

    i

--- src/bitbox/uninstall_bitbox.sh
+++ src/bitbox/uninstall_bitbox.sh
@@ -2,7 +2,7 @@ function uninstall_bitbox {
 set_terminal ; echo "
 ########################################################################################
 
-                                 Uninstall Sparrow 
+                                 Uninstall BitBox 
 
     Are you sure? (y) (n)
 
@@ -11,4 +11,21 @@ set_terminal ; echo "
 choose "x" 
 read choice
 set_terminal
+
+if [[ $choice == "y" || $choice == "Y" ]] ; then true
+    else 
+    return 1
+    fi
+
+set_terminal
+
+rm -rf $HOME/parmanode/bitbox
+
+if [[ $OS == Mac ]] ; then
+rm -rf /Applications/"BitBox.app"
+fi
+
+installed_conf_remove "bitbox"
+success "BitBox App" "being uninstalled."
+
 }
