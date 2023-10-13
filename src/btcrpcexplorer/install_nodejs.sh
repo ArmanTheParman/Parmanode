3166039470d110b7e753adbc4b598a318a911173
Parman <armantheparman@gmail.com>
2023-10-13 13:24:18 +1100

    i

--- src/btcrpcexplorer/install_nodejs.sh
+++ src/btcrpcexplorer/install_nodejs.sh
@@ -37,9 +37,12 @@ fi
 
 function check_nodejs {
 
-
 #extract nodejs version number, and return old vs new (needs to be 16+)
-nodejs_version=$(node --version | cut -d "." -f 1 | cut -d "v" -f 2)
+if which node ; then
+nodejs_version=$(node --version | cut -d "." -f 1 | cut -d "v" -f 2) >/dev/null
+else
+nodejs_version=none
+fi
 
 if [[ $nodejs_version -lt 16 ]] ; then 
     export nodejs_version="old"
