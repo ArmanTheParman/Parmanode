4237995a345ac9d3c31882b11ce32417f3381145
Parman <armantheparman@gmail.com>
2023-10-13 20:42:37 +1100

    typo

--- src/ParmaShell/parmashell_info.sh
+++ src/ParmaShell/parmashell_info.sh
@@ -13,8 +13,10 @@ set_terminal ; echo -e "
     - The refreshed state will show the contents of the directory in colour, and the
       current working directory printed in full.
     - Refresh anytime with \"a\"
-    - If its too cluttered with a refresh, just type \"st\" and the screen will be
-      refreshed without the directory contents. It alse adjusts the screen size.
+    - If its too cluttered with a refresh, just type \"st\" (set terminal), and 
+      the screen will be refreshed without the directory contents. It alse adjusts 
+      the screen size.
+
     - \"aa\" with print out \"ls -lah\" with a refreshed screen and contents printed
       at the top
     - \"..\" you'll already use but it will now refresh the screen and print the
@@ -25,8 +27,7 @@ set_terminal ; echo -e "
     - \"pp\" changes to parman_programs directory
     - \"hp\" stands for 'home parmanode' and changes to the parmanode apps directory
 
-   This list will grow as I think of good ideas.
-   Requests welcom. 
+   This list will grow as I think of good ideas. Requests welcome. 
 "
 enter_continue
 }
