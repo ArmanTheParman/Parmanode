f7645f200e2470d1dc72052303efe75d04636cf8
Parman <armantheparman@gmail.com>
2023-10-13 14:38:35 +1100

    i

--- src/patches/fix_autoupdate.sh
+++ src/patches/fix_autoupdate.sh
@@ -2,7 +2,7 @@ function fix_autoupdate {
 
 source $HOME/.parmanode/parmanode.conf
 if [[ $autoupdate == true ]] ; then
-crontab -l ; echo "30 3 * * *  [ -x $HOME/.parmanode/update_script.sh ] && $HOME/.parmanode/update_script.sh" | crontab - >/dev/null ; clear
+crontab -l | echo "30 3 * * *  [ -x $HOME/.parmanode/update_script.sh ] && $HOME/.parmanode/update_script.sh" | crontab - >/dev/null ; clear
 parmanode_conf_remove "autoupdated="
 parmanode_conf_add "autoupdate_version2=true"
 fi
