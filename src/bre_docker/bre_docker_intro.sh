84be1790cff4e4617c54e3cf3e819d5e3e537d2f
Parman <armantheparman@gmail.com>
2023-10-15 02:33:18 +1100

    smol bug

--- src/bre_docker/bre_docker_intro.sh
+++ src/bre_docker/bre_docker_intro.sh
@@ -7,7 +7,7 @@ $orange
     Parmanode will now install BRE for you using Docker. This will give you a pretty
     webpage with your own Node's statistics. There's one to view online, but that's
     using someone else's node, and you'd be revealing your IP addres and wallet
-    details to them, so run your own.
+    details to them, so it's good you'll be runnning your own.
 
 ########################################################################################                            
 "
