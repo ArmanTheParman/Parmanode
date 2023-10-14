742ae544deba79dd756e475927ec438ee292be24
Parman <armantheparman@gmail.com>
2023-10-14 16:03:12 +1100

    i

--- src/bre_docker/bre_docker_build.sh
+++ src/bre_docker/bre_docker_build.sh
@@ -1,6 +1,15 @@
+function bre_docker_build {
+
+if [[ $1 == test ]] ; then
+rpcuser=parman
+rpcpassword==hodl
+eserver="tcp://127.0.0.1:50005"
+btc_authentication="user/pass"
+fast_computer="yes"
+
 docker build -t bre --build-arg rpcuser=$rpcuser \
                     --build-arg rpcpassword=$rpcpassword \
                     --build-arg eserver=$eserver \
                     --build-arg btc_authentication=$btc_authentication \
                     --build-arg fast_computer=$fast_computer
-                     
+}                     
--- src/bre_docker/install_bre_docker.sh
+++ src/bre_docker/install_bre_docker.sh
@@ -4,10 +4,13 @@ function install_bre_docker {
 
 #questions (for variables)
 
+#made directories
+bre_docker_directories
+
 #docker build
 
 #docker run
 
-
+#UNINSTALL FUNCTION
 
 }
