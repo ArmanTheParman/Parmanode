e004fcb781560a14e397626e4715cf897de67b69
Parman <armantheparman@gmail.com>
2023-10-14 16:03:12 +1100

    i

---
+++ src/bre_docker/bre_docker_directories.sh
@@ -0,0 +1,6 @@
+function bre_docker_directories {
+
+mkdir -p $HOME/parmanode/bre >/dev/null 2>&1
+
+
+}
---
+++ src/bre_docker/bre_docker_run.sh
@@ -0,0 +1,15 @@
+function bre_docker_run {
+
+docker run -d --name bre \
+     -v $HOME/parmanode/bre:/home/parman/bre
+
+
+    
+
+    
+
+    
+
+    
+
+}
