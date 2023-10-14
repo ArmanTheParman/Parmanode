85e55756c7a937573a97777764d9ae8dac525a6f
Parman <armantheparman@gmail.com>
2023-10-15 04:07:49 +1100

    i

--- src/bre_docker/bre_docker_build.sh
+++ src/bre_docker/bre_docker_build.sh
@@ -1,4 +1,10 @@
 function bre_docker_build {
+if [[ $debug == 3 ]] ; then
+cd $HOME/parman_programs/parmanode/src/bre_docker
+docker build -t bre --no-cache .
+return 0
+fi
+
 cd $HOME/parman_programs/parmanode/src/bre_docker
 docker build -t bre .
 }                     
