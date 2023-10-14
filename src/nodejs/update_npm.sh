f8e2843c340fe67d5b4705e0d4618b5fce8d0c58
Parman <armantheparman@gmail.com>
2023-10-14 21:10:23 +1100

    i

--- src/nodejs/update_npm.sh
+++ src/nodejs/update_npm.sh
@@ -1,11 +1,12 @@
 function update_npm {
 
-local version=$(npm -v) >/dev/null 2>&1
+local version=$(npm -v | cut -d . -f 1) >/dev/null 2>&1
 if [[ $version -gt $1 ]] ; then return 0 ; fi
 sudo npm install -g npm
-local version=$(npm -v) >/dev/null 2>&1
+local version=$(npm -v | cut -d . -f 1) >/dev/null 2>&1
 if [[ $version -gt $1 ]] ; then return 0 
 else
+debug2 "before npm error"
 announce "Couldn't get the right version of NPM.
 
     You have $version.
