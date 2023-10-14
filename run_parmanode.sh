2b25aded0ea97b7ada850716bd15abe3a1203d6e
Parman <armantheparman@gmail.com>
2023-10-15 04:08:42 +1100

    i

--- run_parmanode.sh
+++ run_parmanode.sh
@@ -11,7 +11,7 @@
 #program wherever it appears. "export" keeps variable in global memory.
 if [[ $1 == "debug" || $1 == d ]] ; then export debug=1 
 elif [[ $1 == d2 ]] ; then export debug=2  
-elif [[ $1 == d3 ]] ; then export debug=3  
+elif [[ $1 == d3 ]] ; then export debug=3  #bre docker no-cache build
 elif [[ $1 == d4 ]] ; then export debug=4  
 elif [[ $1 == d5 ]] ; then export debug=5  
 elif [[ $1 == d6 ]] ; then export debug=6  
