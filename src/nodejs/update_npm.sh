a2d2ce50b3ac532dde4f3a361ffd06ca5517bd4c
Parman <armantheparman@gmail.com>
2023-10-14 21:06:58 +1100

    i

--- src/nodejs/update_npm.sh
+++ src/nodejs/update_npm.sh
@@ -6,7 +6,11 @@ sudo npm install -g npm
 local version=$(npm -v) >/dev/null 2>&1
 if [[ $version -gt $1 ]] ; then return 0 
 else
-announce "Couldn't get the right version of NPM. Aborting."
+announce "Couldn't get the right version of NPM.
+
+    You have $version.
+    
+    Aborting."
 return 1
 fi
 
