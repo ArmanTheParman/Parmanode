4500d02c9df0f54512e995e43136fb07e28bf03e
Parman <armantheparman@gmail.com>
2023-10-15 14:49:37 +1100

    fulcrum

--- src/fulcrum_mac/start_fulcrum_docker.sh
+++ src/fulcrum_mac/start_fulcrum_docker.sh
@@ -1,12 +1,5 @@
 function start_fulcrum_docker {
 
-if ! grep -q "fulcrum-end" $HOME/.parmanode/installed.conf ; then
-    set_terminal
-    echo "Fulcrum not installed according to settings file (installed.conf)"
-    enter_continue 
-    return 1
-    fi
-
 if ! docker ps -a | grep fulcrum ; then
     set_terminal
     echo "Fulcrum container does not exist."
@@ -14,31 +7,11 @@ if ! docker ps -a | grep fulcrum ; then
     return 1
     fi
 
-if  docker ps | grep fulcrum ; then
-    set_terminal
-    echo "Starting Fulcrum within the running container."
-
-    docker_exec_command
-
-    enter_continue
-    return 0
-else
-    if docker ps -a | grep fulcrum ; then
-       set_terminal 
-        echo "Starting docker container."
-        docker start fulcrum
-
-        echo "Starting Fulcrum within the running container."
-
-        docker_exec_command
-
-        enter_continue
-        return 0
-        fi
-fi
+docker start fulcrum
+fulrcum_docker_start_fulcrum 
 }
 
-function docker_exec_command {
+function fulrcum_docker_start_fulcrum {
 docker exec -d fulcrum /bin/bash -c "/home/parman/parmanode/fulcrum/Fulcrum /home/parman/parmanode/fulcrum/fulcrum.conf \
     >>/home/parman/parmanode/fulcrum/fulcrum.log 2>&1"
 }
