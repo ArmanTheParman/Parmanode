da442849c88f67818879b622b2c3e44021fa0a94
Parman <armantheparman@gmail.com>
2023-10-17 13:06:39 +1100

    debug

--- src/parmabox/parmabox_build.sh
+++ src/parmabox/parmabox_build.sh
@@ -1,7 +1,7 @@
 function parmabox_build  {
 cd $HOME/parman_programs/parmanode/src/parmabox
 
-if [[ debug == fast ]] ; then
+if [[ $fast == debug ]] ; then
 docker build -t parmabox .
 return 0
 fi
