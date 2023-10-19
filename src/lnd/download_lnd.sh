f0b0e0abe50ec3cc9ed6ba742e72f416272487e2
Parman <armantheparman@gmail.com>
2023-10-19 23:04:27 +1100

    {}

--- src/lnd/download_lnd.sh
+++ src/lnd/download_lnd.sh
@@ -7,23 +7,24 @@ lndversion="v0.17.0-beta"
 
 #get signatures and SHA256 file
 cd $HOME/parmanode/lnd
-curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/manifest-$lndversion.txt 
-curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/manifest-roasbeef-$lndversion.sig
+curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.17.0-beta/manifest-${lndversion}.txt
+curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/manifest-${lndversion}.txt 
+curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/manifest-roasbeef-${lndversion}.sig
 
 
 if [[ $chip == "x86_64" ]] ; then
-curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-amd64-$lndversion.tar.gz
+curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-amd64-${lndversion}.tar.gz
 fi
 
 if [[ $chip == "arm64" || $chip == "aarch64" ]] ; then
-curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-arm64-$lndversion.tar.gz 
+curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-arm64-${lndversion}.tar.gz 
 fi
 
 if [[ $chip == "armv6l" ]] ; then
-curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-armv6-$lndversion.tar.gz
+curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-armv6-${lndversion}.tar.gz
 fi
 
 if [[ $chip == "armv7l" ]] ; then
-curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-armv7-$lndversion.tar.gz
+curl -LO https://github.com/lightningnetwork/lnd/releases/download/v0.16.2-beta/lnd-linux-armv7-${lndversion}.tar.gz
 fi
 }
