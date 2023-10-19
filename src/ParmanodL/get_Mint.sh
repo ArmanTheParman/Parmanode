67938701e9bbaf2e2bbc8c447e45221456462570
Parman <armantheparman@gmail.com>
2023-10-19 14:52:08 +1100

    big bug

--- src/ParmanodL/get_Mint.sh
+++ src/ParmanodL/get_Mint.sh
@@ -12,7 +12,7 @@ function verify_Mint {
 
     gpg --import $HOME/parman_programs/parmanode/ParmanodL/Linux_mint.pubkey
 
-if ! gpg --verify --status-fd 1 sha256sum.txt.gpg >/dev/null 2>&1 | grep -q GOOD ; then
+if ! gpg --verify --status-fd 1 sha256sum.txt.gpg 2>&1 | grep -q GOOD ; then
 announce "gpg verification failed. Aborting."
 return 1
 fi
