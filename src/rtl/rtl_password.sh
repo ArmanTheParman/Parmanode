daaf2dcf352314cdc43c3c27026431fc0731d77f
Parman <armantheparman@gmail.com>
2023-10-20 11:14:45 +1100

    RTL

---
+++ src/rtl/rtl_password.sh
@@ -0,0 +1,14 @@
+
+function rtl_password {
+
+set_terminal ; echo -e "
+########################################################################################
+
+    To change the RTL password, you need to find that option within the RTL
+    graphical user interface.
+
+########################################################################################
+"
+enter_continue
+
+}
