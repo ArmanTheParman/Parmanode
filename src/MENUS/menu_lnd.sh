84a1b0f747c378a4ec9b7eff98c60fa0ca5bf25b
Parman <armantheparman@gmail.com>
2023-10-22 13:40:04 +1100

    lnd

--- src/MENUS/menu_lnd.sh
+++ src/MENUS/menu_lnd.sh
@@ -1,5 +1,5 @@
 function menu_lnd {
-lnd_version=$(lncli --version | cut -d - -f 1 | cut -d ' ' -f 3) >/dev/null
+export lnd_version=$(lncli --version | cut -d - -f 1 | cut -d ' ' -f 3) >/dev/null
 while true ; do set_terminal_custom "46" ; echo -e "
 ########################################################################################
                                  ${cyan}LND Menu${orange} - v$lnd_version                               
