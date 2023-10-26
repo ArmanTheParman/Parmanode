9d741ab60598f5027a6a1f4f8f9aad3498539e36
Parman <armantheparman@gmail.com>
2023-10-26 14:57:19 +1100

    m

---
+++ src/MENUS/menu_lnd_wallet.sh
@@ -0,0 +1,55 @@
+function menu_lnd_wallet {
+export lnd_version=$(lncli --version | cut -d - -f 1 | cut -d ' ' -f 3) >/dev/null
+
+while true ; do set_terminal ; echo -e "
+########################################################################################$cyan
+                                LND Menu${orange} - v$lnd_version                               
+########################################################################################
+
+"
+if ps -x | grep lnd | grep bin >/dev/null 2>&1 ; then echo "
+                   LND IS RUNNING -- SEE LOG MENU FOR PROGRESS "
+else
+echo "
+                   LND IS NOT RUNNING -- CHOOSE \"start\" TO RUN"
+fi
+echo -e "
+
+      (ul)             Unlock Wallet
+
+      (wb)             Wallet balance
+
+      (delete)         Delete existing wallet and its files (macaroons, channel.db)
+
+      (create)         Create an LND wallet (or restore a wallet with seed)
+
+########################################################################################
+"
+choose "xpq" ; read choice
+case $choice in
+q|Q) quit ;;
+p|P) return 1 ;;
+
+
+create|CREATE|Create)
+create_wallet ; lncli unlock ;;
+
+ul|UL|Ul|unlock|Unlock) 
+lncli unlock
+;;
+
+wb|WB)
+set_terminal
+lncli walletbalance
+enter_continue
+;;
+
+delete|DELETE|Delete) 
+delete_wallet ;;
+
+*) invalid
+;;
+esac
+done
+
+}
