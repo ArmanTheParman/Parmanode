e45264df1e358841d81145c3efcf57cafd47e12c
Parman <armantheparman@gmail.com>
2023-10-26 15:13:34 +1100

    enable hybrid

--- src/lnd/lnd_TOR.sh
+++ src/lnd/lnd_TOR.sh
@@ -2,7 +2,6 @@ function lnd_enable_tor {
 local file=$HOME/.lnd/lnd.conf
 
 if ! which tor >/dev/null ; then install_tor ; fi
-stop_lnd
 
 cp $file ${dp}/backup_files/lnd.conf$(date | awk '{print $1$2$3}')-presableTor >/dev/null 2>&1
 
@@ -24,35 +23,32 @@ tor.active=1
 
 swap_string "$file" "listen=0.0.0.0:9735" "listen=localhost"
 swap_string "$file" "externalip=" "; externalip=$extIP:9735"
-start_lnd
+restart_lnd
 success "LND Tor enabling"
 }
 
 function lnd_disable_tor {
 local file=$HOME/.lnd/lnd.conf
 
-stop_lnd
-
 cp $file ${dp}/backup_files/lnd.conf$(date | awk '{print $1$2$3}')-preDisableTor >/dev/null 2>&1
 
 sed -i '/Added by Parmanode (start)/,/Added by Parmanode (end)/d' $file >/dev/null 2>&1
 swap_string "$file" "listen=localhost" "listen=0.0.0.0:9735"
 swap_string "$file" "; externalip=" "externalip=$extIP:9735"
-start_lnd
+restart_lnd
 
 success "LND Tor diabling"
 }
 
 function lnd_enable_hybrid {
 local file=$HOME/.lnd/lnd.conf
-
-stop_lnd
+lnd_enable_tor
 
 cp $file ${dp}/backup_files/lnd.conf$(date | awk '{printe $1$2$3}')-preEnableHybrid >/dev/null 2>&1
 
 swap_string $file "; tor.skip-proxy-for-clearnet-targets=true" "tor.skip-proxy-for-clearnet-targets=true"
 
-start_lnd
+restart_lnd
 success "LND hypbrid TOR/Clearnet mode" "being enabled"
 
 }
@@ -60,12 +56,10 @@ success "LND hypbrid TOR/Clearnet mode" "being enabled"
 function lnd_disable_hybrid {
 local file=$HOME/.lnd/lnd.conf
 
-stop_lnd
-
 cp $file ${dp}/backup_files/lnd.conf$(date | awk '{printe $1$2$3}')-preDisableHybrid >/dev/null 2>&1
 
 swap_string $file "tor.skip-proxy-for-clearnet-targets=true" "; tor.skip-proxy-for-clearnet-targets=true"
 
-start_lnd
+restart_lnd
 success "LND hypbrid TOR/Clearnet mode" "being disabled"
 }
