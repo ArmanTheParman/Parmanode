5d6b5e8852498a3f962754143995dc6f1b0242f7
Parman <armantheparman@gmail.com>
2023-10-28 00:21:27 +1100

    i

--- src/MENUS/menu_lnd_more.sh
+++ src/MENUS/menu_lnd_more.sh
@@ -20,7 +20,7 @@ echo "
 
       (alias)          Change LND alias
 
-      (port)           Change port. Current port is $lnd_port [ Not yet available ]
+      (port)           Change port. Current port is $lnd_port [Feature not yet available ]
 "
 if [[ $lnd_version != "v0.17.0" ]] ; then echo -e " 
 $red      (update)         Update LND to version 0.17.0 $orange
