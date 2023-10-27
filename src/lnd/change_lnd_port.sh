7d580ed2b660eb9f52430a175776d47db0e280a3
Parman <armantheparman@gmail.com>
2023-10-28 00:35:43 +1100

    ss

--- src/lnd/change_lnd_port.sh
+++ src/lnd/change_lnd_port.sh
@@ -1,7 +1,7 @@
 function change_lnd_port {
 set_lnd_port || return 1
 get_extIP
-swap_sting "$HOME/.lnd/lnd.conf" "externalip=" "externalip=$extIP:$lnd_port"
+swap_string "$HOME/.lnd/lnd.conf" "externalip=" "externalip=$extIP:$lnd_port"
 restart_lnd
 return 0
 }
