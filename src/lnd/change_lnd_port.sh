5751d42ed5e8171e6106f39b0cf3c30c31e777f2
Parman <armantheparman@gmail.com>
2023-10-28 00:40:52 +1100

    smol bug

--- src/lnd/change_lnd_port.sh
+++ src/lnd/change_lnd_port.sh
@@ -2,6 +2,7 @@ function change_lnd_port {
 set_lnd_port || return 1
 get_extIP
 swap_string "$HOME/.lnd/lnd.conf" "externalip=" "externalip=$extIP:$lnd_port"
+swap_string "$HOME/.lnd/lnd.conf" "listen=0.0.0.0:973" "listen=0.0.0.0:$lnd_port"
 restart_lnd
 return 0
 }
