b7f10689755e8083d7abbe40e87767f8df1b181d
Parman <armantheparman@gmail.com>
2023-10-30 23:49:11 +1100

    pihole

---
+++ src/pihole/intstall_pihole.sh
@@ -0,0 +1,9 @@
+# Need to disable ubuntu/Mint dns server if it's running.
+# sudo systemctl stop systemd-resolved.service
+# then disable
+# then edit /etc/resolv.conf - change name server google, 8.8.8.8
+
+
+#check for docker install
+
+#
