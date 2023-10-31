d9ecd2d3fd102b7b65f7e81b81d471b980247feb
Parman <armantheparman@gmail.com>
2023-11-01 02:44:11 +1100

    v3.15.0

--- src/pihole/start_stop_pihole.sh
+++ src/pihole/start_stop_pihole.sh
@@ -1,8 +1,6 @@
 function start_pihole {
     cd $hp/pihole
-    debug3 "in start pihole"
     docker compose start
-    debug3 "after docker compose start"
 }
 function stop_pihole {
     cd $hp/pihole
