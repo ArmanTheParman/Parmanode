14ad18f7d5d4c5a4a51086205907c1281b91fe23
Parman <armantheparman@gmail.com>
2023-11-02 10:50:13 +1100

    Thu  2 Nov 2023 10:50:13 AEDT

--- src/MENUS/back2main.sh
+++ src/MENUS/back2main.sh
@@ -1,6 +1,7 @@
 function back2main {
 
 export main_loop=$(main_loop + 1) >/dev/null
+debug "main loop $main_loop"
 if [[ $main_loop -gt 2 ]] ; then
 announce "The main menu function has called itself a few too many times. It's
     probably a good idea to quit Parmanode and restart it - this will release
