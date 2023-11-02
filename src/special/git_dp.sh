31a7834fb06fd562bb2ec354f23638a6c2bb389e
Parman <armantheparman@gmail.com>
2023-11-02 14:29:41 +1100

    Thu  2 Nov 2023 14:29:41 AEDT

--- src/special/git_dp.sh
+++ src/special/git_dp.sh
@@ -5,8 +5,9 @@ git init >/dev/null 2>&1
 git add . >/dev/null 2>&1
 git commit -m "initial commit" >/dev/null 2>&1
 else
+debug3 "in git_dp else"
 git add . >/dev/null 2>&1
 git commit -m "$(date)" >/dev/null 2>&1
 fi
-debug3 "in git_dp"
+debug3 "after git_dp if"
 }
