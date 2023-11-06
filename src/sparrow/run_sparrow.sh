b0f4a1b698f78a0094cdd2ecf9c16a79778b8d06
Parman <armantheparman@gmail.com>
2023-11-07 10:45:57 +1100

    run sparrow

--- src/sparrow/run_sparrow.sh
+++ src/sparrow/run_sparrow.sh
@@ -1,6 +1,26 @@
 function run_sparrow {
-if [[ $OS == "Linux" ]] ; then
 
+while true ; do
+if [[ -e $dp/.sparrow_first_run ]] ; then break ; fi
+set_terminal ; echo -e "
+########################################################################################
+
+    Parmanode detected this is the first time you are running Sparrow since its
+    install.
+
+    In order for the connection to your node to work, for an unexplainable reason,
+    Sparrow needs to start, shutdown, then start again.
+$cyan
+    After Sparrow loads, please shut it down, then come back here to start it again.
+$orange
+########################################################################################
+"
+enter_continue
+touch $dp/.sparrow_first_run >/dev/null 2>&1
+break
+done
+
+if [[ $OS == "Linux" ]] ; then
 nohup $HOME/parmanode/Sparrow/bin/Sparrow >/dev/null 2>&1 & 
 please_wait
 sleep 2
