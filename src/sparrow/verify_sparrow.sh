6019e703dc1248e030122b242ec6cc2d58207209
Parman <armantheparman@gmail.com>
2023-11-08 02:55:01 +1100

    ignore missing removed from shasum

--- src/sparrow/verify_sparrow.sh
+++ src/sparrow/verify_sparrow.sh
@@ -5,7 +5,7 @@ curl https://keybase.io/craigraw/pgp_keys.asc | gpg --import
 if ! gpg --verify sparrow*.asc sparrow*.txt >/dev/null 2>&1 ; then
 set_terminal ; echo "GPG verification failed. Aborting. Contact Parman for help." ; enter_continue ; return 1 ; fi
 
-if ! shasum -a 256 --ignore-missing --check *parrow*.txt ; then echo "Checksum failed. Aborting. Contact Parman for help" 
+if ! shasum -a 256 --check *parrow*.txt ; then echo "Checksum failed. Aborting. Contact Parman for help" 
 enter_continue ; return 1 ; fi
 
 }
