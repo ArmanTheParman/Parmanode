feb5e2362298b53ca736a304fcbf45aad8ce047b
Parman <armantheparman@gmail.com>
2023-11-08 22:10:30 +1100

    MacOSVersion

--- src/Mac/get_Mac_version.sh
+++ src/Mac/get_Mac_version.sh
@@ -2,7 +2,7 @@ function get_Mac_version {
 if [[ $(uname) != Darwin ]] ; then return 0 ; fi
 
 export MacOSVersion=$(sw_vers | grep ProductVersion | awk '{print $ 2}')
-export MacOSVersion_major=$(sw_vers | grep ProductVersion | cut -d \. -f 1)
+export MacOSVersion_major=$(sw_vers | grep ProductVersion | cut -d \. -f 1 | grep -Eo '[0-9]+$')
 export MacOSVersion_minor=$(sw_vers | grep ProductVersion | cut -d \. -f 2)
 export MacOSVersion_patch=$(sw_vers | grep ProductVersion | cut -d \. -f 3)
 
