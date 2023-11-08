baad7e237dbdfb3fe65074afbd0a04a2553dc609
Parman <armantheparman@gmail.com>
2023-11-08 22:19:20 +1100

    MacOS version bug

--- src/parmanode.com/install_4mac2.sh
+++ src/parmanode.com/install_4mac2.sh
@@ -35,11 +35,11 @@ fi
 #Check mac version 10.9 or later...
 
 export MacOSVersion=$(sw_vers | grep ProductVersion | awk '{print $ 2}')
-export MacOSVersion_major=$(sw_vers | grep ProductVersion | cut -d \. -f 1)
+export MacOSVersion_major=$(sw_vers | grep ProductVersion | cut -d \. -f 1 | grep -Eo '[0-9]+$')
 export MacOSVersion_minor=$(sw_vers | grep ProductVersion | cut -d \. -f 2)
 export MacOSVersion_patch=$(sw_vers | grep ProductVersion | cut -d \. -f 3)
 
-if [[ ($MacOSVersion -lt 10) || ($MacOSVersion == 10 && $MacOSVersion_major -lt 9) ]] ; then
+if [[ ($MacOSVersion_major -lt 10) || ($MacOSVersion_major == 10 && $MacOSVersion_major -lt 9) ]] ; then
 clear
 echo "
 ########################################################################################
