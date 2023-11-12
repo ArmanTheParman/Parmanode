fe302ceb5dfd04f5d868d956009975b659bbd513
Parman <armantheparman@gmail.com>
2023-11-12 13:50:32 +1100

    nyx

--- src/MENUS/menu_torrelay.sh
+++ src/MENUS/menu_torrelay.sh
@@ -15,7 +15,25 @@ m|M) back2main ;;
 q|Q|QUIT|Quit) exit 0 ;;
 p|P) menu_use ;; 
 i|I|info|Info|INFO)
-#relay_info
+log_counter
+if [[ $log_count -le 100 ]] ; then
+echo -e "
+########################################################################################
+    
+    This next screen will show your Tor Relay status. 
+    
+    You can hit$cyan <control>-c$orange to make it stop.
+
+########################################################################################
+"
+enter_continue
+fi
+set_terminal_wider
+nyx
+tail_PID=$!
+trap 'kill $tail_PID' SIGINT #condition added to memory
+wait $tail_PID # code waits here for user to control-c
+trap - SIGINT # reset the trap so control-c works elsewhere.
 return 0 ;;
 
 *)
