207356e1e254498473788a4b59baff292445306a
Parman <armantheparman@gmail.com>
2023-11-12 14:01:10 +1100

    50

--- src/MENUS/menu_main.sh
+++ src/MENUS/menu_main.sh
@@ -1,6 +1,6 @@
 function menu_main {
 while true ; do
-set_terminal_bit_higher #custom function to resize the window
+set_terminal_custom 50
 source $original_dir/version.conf >/dev/null
 if [[ $vPatch -gt 9 ]] ; then space="" else space =" " ; fi
 # if statements in the menu printout makes the menu dynamic, ie changes accoring to the
