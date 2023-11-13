96fd9eb4595210f1027cf9d0b68a0a1041cd6833
Parman <armantheparman@gmail.com>
2023-11-13 22:02:45 +1100

    a

--- src/electrs/electrs_better_4pi.sh
+++ src/electrs/electrs_better_4pi.sh
@@ -3,7 +3,7 @@ function electrs_better_4pi {
 if [[ $chip == "arm64" || $chip == "aarch64" || $chip == "armv6l" || $chip == "armv7l" ]] ; then
 while true ; do
 set_terminal
-announce "It's best for Raspberry Pi's to use electrs insteat of Fulcrum" \
+echo -e "It's best for Raspberry Pi's to use electrs insteat of Fulcrum" \
 "Continue Fulcrum installation?     y     or     n"
 read choice
 
