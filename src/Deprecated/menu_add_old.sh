51ec314b3121d44227867bc1da67229db3a8d821
Parman <armantheparman@gmail.com>
2023-11-13 21:43:39 +1100

    fulcrum bug

--- src/Deprecated/menu_add_old.sh
+++ src/Deprecated/menu_add_old.sh
@@ -174,8 +174,8 @@ function electrs_better_4pi {
 while true ; do
 if [[ $chip == "arm64" || $chip == "aarch64" || $chip == "armv6l" || $chip == "armv7l" ]] ; then
 set_terminal
-announce "It's best for Raspberry Pi's to use electrs insteat of Fulcrum" \
-"Abort Fulcrum installation?     y     or     n"
+echo -e "It's best for Raspberry Pi's to use electrs insteat of Fulcrum" \
+"Continue Fulcrum installation?     y     or     n"
 read choice
 
 case $choice in 
