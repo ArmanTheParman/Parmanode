8591fa29495d727b562098f529423855ccff3a33
Parman <armantheparman@gmail.com>
2023-11-15 03:36:41 +1100

    a

--- src/patches/fix_parmanode_conf.sh
+++ src/patches/fix_parmanode_conf.sh
@@ -1,7 +1,7 @@
 function fix_parmanode_conf {
 delete_line "$dp/parmanode.conf" "nodejs-end" >/dev/null
 if grep -q rtl_tor < $dp/parmanode.conf ; then
-delete_line "rtl_tor"
+delete_line "$dp/parmanode.conf" "rtl_tor"
 parmanode_conf_add "rtl_tor=true"
 fi
 }
