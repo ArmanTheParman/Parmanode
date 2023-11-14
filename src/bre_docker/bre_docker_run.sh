c7e668e9d76dff29c5a11d7a6e0a5ed06d6f64cd
Parman <armantheparman@gmail.com>
2023-11-15 02:40:14 +1100

    a

--- src/bre_docker/bre_docker_run.sh
+++ src/bre_docker/bre_docker_run.sh
@@ -1,17 +1,17 @@
 function bre_docker_run {
 
-# if [[ $OS == Linux ]] ; then
-# docker run -d --name bre \
-#      -v $HOME/parmanode/bre:/home/parman/parmanode/bre \
-#      --network="host" \
-#      bre || return 1
-# fi
+if [[ $OS == Linux ]] ; then
+docker run -d --name bre \
+     -v $HOME/parmanode/bre:/home/parman/parmanode/bre \
+     --network="host" \
+     bre || return 1
+fi
 
-# if [[ $OS == Mac ]] ; then
+if [[ $OS == Mac ]] ; then
 docker run -d --name bre \
      -v $HOME/parmanode/bre:/home/parman/parmanode/bre \
      -p 3002:3002 \
      bre || return 1
-# fi
+fi
 
 }
