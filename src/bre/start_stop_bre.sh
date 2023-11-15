185b06883f2b84a290ad3e4647b14cd9bf73a904
Parman <armantheparman@gmail.com>
2023-11-15 15:18:55 +1100

    a

--- src/bre/start_stop_bre.sh
+++ src/bre/start_stop_bre.sh
@@ -13,10 +13,17 @@ return 1
 
 function restart_bre {
 if [[ $computer_type == Pi || $OS == Mac ]] ; then bre_docker_restart ; return 0 ; fi
+debug "bypassed Pi"
 stop_bre ; start_bre
 }
 
 function check_config_bre {
+
+if [[ $computer_type == Pi || $OS == Mac ]] ; then
+local file="$HOME/parmanode/bre/.env"
+else
+local file="$HOME/parmanode/btc-rpc-explorer/.env"
+fi
 
 #btc_authentication in parmanode.conf is either "user/pass" or "cookie"
 
@@ -28,19 +35,13 @@ if ! cat ~/.parmanode/parmanode.conf | grep -q btc_authentication ; then #if the
 fi
 
 btc_auth=$(cat ~/.parmanode/parmanode.conf | grep btc_authentication | cut -d = -f 2 ) #get value on right side of =
-if cat $HOME/parmanode/btc-rpc-explorer/.env | grep COOKIE= ; then bre_auth=cookie ; fi
-if cat $HOME/parmanode/btc-rpc-explorer/.env | grep USER= ; then bre_auth="user/pass" ; fi
+if cat $file | grep COOKIE= ; then bre_auth=cookie ; fi
+if cat $file | grep USER= ; then bre_auth="user/pass" ; fi
 
 if [[ $btc_auth == "cookie" && $bre_auth == "cookie" ]] ; then return 0 ; fi #settings match, can proceed
 if [[ $btc_auth == "user/pass" && $bre_auth == "user/pass" ]] ; then return 0 ; fi #settings match, can proceed
 
 # if code reaches here, changes need to be made.
-
-if [[ $computer_type == Pi || $OS == Mac ]] ; then
-local file="$HOME/parmanode/bre/.env"
-else
-local file="$HOME/parmanode/btc-rpc-explorer/.env"
-fi
 
 if [[ $btc_auth == "cookie" ]] ; then
     delete_line "$file" "USER=" 
