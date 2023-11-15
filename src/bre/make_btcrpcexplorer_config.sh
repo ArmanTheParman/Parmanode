7b3a7bad275a5ae79bd520cdfc7e2e64f6fa3282
Parman <armantheparman@gmail.com>
2023-11-15 15:37:17 +1100

    a

--- src/bre/make_btcrpcexplorer_config.sh
+++ src/bre/make_btcrpcexplorer_config.sh
@@ -1,28 +1,27 @@
 function make_btcrpcexplorer_config {
-
+local file="$HOME/parmanode/btc-rpc-explorer/.env"
 source ~/.bitcoin/bitcoin.conf >/dev/null
 source ~/.parmanode/parmanode.conf >/dev/null
-
-if cat ~/.parmanode/installed.conf | grep -q electrs-end ; then eserver="tcp://127.0.0.1:50005" 
-else eserver="tcp://127.0.0.1:50001"
-fi
+eserver1="tcp://127.0.0.1:50001"
+eserver2="tcp://127.0.0.1:50005" 
 
 if [[ $fast_computer == "yes" ]] ; then
-    echo "BTCEXP_SLOW_DEVICE_MODE=false" > $HOME/parmanode/btc-rpc-explorer/.env 
+    echo "BTCEXP_SLOW_DEVICE_MODE=false" > $file 
 else
-    echo "BTCEXP_SLOW_DEVICE_MODE=true" > $HOME/parmanode/btc-rpc-explorer/.env 
+    echo "BTCEXP_SLOW_DEVICE_MODE=true" > $file
 fi
 if [[ $btc_authentication == "user/pass" ]] ; then
-    echo "BTCEXP_BITCOIND_USER=$rpcuser" >> $HOME/parmanode/btc-rpc-explorer/.env 
-    echo "BTCEXP_BITCOIND_PASS=$rpcpassword" >> $HOME/parmanode/btc-rpc-explorer/.env 
+    echo "BTCEXP_BITCOIND_USER=$rpcuser" >> $file
+    echo "BTCEXP_BITCOIND_PASS=$rpcpassword" >> $file
 else
-    echo "BTCEXP_BITCOIND_COOKIE=$HOME/.bitcoin/.cookie" >> $HOME/parmanode/btc-rpc-explorer/.env 
+    echo "BTCEXP_BITCOIND_COOKIE=$HOME/.bitcoin/.cookie" >> $file
 fi
 
-echo "BTCEXP_BITCOIND_RPC_TIMEOUT=50000" >> $HOME/parmanode/btc-rpc-explorer/.env 
-echo "BTCEXP=0.0.0.0" >> $HOME/parmanode/btc-rpc-explorer/.env 
-echo "BTCEXP_ADDRESS_API=electrumx" >> $HOME/parmanode/btc-rpc-explorer/.env 
-echo "BTCEXP_ELECTRUMX_SERVERS=$eserver" >> $HOME/parmanode/btc-rpc-explorer/.env 
-echo "BTCEXP_NO_RATES=false" >> $HOME/parmanode/btc-rpc-explorer/.env 
+echo "BTCEXP_BITCOIND_RPC_TIMEOUT=50000" >> $file
+echo "BTCEXP=0.0.0.0" >> $file
+echo "BTCEXP_ADDRESS_API=electrumx" >> $file
+echo "BTCEXP_ELECTRUMX_SERVERS=$eserver1" >> $file
+echo "BTCEXP_ELECTRUMX_SERVERS=$eserver2" >> $file
+echo "BTCEXP_NO_RATES=false" >> $file
 }
 
--- src/bre_docker/bre_docker_modify_env.sh
+++ src/bre_docker/bre_docker_modify_env.sh
@@ -12,7 +12,6 @@ unset rpcuser rpcpassword
 source $HOME/.bitcoin/bitcoin.conf >/dev/null 2>&1
 source $HOME/.parmanode/parmanode.conf >/dev/null 2>&1
 if [[ -z $rpcuser || -z $rpcpassword ]] ; then  
-
     delete_line "$file" "BITCOIND_USER" 
     delete_line "$file" "BTCEXP_BITCOIND_PASS"
     echo "BTCEXP_BITCOIND_COOKIE=$HOME/.bitcoin/.cookie" | tee -a $file >/dev/null 2>&1
