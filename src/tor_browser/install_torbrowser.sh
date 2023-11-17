156fd8abde1dd2beea3a617c55c1f66425b02a7f
Parman <armantheparman@gmail.com>
2023-11-17 21:15:44 +1100

    torb

--- src/tor_browser/install_torbrowser.sh
+++ src/tor_browser/install_torbrowser.sh
@@ -1,6 +1,22 @@
-# function install_torbrowser {
+function install_torbrowser {
 
-# #sudo apt install software-properties-common
-# #sudo add-apt-repository ppa:micahflee/ppa
+cd $hp
+curl -LO https://github.com/TheTorProject/gettorbrowser/releases/download/linux64-12.5.1/tor-browser-linux64-12.5.1_ALL.tar.xz
+installed_conf_add "torb-start"
+curl -LO https://github.com/TheTorProject/gettorbrowser/releases/download/linux64-12.5.1/tor-browser-linux64-12.5.1_ALL.tar.xz.asc 
 
-# }
+if gpg --verify --status-fd 1 tor*asc 2>&1 | grep -q GOOD ; then
+announce "GPG verification passed."
+esle
+announce "GPG verification failed. Aborting."
+return 1 
+fi
+
+tar -xvf tor* 
+cd tor-browser/Browser
+sudo chmod +x start-tor-browser.desktop
+
+installed_conf_add "torb-end"
+success "Tor Browser" "being installed"
+./start-tor-browser.desktop
+}
