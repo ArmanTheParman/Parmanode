7c81372c05d4d1877ff67bd98309f1d28c11a417
Parman <armantheparman@gmail.com>
2023-11-19 01:38:02 +1100

    smolbug

--- src/patches/patch_3.sh
+++ src/patches/patch_3.sh
@@ -5,7 +5,7 @@ if grep -q electrum-end < $dp/installed.conf || \
    grep -q specter-end  < $dp/installed.conf || \
    grep -q bitbox-end   < $dp/installed.conf || \
    grep -q trezor-end   < $dp/installed.conf || \
-   grep -q ledger-end   < $dp/installed.conf || ; then
+   grep -q ledger-end   < $dp/installed.conf ; then
 
     if ! grep -q udev-end <$dp/installed.conf ; then
         installed_conf_add "udev-end"
