08d173e020dfeca2fe2b7c700db0ce9f534ada50
Parman <armantheparman@gmail.com>
2023-11-19 01:37:35 +1100

    idk

--- src/patches/patch_3.sh
+++ src/patches/patch_3.sh
@@ -1,11 +1,11 @@
 function patch_3 {
 
 if grep -q electrum-end < $dp/installed.conf || \
-   grep -q sparrow-end < $dp/installed.conf  || \
-   grep -q specter-end < $dp/installed.conf  || \
-   grep -q bitbox-end < $dp/installed.conf  || \
-   grep -q trezor-end < $dp/installed.conf  || \
-   grep -q ledger-end < $dp/installed.conf  || ; then
+   grep -q sparrow-end  < $dp/installed.conf || \
+   grep -q specter-end  < $dp/installed.conf || \
+   grep -q bitbox-end   < $dp/installed.conf || \
+   grep -q trezor-end   < $dp/installed.conf || \
+   grep -q ledger-end   < $dp/installed.conf || ; then
 
     if ! grep -q udev-end <$dp/installed.conf ; then
         installed_conf_add "udev-end"
