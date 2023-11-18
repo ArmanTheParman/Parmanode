fa2b4838a50fff9b92b9619c2785d785485bbafd
Parman <armantheparman@gmail.com>
2023-11-19 01:40:55 +1100

    nohup trezor

--- src/trezor/run_trezor.sh
+++ src/trezor/run_trezor.sh
@@ -5,7 +5,7 @@ open /Applications/"Trezor Suite.app"
 fi
 
 if [[ $OS == Linux ]] ; then
-~/parmanode/trezor/Trezor*AppImage & >/dev/null
+nohup ~/parmanode/trezor/Trezor*AppImage & >/dev/null
 fi
 
 }
