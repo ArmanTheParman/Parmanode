5d405c8a90cbd3c7f5a02356ca927dfc5cfb0e1f
Parman <armantheparman@gmail.com>
2023-11-18 22:11:58 +1100

    sed tor lnd adjustment

--- src/MENUS/menu_lnd.sh
+++ src/MENUS/menu_lnd.sh
@@ -295,7 +295,7 @@ local file=$HOME/.lnd/lnd.conf
 
 if grep -q tlsextraip < $file ; then
 if [[ $(cat $file | grep tlsextraip | wc -l) == 1 ]] ; then #if string found only once
-sed -i '/^tlsextraip/s/^; tlsextraip//' $file
+sed -i '/^tlsextraip/s/^/; /' $file
 debug "after sed"
 else
 announce "Unexpectedly found 'tlsextraip' more than once in lnd.conf.
@@ -307,7 +307,7 @@ fi
 
 if grep -q externalip < $file ; then
 if [[ $(cat $file | grep externalip | wc -l) == 1 ]] ; then #if string found only once
-sed -i '/^externalip/s/^; externalip//' $file
+sed -i '/^externalip/s/^/; /' $file
 else
 announce "Unexpectedly found 'externalip' more than once in lnd.conf.
     Abandoning automated modification to avoid errors."
@@ -319,7 +319,7 @@ delete_line "$file" "tlsextradomain=mydomain.com"
 
 if grep -q tlsextradomain < $file ; then
 if [[ $(cat $file | grep tlsextradomain | wc -l) == 1 ]] ; then #if string found only once
-sed -i '/^tlsextradomain/s/^; tlsextradomain//' $file
+sed -i '/^tlsextradomain/s/^/; /' $file
 else
 announce "Unexpectedly found 'tlsextradomain' more than once in lnd.conf.
     Abandoning automated modification to avoid errors."
