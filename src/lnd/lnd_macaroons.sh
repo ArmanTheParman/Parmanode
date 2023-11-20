663c6a960758dea6e052d51e16ae94d364a8077a
Parman <armantheparman@gmail.com>
2023-11-20 16:39:30 +1100

    macaroon for lnd menu

---
+++ src/lnd/lnd_macaroons.sh
@@ -0,0 +1,35 @@
+function lnd_macaroons {
+set_terminal ; echo -e "
+########################################################################################
+
+    You are about to have the macaroons for LND printed to the screen. This 
+    can be used to connect your BTC Pay Server to your LND node. Please be aware,
+    this is sensitive text and if not kept private, other people can control your
+    LND node.
+   $pink 
+    Close the curtains and stop the screen sharing.
+$orange
+########################################################################################
+"
+enter_abort || return 1
+
+lnd_macaroon=$(xxd -p -c 256 $HOME/.lnd/data/chain/bitcoin/mainnet/admin.macaroon | tr -d '\n')
+lnd_certthumbprint=$(openssl x509 -noout -fingerprint -sha256 -in $HOME/.lnd/tls.cert | sed -e 's/.*=//;s/://g')
+
+set_terminal ; echo -e "
+########################################################################################
+
+    The LND macaroon is:
+$cyan
+    $lnd_macaroon
+$orange
+    The certthumbprint is:
+$green
+    $lnd_certthumbprint $orange
+
+    You can use this to set up BTC Pay server to connect to LND by the 'REST proxy'
+
+########################################################################################
+"
+enter_continue
+}
