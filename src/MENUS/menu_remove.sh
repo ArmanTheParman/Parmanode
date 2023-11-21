d4cfb647ab5c21af71cad68470ae8e735da4cd84
Parman <armantheparman@gmail.com>
2023-11-21 22:48:59 +1100

    fi

--- src/MENUS/menu_remove.sh
+++ src/MENUS/menu_remove.sh
@@ -7,7 +7,7 @@ while true ; do
 local num=$(cat $dp/installed.conf | wc -l)
 local num=$(( 42 + (num/2 - 14) ))
 if [[ $num -lt 45 ]] ; then num=45 ; fi
-if [[ $num -gt 44 ]] ; then num=$(( (num -45)*2 + num ))
+if [[ $num -gt 44 ]] ; then num=$(( (num -45)*2 + num )) ; fi
 if [[ $num -gt 66 ]] ; then num=66 ; fi
 set_terminal_custom $num 
 unset bitcoinmenu fulcrummenu dockermenu btcpaymenu lnbitsmenu tormenu lndmenu mempoolmenu 
