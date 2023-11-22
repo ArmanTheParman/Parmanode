dd455f393a70021ad6237b7d518e50f028a301cd
Parman <armantheparman@gmail.com>
2023-11-23 01:34:25 +1100

    parmanode drive

--- src/drive_functions/dd_wipe_drive.sh
+++ src/drive_functions/dd_wipe_drive.sh
@@ -123,7 +123,7 @@ if [[ $OS == "Linux" ]] ; then
     # the yes command prints the string over and over, and given to the dd command
     # The wiping will involved 1 megabyte x 250 times (as per variables below) and the string will
     # be writting in the space. Wiping the entire drive like this takes too long.
-    if echo $disk | grep -q dev ; then debug "In dd_wipe_drive, problem with disk variable, no 'dev' component. 
+    if echo $disk | grep -q dev ; then debug "In dd_wipe_drive, previously problem with disk variable, no 'dev' component. 
         disk is $disk"  
     fi
     
