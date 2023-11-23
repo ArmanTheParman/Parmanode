07932fe50aa52c1c6584885f155fae2080110def
Arman The Parman <77603167+ArmanTheParman@users.noreply.github.com>
2023-11-23 19:41:25 +1100

    Update rp_count.sh

--- src/general_functions/rp_count.sh
+++ src/general_functions/rp_count.sh
@@ -1,4 +1,4 @@
-function rp_count {
+function rp_counter {
 rp_count=$((rp_count + 1))
 parmanode_conf_remove "rp_count" #necessary because cleanup function inside parmanode_conf_add
                                  #detects what is being added, and deletes first in case there 
