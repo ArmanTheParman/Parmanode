97a5dff0149b849123e13f038a8016ee233bf72a
Parman <armantheparman@gmail.com>
2023-11-24 22:07:14 +1100

    debug

--- src/config/overview_conf.sh
+++ src/config/overview_conf.sh
@@ -1,9 +1,6 @@
 function overview_conf_add {
-overview_conf_remove "${1}"
+delete_line "$oc" "${1}"
 debug "overview conf add"
 echo "${1}" | tee -a $oc >/dev/null
-}
-
-function overview_conf_remove {
-delete_line "$oc" "${1}"
+debug "look, ${1}"
 }
