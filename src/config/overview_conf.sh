893c88032502575a38c4610f781621dca5411f29
Parman <armantheparman@gmail.com>
2023-11-26 22:44:24 +1100

    a

--- src/config/overview_conf.sh
+++ src/config/overview_conf.sh
@@ -1,3 +1,6 @@
 function overview_conf_add {
+if [[ -n $2 ]] ; then
+delete_line "$oc" "$2"
+fi
 echo "${1}" | tee -a $oc >/dev/null
 }
--- src/parmanode/runningoverview.sh
+++ src/parmanode/runningoverview.sh
@@ -19,10 +19,10 @@ function isbitcoinrunning {
 unset bitcoinrunning
 if [[ $OS == Mac ]] ; then
     if pgrep Bitcoin-Q >/dev/null ; then 
-    overview_conf_add "bitcoinrunning=true"
+    overview_conf_add "bitcoinrunning=true" "bitcoinrunning="
     export bitcoinrunning=true
     else 
-    overview_conf_add "bitcoinrunning=false"
+    overview_conf_add "bitcoinrunning=false" "bitcoinrunning="
     export bitcoinrunning=false
     fi
     return 0
@@ -31,18 +31,16 @@ fi
 if [[ $OS == Linux ]] ; then
 
     if ! ps -x | grep bitcoind | grep -q "bitcoin.conf" >/dev/null 2>&1 ; then 
-    overview_conf_add "bitcoinrunning=false"
+    overview_conf_add "bitcoinrunning=false" "bitcoinrunning="
     fi
    
 #override...
     if tail -n 1 $HOME/.bitcoin/debug.log | grep -q  "Shutdown: done" ; then 
-    delete_line "$oc" "bitcoinrunning="
-    overview_conf_add "bitcoinrunning=false"
+    overview_conf_add "bitcoinrunning=false" "bitcoinrunning="
     fi
 #override...
     if pgrep bitcoind >/dev/null 2>&1 ; then 
-    delete_line "$oc" "bitcoinrunning="
-    overview_conf_add "bitcoinrunning=true"
+    overview_conf_add "bitcoinrunning=true" "bitcoinrunning="
     fi
 
     return 0
@@ -52,54 +50,54 @@ fi
 function islndrunning {
 unset lndrunning
 if ps -x | grep lnd | grep bin >/dev/null 2>&1 ; then
-overview_conf_add "lndrunning=true"
+overview_conf_add "lndrunning=true" "lndrunning="
 else
-overview_conf_add "lndrunning=false"
+overview_conf_add "lndrunning=false" "lndrunning="
 fi
 
 if lncli walletbalance >/dev/null 2>&1 ; then 
-overview_conf_add "lndwallet=locked"
+overview_conf_add "lndwallet=locked" "lndrunning="
 else
-overview_conf_add "lndwallet=unlocked"
+overview_conf_add "lndwallet=unlocked" "lndrunning="
 fi
 }
 
 function isfulcrumrunning {
 if [[ $OS == "Linux" ]] ; then
     if ps -x | grep fulcrum | grep conf >/dev/null 2>&1 ; then 
-    overview_conf_add "fulcrumrunning=true"
+    overview_conf_add "fulcrumrunning=true" "fulcrumrunning="
     else
-    overview_conf_add "fulcrumrunning=false"
+    overview_conf_add "fulcrumrunning=false" "fulcrumrunning="
     fi 
 fi #end if Linux
 
 if [[ $OS == "Mac" ]] ; then
     if docker ps 2>/dev/null | grep -q fulcrum && docker exec -it fulcrum bash -c "pgrep Fulcrum" >/dev/null 2>&1 ; then
-    overview_conf_add "fulcrumrunning=true"
+    overview_conf_add "fulcrumrunning=true" "fulcrumrunning="
     else
-    overview_conf_add "fulcrumrunning=false"
+    overview_conf_add "fulcrumrunning=false" "fulcrumrunning="
     fi
 fi
 }
 function iselectrsrunning {
 if grep -q electrs- < $ic >/dev/null 2>&1 ; then
     if ps -x | grep electrs | grep -q conf >/dev/null 2>&1 ; then 
-    overview_conf_add "electrsrunning=true"
+    overview_conf_add "electrsrunning=true" "electrsrunning="
     else
-    overview_conf_add "electrsrunning=false"
+    overview_conf_add "electrsrunning=false" "electrsrunning="
     fi
 fi #end if grep
 
 }
 function iselectrsdkrrunning {
 if ! docker ps 2>/dev/null | grep -q electrs ; then
-overview_conf_add "electrsdkrrunning=false"
+overview_conf_add "electrsdkrrunning=false" "electrsdkrrunning="
 return 1
 fi
 if docker exec -it electrs /home/parman/parmanode/electrs/target/release/electrs --version >/dev/null 2>&1 ; then
-overview_conf_add "electrsdkrrunning=true"
+overview_conf_add "electrsdkrrunning=true" "electrsdkrrunning="
 else
-overview_conf_add "electrsdkrrunning=false"
+overview_conf_add "electrsdkrrunning=false" "electrsdkrrunning="
 fi
 }
 
@@ -107,21 +105,21 @@ function isbrerunning {
 
 if [[ $computer_type == LinuxPC ]] ; then
     if sudo systemctl status btcrpcexplorer 2>&1 | grep -q "active (running)" >/dev/null 2>&1 ; then 
-    overview_conf_add "brerunning=true"
+    overview_conf_add "brerunning=true" "brerunning="
     else
-    overview_conf_add "brerunning=false"
+    overview_conf_add "brerunning=false" "brerunning="
     fi
 fi
 
 if [[ $OS == Mac || $computer_type == Pi ]] ; then
     if  docker ps 2>/dev/null | grep -q bre ; then 
         if docker exec -itu root bre /bin/bash -c 'ps -xa | grep "btc-rpc"' | grep -v grep >/dev/null 2>&1 ; then
-        overview_conf_add "brerunning=true"
+        overview_conf_add "brerunning=true" "brerunning="
         else
-        overview_conf_add "brerunning=false"
+        overview_conf_add "brerunning=false" "brerunning="
         fi
     else
-    overview_conf_add "brerunning=false"
+    overview_conf_add "brerunning=false" "brerunning="
     fi
 fi
 
@@ -129,18 +127,18 @@ fi
 
 function isbtcpayrunning {
 if docker ps 2>/dev/null | grep -q btcp >/dev/null 2>&1 ; then 
-overview_conf_add "btcpayrunning=true"
+overview_conf_add "btcpayrunning=true" "btcpayrunning="
 else
-overview_conf_add "btcpayrunning=false"
+overview_conf_add "btcpayrunning=false" "btcpayrunning="
 fi
 }
 
 function isrtlrunning {
 
 if docker ps 2>/dev/null | grep -q rtl ; then
-overview_conf_add "rtlrunning=true"
+overview_conf_add "rtlrunning=true" "rtlrunning="
 else 
-overview_conf_add "rtlrunning=false"
+overview_conf_add "rtlrunning=false" "rtlrunning="
 fi
 
 }
