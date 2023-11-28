1e17bc96abfdfe9d3562618f5519605a1c603075
Parman <armantheparman@gmail.com>
2023-11-29 03:25:53 +1100

    smolbug

--- src/qbittorrent/start_qbittorrent.sh
+++ src/qbittorrent/start_qbittorrent.sh
@@ -3,7 +3,7 @@ function start_qbittorrent {
 if [[ $OS == Mac ]] ; then
 open /Applications/qbittorrent.app 
 
-elif [[ $computer_type == Linux ]] ; then
+elif [[ $computer_type == LinuxPC ]] ; then
 nohup $hp/qbittorrent/qbittorrent*AppImage >dev/null 2>&1 &
 
 else
