a919473f3ed0266b5cdd5761c4c2254f23a705a2
Parman <armantheparman@gmail.com>
2023-12-06 17:12:09 +1100

    installtion continues

--- src/mempool/install_mempool.sh
+++ src/mempool/install_mempool.sh
@@ -12,6 +12,61 @@ git clone --depth 1 https://github.com/mempool/mempool.git
 installed_config_add "mempool-start"
 
 #set variables
+cp $pn/src/mempool/docker-compose.yml $hp/mempool/docker
+
+choose_mempool_backend
+choose_mempool_LND
+choose_mempool_tor
+
+installed_conf_add "mempool-end"
+success "Mempool" "being installed"
+}
+
+
+function choose_mempool_backend {
+
+while true ; do
+
+set_terminal ; echo -e "
+########################################################################################
+
+    What would you like Mempool to connect to for its data?
+
+
+            1)      Bitcoin Core
+
+            2)      Electrs
+
+            3)      Fulcrum
+
+            4)      Manual (can put details of server on another machine)
+
+########################################################################################
+"
+choose "xpmq" ; read choice
+case $choice in
+q|Q) exit ;;
+p|P) return 1 ;;
+m|M) back2main ;;
+1)
+
+
+;;
+2)
+
+
+;;
+3)
+
+
+;;
+4)
+
+;;
+*) invalid ;;
+esac
+done
+
 
 
 }
