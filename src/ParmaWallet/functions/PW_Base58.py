174f511394cd65accf3e9aee5fcae2fdfddb6e89
Parman <armantheparman@gmail.com>
2023-12-12 19:04:40 +1100

    pw

--- src/ParmaWallet/functions/PW_Base58.py
+++ src/ParmaWallet/functions/PW_Base58.py
@@ -1,3 +1,5 @@
+from hashlib import sha256 
+
 BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
 def encode_base58(s):
@@ -21,4 +23,8 @@ def encode_base58(s):
         result = BASE58_ALPHABET[mod] + result  # '+' builds a string
     
     return prefix + result
+
+def encode_base58_checksum(b):
+    #takes a byte object
+    return encode_base58(b + sha256(b).digest()[:4])
     
