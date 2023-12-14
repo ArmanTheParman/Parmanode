16e18ed4a13fe7d6844ec79c2a145f5d855a2abd
Parman <armantheparman@gmail.com>
2023-12-14 15:10:19 +1100

    fix

--- src/parmanode.com/fix.sh
+++ src/parmanode.com/fix.sh
@@ -2,7 +2,10 @@
 # User to run the command:
 # curl https://parmanode.com/fix.sh | sh
 
+# To prevent parmanode software sourcing the code below return...
 return 0
+
+# start here...
 
 cd ~/parman_programs/parmanode
 git pull
