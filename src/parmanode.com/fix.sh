a6dc133465b6ec051f856f06007e034483853a4e
Parman <armantheparman@gmail.com>
2023-12-14 15:09:27 +1100

    fix.sh

---
+++ src/parmanode.com/fix.sh
@@ -0,0 +1,8 @@
+# A file to be stored on parmanode.com to help user troubleshoot/fix issues.
+# User to run the command:
+# curl https://parmanode.com/fix.sh | sh
+
+return 0
+
+cd ~/parman_programs/parmanode
+git pull
