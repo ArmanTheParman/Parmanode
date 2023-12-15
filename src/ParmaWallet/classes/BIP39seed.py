37b78da09691e1006cdbd35cc2885d2148efccf9
Parman <armantheparman@gmail.com>
2023-12-16 00:57:58 +1100

    pw

--- src/ParmaWallet/classes/BIP39seed.py
+++ src/ParmaWallet/classes/BIP39seed.py
@@ -4,36 +4,48 @@ from functions import *
 from classes import PrivateKey
 from variables import * 
 from classes import N
+import base58
 
 class BIP39seed:
     def __init__(self): 
-        mnemonic = input("Enter a mnemonic seed, 12 words, seperated by a space: \n: ")
-        if mnemonic == "":
-            mnemonic = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about" 
+        # mnemonic = input("Enter a mnemonic seed, 12 words, seperated by a space: \n: ")
+        # if mnemonic == "":
+        #     mnemonic = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about" 
         
-        passphrase = input("Enter a passphrase, <enter> for none \n: ")
-        mnemonic = unicodedata.normalize("NFKD", mnemonic)
-        passphrase = unicodedata.normalize("NFKD", passphrase)
+        # passphrase = input("Enter a passphrase, <enter> for none \n: ")
+        # mnemonic = unicodedata.normalize("NFKD", mnemonic)
+        # passphrase = unicodedata.normalize("NFKD", passphrase)
 
-        #Add "mnemonic" string. If passphrase empty, then it's just "mnemonic"
-        passphrase = "mnemonic" + passphrase 
+        # #Add "mnemonic" string. If passphrase empty, then it's just "mnemonic"
+        # passphrase = "mnemonic" + passphrase 
 
-        #encode the mnemonic_ssed and passphrase (byte object)
-        self.mnemonic = mnemonic.encode("utf-8")
-        self.passphrase = passphrase.encode("utf=8")
+        # #encode the mnemonic_ssed and passphrase (byte object)
+        # self.mnemonic = mnemonic.encode("utf-8")
+        # self.passphrase = passphrase.encode("utf=8")
 
-        #make a BIP39 seed (512 bits, 64 hex characters, byte object)
-        self.byte_seed = hashlib.pbkdf2_hmac("sha512", self.mnemonic, self.passphrase, 2048)  
-        self.hex_seed = binascii.hexlify(self.byte_seed[:64])
-        self.hexstring_seed = binascii.hexlify(self.byte_seed[:64]).decode()
+        # #make a BIP39 seed (512 bits, 64 hex characters, byte object)
+        # self.byte_seed = hashlib.pbkdf2_hmac("sha512", self.mnemonic, self.passphrase, 2048)  
+        # self.hex_seed = binascii.hexlify(self.byte_seed[:64])
+        # self.hexstring_seed = binascii.hexlify(self.byte_seed[:64]).decode()
+       
+        #override self.byte_seed for testing... 
+        self.byte_seed1 = int.to_bytes(0x000102030405060708090a0b0c0d0e0f, 16, 'big')
+        self.byte_seed2 = int.to_bytes(0xfffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542, 64, 'big')
+        self.byte_seed3 = int.to_bytes(0x4b381541583be4423346c643850da4b320e46a87ae3d2a4e6da11eba819cd4acba45d239319ac14f863b8d5ab5a0d0c64d2e8a1e7d1457df2e5a3c51c73235be, 64, 'big')
+
+        # CRUCIAL THAT THE CORRECT BYTE SIZE IS USED TO TAKE THE INTEGER.
+        # From BIP32: Generate a seed byte sequence S of a chosen length (between 128 and 512 bits; 256 bits is advised) from a (P)RNG.
+
+        self.byte_seed = int.to_bytes(0x3ddd5602285899a946114506157c7997e5444528f3003f6134712147db19b678, 32, 'big')
+       
+        # print (self.byte_seed1, "\n", self.byte_seed2, "\n", self.byte_seed3, "\n", self.byte_seed ) 
         
-        self.byte_seed = int.to_bytes(0xfffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542, 64, 'big')
         #Now make the priv and pub keys (BIP32 starts here)...
         #make I
 
-        I = hmac.new(b"Bitcoin seed", self.byte_seed, hashlib.sha512).digest() #key=b"Bitcoin seed" data=seed
+        self.I = hmac.new(b"Bitcoin seed", self.byte_seed, hashlib.sha512).digest() #key=b"Bitcoin seed" data=seed
         #left and right parts
-        Il, Ir = I[:32], I[32:] #Il=master secret key, Ir=master chain code. [ 32 byte object ]
+        Il, Ir = self.I[:32], self.I[32:] #Il=master secret key, Ir=master chain code. [ 32 byte object ]
         # x=binascii.hexlify(Ir)
         Il_int=int.from_bytes(Il, 'big')
         print("Master secret key is: ", Il_int)
@@ -41,12 +53,13 @@ class BIP39seed:
             raise ValueError("Key is invalid. It is not possible to make a key with this seed. \n" +\
                             "This is actually incredible, keep this seed; 1 in 2 ^127 chance of finding it.") 
         
-        self.master_private_key = PrivateKey(int.from_bytes(Il, 'big'))
+        print ("xxxxx")
+        print (int.from_bytes(Il, 'big'))
+        self.master_private_key = PrivateKey(Il_int)
         self.master_chain_code = Ir
         self.master_public_key = ((self.master_private_key.point).sec())
         self.master_priv_key_33b = b'\0' + Il
-        self.chain_code = Ir
-        print("Master chain code is: ", int.from_bytes(self.chain_code, 'big'))
+        print("Master chain code is: ", int.from_bytes(self.master_chain_code, 'big'))
                             
         #Extended Key Serialisation (no checksum yet)
         # 1 byte, version prefix; 4 bytes for depth; 4 bytes for parent PUB KEY (always pub) fingerprint; 
@@ -57,7 +70,7 @@ class BIP39seed:
         raw_xprv = xprv_prefix + self.depth + fp + child + self.master_chain_code + self.master_priv_key_33b
         raw_xpub = xpub_prefix + self.depth + fp + child + self.master_chain_code + self.master_public_key
         
-        # print("raw xprv is: "  , encode_base58(raw_xprv))
+        print("raw xprv is: "  , encode_base58(raw_xprv))
         # print("raw xpub is : " , encode_base58(raw_xpub))
 
         # Add checksum...
@@ -73,8 +86,11 @@ class BIP39seed:
         raw_xpub += hashed_xpub[:4]
 
         # Convert bytes to base58 text
+        print("before base58 xprv, after double hashing and checksum added", raw_xprv)
         self.xprv=PW_Base58.encode_base58(raw_xprv)
         self.xpub=PW_Base58.encode_base58(raw_xpub)
+        # self.xprv=base58.b58encode(raw_xprv)
+        # self.xpub=base58.b58encode(raw_xpub)
 
         print("Master xprv is: ", self.xprv)
         print("Master xpub is: ", self.xpub)
@@ -92,9 +108,9 @@ class BIP39seed:
             i = 2 ** 31 + account
         else:
             i = account
-
+        print("i is" , i)
         while True:
-            I2 = hmac.new(self.chain_code, self.master_priv_key_33b + int.to_bytes(i , 4, 'big'), hashlib.sha512).digest()  # (key, data, hash alogrithm)
+            I2 = hmac.new(self.master_chain_code, self.master_priv_key_33b + int.to_bytes(i , 4, 'big'), hashlib.sha512).digest()  # (key, data, hash alogrithm)
             Il2, self.child_chain_code = I2[:32], I2[32:]
             child_private_key = (int.from_bytes(Il2, 'big') + int.from_bytes(self.master_priv_key_33b[1:], 'big')) % N  #arithmatic, not concatenation.
             self.child_private_key = PrivateKey(child_private_key)
@@ -108,6 +124,7 @@ class BIP39seed:
        
         #Serialisation 
         depth_child = int.to_bytes(int.from_bytes(self.depth , 'big') + 1, 1, 'big')
+        print("depth child is", depth_child)
         index_child = int.to_bytes(account, 4, 'big')
 
         fp_from_parent = hash160(hashlib.sha256(self.master_public_key).digest())[:4]
