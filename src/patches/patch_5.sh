9741712e5da2bfcd39552816bcabdf532852ea7e
Parman <armantheparman@gmail.com>
2023-12-17 01:13:35 +1100

    p5

--- src/patches/patch_5.sh
+++ src/patches/patch_5.sh
@@ -3,6 +3,10 @@ debug "patch 5"
 
 mv $hp/*arrow-1.* $hp/Sparrow >/dev/null 2>&1
 
+if [[ -e $HOME/parman_programs/set_terminal ]] ; then
+rm -rf $HOME/parman_programs/set_terminal
+fi
+
 if [[ ! -e $hm ]] ; then touch $hm ; fi
 
 parmanode_conf_remove "patch="
