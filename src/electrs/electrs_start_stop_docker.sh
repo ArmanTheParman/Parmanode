65014c768f0a5715063318f5fbbf0fbfdc6110f5
Parman <armantheparman@gmail.com>
2023-12-17 17:24:27 +1100

    fix electrs log docker

--- src/electrs/electrs_start_stop_docker.sh
+++ src/electrs/electrs_start_stop_docker.sh
@@ -1,6 +1,6 @@
 function docker_start_electrs {
 if docker ps >/dev/null 2>&1 ; then
-docker exec -it electrs /bin/bash -c "/home/parman/parmanode/electrs/target/release/electrs --conf /home/parman/.electrs/config.toml"
+docker exec -it electrs /bin/bash -c "/home/parman/parmanode/electrs/target/release/electrs --conf /home/parman/.electrs/config.toml >> $HOME/.parmanode/run_electrs.log 2>&1"
 return 0
 else
 announce "docker not running. Aborting." 
