c50a5361d3f0871c8e46d852d6b6faaefde45662
Parman <armantheparman@gmail.com>
2023-12-23 14:34:04 +1100

    song

---
+++ src/ParmaWallet/Song/helper.py
@@ -0,0 +1,121 @@
+from unittest import TestCase, TestSuite, TextTestRunner
+
+import hashlib
+
+
+SIGHASH_ALL = 1
+SIGHASH_NONE = 2
+SIGHASH_SINGLE = 3
+BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+
+def run(test):
+    suite = TestSuite()
+    suite.addTest(test)
+    TextTestRunner().run(suite)
+
+
+def hash160(s):
+    '''sha256 followed by ripemd160'''
+    return hashlib.new('ripemd160', hashlib.sha256(s).digest()).digest()
+
+
+def hash256(s):
+    '''two rounds of sha256'''
+    return hashlib.sha256(hashlib.sha256(s).digest()).digest()
+
+
+def encode_base58(s):
+    # determine how many 0 bytes (b'\x00') s starts with
+    count = 0
+    for c in s:
+        if c == 0:
+            count += 1
+        else:
+            break
+    # convert to big endian integer
+    num = int.from_bytes(s, 'big')
+    prefix = '1' * count
+    result = ''
+    while num > 0:
+        num, mod = divmod(num, 58)
+        result = BASE58_ALPHABET[mod] + result
+    return prefix + result
+
+
+def encode_base58_checksum(s):
+    return encode_base58(s + hash256(s)[:4])
+
+
+def decode_base58(s):
+    num = 0
+    for c in s:
+        num *= 58
+        num += BASE58_ALPHABET.index(c)
+    combined = num.to_bytes(25, byteorder='big')
+    checksum = combined[-4:]
+    if hash256(combined[:-4])[:4] != checksum:
+        raise ValueError('bad address: {} {}'.format(checksum, hash256(combined[:-4])[:4]))
+    return combined[1:-4]
+
+
+def little_endian_to_int(b):
+    '''little_endian_to_int takes byte sequence as a little-endian number.
+    Returns an integer'''
+    return int.from_bytes(b, 'little')
+
+
+def int_to_little_endian(n, length):
+    '''endian_to_little_endian takes an integer and returns the little-endian
+    byte sequence of length'''
+    return n.to_bytes(length, 'little')
+
+
+def read_varint(s):
+    '''read_varint reads a variable integer from a stream'''
+    i = s.read(1)[0]
+    if i == 0xfd:
+        # 0xfd means the next two bytes are the number
+        return little_endian_to_int(s.read(2))
+    elif i == 0xfe:
+        # 0xfe means the next four bytes are the number
+        return little_endian_to_int(s.read(4))
+    elif i == 0xff:
+        # 0xff means the next eight bytes are the number
+        return little_endian_to_int(s.read(8))
+    else:
+        # anything else is just the integer
+        return i
+
+
+def encode_varint(i):
+    '''encodes an integer as a varint'''
+    if i < 0xfd:
+        return bytes([i])
+    elif i < 0x10000:
+        return b'\xfd' + int_to_little_endian(i, 2)
+    elif i < 0x100000000:
+        return b'\xfe' + int_to_little_endian(i, 4)
+    elif i < 0x10000000000000000:
+        return b'\xff' + int_to_little_endian(i, 8)
+    else:
+        raise ValueError('integer too large: {}'.format(i))
+
+
+class HelperTest(TestCase):
+
+    def test_little_endian_to_int(self):
+        h = bytes.fromhex('99c3980000000000')
+        want = 10011545
+        self.assertEqual(little_endian_to_int(h), want)
+        h = bytes.fromhex('a135ef0100000000')
+        want = 32454049
+        self.assertEqual(little_endian_to_int(h), want)
+
+    def test_int_to_little_endian(self):
+        n = 1
+        want = b'\x01\x00\x00\x00'
+        self.assertEqual(int_to_little_endian(n, 4), want)
+        n = 10011545
+        want = b'\x99\xc3\x98\x00\x00\x00\x00\x00'
+        self.assertEqual(int_to_little_endian(n, 8), want)
---
+++ src/ParmaWallet/Song/script.py
@@ -0,0 +1,154 @@
+from io import BytesIO
+from logging import getLogger
+from unittest import TestCase
+
+from helper import (
+    encode_varint,
+    int_to_little_endian,
+    little_endian_to_int,
+    read_varint,
+)
+from op import (
+    OP_CODE_FUNCTIONS,
+    OP_CODE_NAMES,
+)
+
+
+LOGGER = getLogger(__name__)
+
+
+# tag::source1[]
+class Script:
+
+    def __init__(self, cmds=None):
+        if cmds is None:
+            self.cmds = []
+        else:
+            self.cmds = cmds  # <1>
+    # end::source1[]
+
+    def __repr__(self):
+        result = []
+        for cmd in self.cmds:
+            if type(cmd) == int:
+                if OP_CODE_NAMES.get(cmd):
+                    name = OP_CODE_NAMES.get(cmd)
+                else:
+                    name = 'OP_[{}]'.format(cmd)
+                result.append(name)
+            else:
+                result.append(cmd.hex())
+        return ' '.join(result)
+
+    # tag::source4[]
+    def __add__(self, other):
+        return Script(self.cmds + other.cmds)  # <1>
+    # end::source4[]
+
+    # tag::source2[]
+    @classmethod
+    def parse(cls, s):
+        length = read_varint(s)  # <2>
+        cmds = []
+        count = 0
+        while count < length:  # <3>
+            current = s.read(1)  # <4>
+            count += 1
+            current_byte = current[0]  # <5>
+            if current_byte >= 1 and current_byte <= 75:  # <6>
+                n = current_byte
+                cmds.append(s.read(n))
+                count += n
+            elif current_byte == 76:  # <7>
+                data_length = little_endian_to_int(s.read(1))
+                cmds.append(s.read(data_length))
+                count += data_length + 1
+            elif current_byte == 77:  # <8>
+                data_length = little_endian_to_int(s.read(2))
+                cmds.append(s.read(data_length))
+                count += data_length + 2
+            else:  # <9>
+                op_code = current_byte
+                cmds.append(op_code)
+        if count != length:  # <10>
+            raise SyntaxError('parsing script failed')
+        return cls(cmds)
+    # end::source2[]
+
+    # tag::source3[]
+    def raw_serialize(self):
+        result = b''
+        for cmd in self.cmds:
+            if type(cmd) == int:  # <1>
+                result += int_to_little_endian(cmd, 1)
+            else:
+                length = len(cmd)
+                if length < 75:  # <2>
+                    result += int_to_little_endian(length, 1)
+                elif length > 75 and length < 0x100:  # <3>
+                    result += int_to_little_endian(76, 1)
+                    result += int_to_little_endian(length, 1)
+                elif length >= 0x100 and length <= 520:  # <4>
+                    result += int_to_little_endian(77, 1)
+                    result += int_to_little_endian(length, 2)
+                else:  # <5>
+                    raise ValueError('too long an cmd')
+                result += cmd
+        return result
+
+    def serialize(self):
+        result = self.raw_serialize()
+        total = len(result)
+        return encode_varint(total) + result  # <6>
+    # end::source3[]
+
+    # tag::source5[]
+    def evaluate(self, z):
+        cmds = self.cmds[:]  # <1>
+        stack = []
+        altstack = []
+        while len(cmds) > 0:  # <2>
+            cmd = cmds.pop(0)
+            if type(cmd) == int:
+                operation = OP_CODE_FUNCTIONS[cmd]  # <3>
+                if cmd in (99, 100):  # <4>
+                    if not operation(stack, cmds):
+                        LOGGER.info('bad op: {}'.format(OP_CODE_NAMES[cmd]))
+                        return False
+                elif cmd in (107, 108):  # <5>
+                    if not operation(stack, altstack):
+                        LOGGER.info('bad op: {}'.format(OP_CODE_NAMES[cmd]))
+                        return False
+                elif cmd in (172, 173, 174, 175):  # <6>
+                    if not operation(stack, z):
+                        LOGGER.info('bad op: {}'.format(OP_CODE_NAMES[cmd]))
+                        return False
+                else:
+                    if not operation(stack):
+                        LOGGER.info('bad op: {}'.format(OP_CODE_NAMES[cmd]))
+                        return False
+            else:
+                stack.append(cmd)  # <7>
+        if len(stack) == 0:
+            return False  # <8>
+        if stack.pop() == b'':
+            return False  # <9>
+        return True  # <10>
+    # end::source5[]
+
+
+class ScriptTest(TestCase):
+
+    def test_parse(self):
+        script_pubkey = BytesIO(bytes.fromhex('6a47304402207899531a52d59a6de200179928ca900254a36b8dff8bb75f5f5d71b1cdc26125022008b422690b8461cb52c3cc30330b23d574351872b7c361e9aae3649071c1a7160121035d5c93d9ac96881f19ba1f686f15f009ded7c62efe85a872e6a19b43c15a2937'))
+        script = Script.parse(script_pubkey)
+        want = bytes.fromhex('304402207899531a52d59a6de200179928ca900254a36b8dff8bb75f5f5d71b1cdc26125022008b422690b8461cb52c3cc30330b23d574351872b7c361e9aae3649071c1a71601')
+        self.assertEqual(script.cmds[0].hex(), want.hex())
+        want = bytes.fromhex('035d5c93d9ac96881f19ba1f686f15f009ded7c62efe85a872e6a19b43c15a2937')
+        self.assertEqual(script.cmds[1], want)
+
+    def test_serialize(self):
+        want = '6a47304402207899531a52d59a6de200179928ca900254a36b8dff8bb75f5f5d71b1cdc26125022008b422690b8461cb52c3cc30330b23d574351872b7c361e9aae3649071c1a7160121035d5c93d9ac96881f19ba1f686f15f009ded7c62efe85a872e6a19b43c15a2937'
+        script_pubkey = BytesIO(bytes.fromhex(want))
+        script = Script.parse(script_pubkey)
+        self.assertEqual(script.serialize().hex(), want)
