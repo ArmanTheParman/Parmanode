3ef8882fcbe018f399c2f1aeaff3a210035dcfc1
Parman <armantheparman@gmail.com>
2023-12-24 21:12:11 +1100

    text

--- src/ParmaWallet/recovery.py
+++ src/ParmaWallet/recovery.py
@@ -5,4 +5,4 @@ import hmac, hashlib, ecdsa, os, unicodedata, binascii
 
 pubkey_target = 0x024ee3afc0eb0abb9f8bfc2b67e394a44fdc97eabe08f0e3d88e1dd9c76371949c
 derivation_target = "m/84'/0'/0'/0/1"
-target_mnemonic = input("please type in seed seperated with spaces:")
+target_mnemonic = input("please type in seed seperated with spaces: ")
