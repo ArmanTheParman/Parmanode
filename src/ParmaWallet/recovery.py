e5fdf2bb45e5a02be52611d91bf05139fad006a2
Parman <armantheparman@gmail.com>
2023-12-26 03:21:08 +1100

    pw

--- src/ParmaWallet/recovery.py
+++ src/ParmaWallet/recovery.py
@@ -10,11 +10,6 @@ from classes import N
 import base58
 from ecdsa import SECP256k1
        
-# pubkey_target = 0x024ee3afc0eb0abb9f8bfc2b67e394a44fdc97eabe08f0e3d88e1dd9c76371949c
-# derivation_target = "m/84'/0'/0'/0/1"
-# target_mnemonic = input("please type in seed seperated with spaces: ")
-# passphrase_target = "a"
-
 a = BIP32_master_node()
 b = child_key(a, depth=1, account=49, hardened=True, serialize=True) #purpose
 c = child_key(b, depth=1, account=0, hardened=True, serialize=True) #coin
