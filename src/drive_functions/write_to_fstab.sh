560b159c279bb0c6c4964e126cfbc4797c5ee387
Parman <armantheparman@gmail.com>
2023-12-27 18:13:10 +1100

    write to fstab function, $TYPE

--- src/drive_functions/write_to_fstab.sh
+++ src/drive_functions/write_to_fstab.sh
@@ -1,8 +1,9 @@
 function write_to_fstab {
    UUID="$1"  
    if [ -z $UUID ] ; then debug "no UUID" ; return 1 ; fi
-   
-        echo "UUID=$UUID /media/$(whoami)/parmanode ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab > /dev/null 2>&1
+        TYPE=$(blkid | grep $UUID | awk '{print $5}' | cut -d \" -f 2)
+        debug "TYPE detected, $TYPE"
+        echo "UUID=$UUID /media/$(whoami)/parmanode $TYPE defaults,nofail 0 2" | sudo tee -a /etc/fstab > /dev/null 2>&1
         log "bitcoin" "fstab grep output for parmanode:" && \
         grep "parmanode" /etc/fstab >> $HOME/.parmanode/bitcoin.log     
 
