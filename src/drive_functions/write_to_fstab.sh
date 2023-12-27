71f4b5a8e9f0225c2757d1f8bd151db161923c37
Parman <armantheparman@gmail.com>
2023-12-27 19:44:42 +1100

    fstab uid gid

--- src/drive_functions/write_to_fstab.sh
+++ src/drive_functions/write_to_fstab.sh
@@ -3,18 +3,15 @@ function write_to_fstab {
    if [ -z $UUID ] ; then debug "no UUID" ; return 1 ; fi
         TYPE=$(blkid | grep $UUID | awk '{print $5}' | cut -d \" -f 2)
         debug "TYPE detected, $TYPE"
-        echo "UUID=$UUID /media/$(whoami)/parmanode $TYPE defaults,nofail 0 2" | sudo tee -a /etc/fstab > /dev/null 2>&1
+        echo "UUID=$UUID /media/$(whoami)/parmanode $TYPE defaults,nofail,uid=$(id -u),gid=$(id -g) 0 2" | sudo tee -a /etc/fstab > /dev/null 
         log "bitcoin" "fstab grep output for parmanode:" && \
         grep "parmanode" /etc/fstab >> $HOME/.parmanode/bitcoin.log     
-
 }
 
 function write_to_fstab2 {
-
 export $(sudo blkid -o export $disk) >/dev/null
 
 if [ -z $UUID ] ; then debug "no UUID" ; return 1 ; fi
 delete_line "/etc/fstab" "$UUID"
-echo "UUID=$UUID /media/$(whoami)/parmanode $TYPE defaults,nofail 0 2" | sudo tee -a /etc/fstab >/dev/null 2>&1
-
+echo "UUID=$UUID /media/$(whoami)/parmanode $TYPE defaults,nofail,uid=$(id -u),gid=$(id -g) 0 2" | sudo tee -a /etc/fstab >/dev/null 
 }
