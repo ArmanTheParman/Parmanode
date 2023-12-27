382c07f52d06042710ac93744560c64ac088d3d7
Parman <armantheparman@gmail.com>
2023-12-27 14:57:42 +1100

    debug

--- src/MENUS/menu_electrs_docker.sh
+++ src/MENUS/menu_electrs_docker.sh
@@ -20,6 +20,7 @@ fi
 if docker exec -it electrs /home/parman/parmanode/electrs/target/release/electrs --version >/dev/null 2>&1 ; then
 electrs_version=$(docker exec -it electrs /home/parman/parmanode/electrs/target/release/electrs --version | tr -d '\r' 2>/dev/null )
 log_size=$(docker exec -it electrs /bin/bash -c "ls -l $logfile | awk '{print \$5}' | grep -oE [0-9]+" 2>/dev/null)
+log_size=$(echo $log_size | tr -d '\r\n')
 fi
 debug "$log_size"
 set_terminal_custom 50
