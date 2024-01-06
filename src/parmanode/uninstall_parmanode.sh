5681af0a2eac2e3d7196c8c6fa2c1d13250f27a2
Parman <armantheparman@gmail.com>
2024-01-06 15:25:17 +1100

    parmashell uninstall text

--- src/parmanode/uninstall_parmanode.sh
+++ src/parmanode/uninstall_parmanode.sh
@@ -149,8 +149,12 @@ crontab -l | sed '/parmanode/d' | crontab - >/dev/null ; clear
 #remove bashrc/zshrc additions
 delete_line "$HOME/.bashrc" "Parmanode..." >/dev/null 2>&1
 delete_line "$HOME/.bashrc" "function rp {" >/dev/null 2>&1
+delete_line "$HOME/.bashrc" "safe to delete" >/dev/null 2>&1
+delete_line "$HOME/.bashrc" "parmashell_functions" >/dev/null 2>&1
 delete_line "$HOME/.zshrc" "Parmanode..." >/dev/null 2>&1
 delete_line "$HOME/.zshrc" "function rp {" >/dev/null 2>&1
+delete_line "$HOME/.zshrc" "safe to delete" >/dev/null 2>&1
+delete_line "$HOME/.zshrc" "parmashell_functions" >/dev/null 2>&1
 
 
 
