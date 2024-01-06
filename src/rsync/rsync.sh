464b04b4bf1cd043eb9eeeaead556e9f2897e731
Parman <armantheparman@gmail.com>
2024-01-07 00:28:33 +1100

    text

--- src/rsync/rsync.sh
+++ src/rsync/rsync.sh
@@ -214,6 +214,8 @@ $pink
         - If the destination directory is a remote computer, you can add '-e ssh' after the options and
           then add username@IP_address: before the destination directory. Right after the ':' add a '/'
           and type out the full path.
+        - add --progress for extra information as it does it's thing.
+        - add --checksum if you want each file's data to be checked for exactness (slower)
 $orange
 ##############################################################################################################
 "
