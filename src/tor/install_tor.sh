97f5e03db045af433f5947e05ec1277836e17af4
Parman <armantheparman@gmail.com>
2024-01-14 11:20:18 +1100

    tor install detection fixed

--- src/tor/install_tor.sh
+++ src/tor/install_tor.sh
@@ -1,10 +1,10 @@
 function install_tor {
 
-set_terminal ; echo "
+set_terminal ; echo -e "
 ########################################################################################
-
+$cyan
                                    Install Tor
-    
+$orange    
     Parmanode will install Tor daemon (not browser). It runs in the background allowing
     you to access the Tor network.
 
@@ -18,26 +18,27 @@ case $choice in Q|q|Quit|QUIT) exit 0 ;; p|P) return 1 ;; *) true ;; esac
 
 set_terminal
 
-if [[ $OS == "Linux" ]] ; then sudo apt-get install tor -y ; enter_continue || errormessage && return 1 ; fi
+if [[ $OS == "Linux" ]] ; then { sudo apt-get install tor -y ; installed_config_add "tor-end" ; enter_continue ; return 0 ; } || errormessage && return 1 ; fi
+
 if [[ $OS == "Mac" ]] ; then
 brew_check Tor || return 1
 brew install tor && brew services start tor || { log "tor" "failed at tor install && start tor" ; errormessage ; return 1 ; }
 fi
 
-set_terminal ; echo "
+set_terminal ; echo -e "
 ########################################################################################
-
-    Tor should now be installed and running in the background. It also starts up
+$cyan
+    Tor $orange should now be installed and running in the background. It also starts up
     again when your computer restarts.
 
     Linux users can stop Tor from the terminal with :
-
+$yellow
             sudo systemctl stop tor
-
+$orange
     Mac users can stop Tor from the terminal with :
-
+$yellow
             brew services stop tor
-
+$orange
 ########################################################################################
 "
 installed_config_add "tor-end"
