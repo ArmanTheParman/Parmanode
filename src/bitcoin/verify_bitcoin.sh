cec7b1be495b5bdd648eb43ad6a52b41f48a609c
Parman <armantheparman@gmail.com>
2024-01-15 21:18:35 +1100

    remove old debug function

--- src/bitcoin/verify_bitcoin.sh
+++ src/bitcoin/verify_bitcoin.sh
@@ -32,8 +32,6 @@ gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys E777299FC265DD04793070EB
 curl https://raw.githubusercontent.com/bitcoin-core/guix.sigs/main/builder-keys/laanwj.gpg | gpg --import >/dev/null 2>&1
 curl https://raw.githubusercontent.com/bitcoin-core/guix.sigs/main/builder-keys/Emzy.gpg | gpg --import >/dev/null 2>&1
 
-debug_user "pause here and report back."
-
     if gpg --verify --status-fd 1 SHA256SUMS.asc 2>&1 | grep -q GOOD
     then
         echo ""
