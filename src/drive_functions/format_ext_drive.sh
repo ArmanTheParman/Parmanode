beac4caf662ddd928d316acb72a45240624f807d
Parman <armantheparman@gmail.com>
2024-01-20 17:17:14 +1100

    confirm format

--- src/drive_functions/format_ext_drive.sh
+++ src/drive_functions/format_ext_drive.sh
@@ -3,24 +3,31 @@ debug "skip formatting variable = $skip_formatting"
 if [[ $skip_formatting == true ]] ; then 
 log "importdrive" "skipped formatting" ; return 0 ; fi
 #quit if internal drive chosen
-if [[ $1 == "Bitcoin" && $drive == "internal" ]] ; then return 0 ; fi
+if [[ $1 == "Bitcoin" && $drive == "internal" ]] ; then return 0 ; fik
 if [[ $1 == "Fulcrum" && $drive_fulcrum == "internal" ]] ; then return 0 ; fi
 if [[ $1 == "electrs" && $drive_electrs == "internal" ]] ; then return 0 ; fi
+debug "passed internal drive choice"
 
 
+#Check if external drive selected for other programs, and warn user.
 if [[ $1 != justFormat ]] ; then
+debug "in not justFormat"
 
-#quit if external drive set for either of the other programs that use this function
 #parenteses added once for readability, but not required as && takes precedence over || ,so logic doesn't change
 if [[ ( $1 == "Bitcoin" && $drive == "external" ) && ( $drive_fulcrum == "external" || $drive_electrs == "external" ) ]] ; then 
-skip_formatting=true
+confirm_format "Bitcoin" || skip_formatting=true
 fi
+
 if [[ ( $1 == "Fulcrum" && $drive_fulcrum == "external" ) && ( $drive == "external" || $drive_electrs == "external" ) ]] ; then 
-skip_formatting=true
+confirm_format "Fulcrum" || skip_formatting=true
 fi
+
 if [[ ( $1 == "electrs" && $drive_electrs == "external" ) && ( $drive == "external" || $drive_fulcrum == "external" ) ]] ; then 
-skip_formatting=true
+confirm_format "electrs" || skip_formatting=true
 fi
+
+debug "check external drive selected for other programs done.
+skip formatting is = $skip_formatting"
 
 if [[ $skip_formatting == true || $bitcoin_drive_import == true ]] ; then 
     return 0 
@@ -105,4 +112,36 @@ if [[ $OS == "Linux" ]] ; then
         enter_continue
 fi
 return 0
+}
+
+
+function confirm_format {
+
+while true ; do
+clear ; echo -e "
+########################################################################################
+
+    Parmanode has detected that you previously have chosen an external drive for
+    programs other than $1 
+
+    Please confirm if you really want to format the drive?
+$red
+            y)      Yes, format it. Format it good.
+$green
+            n)      Nooooo, I was wrong. Skip formating
+$orange
+            a)      Abort everything
+
+########################################################################################
+"
+choose "xpmq"
+read choice
+case $choice in
+q|Q) exit 0 ;; a|A|p|P|m|M) back2main ;;  
+y|Y) return 0 ;;
+n|N) return 1 ;;
+*) invalid ;;
+esac
+done
+
 }
