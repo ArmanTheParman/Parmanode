a4cf973603d175b9ed5419581c140e23a2a90c34
Parman <armantheparman@gmail.com>
2024-01-23 18:04:57 +1100

    colours

--- src/sparrow/verify_sparrow.sh
+++ src/sparrow/verify_sparrow.sh
@@ -6,10 +6,10 @@ if ! gpg --verify sparrow*.asc sparrow*.txt >/dev/null 2>&1 ; then
 set_terminal ; echo "GPG verification failed. Aborting. Contact Parman for help." ; enter_continue ; return 1 ; fi
 
 if which sha256sum >/dev/null ; then
-    if ! sha256sum --ignore-missing --check *parrow*.txt ; then echo "Checksum failed. Aborting. Contact Parman for help" 
+    if ! sha256sum --ignore-missing --check *parrow*.txt ; then echo -e "Checksum$red failed$orange. Aborting. Contact Parman for help." 
     enter_continue ; return 1 ; fi
 else
-    if ! shasum -a 256 --check *parrow*.txt | grep -q OK ; then echo "Checksum failed. Aborting. Contact Parman for help" 
+    if ! shasum -a 256 --check *parrow*.txt | grep -q OK ; then echo "Checksum$red failed$orange. Aborting. Contact Parman for help." 
     enter_continue ; return 1 ; fi
 fi
 
