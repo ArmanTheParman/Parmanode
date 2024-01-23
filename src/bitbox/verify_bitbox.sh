19f77c7cdefdf323f81c798868d81a9fc485f112
Parman <armantheparman@gmail.com>
2024-01-23 17:06:15 +1100

    bitbox update verify

--- src/bitbox/verify_bitbox.sh
+++ src/bitbox/verify_bitbox.sh
@@ -2,7 +2,7 @@ function verify_bitbox {
 
 curl https://bitbox.swiss/download/shiftcryptosec-509249B068D215AE.gpg.asc | gpg --import
 
-if ! gpg --verify --status-fd 1 SHA256SUMS.asc  2>&1 | grep -q GOOD ; then
+if ! gpg --verify --status-fd 1 B*.asc  2>&1 | grep -q GOOD ; then
 announce "gpg verification failed. Aborting."
 return 1
 else
