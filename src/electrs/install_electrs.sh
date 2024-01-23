4f6c8648acc97c9fd0347aadcf70693c7c5976ef
Parman <armantheparman@gmail.com>
2024-01-24 08:39:32 +1100

    debug

--- src/electrs/install_electrs.sh
+++ src/electrs/install_electrs.sh
@@ -99,7 +99,7 @@ else #if [[ $electrs_compile == "true" ]] ; then
     set_terminal ; please_wait
     build_dependencies_electrs || return 1 
     download_electrs && log "electrs" "download_electrs success" 
-    compile_electrs || { debug "compile failed." ; return 1 ; } 
+    compile_electrs || return 1
     log "electrs" "compile_electrs done" 
 
 fi
