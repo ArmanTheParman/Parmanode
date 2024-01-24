c8b66d9b63735a3ca778610e25d3d470bb1729ff
Parman <armantheparman@gmail.com>
2024-01-25 03:44:32 +1100

    delete option 6

--- src/bitcoin/choose_bitcoin_version.sh
+++ src/bitcoin/choose_bitcoin_version.sh
@@ -24,15 +24,13 @@ $red
        5)  Guided compile Knots Bitcoin (Luke Dashr Jr's version of Bitcoin Core,
            which also has FILTER-ORDINALS patch) version v25.1.knots20231115
 $red
-       6)  BYO Bitcoin binary installation (imports to Parmanode) - not available yet
-
-       7)  Guided compile of most recent Github update, ie pre-release
+       6)  Guided compile of most recent Github update, ie pre-release
            (for testing only)
 $red
-       8)  Read how to compile yourself, and import the installation to Parmanode. 
+       7)  Read how to compile yourself, and import the installation to Parmanode. 
            You can come back to this menu after selecting this. 
 $green       
-       9)  IMPORT binaries you have created yourself or previously downloaded without
+       8)  IMPORT binaries you have created yourself or previously downloaded without
            the help of the Parmanode install process.
 $orange
 ########################################################################################   
@@ -59,20 +57,14 @@ export bitcoin_compile=true ; export version=choose ; export ordinals_patch=true
 parmanode_conf_add "bitcoin_choice=knots"
 export knotsbitcoin=true ; export version="v25.1.knots20231115" ; break ;;
 6)
-parmanode_conf_add "bitcoin_choice=byo"
-clear ; echo "not available yet" ; sleep 3 
-continue
-# export byo_bitcoin=true ; break 
-;;
-7)
 parmanode_conf_add "bitcoin_choice=compiled"
 export bitcoin_compile=true ; export version=latest ; break ;;
-8)
+7)
 bitcoin_compile_instructions
 return 0
 ;;
 
-9)
+8)
 set_terminal ; echo -e "
 ########################################################################################
   Make sure the Bitcoin binary files have been placed in the /usr/local/bin/ directory
