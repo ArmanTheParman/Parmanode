31c9d1cd36d44a794e2836df351a7ff117e6962b
Parman <armantheparman@gmail.com>
2024-01-27 21:25:23 +1100

    fix mempool

--- src/mempool/install_mempool.sh
+++ src/mempool/install_mempool.sh
@@ -17,6 +17,10 @@ fi
 if ! grep -q bitcoin-end < $HOME/.parmanode/installed.conf ; then
 announce "Need to install Bitcoin first from Parmanode menu. Aborting." ; return 1 ; fi
 
+if ! grep -q "rpcallowip=172.18.0.0/16" < $bc >/dev/null 2>&1 ; then
+echo "rpcallowip=172.18.0.0/16" | tee sudo -a $bc >/dev/null 2>&1 ;
+fi
+
 if ! docker ps >/dev/null ; then announce "Please install Docker first from Parmanode Add/Other menu, and START it. Aborting." ; return 1 ; fi
 
 source $bc
@@ -51,6 +55,25 @@ mempool_backend
 
 cd $hp/mempool/docker 
 docker-compose up -d
+
+#Final check to make sure the docker gatway IP is included in bitcoin.conf
+string="$(docker network inspect docker_PM_network | grep Gateway | awk '{print $2}' | tr -d ' ' | tr -d \" | cut -d \. -f 1)"
+if [[ $string == 172 ]] ; then #would be unusualy for it not to be 172
+    #expecting 17 or 18
+    string2="$(docker network inspect docker_PM_network | grep Gateway | awk '{print $2}' | tr -d ' ' | tr -d \" | cut -d \. -f 2)"
+    target="172.$string2.0.0/16"
+    if ! grep "$target" < $bc >/dev/null 2>&1 ; then
+    echo "$target" | sudo tee -a $bc >/dev/null 2>&1
+    sudo systemctl restart bitcoind.service
+    restart_mempool
+    fi
+else
+    #even if the gateway does not start with 172, add it to bitcoin.conf
+    stringIP="$(docker network inspect docker_PM_network | grep Gateway | awk '{print $2}' | tr -d ' ' | tr -d \" )"
+    echo "$stringIP"/16 | sudo tee -a $bc >/dev/null 2>&1
+    sudo systemctl restart bitcoind.service
+    restart_mempool
+fi
 
 installed_conf_add "mempool-end"
 success "Mempool" "being installed"
