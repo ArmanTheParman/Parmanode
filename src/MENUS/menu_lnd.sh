661b8225225d23808d7350930d5e939f01f6e914
Parman <armantheparman@gmail.com>
2024-01-27 13:21:33 +1100

    text

--- src/MENUS/menu_lnd.sh
+++ src/MENUS/menu_lnd.sh
@@ -50,7 +50,7 @@ Clearnet URI:
 
 $(cat $dp/lndinfo.log | grep :973 | grep -v onion | cut -d \" -f 2)
 $orange
-Parmanode will not set up or detect port forwarding (allows others to connect to you)
+Parmanode will not set up nor detect port forwarding (allows others to connect to you)
 To set it up, forward port $lnd_port to IP: $IP
 $orange"
 fi
