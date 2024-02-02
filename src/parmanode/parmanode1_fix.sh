4357037a1cd8bf032c7e111f472753d3a77716fe
Parman <armantheparman@gmail.com>
2024-02-02 18:12:27 +1100

    mod parmanode1 fix

--- src/parmanode/parmanode1_fix.sh
+++ src/parmanode/parmanode1_fix.sh
@@ -2,20 +2,16 @@ function parmanode1_fix {
 
 if [[ -d /media/$USER/parmanode1 ]] ; then
 
+sudo umount /media/$USER/parmanode1 && sudo rm -rf /media/$USER/parmanode1
+
+
 set_terminal ; echo -e "
 ########################################################################################
 
     Parmanode has detected a drive mounting glitch, possibley due to previous
-    install errors.
-
-    There exists a directory called /media/$USER/parmanode1 indicated a previous
-    problem. If you are having issues you can do one of the following two things:
-
-        1) Uninstall Parmanode completely, unmount the parmanode drive, then look
-           /media/$USER/ and delete any directories containing the parmanode
-           name. Then reinstall Parmanode.
-
-        2) Ask Parman for help in the Telegram chat group.
+    install errors, or you connected a Parmanode drive that hasn't been properly
+    'imported' yet. You can go ahead and import the drive from the Parmanode tools
+    menu.
 
 ########################################################################################
 "
