aa6cb249320c1f0ada8b22f2424b5dfbda0aec14
Parman <armantheparman@gmail.com>
2024-02-03 16:50:06 +1100

    f

--- src/MENUS/menu_overview.sh
+++ src/MENUS/menu_overview.sh
@@ -162,6 +162,15 @@ for n in $(seq 1 9) ; do
 if [[ $menub$n == false ]] ; then unset "m$n" "s$n" "r$n" ; fi
 done
 
+if [[ $menub1 == false ]] ; then unset m1 s1 r1 ; fi
+if [[ $menub2 == false ]] ; then unset m2 s2 r2 ; fi
+if [[ $menub3 == false ]] ; then unset m3 s3 r3 ; fi
+if [[ $menub4 == false ]] ; then unset m4 s4 r4 ; fi
+if [[ $menub5 == false ]] ; then unset m5 s5 r5 ; fi
+if [[ $menub1 == false ]] ; then unset m6 s6 r6 ; fi
+if [[ $menub1 == false ]] ; then unset m7 s7 r7 ; fi
+if [[ $menub1 == false ]] ; then unset m8 s8 r8 ; fi
+if [[ $menub1 == false ]] ; then unset m9 s9 r9 ; fi
 
 x="$orange|$bright_blue"
 
