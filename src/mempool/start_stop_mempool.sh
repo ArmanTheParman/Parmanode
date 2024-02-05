74c925743068488df6670cb627c166daa8318b29
Parman <armantheparman@gmail.com>
2024-02-05 16:43:53 +1100

    cd -

--- src/mempool/start_stop_mempool.sh
+++ src/mempool/start_stop_mempool.sh
@@ -3,12 +3,14 @@ docker ps >/dev/null 2>&1 || { announce "Docker not running. Aborting." && retur
 
 cd $hp/mempool/docker
 docker-compose up -d 
+cd - >/dev/null
 }
 
 function stop_mempool {
 
 cd $hp/mempool/docker
 docker-compose stop 
+cd - >/dev/null
 }
 
 function restart_mempool {
@@ -19,5 +21,6 @@ cd $hp/mempool/docker
 docker-compose stop 
 docker-compose up -d
 debug "restart_mempool function about to exit"
+cd - >/dev/null
 
 }
