9eb14a829827b3557567ca4e4b9c357de48b1845
Parman <armantheparman@gmail.com>
2024-02-06 23:22:07 +1100

    ParmanodL_directories

--- src/ParmanodL/ParmanodL_directories.sh
+++ src/ParmanodL/ParmanodL_directories.sh
@@ -1,7 +1,7 @@
 function ParmanodL_directories {
 
 # Prepare directories
-    if [[ ! -d /mnt ]] ; then sudo mkdir /mnt ; fi
+    if [[ ! -d $mnt ]] ; then sudo mkdir $mnt ; fi
 	mkdir -p $HOME/ParmanodL 
 	mkdir -p $HOME/.parmanode # Borrowing parmanode's config directory
 
