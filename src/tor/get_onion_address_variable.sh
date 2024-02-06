91f51828475d0cc12eceb0d9e3b777fef45c0459
Parman <armantheparman@gmail.com>
2024-02-07 04:11:48 +1100

    $1

--- src/tor/get_onion_address_variable.sh
+++ src/tor/get_onion_address_variable.sh
@@ -1,36 +1,36 @@
 function get_onion_address_variable {
 
 if [[ $1 == "bitcoin" ]] ; then
-export ONION_ADDR="$(sudo cat /var/lib/tor/${1}-service/hostname 2>/dev/null)"
+export ONION_ADDR="$(sudo cat /var/lib/tor/bitcoin-service/hostname 2>/dev/null)"
 return 0
 fi
 
 if [[ $1 == "fulcrum" ]] ; then
-export ONION_ADDR_FULCRUM="$(sudo cat /var/lib/tor/$1-service/hostname 2>/dev/null)"
+export ONION_ADDR_FULCRUM="$(sudo cat /var/lib/tor/fulcrum-service/hostname 2>/dev/null)"
 return 0
 fi
 
 if [[ $1 == "bre" ]] ; then
-export ONION_ADDR_BRE="$(sudo cat /var/lib/tor/$1-service/hostname 2>/dev/null)" 
+export ONION_ADDR_BRE="$(sudo cat /var/lib/tor/bre-service/hostname 2>/dev/null)" 
 return 0
 fi
 
 if [[ $1 == "electrs" ]] ; then
-export ONION_ADDR_ELECTRS="$(sudo cat /var/lib/tor/$1-service/hostname 2>/dev/null)" 
+export ONION_ADDR_ELECTRS="$(sudo cat /var/lib/tor/electrs-service/hostname 2>/dev/null)" 
 return 0
 fi
 
 if [[ $1 == "rtl" ]] ; then
-export ONION_ADDR_RTL="$(sudo cat /var/lib/tor/$1-service/hostname 2>/dev/null)"
+export ONION_ADDR_RTL="$(sudo cat /var/lib/tor/rtl-service/hostname 2>/dev/null)"
 return 0
 fi
 
 if [[ $1 == "mempool" ]] ; then
-export ONION_ADDR_MEM="$(sudo cat /var/lib/tor/$1-service/hostname 2>/dev/null)"
+export ONION_ADDR_MEM="$(sudo cat /var/lib/tor/mempool-service/hostname 2>/dev/null)"
 return 0
 
 if [[ $1 == "ssh" ]] ; then
-export ONION_ADDR_SSH="$(sudo cat /var/lib/tor/$1-service/hostname)" 
+export ONION_ADDR_SSH="$(sudo cat /var/lib/tor/ssh-service/hostname 2>/dev/null)" 
 debug "pause"
 return 0
 fi
