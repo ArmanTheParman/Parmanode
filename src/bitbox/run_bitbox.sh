3397c9afcf14bd474e76fd0252691b84b2487c10
Parman <armantheparman@gmail.com>
2024-02-07 16:31:56 +1100

    text

--- src/bitbox/run_bitbox.sh
+++ src/bitbox/run_bitbox.sh
@@ -5,5 +5,14 @@ open /Applications/BitBox.app
 elif [[ $OS == Linux ]] ; then
 ~/parmanode/bitbox/Bit*AppImage >/dev/null 2>&1 &
 fi
+set_terminal ; echo -e "
+########################################################################################
+$cyan
+   BitBox App$orange will open in a moment in it's own window. You can continue to use
+   Parmanode as normal, or minimise it. 
+
+########################################################################################
+"
+enter_continue
 
 }
--- src/ledger/run_ledger.sh
+++ src/ledger/run_ledger.sh
@@ -7,5 +7,14 @@ fi
 if [[ $OS == Linux ]] ; then
 $HOME/parman_programs/ledger/*AppImage
 fi
+set_terminal ; echo -e "
+########################################################################################
+$cyan
+   Ledger Live$orange will open in a moment in it's own window. You can continue to use
+   Parmanode as normal, or minimise it. 
+
+########################################################################################
+"
+enter_continue
 
 }
