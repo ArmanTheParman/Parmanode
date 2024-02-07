49512abb24208b86ca3f8762e03ab6dd06d65599
Parman <armantheparman@gmail.com>
2024-02-07 18:10:13 +1100

    order bug

--- src/MENUS/menu_electrs.sh
+++ src/MENUS/menu_electrs.sh
@@ -281,8 +281,8 @@ elif [[ $bsync == false ]] ; then
 
     #in case an unexpected non-number string
     if ! echo $electrs_sync | grep -E '^[0-9]+$' ; then
-    export electrs_sync="SEE LOGS"
     echo "electrs sync: $electrs_sync" | tee -a $dp/electrs.log 
+    export electrs_sync="SEE LOGS"
     fi
 
 fi
