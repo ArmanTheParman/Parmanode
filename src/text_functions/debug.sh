cfcaa08ccc0f7e98ce7df605a7cd3d51c70c65fd
Parman <armantheparman@gmail.com>
2024-02-08 18:40:25 +1100

    d2

--- src/text_functions/debug.sh
+++ src/text_functions/debug.sh
@@ -13,7 +13,7 @@ echo -e "$1
 "
 log "debug" "${1}"
 enter_continue
-if [[ $enter_cont == q ]] ; then unset enter_cont ; exit 0 ;; fi
+if [[ $enter_cont == q ]] ; then unset enter_cont ; exit 0 ; fi
 return 0
 fi
 }
@@ -23,7 +23,7 @@ if [[ $debug == 2 ]] ; then
 echo -e "${1}"
 log "debug2" "${1}"
 enter_continue
-if [[ $enter_cont == q ]] ; then unset enter_cont ; exit 0 ;; fi
+if [[ $enter_cont == q ]] ; then unset enter_cont ; exit 0 ; fi
 return 0
 fi
 }
@@ -33,7 +33,7 @@ if [[ $debug == 3 ]] ; then
 echo -e "${1}"
 log "debug3" "${1}"
 enter_continue
-if [[ $enter_cont == q ]] ; then unset enter_cont ; exit 0 ;; fi
+if [[ $enter_cont == q ]] ; then unset enter_cont ; exit 0 ; fi
 return 0
 fi
 }
@@ -43,7 +43,7 @@ if [[ $debug == 4 ]] ; then
 echo -e "${1}"
 log "debug4" "${1}"
 enter_continue
-if [[ $enter_cont == q ]] ; then unset enter_cont ; exit 0 ;; fi
+if [[ $enter_cont == q ]] ; then unset enter_cont ; exit 0 ; fi
 return 0
 fi
 }
