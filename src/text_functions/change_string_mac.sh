ef89868845f6cafac6181a6124e1eb3d6a1d1c2c
Parman <armantheparman@gmail.com>
2024-02-11 14:27:43 +1100

    fi

--- src/text_functions/change_string_mac.sh
+++ src/text_functions/change_string_mac.sh
@@ -20,6 +20,7 @@ positionnewline="$4" #("before" or "after" or "swap" or "delete")
 #$5 signals with bs for a backslash at end of newline
 if [[ $5 == bs ]] ; then
 newline="$3\\\\" #I think 4 are needed because it resolves twice whenever newline is called, once here and once there.
+fi
 
 if [[ ! -f $inputfile ]] ; then # if file doesn't exist
 return 1
