2abd3d14cef456f17eec840c9ead7e3d0ba7869a
Parman <armantheparman@gmail.com>
2024-02-11 14:26:41 +1100

    swap string with backslash

--- src/text_functions/swap_string.sh
+++ src/text_functions/swap_string.sh
@@ -1,10 +1,12 @@
 # $1 file name 
 # $2 search string 
 # $3 change whole line to
+# $4 a way to add a backslash, expecting 'backslash'; added to end of newline
+alias string_swap='swap_string' >/dev/null
 
 function swap_string {
 if [[ $OS == "Mac" ]] ; then
-change_string_mac "$1" "$2" "$3" swap >> $HOME/.parmanode/change_string_mac.log 2>&1
+change_string_mac "$1" "$2" "$3" swap "$4" >> $HOME/.parmanode/change_string_mac.log 2>&1
 return 0
 fi
 
@@ -12,18 +14,18 @@ fi
 input_file="$1"
 search_string="$2"
 new_line="$3"
+if [[ $4 == bs ]] ; then
+newline="$3\\\\"
 
 if [[ ! -f "$input_file" ]]; then
-    echo "Error: $input_file does not exist."
+    echo -e "Error: $input_file does not exist."
     enter_continue
     return 1
 fi
 
 #every ocurranced swapped - just some log info...
-echo "swap string: $search_string new line: $new_line input file: $input_file" >> $HOME/.parmanode/sed.log 
+echo -e "swap string: $search_string new line: $new_line input file: $input_file" >> $HOME/.parmanode/sed.log 
 
 #be very careful modifying this...
 sudo sed -i "/$search_string/c\\$new_line" "$input_file" >> $HOME/.parmanode/sed.log 2>&1
-
 }
-alias string_swap='swap_string' 
