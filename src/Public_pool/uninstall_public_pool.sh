0d09222616ff8eb506093db706f28e6f44140df2
Parman <armantheparman@gmail.com>
2024-02-12 02:14:20 +1100

    menu urls

--- src/Public_pool/uninstall_public_pool.sh
+++ src/Public_pool/uninstall_public_pool.sh
@@ -18,7 +18,34 @@ if [[ $choice == "y" || $choice == "Y" ]] ; then true
     else 
     return 1
     fi
+#check Docker running, esp Mac
+if ! docker ps >/dev/null 2>&1 ; then echo -e "
+########################################################################################
 
+    Docker doesn't seem to be running. Please start it and, once it's running, hit $green 
+    <enter>$orange to continue.
+
+########################################################################################
+"
+choose "emq"
+read choice ; case $choice in Q|q) exit 0 ;; m|M) back2main ;; esac
+set_terminal
+if ! docker ps >/dev/null 2>&1 ; then echo -e "
+########################################################################################
+
+    Docker is still$red not running$orange. 
+
+    It can take a while to be in a 'ready state' even though you started it. Try
+    again later. 
+    
+    Aborting. 
+
+########################################################################################
+"
+enter_continue
+return 1
+fi
+fi
 stop_public_pool 
 docker rm public_pool public_pool_ui ; debug "containers stopped and removed"
 delete_line $bc "zmqpubrawblock=tcp://*:5000" >/dev/null 2>&1
