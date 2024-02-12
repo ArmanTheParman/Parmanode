46459cecab1aaf093ea6d4eb0b1793c8c294a7cc
Parman <armantheparman@gmail.com>
2024-02-12 13:46:33 +1100

    docker started checks added

--- src/bre_docker/bre_docker_start_stop.sh
+++ src/bre_docker/bre_docker_start_stop.sh
@@ -1,14 +1,42 @@
 function bre_docker_start {
+
+if ! docker ps >/dev/null 2>&1 ; then set_terminal ; echo -e "
+########################################################################################$red
+                              Docker is not running. $orange
+########################################################################################
+"
+enter_continue
+return 1
+fi
 check_config_bre || return 1
 debug "look"
 bre_docker_start_bre
 }
 
 function bre_docker_stop {
+
+if ! docker ps >/dev/null 2>&1 ; then set_terminal ; echo -e "
+########################################################################################$red
+                              Docker is not running. $orange
+########################################################################################
+"
+enter_continue
+return 1
+fi
 docker stop bre
 }
 
 function bre_docker_restart {
+
+if ! docker ps >/dev/null 2>&1 ; then set_terminal ; echo -e "
+########################################################################################$red
+                              Docker is not running. $orange
+########################################################################################
+"
+enter_continue
+return 1
+fi
+
 bre_docker_stop
 bre_docker_start
 }
--- src/fulcrum_mac/start_fulcrum_docker.sh
+++ src/fulcrum_mac/start_fulcrum_docker.sh
@@ -1,5 +1,13 @@
 function start_fulcrum_docker {
 
+if ! docker ps >/dev/null 2>&1 ; then set_terminal ; echo -e "
+########################################################################################$red
+                              Docker is not running. $orange
+########################################################################################
+"
+enter_continue
+return 1
+fi
 if ! docker ps -a | grep -q fulcrum ; then
     set_terminal
     echo "Fulcrum container does not exist."
@@ -12,6 +20,16 @@ fulrcum_docker_start_fulcrum
 }
 
 function fulrcum_docker_start_fulcrum {
+
+if ! docker ps >/dev/null 2>&1 ; then set_terminal ; echo -e "
+########################################################################################$red
+                              Docker is not running. $orange
+########################################################################################
+"
+enter_continue
+return 1
+fi
+
 docker exec -d fulcrum /bin/bash -c "/home/parman/parmanode/fulcrum/Fulcrum /home/parman/parmanode/fulcrum/config/fulcrum.conf \
     >>/home/parman/parmanode/fulcrum/fulcrum.log 2>&1" >/dev/null 2>&1
 }
