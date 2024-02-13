3e58c60016810bed1a900d95ede13ed50ef75efc
Parman <armantheparman@gmail.com>
2024-02-14 03:23:40 +1100

    tor path spelling error

--- src/MENUS/menu_public_pool.sh
+++ src/MENUS/menu_public_pool.sh
@@ -4,7 +4,7 @@ while true ; do
 unset status_tor_text status_tor
 
 if [[ -e /var/lib/tor/public_pool-service ]] && \
-sudo cat /etc/torrc | grep -q localhost:5050 ; then
+sudo cat /etc/tor/torrc | grep -q localhost:5050 ; then
       get_onion_address_variable "public_pool"
       status_tor_text="${green}Enabled$orange"
       status_tor=enabled
