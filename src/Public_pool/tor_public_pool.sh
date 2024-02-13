489f44c41adbe7744bed96da836e02832eab23cf
Parman <armantheparman@gmail.com>
2024-02-14 03:13:44 +1100

    tor directory default

--- src/Public_pool/tor_public_pool.sh
+++ src/Public_pool/tor_public_pool.sh
@@ -3,9 +3,9 @@ if [[ $OS == Mac ]] ; then no_mac ; return 1 ; fi
 
 enable_tor_general || return 1
 
-if sudo grep "HiddenServiceDir $dp/tor/public_pool-service/" \
+if sudo grep "HiddenServiceDir /var/lib/tor/public_pool-service/" \
     /etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; debug "true" ; else debug "else"
-    echo "HiddenServiceDir $dp/tor/public_pool-service/" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
+    echo "HiddenServiceDir /var/lib/tor/public_pool-service/" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
     fi
 
 if sudo grep "HiddenServicePort 5055 localhost:5050" \
