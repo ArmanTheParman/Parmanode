312adc44c0ce4deb9dc3e1f03802d1a2ef560648
Parman <armantheparman@gmail.com>
2024-02-15 03:57:25 +1100

    libzmq3-dev

--- src/text_functions/parmansed.sh
+++ src/text_functions/parmansed.sh
@@ -4,9 +4,9 @@ function parmased {
 # $3 is new strin
 # $4 is placment
 
-search_count=$(cat $1 | grep $2 | wc -l)
+search_lines=$(cat $1 | grep $2 | wc -l)
 
-if [[ $search_count -gt 1 ]] ; then
+if [[ $search_lines -gt 1 ]] ; then
 echo -e "
 
 More than one search match found. Continue?
@@ -16,7 +16,14 @@ y -- yes
 a -- abort
 "
 read choice
-case $choice in a) return 1 ;; esac
+case $choice in a) return 1 ;;
+y)
+echo "just doing first search found..."
+sleep 3
+;;
+esac
+
+
 
 
 
