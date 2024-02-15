48a67be335d110fbc03229fbf010e6e246133f8d
Parman <armantheparman@gmail.com>
2024-02-16 03:01:09 +1100

    uninstall electrum x

--- src/electrs/uninstall_electrs2.sh
+++ src/electrs/uninstall_electrs2.sh
@@ -57,7 +57,7 @@ fi
 
 nginx_electrs remove
 
-if [[ $OS == Linux ]] ; then electrs_tor_remove ; fi
+if [[ $OS == Linux ]] ; then electrs_tor_remove uninstall ; fi
 
 if [[ $OS == Linux ]] ; then
 sudo systemctl stop electrs.service >/dev/null 2>&1
@@ -65,58 +65,10 @@ sudo systemctl disable electrs.service >/dev/null 2>&1
 sudo rm /etc/systemd/system/electrs.service >/dev/null 2>&1
 fi
 
-# Uninstall - electrs_db
-
-if [[ $drive_electrs == external ]] ; then export e_db="$parmanode_drive/electrs_db" ; fi
-if [[ $drive_electrs == internal ]] ; then export e_db="$HOME/.electrs_db" ; fi
-if [[ -e $e_db ]] ; then
-while true ; do
-set_terminal ; echo -e "
-########################################################################################
-
-    Do you want to delete the$cyan electrs_db database directory$orange, or leave it, or back
-    it up as electrs_db_backup (remaining on the drive)?
-$cyan
-                          $e_db 
-
-$red
-                delete)   Delete
-$green
-                l)        Leave it there
-$white
-                b)        Back it up 
-$orange
-########################################################################################
-"
-choose "xpmq"  
-read choice
-set_terminal
-case $choice in
-m|M) back2main ;;
-q|Q) 
-exit 0 ;; 
-p|P) 
-return 1 ;;
-delete) 
-sudo rm -rf $e_db ; break ;;
-l|L) 
-break ;;
-b|B) 
-if [[ -d ${e_db}_backup ]] ; then
-    electrs_backup_exists #function defined below
-    else
-    sudo mv $e_db ${e_db}_backup
-    #if internal, moved to $HOME/parmanode/ later
-fi
-break
-;;
-*) invalid ;;
-esac
-done
-fi
+#asks for confirmation
+remove_electrs_db 
 
 # Uninstall electrs github
-
 mv $HOME/parmanode/electrs/electrs_db_backup* $HOME/parmanode/                        >/dev/null 2>&1
 rm -rf $HOME/parmanode/electrs && rm -rf $HOME/.electrs                        >/dev/null 2>&1
 
--- src/electrumx/uninstall_electrumx.sh
+++ src/electrumx/uninstall_electrumx.sh
@@ -17,9 +17,19 @@ if [[ $choice == "y" || $choice == "Y" ]] ; then true
     return 1
     fi
 
-sudo rm -rf $hp/electrumx
+nginx_electrumx remove
 
+electrumx_tor_remove uninstall
+
+if [[ $OS == Linux ]] ; then
+sudo systemctl stop electrs.service >/dev/null 2>&1
+sudo systemctl disable electrs.service >/dev/null 2>&1
+sudo rm /etc/systemd/system/electrs.service >/dev/null 2>&1
+fi
+
+sudo rm -rf $hp/electrumx
 pip3 uninstall electrumx
 parmanode_conf_remove "drive_electrumx"
 installed_conf_remove "electrumx="
+success "Electrum X" "being uninstalled"
 }
