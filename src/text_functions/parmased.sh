882a0291c2c8f2239217362af6c4a9efaadfe5d4
Parman <armantheparman@gmail.com>
2024-02-15 14:59:53 +1100

    clean up

--- src/text_functions/parmased.sh
+++ src/text_functions/parmased.sh
@@ -5,25 +5,13 @@ new_string="$3"
 placement="$4"
 silent="$5"
 
-debug " 1 2 3 4 5
-$1
-$2
-$3
-$4
-$5
-"
-
-
 newfile=/tmp/string.txt
 
 # Find how many lines in the file have the string; remove whitespace
 search_lines=$(sudo cat $file | grep "$string" | wc -l | tr -d ' ')
 
-debug "after first grep"
-
 # Find how big is the document (lines)
 file_lines=$(sudo cat $1 | wc -l | tr -d ' ')
-debug "file_lines, $file_lines"
 
 # Function is designed for one instance of string found; ward otherwise
 if [[ $search_lines -gt 1 && $silent != silent ]] ; then
@@ -51,31 +39,21 @@ fi
 
 #get the line_number of the search (document counts starts at line 1 not zero)
 line_number=$(cat $file | grep -n "$string" | head -n 1 | cut -d : -f 1 | tr -d ' ')
-debug "line_number, $line_number"
 
 # For "after" placement - insert a new line after the string is found.
 if [[ $placement == "after" ]] ; then
 
     #Write a new file up to and including the search string.
     head -n $line_number $file | sudo tee $newfile >/dev/null 2>&1
-debug "part 1 written"
+
     #print the new string to add
     echo "$new_string" | sudo tee -a $newfile >/dev/null 2>&1
-debug "string written"
 
     #add the rest
     remaining_lines=$((file_lines - line_number))
-debug "remaining_lines, $remaining_lines"
-
     tail -n $remaining_lines $file | sudo tee -a $newfile 
-
-debug "remainder written"
 
     #rename
     sudo mv $newfile $1
-debug "moved"
 fi
-
-debug "parmased finished"
-
 }
