275ca30bda4ba575aa3019df5c716b7767956191
Parman <armantheparman@gmail.com>
2024-02-15 14:28:35 +1100

    d

--- src/special/do_loop.sh
+++ src/special/do_loop.sh
@@ -48,8 +48,8 @@ parmanode_variables $@ #CANNOT USE CUSTOM DEBUG FUNCTION BEFORE THIS"
 
 set_colours #just exports variables with colour settings to make it easier to code with colours
             #parmanode.conf later may override theme
-
-if [[ $debug == 1 ]] ; then echo -e "${orange}printed colours, hit <enter>" ; read ; fi
+debug "printed colours"
+#if [[ $debug == 1 ]] ; then echo -e "${orange}printed colours, hit <enter>" ; read ; fi
 
 # Makes sure parmanode git directory is not place in $HOME directory, or it will be wiped
 # out by the program. Parmanode installs itself (and uninstalls) from $HOME/parmanode.
