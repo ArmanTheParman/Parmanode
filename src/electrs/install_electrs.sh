5518da5694d0eb14ab15671dc8da047763eca078
Parman <armantheparman@gmail.com>
2024-02-15 14:34:29 +1100

    verbose copy

--- src/electrs/install_electrs.sh
+++ src/electrs/install_electrs.sh
@@ -88,7 +88,7 @@ if [[ $electrs_compile == "false" ]] ; then
     please_wait
     echo -e "$pink copying/moving files...$orange"
     rm -rf $HOME/parmanode/electrs/ 
-    cp -R $HOME/.electrs_backup $HOME/parmanode/electrs
+    cp -Rv $HOME/.electrs_backup $HOME/parmanode/electrs
 
     installed_config_add "electrs2-start"
 
