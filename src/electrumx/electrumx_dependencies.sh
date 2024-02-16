ae3ab0374539267148bba631c80270b432f9c3cc
Parman <armantheparman@gmail.com>
2024-02-16 16:59:24 +1100

    titrate dependencies

--- src/electrumx/electrumx_dependencies.sh
+++ src/electrumx/electrumx_dependencies.sh
@@ -1,7 +1,8 @@
 function electrumx_dependencies {
 update_computer silent
-sudo apt-get install -y fakeroot python3 python3-pip python3-dev build-essential \
-libsnappy-dev zlib1g-dev libbz2-dev libgflags-dev liblz4-dev libzstd-dev librocksdb-dev --no-install-recommends
+sudo apt-get install -y fakeroot python3 python3-pip python3-dev build-essential librocksdb-dev --no-install-recommends
+
+#libsnappy-dev zlib1g-dev libbz2-dev libgflags-dev liblz4-dev libzstd-dev
 
 #recommended in docs, but there's no need unless building applications...
 #pip3 install aiohttp -y
