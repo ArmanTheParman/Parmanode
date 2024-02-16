6c7ce0e23a4f15253e26061f95be4499d0dca1e6
Parman <armantheparman@gmail.com>
2024-02-16 16:36:44 +1100

    more dependencies

--- src/electrumx/electrumx_dependencies.sh
+++ src/electrumx/electrumx_dependencies.sh
@@ -1,6 +1,6 @@
 function electrumx_dependencies {
 sudo apt-get update -y
-sudo apt-get install -y librocksdb-dev python3 python3-pip python3-dev
+sudo apt-get install -y librocksdb-dev python3 python3-pip python3-dev build-essential libsnappy-dev zlib1g-dev libbz2-dev libgflags-dev liblz4-dev libzstd-dev
 
 #recommended in docs, but there's no need unless building applications...
 #pip3 install aiohttp -y
