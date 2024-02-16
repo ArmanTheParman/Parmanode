53a584d87094089213708da49fbe9a259b3d3e2e
Parman <armantheparman@gmail.com>
2024-02-16 19:03:36 +1100

    pip

--- src/electrumx/electrumx_dependencies.sh
+++ src/electrumx/electrumx_dependencies.sh
@@ -1,5 +1,6 @@
 function electrumx_dependencies {
 update_computer silent
+python3 -m pip install --upgrade pip 
 sudo apt-get remove -y libdpkg-perl #my machine had a newer version, preventing build-essential to be installed
 sudo apt-get install -y --fix-broken --no-install-recommends fakeroot python3 python3-pip python3-dev \
 build-essential librocksdb-dev libsnappy-dev zlib1g-dev libbz2-dev libgflags-dev liblz4-dev libzstd-dev \
