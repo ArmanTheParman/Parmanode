df07499032b5dfc8796b7b1123630c6dfab5acc8
Parman <armantheparman@gmail.com>
2024-02-16 19:13:53 +1100

    d

--- src/electrumx/electrumx_dependencies.sh
+++ src/electrumx/electrumx_dependencies.sh
@@ -3,7 +3,7 @@ update_computer silent
 sudo apt-get remove -y libdpkg-perl #my machine had a newer version, preventing build-essential to be installed
 sudo apt-get install -y --fix-broken --no-install-recommends fakeroot python3 python3-pip python3-dev \
 build-essential librocksdb-dev libsnappy-dev zlib1g-dev libbz2-dev libgflags-dev liblz4-dev libzstd-dev \
-rocksdb-tools
+rocksdb-tools librocksdb-6.11
 python3 -m pip install --upgrade pip 
 
 #recommended in docs, but there's no need unless building applications...
