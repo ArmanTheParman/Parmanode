5952c9b40faa292f139534ac6fc5dc60f03093dd
Parman <armantheparman@gmail.com>
2024-02-16 16:20:05 +1100

    pip3

--- src/electrumx/electrumx_dependencies.sh
+++ src/electrumx/electrumx_dependencies.sh
@@ -1,7 +1,8 @@
 function electrumx_dependencies {
-if ! which python3 >/dev/null ; then sudo apt-get update -y && sudo apt-get install python3 
+if ! which python3 >/dev/null ; then sudo apt-get update -y && sudo apt-get install python3 python3-pip
 fi
-
+if ! which pip3 ; then sudo apt-get update -y && sudo apt-get install python3-pip
+fi
 #recommended in docs, but there's no need unless building applications...
 #pip3 install aiohttp -y
 
