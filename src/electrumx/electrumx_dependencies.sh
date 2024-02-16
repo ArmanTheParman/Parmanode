f592c52c3e5978662d91f5c74006bd9562c15fc4
Parman <armantheparman@gmail.com>
2024-02-16 19:03:54 +1100

    t

--- src/electrumx/electrumx_dependencies.sh
+++ src/electrumx/electrumx_dependencies.sh
@@ -1,10 +1,10 @@
 function electrumx_dependencies {
 update_computer silent
-python3 -m pip install --upgrade pip 
 sudo apt-get remove -y libdpkg-perl #my machine had a newer version, preventing build-essential to be installed
 sudo apt-get install -y --fix-broken --no-install-recommends fakeroot python3 python3-pip python3-dev \
 build-essential librocksdb-dev libsnappy-dev zlib1g-dev libbz2-dev libgflags-dev liblz4-dev libzstd-dev \
 rocksdb-tools
+python3 -m pip install --upgrade pip 
 
 #recommended in docs, but there's no need unless building applications...
 #pip3 install aiohttp -y
