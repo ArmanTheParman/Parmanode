d9bbddf5d65af9c76b2e6be41eb1279b179c20dc
Parman <armantheparman@gmail.com>
2024-02-19 23:31:50 +1100

    pn refresh

--- src/MENUS/menu_tools2.sh
+++ src/MENUS/menu_tools2.sh
@@ -28,7 +28,7 @@ return 0
 ;;
 
 rf)
-pn_refresh
+parmanode_refresh
 return 0
 ;;
 
@@ -45,73 +45,4 @@ done
 return 0
 }
 
-function bitcoin_curl {
-source $bc
 
-while true ; do
-set_terminal ; echo -en "
-########################################################################################
-
-    The curl command to the bitcoin daemon, to run in terminal is...
-$cyan
-
-curl --user $rpcuser:$rpcpassword --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' -H 'content-type: text/plain;' http://$IP:8332
-$orange
-    Parmanode can run this command for you or you can copy/paste it yourself, and
-    make edits as needed.
-$green
-               d)      Do it for me
-$orange
-            <enter>    I'll do it, moving on.
-
-########################################################################################
-"
-choose "xpmq" ; read choice
-set_terminal
-case $choice in
-q|Q) exit 0 ;; p|P) return 1 ;; M|m) back2main ;;
-"") break ;;
-d)
-curl --user $rpcuser:$rpcpassword --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' -H 'content-type: text/plain;' http://$IP:8332
-enter_continue
-break
-;;
-*)
-invalid
-;;
-esac
-done
-}
-
-function pn_refresh {
-while true ; do
-set_terminal ; echo -e "
-########################################################################################
-
-    Sometimes, especially if you manually edit the code, updates to Parmanode
-    might do strange things. You can get a fresh copy of the latest version of
-    Parmanode without affecting any of the programs you installed or any configuration
-    files - totally safe.
-$green
-                        rf)       Refresh Parmanode
-$orange
-                        a)        Abort
-
-########################################################################################
-"
-choose "xpmq" ; read choice ; set_terminal
-case $choice in
-q|Q) exit 0 ;; p|P) return 0 ;; m|M) back2main ;;
-a|A) return 1 ;;
-rf)
-cd $HOME/parman_programs && rm -rf ./parmanode
-git clone https://github.com/armantheparman/parmanode.git
-success "The Parmanode script directory has been refreshed"
-announce "Please quit and restart Parmanode to see changes take effect."
-;;
-*)
-invalid
-esac
-done
-
-}
