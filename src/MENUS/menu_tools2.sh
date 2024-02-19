671229807c0559225faefb5dad06b2cb3d6a96ec
Parman <armantheparman@gmail.com>
2024-02-19 23:30:11 +1100

    parmanode refersh

--- src/MENUS/menu_tools2.sh
+++ src/MENUS/menu_tools2.sh
@@ -10,6 +10,8 @@ echo -e "
 
               (curl)      Test bitcoin curl/rpc command (for troubleshooting)
 
+              (rf)        Refresh Parmanode script directory              
+
               (sr)        System report (for getting troubleshooting help)
 $orange
 ########################################################################################
@@ -22,6 +24,11 @@ q|Q) exit ;;  m|M) back2main ;; p|P) return 0 ;;
 
 curl)
 bitcoin_curl
+return 0
+;;
+
+rf)
+pn_refresh
 return 0
 ;;
 
@@ -74,4 +81,37 @@ invalid
 ;;
 esac
 done
+}
+
+function pn_refresh {
+while true ; do
+set_terminal ; echo -e "
+########################################################################################
+
+    Sometimes, especially if you manually edit the code, updates to Parmanode
+    might do strange things. You can get a fresh copy of the latest version of
+    Parmanode without affecting any of the programs you installed or any configuration
+    files - totally safe.
+$green
+                        rf)       Refresh Parmanode
+$orange
+                        a)        Abort
+
+########################################################################################
+"
+choose "xpmq" ; read choice ; set_terminal
+case $choice in
+q|Q) exit 0 ;; p|P) return 0 ;; m|M) back2main ;;
+a|A) return 1 ;;
+rf)
+cd $HOME/parman_programs && rm -rf ./parmanode
+git clone https://github.com/armantheparman/parmanode.git
+success "The Parmanode script directory has been refreshed"
+announce "Please quit and restart Parmanode to see changes take effect."
+;;
+*)
+invalid
+esac
+done
+
 }
