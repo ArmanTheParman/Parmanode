fff7a130ccf043dd7b5486b4ccbfc4e299d7dca3
Parman <armantheparman@gmail.com>
2024-02-19 18:28:27 +1100

    d

--- src/general_functions/system_report.sh
+++ src/general_functions/system_report.sh
@@ -31,13 +31,13 @@ esac
 done
 
 
-report="/tmp/system_report.txt" && echo "PARMANODL SYSTEM REPORT $(date)" > /tmp/tempreport && mv /tmp/tempreport $report
+report="/tmp/system_report.txt" && echo "PARMANODL SYSTEM REPORT $(date)" > $report
 
 function delete_private {
 if [[ $omit == true ]] ; then
 cat $report | sed /rpcuser/d | sed /rpcpass/d | sed /auth = /d | sed /btc.rpc.user=/d | sed /btc.rpc.password=/d | \
  sed /alias=/d | sed /bitcoind.rpc/d | sed /DAEMON_URL =/d | sed /CORE_RPC_USERNAME/d | sed /CORE_RPC_PASSWORD/d | \
- sed /BITCOIN_RPC_PASSWORD/d | sed /BITCOIN_RPC_USER/d | sed /multiPass/d > $report
+ sed /BITCOIN_RPC_PASSWORD/d | sed /BITCOIN_RPC_USER/d | sed /multiPass/d > /tmp/tempreport && mv /tmp/tmpreport $report
 fi
 }
 
