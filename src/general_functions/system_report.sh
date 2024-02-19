210f74b2c6007b462b9a1f7e5f92fc521adedcf8
Parman <armantheparman@gmail.com>
2024-02-19 17:42:13 +1100

    delete_private

--- src/general_functions/system_report.sh
+++ src/general_functions/system_report.sh
@@ -1,5 +1,45 @@
 function get_system_report {
+while true ; do
+set_terminal ; echo -e "
+########################################################################################
+
+    Parmanode will generate a$cyan System Report File$orange automatically. It will
+    then save the file to your desktop, and you can then email it to me for help.
+
+    One question, do you want to omit bitcoin rpcuser and bitcoinrpc pass and Tor
+    addresses from the report? 
+    
+    These details might help the troubleshooting process and I can't really do 
+    anything dodgy with the info, but if you feel more comfortable, they can be 
+    excluded form the report...
+$red
+                                o)     omit
+$green
+                                i)     include
+$orange
+########################################################################################
+"
+choose "xpmq" ; read choice ; set_terminal
+case $choice in
+q|Q) exit ;; p|P) return 1 ;; m|M) back2main ;;
+o) 
+export omit=true ; break ;;
+
+i)
+export omit=false ; break ;;
+esac
+done
+
+
 report="/tmp/system_report.txt" && echo "PARMANODL SYSTEM REPORT $(date)" > $report
+
+function delete_private {
+if [[ $omit == true ]] ; then
+cat $report | sed /rpcuser/d | sed /rpcpass/d | sed /auth = /d | sed /btc.rpc.user=/d | sed /btc.rpc.password=/d | \
+ sed /alias=/d | sed /bitcoind.rpc/d | sed /DAEMON_URL =/d | sed /CORE_RPC_USERNAME/d | sed /CORE_RPC_PASSWORD/d | \
+ sed /BITCOIN_RPC_PASSWORD/d | sed /BITCOIN_RPC_USER/d | sed /multiPass/d > $report
+fi
+}
 
 function echor {
 echo -e "$1" >> "$report" 2>&1
@@ -21,6 +61,7 @@ echor "$(git log | head -n30)"
 #parmanode.conf
 echor "#PARMNODE CONF"
 echor "$(cat $HOME/.parmanode/parmanode.conf)"
+
 
 #system info
 echor "#SYSTEM INFO"
@@ -125,18 +166,24 @@ echor "#BRE TOR"
 sudo cat /var/lib/tor/bre-service/hostname
 echor "#ELECTRS TOR"
 echor "$(sudo cat /var/lib/tor/electrs-service/hostname)"
+echor "#ELECTRUMX TOR"
 echor "$(sudo cat /var/lib/tor/electrumx-service/hostname)"
+echor "#MEMPOOL TOR"
 echor "$(sudo cat /var/lib/tor/mempool-service/hostname)"
+echor "#FULCRUM TOR"
 echor "$(sudo cat /var/lib/tor/fulcrum-service/hostname)"
+echor "#PUBLIC POOL TOR"
 echor "$(sudo cat /var/lib/tor/public_pool-service/hostname)"
+echor "#BTCPAY TOR"
 echor "$(sudo cat /var/lib/tor/btcpayTOR-server/hostname)"
+echor "#BITCOIN TOR"
 echor "$(sudo cat /var/lib/tor/bitcoin-service/hostname)"
 
 #EXTRA STUFF
 echor "#EXTRA STUFF"
 echor "$(cat /etc/cpuinfo)"
 
-
+delete_private
 mv $report $HOME/Desktop/
 
 set_terminal ; echo -e "
