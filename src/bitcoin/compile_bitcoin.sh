d4789c39f33988b456ed774ab8d9d72951a03627
Parman <armantheparman@gmail.com>
2024-02-21 05:10:24 +1100

    compile with wallet - berkelydb

--- src/bitcoin/compile_bitcoin.sh
+++ src/bitcoin/compile_bitcoin.sh
@@ -19,6 +19,13 @@ sudo apt-get install make automake cmake curl g++-multilib libtool binutils bsdm
 sudo apt-get install pkg-config python3 patch bison autoconf libboost-all-dev autoconf -y
 sudo apt-get install imagemagick -y
 sudo apt-get install librsvg2-bin -y
+sudo apt-get install libdb5.3-dev libdb++-dev #berkley db stuff
+
+#for later when mac is supported
+if [[ $OS == Mac ]] ; then
+brew install berkeley-db@4
+fi
+
 
 
 cd $hp || { echo "can't change directory. Aborting." ; enter_continue ; return 1 ; }
@@ -134,7 +141,7 @@ clear ; echo -e "
 
    The configure command that will be run is the following: 
 $cyan
-   ./configure --with-gui=$gui --enable-wallet
+   ./configure --with-gui=$gui --enable-wallet --with-incompatible-bdb
 $orange
 
    Hit$green <enter>$orange to continue, or,$yellow type in$orange additional options you
@@ -167,7 +174,7 @@ done
 
 set_terminal
 
-./configure --with-gui=$gui --enable-wallet $options
+./configure --with-gui=$gui --enable-wallet --with-incompatible-bdb $options
 
 echo -e "
 ########################################################################################
