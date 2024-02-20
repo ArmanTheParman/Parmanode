1f695b160e9d359ca8a33091a787673963afc641
Arman The Parman <77603167+ArmanTheParman@users.noreply.github.com>
2024-02-21 05:39:18 +1100

    Update run_fulcrum_docker.sh

--- src/fulcrum_mac/run_fulcrum_docker.sh
+++ src/fulcrum_mac/run_fulcrum_docker.sh
@@ -29,6 +29,8 @@ docker run -d --name fulcrum \
                 -p 50001:50001 \
                 -p 50002:50002 \
                 -p 50003:50003 \
+                -v $hp/fulcrum/key.pem:/home/parman/parmanode/fulcrum/key.pem
+                -v $hp/fulcrum/cert.pem:/home/parman/parmanode/fulcrum/cert.pem
                 -v $HOME/parmanode/fulcrum/config:/home/parman/parmanode/fulcrum/config \
                 fulcrum >/$HOME/parmanode/fulcrum.log 2>&1 
 
