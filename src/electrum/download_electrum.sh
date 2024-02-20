2b4dd028f1fc9cf9e2769985279b2b65bf0ca6c3
Parman <armantheparman@gmail.com>
2024-02-21 02:56:50 +1100

    electrum refactor

--- src/electrum/download_electrum.sh
+++ src/electrum/download_electrum.sh
@@ -33,47 +33,48 @@ if [[ $computer_type == "Pi" ]] ; then
     return 0
     fi
 
-while true ; do
+# while true ; do
 if [[ $OS == Mac ]] ; then
-clear ; echo -e "
-########################################################################################
+clear 
+# echo -e "
+# ########################################################################################
 
-    If you have a Mac with a newer M1/M2 chip, you might run in to problems with
-    the standard Electrum install. If this has happened, or you don't want to take
-    any chances, you can select to install the 'python' version instead. It works
-    the same, you just won'g get a Mac executable in your Applications directory.
+#     If you have a Mac with a newer M1/M2 chip, you might run in to problems with
+#     the standard Electrum install. If this has happened, or you don't want to take
+#     any chances, you can select to install the 'python' version instead. It works
+#     the same, you just won'g get a Mac executable in your Applications directory.
 
-                        1)         Regular install
+#                         1)         Regular install
 
-                        2)         Python install 
+#                         2)         Python install 
 
-########################################################################################
-"
-read choice
-case $choice in 
-1)
+# ########################################################################################
+# "
+# read choice
+# case $choice in 
+# 1)
 curl -LO https://download.electrum.org/${electrum_version}/electrum-${electrum_version}.dmg && \
 curl -LO https://download.electrum.org/${electrum_version}/electrum-${electrum_version}.dmg.asc && \
 curl -LO https://raw.githubusercontent.com/spesmilo/electrum/master/pubkeys/ThomasV.asc 
-break
-;;
+# break
+# ;;
 
-2)
-export python_install=true
-curl -LO https://download.electrum.org/${electrum_version}/Electrum-${electrum_version}.tar.gz
-curl -LO https://download.electrum.org/${electrum_version}/Electrum-${electrum_version}.tar.gz.asc
-curl -LO https://raw.githubusercontent.com/spesmilo/electrum/master/pubkeys/ThomasV.asc 
-break
-;;
+# 2)
+# export python_install=true
+# curl -LO https://download.electrum.org/${electrum_version}/Electrum-${electrum_version}.tar.gz
+# curl -LO https://download.electrum.org/${electrum_version}/Electrum-${electrum_version}.tar.gz.asc
+# curl -LO https://raw.githubusercontent.com/spesmilo/electrum/master/pubkeys/ThomasV.asc 
+# break
+# ;;
 
-*)
-invalid ;;
-esac
+# *)
+# invalid ;;
+# esac
 
-else
-   return
+# else
+#    return
 fi
 
-done
+# done
 
 }
