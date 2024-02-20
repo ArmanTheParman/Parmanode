6e5a09a15e4108c604569486a7ad7ed762d2f40b
Parman <armantheparman@gmail.com>
2024-02-20 13:01:52 +1100

    refactor

--- src/fulcrum_linux/fulcrum_make_directories.sh
+++ src/fulcrum_linux/fulcrum_make_directories.sh
@@ -7,16 +7,16 @@ installed_config_add "fulcrum-start"
 #Make config directory for docker; useful now or later
 mkdir $HOME/parmanode/fulcrum/config 2>&1
 
-# Make fulcrum_db on the internal or external drive
-source $HOME/.parmanode/parmanode.conf >/dev/null 2>&1
-
+#External drive DB directory
 if [[ $drive_fulcrum == "external" ]] ; then
 mount_drive && log "fulcrum" "Drive mounted."
 mkdir $parmanode_drive/fulcrum_db >/dev/null 2>&1 
 fi
 
+#Internal drive DB directory
 if [[ $drive_fulcrum == "internal" ]] ; then
 mkdir $HOME/parmanode/fulcrum_db >/dev/null 2>&1
+mkdir $HOME/.fulcrum_db >/dev/null 2>&1
 log "fulcrum" "fulcrum_db made - internal" 
 fi
 
