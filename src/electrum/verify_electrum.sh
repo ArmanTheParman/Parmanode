f98346fd1fae0745c7329a47bde35af2bba85ab7
Parman <armantheparman@gmail.com>
2024-02-21 02:01:52 +1100

    electrum python verify bug

--- src/electrum/verify_electrum.sh
+++ src/electrum/verify_electrum.sh
@@ -5,31 +5,40 @@ cd $HOME/parmanode/electrum
 gpg --import ./*Thomas*
 
 if [[ $computer_type == "LinuxPC" ]] ; then
-if ! gpg --verify --status-fd 1 electrum*.asc electrum*.AppImage 2>&1 | grep "GOOD" ; then 
-    echo "GPG verification failed. Aborting."
-    log "electrum" "verification failed. Aborting."
-    enter_continue
-    return 1
+    if ! gpg --verify --status-fd 1 electrum*.asc electrum*.AppImage 2>&1 | grep "GOOD" ; then 
+        echo "GPG verification failed. Aborting."
+        log "electrum" "verification failed. Aborting."
+        enter_continue
+        return 1
     fi
-    fi
+fi
 
 if [[ $computer_type == "Pi" ]] ; then
-if ! gpg --verify --status-fd 1 Electrum*.asc Electrum*.tar.gz 2>&1 | grep "GOOD" ; then
-    echo "GPG verification failed. Aborting."
-    log "electrum" "verification failed. Aborting."
-    enter_continue
-    return 1
+    if ! gpg --verify --status-fd 1 Electrum*.asc Electrum*.tar.gz 2>&1 | grep "GOOD" ; then
+        echo "GPG verification failed. Aborting."
+        log "electrum" "verification failed. Aborting."
+        enter_continue
+        return 1
     fi
-    fi
+fi
 
-if [[ $OS == "Mac" ]] ; then
-if ! gpg --verify --status-fd 1 electrum*.asc electrum*.dmg 2>&1 | grep "GOOD" ; then 
-    echo "GPG verification failed. Aborting."
-    log "electrum" "verification failed. Aborting."
-    enter_continue
-    return 1
+if [[ $OS == "Mac" && $python_install != true ]] ; then
+    if ! gpg --verify --status-fd 1 electrum*.asc electrum*.dmg 2>&1 | grep "GOOD" ; then 
+        echo "GPG verification failed. Aborting."
+        log "electrum" "verification failed. Aborting."
+        enter_continue
+        return 1
     fi
+elif [[ $OS == Mac && $python_install == true ]] ; then
+    if ! gpg --verify --status-fd 1 Electrum*.asc Electrum*.gz 2>&1 | grep "GOOD" ; then 
+        echo "GPG verification failed. Aborting."
+        log "electrum" "verification failed. Aborting."
+        enter_continue
+        return 1
     fi
+fi
+
+
 
 
 set_terminal ; echo -e "
