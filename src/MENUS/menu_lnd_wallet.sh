ad8dc84f4b6fb443818243969f1eabd7c18d8544
Parman <armantheparman@gmail.com>
2024-02-21 19:45:48 +1100

    wallet location move again

--- src/MENUS/menu_lnd_wallet.sh
+++ src/MENUS/menu_lnd_wallet.sh
@@ -14,6 +14,7 @@ echo "
                    LND IS NOT RUNNING -- CHOOSE \"start\" TO RUN"
 fi
 echo -e "
+      (pw)             Change LND password 
 
       (ul)             Unlock Wallet
 
@@ -35,6 +36,35 @@ case $choice in
 m|M) back2main ;;
 q|Q) exit ;;
 p|P) return 1 ;;
+
+pw|Pw|PW|password|PASSWORD|Password)
+echo -e "
+########################################################################################
+
+    If you already have a lightning wallet loaded, changing your password will make 
+    you lose access to it. Not a disaster, you just have to change the password back 
+    to the original. Even though passwords in this context are not passphrases, they 
+    are just as important. A password locks the wallet, whereas a passphrase 
+    contributes to the entropy of the wallet.
+
+    If your intentions are to delete the wallet and start fresh, and create a new
+    password, then delete the wallet first, then change the password, then create
+    your new wallet.
+
+    Note, deleting a wallet with bitcoin in it does not delete the bitcoin. You can
+    recover the wallet as long as you have a copy of the seed phrase.
+
+    Also note that$green funds in lightning channels are NOT recoverable by the
+    seed phrase$orange - those funds are shared in 2-of-2 multisignature addresses, that are
+    returned to your wallet when the channel is closed. To keep access to those
+    funds in a channel, you need to keep your lightning node running, or restore
+    your lightning node with both the seed AND the channel backup file.
+
+########################################################################################
+"
+enter_continue
+lnd_wallet_unlock_password
+;;
 
 au|AU|Au)
 lnd_wallet_unlock_password
