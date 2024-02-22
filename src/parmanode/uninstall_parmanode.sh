5a03749629384e56dc9e5967316da4452a747071
Parman <armantheparman@gmail.com>
2024-02-23 01:58:07 +1100

    t

--- src/parmanode/uninstall_parmanode.sh
+++ src/parmanode/uninstall_parmanode.sh
@@ -151,6 +151,7 @@ delete_line "$HOME/.bashrc" "Parmanode..." >/dev/null 2>&1
 delete_line "$HOME/.bashrc" "function rp {" >/dev/null 2>&1
 delete_line "$HOME/.bashrc" "safe to delete" >/dev/null 2>&1
 delete_line "$HOME/.bashrc" "parmashell_functions" >/dev/null 2>&1
+
 delete_line "$HOME/.zshrc" "Parmanode..." >/dev/null 2>&1
 delete_line "$HOME/.zshrc" "function rp {" >/dev/null 2>&1
 delete_line "$HOME/.zshrc" "safe to delete" >/dev/null 2>&1
