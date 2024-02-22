59f1ab41792e75b0ee87c0f3f909f876df439474
Parman <armantheparman@gmail.com>
2024-02-23 01:57:00 +1100

    parmashell default

--- src/ParmaShell/install_parmashell.sh
+++ src/ParmaShell/install_parmashell.sh
@@ -1,18 +1,26 @@
 function install_parmashell {
-
+if [[ $1 != silent ]] ; then
 parmashell_intro || return 1
+fi
+
 #back up original bashrc, being responsible and stuff.
 if [[ $OS == Mac ]] ; then rc=zshrc ; fi
 if [[ $OS == Linux ]] ; then rc=bashrc ; fi
 
 cp ~/.$rc ~/.${rc}_parmanodebackup
 
+#Do not change echo statements
+if ! grep -q "Added by Parmanode below, safe to delete" < ~/.$rc ; then
 echo "#Added by Parmanode below, safe to delete..." | sudo tee -a ~/.$rc >/dev/null
-installed_config_add "parmashell-start"
 echo "source $HOME/parman_programs/parmanode/src/ParmaShell/parmashell_functions" | sudo tee -a ~/.$rc >/dev/null
 echo "#Added by Parmanode above, safe to delete..." | sudo tee -a ~/.$rc >/dev/null
+else
+return 1
+fi
 
 installed_config_add "parmashell-end"
+if [[ $1 != silent ]] ; then
 success "ParmaShell" "being installed"
 announce "You may need to reload terminal to see the changes."
+fi
 }
