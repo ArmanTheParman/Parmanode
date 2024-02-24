b4a38eff1e73c6ec28cf7851a5e7821b1be14457
Arman The Parman <77603167+ArmanTheParman@users.noreply.github.com>
2024-02-24 14:03:05 +1100

    Update install_bitcoin.sh
    Bitcoin conf

--- src/bitcoin/install_bitcoin.sh
+++ src/bitcoin/install_bitcoin.sh
@@ -57,10 +57,8 @@ download_bitcoin || return 1
 fi
 
 #setup bitcoin.conf
-if [[ $importdrive != true ]] ; then
-    log "bitcoin" "make_bitcoin_conf function ..."
+    debug "make_bitcoin_conf function ..."
     make_bitcoin_conf || return 1
-fi
 
 #make a script that service file will use
 if [[ $OS == "Linux" ]] ; then
