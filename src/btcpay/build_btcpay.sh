9d650db773b253d909f55df9962ec8d823516155
Parman <armantheparman@gmail.com>
2024-02-26 03:21:03 +1100

    fixed btcp with variable parman UID

--- src/btcpay/build_btcpay.sh
+++ src/btcpay/build_btcpay.sh
@@ -1,6 +1,10 @@
 function build_btcpay {
 
-docker build -t btcpay $original_dir/src/btcpay && \
+#get the USER ID, and match it to the parman ID inside the container.
+
+thisID=$(id -u) 
+
+docker build --build-arg parmanID=$thisID -t btcpay $original_dir/src/btcpay && \
     log "btcpay" "build success" && return 0 || log "btcpay" "build failure" && enter_continue \
     return 1
 }
