e7c355fd660ccba37c161ffb19566029bb2cb181
Parman <armantheparman@gmail.com>
2024-02-26 02:03:03 +1100

    free_up_space tool

---
+++ src/drive_functions/free_up_space.sh
@@ -0,0 +1,172 @@
+function free_up_space {
+
+while true ; do
+set_terminal ; echo -e "
+########################################################################################
+
+    Paramanode might be responsible for adding extra load on the internal drive due
+    to growing log files and such.
+
+    This is an interactive clean up tool.
+
+    Proceed?
+$green
+                              y)          Yes
+$red
+                              n)          Nah
+$orange
+########################################################################################
+"
+choose "xpmq" ; read choice ; set_terminal
+case $choice in
+q|Q) exit 0 ;; p|P) return 0 ;; m|M) back2main ;; n|N) return 0 ;;
+y|Y) break ;;
+*) invalid ;;
+esac
+done
+
+while true ; do
+set_terminal
+echo -e "
+########################################################################################
+
+    Delete Parmanode log files (mainly useful for sending error reports to Parman)
+$green
+    y)        yes
+$red
+    n)        no
+$orange
+########################################################################################
+"
+choose "xpmq" ; read choice ; set_terminal
+case choice in
+q|Q) exit 0 ;; p|P) return 0 ;; n|N) break ;;
+y|Y)
+sudo rm -rf $dp/*.log ; rm -rf $dp/.*.log
+break
+;;
+*)
+invalid
+;;
+esac
+done
+
+while true ; do
+set_terminal
+echo -e "
+########################################################################################
+
+    Delete unused Docker containers/images etc?
+$green
+    y)        yes
+$red
+    n)        no
+$orange
+########################################################################################
+"
+choose "xpmq" ; read choice ; set_terminal
+case choice in
+q|Q) exit 0 ;; p|P) return 0 ;; n|N) break ;;
+y|Y)
+if ! docker ps >/dev/null 2>&1 ; then
+set_terminal ; echo "Please run Docker and hit enter to try again." ; enter_continue 
+  if ! docker ps >/dev/null 2>&1 ; then
+  set_terminal ; echo "Docker not running, skipping." ; break
+  fi
+else
+docker system prune
+break
+fi
+;;
+
+*)
+invalid
+;;
+esac
+done
+
+while [[ $OS == Linux ]] ; do
+set_terminal
+echo -e "
+########################################################################################
+
+    Empty system recycle bin?
+$green
+    y)        yes
+$red
+    n)        no
+$orange
+########################################################################################
+"
+choose "xpmq" ; read choice ; set_terminal
+case choice in
+q|Q) exit 0 ;; p|P) return 0 ;; n|N) break ;;
+y|Y)
+sudo rm $HOME/.local/share/Trash/*
+break
+;;
+*)
+invalid
+;;
+esac
+done
+
+while true ; do
+set_terminal
+echo -e "
+########################################################################################
+
+    Delete everything in the /tmp directory?
+$green
+    y)        yes
+$red
+    n)        no
+$orange
+########################################################################################
+"
+choose "xpmq" ; read choice ; set_terminal
+case choice in
+q|Q) exit 0 ;; p|P) return 0 ;; n|N) break ;;
+y|Y)
+sudo rm -rf /tmp/*
+break
+;;
+*)
+invalid
+;;
+esac
+done
+
+while true ; do
+set_terminal
+echo -e "
+########################################################################################
+
+   Install a graphical program to help you search for large unneccesary files?
+   It's called Baobab? 
+$green
+    y)        yes
+$red
+    n)        no
+$orange
+########################################################################################
+"
+choose "xpmq" ; read choice ; set_terminal
+case choice in
+q|Q) exit 0 ;; p|P) return 0 ;; n|N) break ;;
+y|Y)
+sudo apt-get update -y
+sudo apt-get --fix-broken install
+sudo apt-get install baobab
+success "Baobab has been installed. Just type 'baobab' in the terminal to run it.
+    Of course, it won't work if you are using Parmanode through SSH."
+break
+;;
+*)
+invalid
+;;
+esac
+done
+
+success "Here ends the internal disk cleanup tool"
+}
