c8eb16eb21a53bee444e06edd8e63c6e5f6e1927
Parman <armantheparman@gmail.com>
2024-02-26 21:00:42 +1100

    electrs2-start

--- src/electrs/download_electrs.sh
+++ src/electrs/download_electrs.sh
@@ -1,3 +1,3 @@
 function download_electrs {
-cd $HOME/parmanode/ && git clone --depth 1 https://github.com/romanz/electrs && installed_config_add "electrs-start"
+cd $HOME/parmanode/ && git clone --depth 1 https://github.com/romanz/electrs && installed_config_add "electrs2-start"
 }
