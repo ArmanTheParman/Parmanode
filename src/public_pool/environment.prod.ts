fe6744a42813d64dfb1a2d6c60e86b0395b301e6
Parman <armantheparman@gmail.com>
2024-02-27 21:08:13 +1100

    a

--- src/public_pool/environment.prod.ts
+++ src/public_pool/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL: 'https://localhost:40557',
-    STRATUM_URL: '127.0.0.1:3333'
+    API_URL: 'https://127.0.0.1:40557',
+    STRATUM_URL: '127.0.0.1:3333 Use port 3335 if from a different computer.'
 };
--- src/public_pool/environment.ts
+++ src/public_pool/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    API_URL: 'http://localhost:3334',
+    API_URL: 'http://127.0.0.1:3334',
     STRATUM_URL: '127.0.0.1:3333'
 };
