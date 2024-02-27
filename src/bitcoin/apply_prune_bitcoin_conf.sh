020075e2b2b930057a2118962dd8eaaaa070420e
Parman <armantheparman@gmail.com>
2024-02-28 02:17:12 +1100

    fix prune bug, -z

--- src/bitcoin/apply_prune_bitcoin_conf.sh
+++ src/bitcoin/apply_prune_bitcoin_conf.sh
@@ -13,14 +13,7 @@ if [[ -z ${prune_value} ]] ; then echo "Prune choice not detected. Needs to be s
 
 #cannot have prune 0 with txindex and blockfilterindex.
 
-if [[ $prune_value != "0" ]] ; then
-	delete_line "$file" "txindex=1"
-	delete_line "$file" "blockfilterindex=1"
-	delete_line "$file" "prune="
-	echo "prune=$prune_value" | sudo tee -a $file >/dev/null  
-fi
-
-if [[ $prune_value == "0" ]] ; then
+if [[ $prune_value == "0" || -z $prune_value ]] ; then
 	#delete all first, in case of multiple occurrences.
 	delete_line "$file" "txindex="
 	delete_line "$file" "blockfilterindex="
@@ -28,6 +21,14 @@ if [[ $prune_value == "0" ]] ; then
 
 	echo "txindex=1" | sudo tee -a $file >/dev/null
 	echo "blockfilterindex=1" | sudo tee -a $file >/dev/null
+	skipnext=true
+fi
+
+if [[ $prune_value != "0" && $skipnext != true ]] ; then
+	delete_line "$file" "txindex=1"
+	delete_line "$file" "blockfilterindex=1"
+	delete_line "$file" "prune="
+	echo "prune=$prune_value" | sudo tee -a $file >/dev/null  
 fi
 
 log "bitcoin" "end of apply_prune_bitcoin_conf function"
