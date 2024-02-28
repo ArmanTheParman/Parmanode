7ca3e1980a961f2424931d289d57522ece34ee20
Parman <armantheparman@gmail.com>
2024-02-29 01:02:13 +1100

    3333

--- src/public_pool/environment.prod.ts
+++ src/public_pool/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    API_URL: 'https://127.0.0.1:3344',
-    STRATUM_URL: '127.0.0.1:3343'
+    API_URL: 'https://127.0.0.1:3334',
+    STRATUM_URL: '127.0.0.1:3333'
 };
