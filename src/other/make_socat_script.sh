b719a3b6955fd4f165e02993e666d44ede6efdf9
Parman <armantheparman@gmail.com>
2024-02-29 03:52:21 +1100

    log

--- src/other/make_socat_script.sh
+++ src/other/make_socat_script.sh
@@ -3,9 +3,9 @@ function make_socat_script {
 if [[ $1 == electrs ]] ; then
 #start script (run by service file)
 echo "#!/bin/bash
-socat OPENSSL-LISTEN:50006,reuseaddr,fork,cert=$hp/electrs/cert.pem,key=$hp/electrs/key.pem,verify=0 TCP:127.0.0.1:50055 &
+socat OPENSSL-LISTEN:50006,reuseaddr,fork,cert=$hp/electrs/cert.pem,key=$hp/electrs/key.pem,verify=0 TCP:127.0.0.1:50055 > $dp/socat.log 2>&1 &
 echo \"\$!\" > $dp/.socat1 
-socat TCP-LISTEN:50055,reuseaddr,fork TCP:127.0.0.1:50005 &
+socat TCP-LISTEN:50055,reuseaddr,fork TCP:127.0.0.1:50005 >$dp/socat.log 2>&1 &
 echo \"\$!\" > $dp/.socat2 
 " | tee $dp/start_socat_electrs.sh >/dev/null
 
