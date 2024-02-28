303d6c050da67620893fa92ea9afe2ac7afb432f
Parman <armantheparman@gmail.com>
2024-02-29 02:05:23 +1100

    dev null

--- src/other/make_socat_script.sh
+++ src/other/make_socat_script.sh
@@ -7,13 +7,13 @@ socat OPENSSL-LISTEN:50006,reuseaddr,fork,cert=$hp/electrs/cert.pem,key=$hp/elec
 echo "$!" > $dp/.socat1 
 socat TCP-LISTEN:50055,reuseaddr,fork TCP:127.0.0.1:50005 &
 echo "$!" > $dp/.socat2 
-" | tee $dp/start_socat_electrs.sh
+" | tee $dp/start_socat_electrs.sh >/dev/null
 
 #stop script (run by service file)
 echo "#!/bin/bash
 kill -9 $(cat $dp/.socat1)
 kill -9 $(cat $dp/.socat1)
-" | tee $dp/stop_socat_electrs.sh
+" | tee $dp/stop_socat_electrs.sh >/dev/null
 
 # make executable:
 sudo chmod +x $dp/stop_socat_electrs.sh
