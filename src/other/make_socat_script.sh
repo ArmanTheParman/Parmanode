61276b2e4568654df606037ba70a7d5c9ff42cd0
Parman <armantheparman@gmail.com>
2024-02-29 19:32:55 +1100

    certs order

--- src/other/make_socat_script.sh
+++ src/other/make_socat_script.sh
@@ -3,9 +3,9 @@ function make_socat_script {
 if [[ $1 == electrs ]] ; then
 #start script (run by service file)
 echo "#!/bin/bash
-socat OPENSSL-LISTEN:50006,reuseaddr,fork,cert=$hp/electrs/cert.pem,key=$hp/electrs/key.pem,verify=0 TCP:127.0.0.1:50055 > $dp/socat.log 2>&1 &
+socat OPENSSL-LISTEN:50006,reuseaddr,fork,cert=$hp/electrs/cert.pem,key=$hp/electrs/key.pem,verify=0 TCP:127.0.0.1:50055 >> $dp/socat.log 2>&1 &
 echo \"\$!\" > $dp/.socat1 
-socat TCP-LISTEN:50055,reuseaddr,fork TCP:127.0.0.1:50005 > $dp/socat.log 2>&1 &
+socat TCP-LISTEN:50055,reuseaddr,fork TCP:127.0.0.1:50005 >> $dp/socat.log 2>&1 &
 echo \"\$!\" > $dp/.socat2 
 " | tee $dp/start_socat_electrs.sh >/dev/null
 
@@ -23,9 +23,9 @@ fi
 if [[ $1 == public_pool_ui ]] ; then
 #start script (run by service file)
 echo "#!/bin/bash
-socat OPENSSL-LISTEN:5052,reuseaddr,fork,cert=$hp/public_pool_ui/cert.pem,key=$hp/public_pool_ui/key.pem,verify=0 TCP:127.0.0.1:5062 > $dp/socat.log 2>&1 &
+socat OPENSSL-LISTEN:5052,reuseaddr,fork,cert=$hp/public_pool_ui/cert.pem,key=$hp/public_pool_ui/key.pem,verify=0 TCP:127.0.0.1:5062 >> $dp/socat.log 2>&1 &
 echo \"\$!\" > $dp/.socat1_public_pool_ui
-socat TCP-LISTEN:5062,reuseaddr,fork TCP:127.0.0.1:80 > $dp/socat.log 2>&1 &
+socat TCP-LISTEN:5062,reuseaddr,fork TCP:127.0.0.1:80 >> $dp/socat.log 2>&1 &
 
 echo \"\$!\" > $dp/.socat2_public_pool_ui
 " | tee $dp/start_socat_public_pool_ui.sh >/dev/null
