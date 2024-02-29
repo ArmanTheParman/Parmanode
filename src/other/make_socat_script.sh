aa28f31a6ee41bcb7f5a2da359401b91f74c8496
Parman <armantheparman@gmail.com>
2024-02-29 22:34:07 +1100

    electrs processes

--- src/other/make_socat_script.sh
+++ src/other/make_socat_script.sh
@@ -4,16 +4,13 @@ if [[ $1 == electrs ]] ; then
 #start script (run by service file)
 echo "#!/bin/bash
 socat OPENSSL-LISTEN:50006,reuseaddr,fork,cert=$hp/electrs/cert.pem,key=$hp/electrs/key.pem,verify=0 TCP:127.0.0.1:50055 >> $dp/socat.log 2>&1 
-ps -xau | grep socat | grep 50006 | awk '{print $2}' > $dp/.socat1_public_pool_ui
-
 socat TCP-LISTEN:50055,reuseaddr,fork TCP:127.0.0.1:50005 >> $dp/socat.log 2>&1 
-ps -xau | grep socat | grep 50005 | awk '{print $2}' > $dp/.socat2_public_pool_ui
 " | tee $dp/start_socat_electrs.sh >/dev/null
 
 #stop script (run by service file)
 echo "#!/bin/bash
-kill -9 \$(cat $dp/.socat1)
-kill -9 \$(cat $dp/.socat2)
+kill -9 \$(ps -xau | grep socat | grep 50006 | awk '{print $2}') >> $dp/socat.log 2>&1
+kill -9 \$(ps -xau | grep socat | grep 50005 | awk '{print $2}') >> $dp/socat.log 2>&1
 " | tee $dp/stop_socat_electrs.sh >/dev/null
 
 # make executable:
