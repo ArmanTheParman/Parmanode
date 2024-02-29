5bb9e9ff7619923648c474ff29dca3dd14562984
Parman <armantheparman@gmail.com>
2024-02-29 19:55:41 +1100

    no deamon

--- src/other/make_socat_script.sh
+++ src/other/make_socat_script.sh
@@ -3,10 +3,11 @@ function make_socat_script {
 if [[ $1 == electrs ]] ; then
 #start script (run by service file)
 echo "#!/bin/bash
-socat OPENSSL-LISTEN:50006,reuseaddr,fork,cert=$hp/electrs/cert.pem,key=$hp/electrs/key.pem,verify=0 TCP:127.0.0.1:50055 >> $dp/socat.log 2>&1 &
-echo \"\$!\" > $dp/.socat1 
-socat TCP-LISTEN:50055,reuseaddr,fork TCP:127.0.0.1:50005 >> $dp/socat.log 2>&1 &
-echo \"\$!\" > $dp/.socat2 
+socat OPENSSL-LISTEN:50006,reuseaddr,fork,cert=$hp/electrs/cert.pem,key=$hp/electrs/key.pem,verify=0 TCP:127.0.0.1:50055 >> $dp/socat.log 2>&1 
+ps -xau | grep socat | grep 50006 | awk '{print $2}' > $dp/.socat1_public_pool_ui
+
+socat TCP-LISTEN:50055,reuseaddr,fork TCP:127.0.0.1:50005 >> $dp/socat.log 2>&1 
+ps -xau | grep socat | grep 50005 | awk '{print $2}' > $dp/.socat2_public_pool_ui
 " | tee $dp/start_socat_electrs.sh >/dev/null
 
 #stop script (run by service file)
@@ -23,11 +24,11 @@ fi
 if [[ $1 == public_pool_ui ]] ; then
 #start script (run by service file)
 echo "#!/bin/bash
-socat OPENSSL-LISTEN:5052,reuseaddr,fork,cert=$hp/public_pool_ui/cert.pem,key=$hp/public_pool_ui/key.pem,verify=0 TCP:127.0.0.1:5062 >> $dp/socat.log 2>&1 &
-echo \"\$!\" > $dp/.socat1_public_pool_ui
+socat OPENSSL-LISTEN:5052,reuseaddr,fork,cert=$hp/public_pool_ui/cert.pem,key=$hp/public_pool_ui/key.pem,verify=0 TCP:127.0.0.1:5062 >> $dp/socat.log 2>&1 
+ps -xau | grep socat | grep 5052 | awk '{print $2}' > $dp/.socat1_public_pool_ui
 
-socat TCP-LISTEN:5062,reuseaddr,fork TCP:127.0.0.1:80 >> $dp/socat.log 2>&1 &
-echo \"\$!\" > $dp/.socat2_public_pool_ui
+socat TCP-LISTEN:5062,reuseaddr,fork TCP:127.0.0.1:80 >> $dp/socat.log 2>&1 
+ps -xau | grep socat | grep 5062 | awk '{print $2}' > $dp/.socat2_public_pool_ui
 " | tee $dp/start_socat_public_pool_ui.sh >/dev/null
 
 #stop script (run by service file)
