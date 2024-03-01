ff0c05cd2e6515f4de4d59c23e877872b3e3e7ca
Parman <armantheparman@gmail.com>
2024-03-02 02:42:11 +1100

    begin coding thunberhub

--- src/Thunderhub/install_thunderhub.sh
+++ src/Thunderhub/install_thunderhub.sh
@@ -1,24 +1,18 @@
 function install_thunderhub {
-set_terminal
-please_wait
 
-install_nodejs
 #make sure bitcoin installed
 #make sure lightning running
+#make sure docker installed
+if ! which docker >/dev/null 2>&1 ; then announce "Please install Docker first." ; return 1 ; fi
 
+ #git clone --depth 1 https://github.com/apotdevin/thunderhub.git
 
-cd $hp
-git clone --depth 1 https://github.com/apotdevin/thunderhub.git
+#docker install
+docker pull apotdevin/thunderhub:v0.13.30
+docker run --rm -d -p 2999:3000/tcp apotdevin/thunderhub:v0.5.5
+
+#https://docs.thunderhub.io/setup#server-accounts
+
 installed_conf_add "thunderhub-start"
 
-export PORT=4000
-npm install
-npm run build && installed_conf_add "thunderhub-end" && success "Thunderhub" "being installed"
 }
-
-function start_thunderhub {
-cd $pp/thunderhub
-npm start
-cd -
-}
-
