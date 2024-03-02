a78fc47dc656412fe47608b5546d7f3d7d5ccef7
Parman <armantheparman@gmail.com>
2024-03-03 03:21:54 +1100

    a

--- src/Thunderhub/install_thub.sh
+++ src/Thunderhub/install_thub.sh
@@ -1,4 +1,7 @@
 function install_thunderhub {
+
+#NOT FINISHED. NEED TO DO ACCOUNTS
+
 set_terminal
 export version="v0.13.30"
 export file=$hp/thunderhub/.env.local #adding '.local' prevents overriding file when updating.
