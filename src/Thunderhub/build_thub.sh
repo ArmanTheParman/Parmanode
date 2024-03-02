cd9c9cb48dd81c905dc94dc3399803e46e9ac93b
Parman <armantheparman@gmail.com>
2024-03-03 01:42:03 +1100

    install thunderhub

--- src/Thunderhub/build_thub.sh
+++ src/Thunderhub/build_thub.sh
@@ -1,3 +1,12 @@
+function build_thub {
+cd $hp/thunderhub
+docker build -t thunderhub . | tee -a $dp/thunderhub.log || buildfailed=true
+echo "Pausing to check docker biuld"
+enter_continue
+if [[ $buildfailed == true ]] ; then unset buildfailed ; return 1 ; fi
+}
+    
+    
 # To build your own docker image with the basePath of your choice you can use docker 
 # build --build-arg BASE_PATH='/thub' -t myOwnDockerImage .
 # You can run ThunderHub behind a proxy with the following configuration (NGINX example):
--- src/Thunderhub/thub_lnd.sh
+++ src/Thunderhub/thub_lnd.sh
@@ -1,8 +1,14 @@
 function thub_lnd {
 
 lnd_rpc_port=$(cat $HOME/.lnd/lnd.conf | grep -E '^rpclisten=' | grep -Eo ':.+' | tr -d :)
-"SSO_SERVER_URL='127.0.0.1:$lnd_rpc_port'"
-rpclisten=localhost:10009
-rpclisten=localhost:10009
+swap_string "$file" "SSO_SERVER_URL=" "SSO_SERVER_URL='127.0.0.1:$lnd_rpc_port'"
+
+swap_string "$file" "SSO_CERT_PATH=" "SSO_CERT_PATH=/$HOME/.lnd/tls.cert"   
+
+swap_string "$file" "SSO_MACAROON_PATH=" "SSO_MACAROON_PATH=/$HOME/.lnd/data/chain/bitcoin/mainnet"
+
+
+SSO_MACAROON_PATH='/path/to/folder/containing/macaroons'
+
 }
 
