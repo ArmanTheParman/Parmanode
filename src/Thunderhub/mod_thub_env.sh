e65af0f42e4f8fd32af9044be444dab81812a2db
Parman <armantheparman@gmail.com>
2024-03-03 00:47:20 +1100

    d

--- src/Thunderhub/mod_thub_env.sh
+++ src/Thunderhub/mod_thub_env.sh
@@ -1,6 +1,5 @@
 function mod_thub_env {
 file=$hp/thunderhub/.env.local #adding '.local' prevents overriding file when updating.
-make_thub_env
 
 while true ; do
 set_terminal ; echo -e "
@@ -23,4 +22,46 @@ break ;;
 *) invalid ;;
 esac
 done
+
+while true ; do
+set_terminal ; echo -e "
+########################################################################################
+
+    Run over Tor?
+$green
+                              y)       yes $bright_blue    (will set to proxy 127.0.0.1:9050)
+$red
+                              n)       no
+$orange
+########################################################################################
+"
+choose "xpmq" ; read choice
+case $choice in
+q|Q) exit ;; p|P) return 1 ;; n|N) break ;;
+y)
+if ! which tor >/dev/null 2>&1 ; then install_tor ; fi
+swap_string "$file" "TOR_PROXY_SERVER=" "TOR_PROXY_SERVER=socks://127.0.0.1:9050"
+;;
+*)
+invalid ;;
+esac
+done
+
+set_terminal ; echo -e "
+########################################################################################
+
+    Please note, Thunderhub will allow connections from computers without cookie
+    authentication, which is not considered best security practice - It's fine for
+    your own home network, that's secure, but if you were to expose the wallet to
+    the internet, eg access via a domain, open ports on your router etc, then this
+    method is suboptimal. 
+    
+    I may incorporate cookie authentication options later, but for now this will be 
+    perfectly sufficient and private for most people. If you have the skills you can 
+    modify the settings yourself insted of waiting for me.
+
+########################################################################################
+"
+
+
 }
--- src/electrumx/electrumx_dependencies.sh
+++ src/electrumx/electrumx_dependencies.sh
@@ -3,9 +3,9 @@ update_computer silent || paus
 sudo apt-get remove -y libdpkg-perl #my machine had a newer version, preventing build-essential to be installed
 sudo apt-get install -y --fix-broken --no-install-recommends gcc g++ fakeroot python3 python3-pip python3-dev \
 build-essential librocksdb-dev libsnappy-dev zlib1g-dev libbz2-dev libgflags-dev liblz4-dev libzstd-dev \
-rocksdb-tools librocksdb-6.11 | tee $dp/electrumx.log
-sudo python3 -m pip install --upgrade pip | tee $dp/electrumx.log
-sudo pip3 install virtual env | tee $dp/electrumx.log
+rocksdb-tools librocksdb-6.11 | tee -a $dp/electrumx.log
+sudo python3 -m pip install --upgrade pip | tee -a $dp/electrumx.log
+sudo pip3 install virtual env | tee -a $dp/electrumx.log
 debug "virtual env version... $(virtualenv --version)" 
 #virtual env install
 
@@ -13,7 +13,7 @@ debug "virtual env version... $(virtualenv --version)"
 source parmanenv/bin/activate
 debug "virtual environment set? ... $VIRTUAL_ENV"
 cd $hp/electrumx
-pip install plyvel | tee $dp/electrumx.log
+pip install plyvel | tee -a $dp/electrumx.log
 python3 -c 'import plyvel' || { announce "plyvel failed. Aborting." ; return 1 ; } ; } &&
 deactivate #exits virtual env
 debug "after deactivate command"
