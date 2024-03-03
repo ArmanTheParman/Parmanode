9bff51d8b92ca50dd9000763bd147ea1e36238fd
Parman <armantheparman@gmail.com>
2024-03-03 22:20:38 +1100

    /

--- src/Thunderhub/make_thunderhub_account.sh
+++ src/Thunderhub/make_thunderhub_account.sh
@@ -5,7 +5,7 @@ echo -e "
 accounts:
   - name: 'Account 1'
     serverUrl: '127.0.0.1:$lnd__rpc_port'
-    macaroonPath: '$HOME/.lnd/data/chain/bitcoin/mainnet/'
+    macaroonPath: '$HOME/.lnd/data/chain/bitcoin/mainnet'
     certificatePath: '$HOME/.lnd/tls.cert'
     password: '$password'                        
 }" | tee $hp/thunderhub/account_1.yaml >/dev/null 2>&1
