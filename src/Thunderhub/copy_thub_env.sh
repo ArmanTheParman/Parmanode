c0f541ce8d980a35ad618df788c4de8ae3f82dfc
Parman <armantheparman@gmail.com>
2024-03-03 20:52:07 +1100

    account path

--- src/Thunderhub/copy_thub_env.sh
+++ src/Thunderhub/copy_thub_env.sh
@@ -58,7 +58,7 @@ NO_VERSION_CHECK=true
 # -----------
 # Account Configs
 # -----------
-ACCOUNT_CONFIG_PATH='$HOME/.thunderhub/account_1.yaml'
+ACCOUNT_CONFIG_PATH='$hp/thunderhub/account_1.yaml'
 MASTER_PASSWORD_OVERRIDE='password'
 # YML_ENV_1=''
 # YML_ENV_2=''
