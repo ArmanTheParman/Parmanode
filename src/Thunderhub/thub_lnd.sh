d3da1f2ddbc680ee39886d01a0a0e864557b485d
Parman <armantheparman@gmail.com>
2024-03-03 21:09:02 +1100

    d

--- src/Thunderhub/thub_lnd.sh
+++ src/Thunderhub/thub_lnd.sh
@@ -3,12 +3,9 @@ function thub_lnd {
 lnd_rpc_port=$(cat $HOME/.lnd/lnd.conf | grep -E '^rpclisten=' | grep -Eo ':.+' | tr -d :)
 swap_string "$file" "SSO_SERVER_URL=" "SSO_SERVER_URL='127.0.0.1:$lnd_rpc_port'"
 
-swap_string "$file" "SSO_CERT_PATH=" "SSO_CERT_PATH=/$HOME/.lnd/tls.cert"   
+swap_string "$file" "SSO_CERT_PATH=" "SSO_CERT_PATH=$HOME/.lnd/tls.cert"   
 
-swap_string "$file" "SSO_MACAROON_PATH=" "SSO_MACAROON_PATH=/$HOME/.lnd/data/chain/bitcoin/mainnet"
-
-
-SSO_MACAROON_PATH='/path/to/folder/containing/macaroons'
+swap_string "$file" "SSO_MACAROON_PATH=" "SSO_MACAROON_PATH=$HOME/.lnd/data/chain/bitcoin/mainnet"
 
 }
 
