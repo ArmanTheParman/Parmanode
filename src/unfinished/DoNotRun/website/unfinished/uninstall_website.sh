16a2aeaaea683a7476f300f85d6958635ea8af27
Parman <armantheparman@gmail.com>
2024-03-08 00:30:06 +1100

    website

--- src/unfinished/DoNotRun/website/unfinished/uninstall_website.sh
+++ src/unfinished/DoNotRun/website/unfinished/uninstall_website.sh
@@ -18,9 +18,33 @@ if [[ $choice == "y" || $choice == "Y" ]] ; then true
     return 1
     fi
 
+while true ; do
+set_terminal ; echo -e "
+########################################################################################
+
+    Do you want Parmanode to delete your website directory as well? This is the 
+    directory with your website pages, located at: $cyan
+
+        $hp/website/
+$orange
+    Please choose: $red
+                             delete)     Delete
+$green
+                             l)          Leave it
+$orange
+########################################################################################
+" ; choose "xpmq" ; read choice ; set_terminal
+case $choice in q|Q) exit 0 ;; p|P) return 1 ;; l|L) break ;;
+delete) 
+sudo rm -rf $hp/website >/dev/null 2>&1
+;;
+*) invalid ;;
+esac
+done
+
+installed_conf_remove "website-end"
 
 sudo apt-get remove mariadb-server -y
-
 
 while true ; do
 set_terminal ; echo -e "
@@ -48,5 +72,7 @@ invalid
 ;;
 esac
 done
-    
+
+
+
 }
