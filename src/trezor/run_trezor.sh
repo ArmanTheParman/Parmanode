75f2e967a56a33d128181278f77af9dd98256722
Parman <armantheparman@gmail.com>
2024-03-09 01:37:52 +1100

    trezor &

--- src/trezor/run_trezor.sh
+++ src/trezor/run_trezor.sh
@@ -5,7 +5,7 @@ open /Applications/"Trezor Suite.app"
 fi
 
 if [[ $OS == Linux ]] ; then
-nohup ~/parmanode/trezor/Trezor*AppImage >/dev/null 2>&1
+nohup ~/parmanode/trezor/Trezor*AppImage >/dev/null 2>&1 &
 fi
 set_terminal ; echo -e "
 ########################################################################################
