7f1cc97d747ba43512ae1d2e8764564d7d17e6ae
deerwhisper2310 <142937144+deerwhisper2310@users.noreply.github.com>
2024-03-09 18:01:08 -0500

    Update install_docker_linux.sh
    typos

--- src/docker/install_docker_linux.sh
+++ src/docker/install_docker_linux.sh
@@ -56,7 +56,7 @@ set_terminal "pink" ; echo -e "
 ######################################################################################## 
 $orange
     In order for Docker to run properly, the computer must be restarted. It is safe
-    to reboot the computer for the all the software Parmanode has installed. 
+    to reboot the computer for all the software Parmanode has installed. 
     
     Eg, for Bitcoin Core, if it is syncing, a reboot will not harm it - it knows how 
     to shut down safely, and should restart on its own when the computer boots up.
