9bff24b0c8c657145b396d9231ea6514a2a78850
Parman <armantheparman@gmail.com>
2024-03-15 23:29:52 +1100

    test

--- src/electrumx/uninstall_electrumx.sh
+++ src/electrumx/uninstall_electrumx.sh
@@ -16,24 +16,24 @@ if [[ $choice == "y" || $choice == "Y" ]] ; then true
     else 
     return 1
     fi
-if [[ $test == 1 ]] ; then echo 1 ; fi
+if [[ $test == 1 ]] ; then echo 1 ; enter_continue ; fi
 #leave here for old installations. Electrum x new installs don't use nginx
 nginx_stream electrumx remove
-if [[ $test == 1 ]] ; then echo 2 ; fi
+if [[ $test == 1 ]] ; then echo 2 ; enter_continue ; fi
 
 electrumx_tor_remove uninstall || log electrumx "electrumx_tor_remove return 1"
-if [[ $test == 1 ]] ; then echo 3 ; fi
+if [[ $test == 1 ]] ; then echo 3 ; enter_continue ; fi
 
 if [[ $OS == Linux ]] ; then
 sudo systemctl stop electrumx.service >/dev/null 2>&1
 sudo systemctl disable electrumx.service >/dev/null 2>&1
 sudo rm /etc/systemd/system/electrumx.service >/dev/null 2>&1
 fi
-if [[ $test == 1 ]] ; then echo 4 ; fi
+if [[ $test == 1 ]] ; then echo 4 ; enter_continue ; fi
 
 sudo rm -rf $hp/electrumx
 pip3 uninstall electrumx || log electrumx "pip3 uninstall electrumx fail"
-if [[ $test == 1 ]] ; then echo 5 ; fi
+if [[ $test == 1 ]] ; then echo 5 ; enter_continue ; fi
 debug "/usr/local/bin/elecrumx* removed?"
 sudo rm -rf $HOME/.local/bin/electrumx* >/dev/null 2>&1
 parmanode_conf_remove "drive_electrumx"
