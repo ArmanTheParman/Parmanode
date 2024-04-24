2f0070613930d9064c0de924dc1469e16eacbfc8
Parman <armantheparman@gmail.com>
2024-04-24 17:24:49 +1000

    assemble

--- src/ParmaRaid/assemble_RAID.sh
+++ src/ParmaRaid/assemble_RAID.sh
@@ -1,5 +1,14 @@
 function assemble_RAID {
 sudo mdadm --assemble --scan
+echo "
+Mount too?  $green y $red n $orange
+
+"
+choose x ; set_terminal 
+if [[ $choose == x ]] ; then
+mount_RAID || return 1
+fi
+
 enter_continue
 return 0
 }
