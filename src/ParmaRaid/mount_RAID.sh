11dfb90349fc3578574292ea86e51318002bf7cf
Parman <armantheparman@gmail.com>
2024-04-24 18:23:21 +1000

    mount

--- src/ParmaRaid/mount_RAID.sh
+++ src/ParmaRaid/mount_RAID.sh
@@ -1,4 +1,8 @@
 function mount_RAID {
+if [[ $(sudo mdadm --detail | wc -l) == 0 ]] ; then
+announce "No active RAID devices. Might need to 'assemble' first."
+fi
+
 for i in $(sudo mdadm --detail --scan | awk '{print $2}') ; do
 mount | grep -q "$i" && { set_terminal ; echo "$1 is already mounted" ; sleep 1.5 ; continue ; }
 set_terminal
