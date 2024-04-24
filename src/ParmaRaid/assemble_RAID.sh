0305975df7609e0eb80598a08fb726f0031438bd
Parman <armantheparman@gmail.com>
2024-04-24 17:27:05 +1000

    choice y not x

--- src/ParmaRaid/assemble_RAID.sh
+++ src/ParmaRaid/assemble_RAID.sh
@@ -1,11 +1,11 @@
 function assemble_RAID {
 sudo mdadm --assemble --scan
-echo "
+echo  -e "
 Mount too?  $green y $red n $orange
 
 "
 choose x ; read choice ; set_terminal 
-if [[ $choice == x ]] ; then
+if [[ $choice == y ]] ; then
 mount_RAID || return 1
 fi
 
