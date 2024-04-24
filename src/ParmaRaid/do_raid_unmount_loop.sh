198d6ec9158dad989dc2d99f7614e1fdb8e52ed9
Parman <armantheparman@gmail.com>
2024-04-24 17:11:03 +1000

    umount loop

--- src/ParmaRaid/do_raid_unmount_loop.sh
+++ src/ParmaRaid/do_raid_unmount_loop.sh
@@ -2,7 +2,7 @@ function do_RAID_unmount_loop {
 
 mount | grep -q '/dev/md' || { announce "No RAIDs mounted" ; return 1 ; }
 
-for i in $(mount | grep '/dev/md') ; do
+for i in $(mount | grep '/dev/md' | awk '{print $1}') ; do
 set_terminal ; echo -e "
 ########################################################################################
 
@@ -17,7 +17,7 @@ case $choice in
 q|Q) exit ;; p|P) return 1 ;; m|M) back2main ;;
 n) continue ;;
 y)
-sudo umount $(echo $i | awk '{print $1}') >/dev/null && success "RAID unmounted"
+sudo umount $i >/dev/null && success "RAID unmounted"
 ;;
 *)
 continue ;;
