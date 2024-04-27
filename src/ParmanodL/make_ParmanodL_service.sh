164359b7fc1e2656735ceb4258aa4d710e9ada6f
Parman <armantheparman@gmail.com>
2024-04-28 02:48:37 +1000

    smol bug

--- src/ParmanodL/make_ParmanodL_service.sh
+++ src/ParmanodL/make_ParmanodL_service.sh
@@ -1,5 +1,9 @@
 function make_ParmanodL_service {
 
+#available in container at /mnt/ParmanodL
+cp /home/parman/parman_programs/parmanode/src/ParmanodL/parmanodl_gui $HOME/ParmanodL/parmanodl_gui
+
+
 cat << 'EOS' >> ~/ParmanodL/chroot_function.sh 
 echo "
 [Unit]
@@ -19,7 +23,7 @@ WantedBy=multi-user.target
 " | tee /tmp/parmanodl_gui.service 
 mv /tmp/parmanodl_gui.service /tmp/mnt/raspi/etc/systemd/system/parmanodl_gui.service
 
-cp /home/parman/parman_programs/parmanode/src/ParmanodL/parmanodl_gui /home/parman/.parmanode/parmanodl_gui
+chroot /tmp/mnt/raspi /bin/bash -c 'mv /mnt/ParmanodL/parmanodl_gui /home/parman/.parmanode/parmanodl_gui'
 chroot /tmp/mnt/raspi /bin/bash -c 'chown -R parman:parman /home/parman/.parmanode'
 chroot /tmp/mnt/raspi /bin/bash -c 'chmod +x /home/parman/.parmanode/parmanodl_gui'
 chroot /tmp/mnt/raspi /bin/bash -c 'systemctl enable parmanodl_gui.service"
