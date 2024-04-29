8870786557aa9f3a9bb1ca8d2dc4f12fe1620698
Parman <armantheparman@gmail.com>
2024-04-30 00:43:46 +1000

    parmanodL torrent

---
+++ src/ParmanodL/download_ParmanodL_image.sh
@@ -0,0 +1,78 @@
+function download_ParmanodL_image {
+while true ; do
+set_terminal
+set_terminal ; echo -e "
+########################################################################################
+
+    There are various ways to get the$cyan ParmanodL-PI-v3.0.0.img$orange file.
+
+$green        t)$orange        Torrent (downloads torrent link to desktop)
+
+$green        dl)$orange       Download img file from archive.org
+
+$green        pt)$orange       Parman's Tor server
+
+########################################################################################
+"
+choose xpmq ; read choice ; set_terminal
+case $choice in 
+q|Q) exit ;; p|P) return 1 ;;
+t)
+cp $pn/src/ParmanodL/ParmanodL-PI-v3.0.0.torrent $HOME/Downloads/
+announce "
+    ParmanodL-PI-v3.0.0.torrent can be found in your Downloads
+    directory. Use a program like qBittorrent (available with Parmanode)
+    to import it and start downloading. Please leave it open and seed
+    for others if you can.
+
+    There is info to read in the torrent file. If you need my public key
+    you can get it here: $cyan
+
+    https://armantheparman.com/pubkey $orange
+
+    and like this: $cyan
+
+    gpg --keyserver keyserver.ubuntu.com --recv-key E7C061D4C5E5BC98 $orange
+
+    Use the command $cyan
+
+    tar -xvf ParmanodL-PI-v3.0.0.tar $orange
+
+    to extract the data. You can then flash the .img.gz file with Balena
+    Etcher, and you can also verify the signature file. $cyan
+
+    gpg --keyserver keyserver.ubuntu.com --recv-key E7C061D4C5E5BC98 
+    gpg --verify ParmanodL-PI-v3.0.0.img.xz.sig ParmanodL-PI-v3.0.0.img.xz
+    $orange
+    "
+return 0
+;;
+dl)
+set_terminal
+cd $HOME/Downloads/
+curl -LO https://archive.org/download/parmanod-l-pi-v-3.0.0.img/ParmanodL-PI-v3.0.0.img.xz
+curl -LO https://archive.org/download/parmanod-l-pi-v-3.0.0.img/ParmanodL-PI-v3.0.0.img.xz.sig
+announce "ParmanodL-Pi-v3.0.0.img.xz and the corresponding signature file
+    has been downloaded to your Downloads directory.
+
+    You can then flash the .img.gz file with Balena
+    Etcher, and you can also verify the signature file. $cyan
+    gpg --keyserver keyserver.ubuntu.com --recv-key E7C061D4C5E5BC98 
+    gpg --verify ParmanodL-PI-v3.0.0.img.xz.sig ParmanodL-PI-v3.0.0.img.xz
+    $orange"
+curl -LO
+return 0
+;;
+pt)
+announce "The onion address to download this from is here... $bright_blue
+
+    https://7zatnd4wode263mlx5tmvvbyon4ej64noftfp3lloayhiwbogm63kdad.onion:7777/
+$orange"
+return 0
+;;
+*)
+invalid
+;;
+esac
+done
+}
--- src/ParmanodL/get_parmanodl.sh
+++ src/ParmanodL/get_parmanodl.sh
@@ -5,13 +5,19 @@ set_terminal ; echo -e "
 $green
                                  P A R M A N O D L
 $orange
-$cyan    ParmanodL$orange is a Raspberry Pi that runs Parmanode software preconfigured.
+$cyan    ParmanodL$orange is a computer that runs Parmanode software preconfigured.
+    
+    Currently this packaged option is available for Pi4/5.
 
     INSTALLATION:
 
     An image file (.img) file which contains the operating system will be downloaded
-    on this computer, configured, and then flashd to a microSD card (have it ready, 
-    minimum 16GB), for use in the Raspberry Pi computer. 
+    on this computer, then configured with the help of a Docker container
+    running a portable Linux OS, and then flashd to a microSD card (have it ready, 
+    minimum 16GB, best 64GB+), for use in the Raspberry Pi computer. 
+
+    Alternatively, you can opt to just download the image file and flash the image
+    yourself to the microSD card.
 
     You will be able to connect to the Pi via a program call SSH, which is a window 
     into the device's terminal screen. There'll be an icon for that on the desktop on
@@ -20,7 +26,13 @@ $cyan    ParmanodL$orange is a Raspberry Pi that runs Parmanode software preconf
     You can also hook up a moniter/mouse/keyboard to the Pi and use it as a full 
     desktop computer.
 
-    Proceed with installation?   $cyan  y   or   n$orange    then <enter>
+    Proceed with installation? 
+
+$green                y)$orange        Create the image file on this computer
+
+$green                d)$orange        I'll just download the ready made one, thanks
+
+$red                n)$orange        Get me out of here
 
 ########################################################################################
 "
@@ -30,6 +42,7 @@ m) return 0 ;;
 p|P) return 0 ;;
 n|N|no|NO) return 1 ;; 
 y|Y|Yes|YES|yes) break ;;
+d) download_ParmanodL_image ; return 1 ;;
 q|Q|Quit|QUIT) exit 0 ;;
 *) invalid ;;
 esac
