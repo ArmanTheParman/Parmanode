c69e264967d168b08852fe88d1f6b08d5a7a0f01
Parman <armantheparman@gmail.com>
2024-04-30 20:44:02 +1000

    d

--- src/ParmaWallet/recovery_passphrase.py
+++ src/ParmaWallet/recovery_passphrase.py
@@ -7,6 +7,7 @@ from functions import *
 from variables import * 
 
 aa = BIP32_master_node()
+print(aa)
 exit
 
 # charlist = ( b'a', b'b', b'c', b'c', b'd', b'e', b'f', b'g', b'h', b'i', b'j', b'k', b'l', b'm', b'n', b'o', b'p', b'q', b'r', b's', b't', b'u', b'v', b'w', b'x', b'y', b'z')
