349d1e18e6289d513dceae3c034634a58c5d0598
Parman <armantheparman@gmail.com>
2024-05-01 02:37:32 +1000

    a

--- src/ParmaWallet/functions2/pubkey_to_bech32.py
+++ src/ParmaWallet/functions2/pubkey_to_bech32.py
@@ -10,6 +10,24 @@ import os
 from bip_utils import *
 from bitcoinlib import *
 
+def convertbits_custom(data, frombits, tobits, pad=True):
+    """General power-of-2 base conversion."""
+    acc = 0
+    bits = 0
+    ret = []
+    maxv = (1 << tobits) - 1
+    max_acc = (1 << (frombits + tobits - 1)) - 1
+    for value in data:
+        if value < 0 or (value >> frombits):
+            return None
+        acc = ((acc << frombits) | value) & max_acc
+        bits += frombits
+        while bits >= tobits:
+            bits -= tobits
+            ret.append((acc >> bits) & maxv)
+    if pad and bits:
+        ret.append((acc << (tobits - bits)) & maxv)
+    return ret
 
 def pubkey_to_bech32(pubkey, version=0, hrp='bc'):
     # Compute the SHA-256 hash of the public key
@@ -20,7 +38,7 @@ def pubkey_to_bech32(pubkey, version=0, hrp='bc'):
     
     # Convert to Bech32 address
     # 0 is the witness version for "bc1" addresses (for version 0 SegWit addresses)
-    five_bit_r = bech32.convertbits(ripemd_pubkey, 8, 5)
+    five_bit_r = convertbits_custom(ripemd_pubkey, 8, 5)
     return bech32.bech32_encode(hrp, [version] + five_bit_r)
 
     
