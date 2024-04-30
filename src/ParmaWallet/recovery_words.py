a5ca82c7fb040fb4a79bcda0927df6f2355594db
Parman <armantheparman@gmail.com>
2024-05-01 04:02:46 +1000

    pause

--- src/ParmaWallet/recovery_words.py
+++ src/ParmaWallet/recovery_words.py
@@ -48,7 +48,8 @@ with open(file_store , "a") as file:  # Open in append mode to add to the file
                 print(address)
                 file.write(address + "\n")  # Convert result to string and append a newline
                 if address == 'bc1qfmqe4296g04eaczsns22exhqhmlvdx57xzx8tw':
-                    input("words found, i j k:" , mnemonic_end_string)
+                    print("words found, i j k:" , mnemonic_end_string)
+                    input("<enter> to stop program and clear screan")
  
 ########################################################################################
 
