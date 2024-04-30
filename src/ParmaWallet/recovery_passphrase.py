7d01cf0b85d56e0dd271f559c1558640308b093b
Parman <armantheparman@gmail.com>
2024-04-30 23:46:43 +1000

    a

--- src/ParmaWallet/recovery_passphrase.py
+++ src/ParmaWallet/recovery_passphrase.py
@@ -45,7 +45,10 @@ k = int(k)
 test_string = known_string + bin(i)[2:].zfill(11) + bin(j)[2:].zfill(11) + bin(k)[2:].zfill(7)
 test_string = int(test_string, 2)
 test_string = test_string.to_bytes(32, 'big')
-print(int(hash256(test_string)[:1], 2))
+hashbyte = hash256(test_string)[:1]
+hashval = int.from_bytes(hashbyte, 'big')
+print (hashval)
+
 
        
        
