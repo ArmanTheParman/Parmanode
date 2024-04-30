c6f0edf8daea3843f49b2b75b0caa4a7ada2a0d8
Parman <armantheparman@gmail.com>
2024-05-01 02:17:43 +1000

    import

--- src/ParmaWallet/functions/__init__.py
+++ src/ParmaWallet/functions/__init__.py
@@ -2,5 +2,3 @@ from .PW_Base58 import *
 from .PW_functions import *
 from .PW_signing import *
 from .opcodes import *
-from ..functions2.pubkey_to_bech32 import *
-from ..functions2.get_all_child_keys import *
---
+++ src/ParmaWallet/functions2/__init__.py
@@ -0,0 +1,2 @@
+from get_all_child_keys import *
+from pubkey_to_bech32 import *
