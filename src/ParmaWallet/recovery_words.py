445c85be279d4e27c8a6e1d2a81b9cc8aba155ab
Parman <armantheparman@gmail.com>
2024-05-01 01:47:32 +1000

    a

--- src/ParmaWallet/recovery_words.py
+++ src/ParmaWallet/recovery_words.py
@@ -17,18 +17,18 @@ def get_all_child_keys(depth: str='f', mnemonic: str=None, passphrase: str=''):
     e = child_key(d, depth=1, account=0, hardened=False, serialize=False) #int/ext
     f = child_key(e, depth=1, account=0, hardened=False, serialize=True) #address
 
-    # if depth == 'a':
-    #     return a
-    # if depth == 'b':
-    #     return b
-    # if depth == 'c':
-    #     return c
-    # if depth == 'd':
-    #     return d
-    # if depth == 'e':
-    #     return e
-    # if depth == 'f':
-    #     return f
+    if depth == 'a':
+        return a
+    if depth == 'b':
+        return b
+    if depth == 'c':
+        return c
+    if depth == 'd':
+        return d
+    if depth == 'e':
+        return e
+    if depth == 'f':
+        return f
     
 # def make_segwit_address(keypair_depth, address_index: int=0):
 #     return  
