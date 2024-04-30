07084efe71eb119b7faacbd260a71a1bf1e99bfe
parman <armantheparman@protonmail.com>
2024-04-30 13:39:04 +1000

    d

--- src/ParmaWallet/seed_iteration.py
+++ src/ParmaWallet/seed_iteration.py
@@ -16,30 +16,35 @@ y = int(x)
 
 
 def seed_checksum(decimal_array):
-    #partial seed is a string
-    print(decimal_array) 
-
+    result = []
+    for i in decimal_array:
+        result.append(bin(i)[2:])
+    print(result)
 
 
 
 ########################################################################################
-da = []
-da.append(0)
-da.append(2047)
+# da = []
+# da.append(0)
+# da.append(2047)
 
-decimal_array = [1,2,3,4]
-binary_array = [b'00000000001', b'002']
+# decimal_array = [1,2,3,4]
+# binary_array = [b'00000000001', b'002']
 
-print( b'01' + b'01')
+# print( b'01' + b'01')
 
-def binary_string_to_int(binary):
-    int(binary)
+# def binary_string_to_int(binary):
+#     int(binary)
 
-print("xxxx")
+# print("xxxx")
 
-b1=int('111000',2)
-for i in range(7):
-    b1+=1
-    print(bin(b1)[2:])
-    b3=b1.to_bytes(1, 'big')
-    print(hash256(b3))
+# b1=int('111000',2)
+# for i in range(7):
+#     b1+=1
+# #    print(bin(b1)[2:])
+#     b3=b1.to_bytes(1, 'big')
+# #    print(hash256(b3))
+
+########################################################################################
+
+seed_checksum([1,2,3])
