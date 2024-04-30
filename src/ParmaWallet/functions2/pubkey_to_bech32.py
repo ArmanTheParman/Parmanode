ace19c915b9a56dd483377d3ac512213987ba23a
Parman <armantheparman@gmail.com>
2024-05-01 03:05:37 +1000

    a

--- src/ParmaWallet/functions2/pubkey_to_bech32.py
+++ src/ParmaWallet/functions2/pubkey_to_bech32.py
@@ -43,6 +43,6 @@ def pubkey_to_bech32(pubkey, version=0, hrp='bc'):
     # Convert to Bech32 address
     # 0 is the witness version for "bc1" addresses (for version 0 SegWit addresses)
     five_bit_r = convertbits_custom(ripemd_pubkey, 8, 5)
-    return bech32.Bech32Encoder.Encode(hrp, [version] + five_bit_r)
+    return bech32.bech32_encode(hrp, [version] + five_bit_r)
 
     
