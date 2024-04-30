de433e4cdab9b09fe2f95550b0214a11c4ddaa7a
Parman <armantheparman@gmail.com>
2024-04-30 20:27:45 +1000

    test

--- src/ParmaWallet/recovery_passphrase.py
+++ src/ParmaWallet/recovery_passphrase.py
@@ -6,6 +6,10 @@ from classes import *
 from functions import *
 from variables import * 
 
+aa = BIP32_master_node()
+print (aa)
+exit
+
 charlist = ( b'a', b'b', b'c', b'c', b'd', b'e', b'f', b'g', b'h', b'i', b'j', b'k', b'l', b'm', b'n', b'o', b'p', b'q', b'r', b's', b't', b'u', b'v', b'w', b'x', b'y', b'z')
 mnemonic = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
 for ii in charlist:
