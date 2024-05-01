20507c2b692cf92069136f50742d91a971037c84
Parman <armantheparman@gmail.com>
2024-05-02 02:23:27 +1000

    d

--- src/ParmaWallet/PW_dependencies.sh
+++ src/ParmaWallet/PW_dependencies.sh
@@ -1,21 +1,21 @@
 function PW_dependencies {
-
+debug "pause before skip"
 if [[ $1 != skip ]] ; then
 sudo apt-get install libgmp-dev #?cargo, not sure
 sudo pip3 install bitcoinlib base48 bech32
 fi
-
+debug "pause after skip"
 opensslconf="$(openssl info -configdir)/openssl.cnf"
-
+debug "pause a"
 if [[ -e ${opensslconf}_backup ]] ; then
 sudo cp ${opensslconf}_backup $opensslconf  #restores original
 fi
-
+debug "pause b"
 sudo cp $opensslconf ${opensslconf}_backup #backs up original
-
+debug "pause c"
 swap_string "$opensslconf" "# activate = 1" "activate = 1
 
 [legacy sect]
 activate = 1"
-debug "pause"
+debug "pause d"
 }
