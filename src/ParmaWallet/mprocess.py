c7b55cad03121393ae7a7c7ae0228d54fa7dcada
Parman <armantheparman@gmail.com>
2024-05-01 17:07:46 +1000

    a

--- src/ParmaWallet/mprocess.py
+++ src/ParmaWallet/mprocess.py
@@ -5,6 +5,6 @@ from functions import *
 from functions2 import *
 from variables import *
 import unicodedata, hashlib, binascii, hmac
-import recovery_words
+from recovery_words import *
  
 dosearch()   
