ac4266b3eee5074faaf8f9dd70f20378be78e8cd
Parman <armantheparman@gmail.com>
2024-05-01 18:18:08 +1000

    a

--- src/ParmaWallet/recovery_words.py
+++ src/ParmaWallet/recovery_words.py
@@ -51,6 +51,9 @@ def dosearch(file_store: str, i0: int=0, i1: int=2048, j0: int=0, j1: int=2048):
                     file.write(address + "\n")  # Convert result to string and append a newline
                     if address == 'bc1qfmqe4296g04eaczsns22exhqhmlvdx57xzx8tw':
                         print("words found, i j k:" , mnemonic_end_string)
+                        with open ("/home/parman/Desktop/SUCCESS.TXT", 'w') as file:
+                            file.write("SUCCESS")
+                        return 0
     
 ########################################################################################
 
