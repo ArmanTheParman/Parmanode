ca9afdf3058043ee2e1123d77dad1e2bfe70c237
Parman <armantheparman@gmail.com>
2024-05-01 16:56:04 +1000

    dosearch

---
+++ src/ParmaWallet/mprocess.py
@@ -0,0 +1,4 @@
+import multiprocessing
+
+def worker1():
+    
--- src/ParmaWallet/recovery_words.py
+++ src/ParmaWallet/recovery_words.py
@@ -7,51 +7,54 @@ import unicodedata, hashlib, binascii, hmac
 
 ########################################################################################
 
-file_path = './english.txt'
-file_store = '/home/parman/Desktop/file_store.txt'
-with open (file_path, 'r') as file:
-    seedlist = file.readlines()
+def dosearch():
 
-def word_look_up(value):
-    return seedlist[value].strip()
+    file_path = './english.txt'
+    file_store = '/home/parman/Desktop/file_store.txt'
+    with open (file_path, 'r') as file:
+        seedlist = file.readlines()
 
-known_string = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon" 
+    def word_look_up(value):
+        return seedlist[value].strip()
 
-known_string_bin_string = 21 * "00000000000"
-known_string_int = int(known_string_bin_string)
-valid_combinations = []
+    known_string = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon" 
 
-with open(file_store , "w") as file:  # truncates any existing file and starts over
+    known_string_bin_string = 21 * "00000000000"
+    known_string_int = int(known_string_bin_string)
+    valid_combinations = []
 
-    for i in range(0, 2048):
-        for j in range (0, 2048):
-            for k in range (0, 8): 
-                test_string = known_string_bin_string + bin(i)[2:].zfill(11) + bin(j)[2:].zfill(11) + bin(k)[2:].zfill(3)
-                test_string_int = int(test_string, 2)
-                test_string_bytes = test_string_int.to_bytes(32, 'big')
-                hashbyte = hash256(test_string_bytes)[:1]
-    #            print("hashbyte", hashbyte)
-                hashval_int = int.from_bytes(hashbyte, 'big')
-    #            print("hashval_int", hashval_int)
-                hashval_string = bin(hashval_int)[2:].zfill(8)
-    #            print ("hashval_string", hashval_string)
-    #            print ('ijk' , i , j, k)
-                final_word_val = k * 256 + hashval_int
-    #            print( 'last 3 indexes' , i , j, final_word_val)
+    with open(file_store , "w") as file:  # truncates any existing file and starts over
 
-                mnemonic_end_string = word_look_up(i) + ' ' + word_look_up(j) + ' ' + word_look_up(final_word_val)
-#                print(mnemonic_end_string)
-                complete_string = known_string + ' ' + mnemonic_end_string
-                test_keypair=get_all_child_keys('f', mnemonic=complete_string)
-    #            print(len(test_keypair.public_key), type(test_keypair.public_key))
-                address=pubkey_to_bech32_custom(test_keypair.public_key)
-                print(address)
-                file.write(address + "\n")  # Convert result to string and append a newline
-                if address == 'bc1qfmqe4296g04eaczsns22exhqhmlvdx57xzx8tw':
-                    print("words found, i j k:" , mnemonic_end_string)
-                    input("<enter> to stop program")
-                    exit()
- 
+        for i in range(0, 2048):
+            for j in range (0, 2048):
+                for k in range (0, 8): 
+                    test_string = known_string_bin_string + bin(i)[2:].zfill(11) + bin(j)[2:].zfill(11) + bin(k)[2:].zfill(3)
+                    test_string_int = int(test_string, 2)
+                    test_string_bytes = test_string_int.to_bytes(32, 'big')
+                    hashbyte = hash256(test_string_bytes)[:1]
+        #            print("hashbyte", hashbyte)
+                    hashval_int = int.from_bytes(hashbyte, 'big')
+        #            print("hashval_int", hashval_int)
+                    hashval_string = bin(hashval_int)[2:].zfill(8)
+        #            print ("hashval_string", hashval_string)
+        #            print ('ijk' , i , j, k)
+                    final_word_val = k * 256 + hashval_int
+        #            print( 'last 3 indexes' , i , j, final_word_val)
+
+                    mnemonic_end_string = word_look_up(i) + ' ' + word_look_up(j) + ' ' + word_look_up(final_word_val)
+    #                print(mnemonic_end_string)
+                    complete_string = known_string + ' ' + mnemonic_end_string
+                    test_keypair=get_all_child_keys('f', mnemonic=complete_string)
+        #            print(len(test_keypair.public_key), type(test_keypair.public_key))
+                    address=pubkey_to_bech32_custom(test_keypair.public_key)
+                    print(address)
+                    file.write(address + "\n")  # Convert result to string and append a newline
+                    if address == 'bc1qfmqe4296g04eaczsns22exhqhmlvdx57xzx8tw':
+                        print("words found, i j k:" , mnemonic_end_string)
+                        input("<enter> to stop program")
+                        exit()
+    
 ########################################################################################
 
 ########################################################################################
+dosearch
