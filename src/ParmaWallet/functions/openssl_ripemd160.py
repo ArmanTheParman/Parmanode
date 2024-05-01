ce2012a6a4358860c4d7ca8517273ecd0f1c4fb9
Parman <armantheparman@gmail.com>
2024-05-02 02:57:27 +1000

    ripemd160, 3

--- src/ParmaWallet/functions/openssl_ripemd160.py
+++ src/ParmaWallet/functions/openssl_ripemd160.py
@@ -4,25 +4,38 @@ import ctypes.util
 # Load the OpenSSL library
 libcrypto = ctypes.CDLL(ctypes.util.find_library('crypto'))
 
-# Set up the RIPEMD-160 functions
-libcrypto.RIPEMD160_Init.argtypes = [ctypes.c_void_p]
-libcrypto.RIPEMD160_Update.argtypes = [ctypes.c_void_p, ctypes.c_void_p, ctypes.c_size_t]
-libcrypto.RIPEMD160_Final.argtypes = [ctypes.c_void_p, ctypes.c_void_p]
+class RIPEMD160:
+    class EVP_MD_CTX(ctypes.Structure):
+        pass
 
-class RIPEMD160_CTX(ctypes.Structure):
-    _fields_ = [("data", ctypes.c_ubyte * 144)]  # Size may need adjustment
+    EVP_MD_CTX_p = ctypes.POINTER(EVP_MD_CTX)
 
-def hash_ripemd160(data):
-    ctx = RIPEMD160_CTX()
-    result = (ctypes.c_ubyte * 20)()  # RIPEMD-160 produces a 160-bit hash (20 bytes)
-    
-    libcrypto.RIPEMD160_Init(ctypes.byref(ctx))
-    libcrypto.RIPEMD160_Update(ctypes.byref(ctx), data, len(data))
-    libcrypto.RIPEMD160_Final(ctypes.byref(result), ctypes.byref(ctx))
-    
-    return ''.join(format(x, '02x') for x in result)
+    # Define the function prototypes
+    libcrypto.EVP_MD_CTX_new.restype = EVP_MD_CTX_p
+    libcrypto.EVP_get_digestbyname.argtypes = [ctypes.c_char_p]
+    libcrypto.EVP_get_digestbyname.restype = ctypes.c_void_p
+    libcrypto.EVP_DigestInit_ex.argtypes = [EVP_MD_CTX_p, ctypes.c_void_p, ctypes.c_void_p]
+    libcrypto.EVP_DigestUpdate.argtypes = [EVP_MD_CTX_p, ctypes.c_void_p, ctypes.c_size_t]
+    libcrypto.EVP_DigestFinal_ex.argtypes = [EVP_MD_CTX_p, ctypes.POINTER(ctypes.c_ubyte), ctypes.POINTER(ctypes.c_uint)]
+    libcrypto.EVP_MD_CTX_free.argtypes = [EVP_MD_CTX_p]
+
+    def __init__(self):
+        self.md_ctx = libcrypto.EVP_MD_CTX_new()
+        md_type = libcrypto.EVP_get_digestbyname(b"RIPEMD160")
+        libcrypto.EVP_DigestInit_ex(self.md_ctx, md_type, None)
+
+    def update(self, data):
+        libcrypto.EVP_DigestUpdate(self.md_ctx, data, len(data))
+
+    def digest(self):
+        digest = (ctypes.c_ubyte * 20)()  # RIPEMD-160 digest size
+        digest_len = ctypes.c_uint()
+        libcrypto.EVP_DigestFinal_ex(self.md_ctx, digest, ctypes.byref(digest_len))
+        libcrypto.EVP_MD_CTX_free(self.md_ctx)
+        return bytes(digest)
 
 # Example usage
-data = b"Hello, world!"
-hashed_data = hash_ripemd160(data)
-print("RIPEMD-160 Hash:", hashed_data)
+# hasher = RIPEMD160()
+# hasher.update(b"Hello, world!")
+# hashed_data = hasher.digest()
+# print("RIPEMD-160 Hash:", hashed_data.hex())
