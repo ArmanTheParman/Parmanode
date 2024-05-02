98d0f1ecbe9b921a8eefff90bd9b56e1211dc3eb
Parman <armantheparman@gmail.com>
2024-05-02 22:15:24 +1000

    a

--- src/ParmaWallet/PW_dependencies.sh
+++ src/ParmaWallet/PW_dependencies.sh
@@ -3,7 +3,9 @@ debug "pause before skip"
 if [[ $1 != skip ]] ; then
 sudo apt-get install libgmp-dev python3 pip #?cargo, not sure
 sudo apt-get install python3-bitcoinlib python3-base58
+#depending on the system, works with or without --break-system-packages. Try both
 sudo pip3 install pycryptodome bech32 cryptography bip_utils bitcoinlib base58 --break-system-packages
+sudo pip3 install pycryptodome bech32 cryptography bip_utils bitcoinlib base58 
 fi
 debug "pause after skip"
 opensslconf="$(openssl info -configdir)/openssl.cnf"
