45cfc5fe86f34ef6deaa662b33aa267affcb1079
Parman <armantheparman@gmail.com>
2024-05-03 02:31:03 +1000

    btcrecover

--- src/ParmaWallet/PW_dependencies.sh
+++ src/ParmaWallet/PW_dependencies.sh
@@ -1,24 +1,24 @@
 function PW_dependencies {
-debug "pause before skip"
+
 if [[ $1 != skip ]] ; then
-sudo apt-get install libgmp-dev python3 pip #?cargo, not sure
-sudo apt-get install python3-bitcoinlib python3-base58
+sudo apt-get install libgmp-dev python3 pip -y #?cargo, not sure
+sudo apt-get install python3-bitcoinlib python3-base58 -y
 #depending on the system, works with or without --break-system-packages. Try both
 sudo pip3 install pycryptodome bech32 cryptography bip_utils bitcoinlib base58 --break-system-packages
 sudo pip3 install pycryptodome bech32 cryptography bip_utils bitcoinlib base58 
 fi
-debug "pause after skip"
+
+fix_openssl_ripemd160
+}
+
+
+function fix_openssl_ripemd160 {
 opensslconf="$(openssl info -configdir)/openssl.cnf"
-debug "pause a"
 if [[ -e ${opensslconf}_backup ]] ; then
 sudo cp ${opensslconf}_backup $opensslconf  #restores original
 fi
-debug "pause b"
 sudo cp $opensslconf ${opensslconf}_backup #backs up original
-debug "pause c"
-
 swap_string "$opensslconf" "# activate = 1" "activate = 1"
-
 echo "[legacy sect]
 activate = 1" | sudo tee -a $opensslconf >/dev/null
 debug "pause d"
---
+++ src/ParmaWallet/btcrecover/btcrecover.sh
@@ -0,0 +1,7 @@
+function install_btcrecover {
+
+if [[ $install_btcrecover == true ]] ; then
+git clone --depth 1 https://github.com/3rdIteration/btcrecover.git
+fi
+
+}
