a391bf0d3ba531a595e16a419ee0ca0e70e12ddd
Parman <armantheparman@gmail.com>
2024-05-03 20:09:26 +1000

    parmawallet dockerfile

--- src/ParmaWallet/PW_dependencies.sh
+++ src/ParmaWallet/PW_dependencies.sh
@@ -12,14 +12,3 @@ fix_openssl_ripemd160
 }
 
 
-function fix_openssl_ripemd160 {
-opensslconf="$(openssl info -configdir)/openssl.cnf"
-if [[ -e ${opensslconf}_backup ]] ; then
-sudo cp ${opensslconf}_backup $opensslconf  #restores original
-fi
-sudo cp $opensslconf ${opensslconf}_backup #backs up original
-swap_string "$opensslconf" "# activate = 1" "activate = 1"
-echo "[legacy sect]
-activate = 1" | sudo tee -a $opensslconf >/dev/null
-debug "pause d"
-}
