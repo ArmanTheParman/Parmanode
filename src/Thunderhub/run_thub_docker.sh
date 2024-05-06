82627d70dc8ba231c63d5048352d0da2f54d66fd
Parman <armantheparman@gmail.com>
2024-05-07 06:01:11 +1000

    d

--- src/Thunderhub/run_thub_docker.sh
+++ src/Thunderhub/run_thub_docker.sh
@@ -2,6 +2,8 @@ function run_thub_docker {
 docker run --rm -d --name thunderhub -p $thub_port:3000/tcp \
                                      -v $hp/thunderhub/account_1.yaml:/app/account_1.yaml \
                                      -v $hp/thunderhub/.env.local:/app/.env.local \
+                                     -v $HOME/.lnd/data/chain/bitcoin/mainnet:/app/.lnd_host \
+                                     -v $HOME/.lnd/tls.cert:/app/.lnd_host \
                                      thunderhub || runfailed=true
 echo "pausing to see if run command successful"
 enter_continue
