e599d492af228d325080ba1d208ccbc30bbb6829
Parman <armantheparman@gmail.com>
2024-05-07 04:34:32 +1000

    d

--- src/Thunderhub/run_thub_docker.sh
+++ src/Thunderhub/run_thub_docker.sh
@@ -1,5 +1,7 @@
 function run_thub_docker {
-docker run --rm -d --name thunderhub -p $thub_port:3000/tcp -v $hp/thunderhub/.env.local:/app/.env.local thunderhub || runfailed=true
+docker run --rm -d --name thunderhub -p $thub_port:3000/tcp \
+                                     -v $hp/thunderhub/.account_1.yaml:/app/.account_1.yaml \
+                                     -v $hp/thunderhub/.env.local:/app/.env.local thunderhub || runfailed=true
 echo "pausing to see if run command successful"
 enter_continue
 if [[ $runfailed == true ]] ; then 
