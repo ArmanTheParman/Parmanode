3791290b11415a990de87b24ddc6d5c3b2f9b14c
Arman The Parman <77603167+ArmanTheParman@users.noreply.github.com>
2024-05-07 18:45:35 +1000

    Update menu_lnd_more.sh
    V17.3

--- src/MENUS/menu_lnd_more.sh
+++ src/MENUS/menu_lnd_more.sh
@@ -25,8 +25,8 @@ echo "
 
       (mm)             Macaroon information (private and sensitive)
 "
-if [[ $lnd_version != "0.17.0" ]] ; then echo -e " 
-$red      (update)         Update LND to version 0.17.0 $orange
+if [[ $lnd_version != "0.17.3" ]] ; then echo -e " 
+$red      (update)         Update LND to version 0.17.3 $orange
       "
 fi
 echo "########################################################################################
