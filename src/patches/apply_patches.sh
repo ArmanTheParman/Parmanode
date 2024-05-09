3ac9ab332c2eb488b7b823e0170c09b470926a59
Parman <armantheparman@gmail.com>
2024-05-09 22:49:41 +1000

    p6

--- src/patches/apply_patches.sh
+++ src/patches/apply_patches.sh
@@ -1,21 +1,24 @@
 function apply_patches {
 #patches ; each patch adds variable to parmanode.conf, sourced higher up
 #patch=n
+#get $patch from parmanode.conf
 
 temp_patch
 
-case $patch in
+case $patch in 
 1) 
-patch_2 ; patch_3 ; patch_4 ; patch_5 ;;
+patch_2 ; patch_3 ; patch_4 ; patch_5 ; patch_6 ;;
 2)
-patch_3 ; patch_4 ; patch_5 ;;
+patch_3 ; patch_4 ; patch_5 ; patch_6 ;;
 3)
-patch_4 ; patch_5 ;;
+patch_4 ; patch_5 ; patch_6 ;;
 4)
-patch_5 ;;
+patch_5 ; patch_6 ;;
 5)
-true ;;
+; patch_6 ;;
+6)
+return 0 ;;
 *) 
-patch_1 ; patch_2 ; patch_3 ; patch_4 ; patch_5 ;; 
+patch_1 ; patch_2 ; patch_3 ; patch_4 ; patch_5 ; patch_6 ;; 
 esac
 }
--- src/patches/temp_patch.sh
+++ src/patches/temp_patch.sh
@@ -1,5 +1,6 @@
 function temp_patch {
 
+#remove in 2025
 #because of version2 of electrs install, small bug introduced in the
 #install detection. This fixes it.
 if grep -q "electrs-start" < $ic && grep -q "electrs2-end" < $ic ; then
@@ -7,6 +8,7 @@ delete_line "$ic" "electrs-start"
 parmanode_conf_add "electrs2-start"
 fi
 
+#remove in 2025
 #stream directive now in "stream.conf"
 if [[ $OS == "Linux" && -f /etc/nginx/nginx.conf ]] ; then
 if grep -q "include electrs.conf;" < /etc/nginx/nginx.conf ; then
@@ -15,25 +17,14 @@ sudo rm /etc/nginx/electrs.conf >/dev/null 2>&1
 fi
 fi
 
-# I have notice duplicates of this script in crontab, could be a result of hitting
-# control-c during start up before a password request. This block keeps it clean.
-if [[ $(grep "parmanode/update_script.sh" < /etc/crontab | wc -l) -gt 1 ]] ; then
-autoupdate off
-autoupdate on
-fi
-
 if [[ -e $bc ]] ; then
     
+    #Remove in 2025
     if grep -q "electrumx-end" < $ic && ! grep -q "rest=1" < $bc ; then
     echo "rest=1" | sudo tee -a $bc >/dev/null 2>&1
     fi
-
-
-    #erroneous entry
-    if grep -Eq '^rpcservertimeout$' < $bc ; then
-    delete_line $bc "rpcservertimeout"
-    fi
-
+    
+    #Remove in 2025
     #recommended by electrumX docs
     if ! grep -q "rpcservertimeout=" < $bc ; then
     echo "rpcservertimeout=120" | sudo tee -a $bc >/dev/null 2>&1
@@ -50,25 +41,20 @@ IP1and2="$IP1.$IP2."
     fi
 fi
 
-#There was a bug restulting in multiple same lines. This will fix it.
-if [[ -e $bc ]] && grep -q ":5000" < $bc ; then
-delete_line $bc ":5000" >/dev/null 2>&1
-fi
-if [[ -e $bc ]] && grep -q ":3000" < $bc ; then
-delete_line $bc ":3000" >/dev/null 2>&1
-fi
-
 #strange behaviour with capitalisation changing frequently.
+#remove in 2025
 if [[ -d $pp/parmanode/src/Public_pool ]] ; then
 mv $pp/parmanode/src/Public_pool $pp/parmanode/src/public_pool >/dev/null 2>&1
 fi
 
+#remove in 2025
 swap_string "$ic" "piassps-end" "piapps-end"
 if [[ -e $bc ]] ; then
 delete_line "$bc" "rpcallowip=172"
 echo "rpcallowip=172.0.0.0/8" | sudo tee -a $bc >/dev/null 2>&1
 fi
 
+#remove in 2025
 if [[ ${message_jq} != 1 ]] && grep -q "electrs" < $ic && ! which jq > /dev/null ; then
 while true ; do
 set_terminal ; echo -e "
