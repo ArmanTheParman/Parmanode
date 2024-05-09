7e8e88bb54cd421cd4b7aed3333481f7f973e259
Parman <armantheparman@gmail.com>
2024-05-10 01:59:03 +1000

    t

--- src/Thunderhub/start_stop_thub.sh
+++ src/Thunderhub/start_stop_thub.sh
@@ -1,8 +1,10 @@
 function start_thunderhub {
 docker ps >/dev/null 2>&1 || { announce "Please start Docker" ; return 1 ; } 
+please_wait
 docker start thunderhub
 }
 
 function stop_thunderhub {
+pleas_wait
 docker stop thunderhub
 }
