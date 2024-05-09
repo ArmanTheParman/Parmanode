daf789e9c11656d075e9de0e819aecb5385994cd
Parman <armantheparman@gmail.com>
2024-05-10 04:45:46 +1000

    islndrunning

--- src/parmanode/runningoverview.sh
+++ src/parmanode/runningoverview.sh
@@ -82,7 +82,7 @@ fi
 function islndrunning {
 unset lndrunning
 if ps -x | grep lnd | grep bin >/dev/null 2>&1 || \
-   docker ps | grep -q lnd ; then
+   docker exec lnd pgrep lnd ; then
 export lndrunning=true
 overview_conf_add "lndrunning=true" "lndrunning="
 else
