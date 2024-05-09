ab08881c2fe95808d758cb8ad43d704b7e67b759
Parman <armantheparman@gmail.com>
2024-05-10 04:55:50 +1000

    t

--- src/Thunderhub/thub_lnd.sh
+++ src/Thunderhub/thub_lnd.sh
@@ -2,6 +2,8 @@ function thub_lnd {
 
 export lnd_rpc_port=$(cat $HOME/.lnd/lnd.conf | grep -E '^rpclisten=' | grep -Eo ':.+' | tr -d :)
 
+
+#eliminating all this seems to have failed...
 swap_string "$file" "SSO_SERVER_URL=" "SSO_SERVER_URL='$LNDIP:$lnd_rpc_port'"
 
 swap_string "$file" "SSO_CERT_PATH=" "SSO_CERT_PATH=$HOME/.lnd/tls.cert"   
