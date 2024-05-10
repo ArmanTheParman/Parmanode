cd29a302dbeab69d7ab39acdcbaf688d835029a0
Parman <armantheparman@gmail.com>
2024-05-11 00:04:24 +1000

    d

--- src/lnd_docker/build_lnd_dockerfile.sh
+++ src/lnd_docker/build_lnd_dockerfile.sh
@@ -1,4 +1,6 @@
 function build_lnd_docker {
+#user ID needs to be the same as container ID so when volumes are mounted,
+#processes inside have permission
 thisID=$(id -u) 
 cd $hp/lnd
 docker build --build-arg parmanID=$thisID -t lnd .
