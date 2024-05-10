24d0323d063fa536bf7f17515b35afe854a7492b
Parman <armantheparman@gmail.com>
2024-05-10 15:14:27 +1000

    install grpccurl

--- src/general_functions/grpcurl.sh
+++ src/general_functions/grpcurl.sh
@@ -1,5 +1,17 @@
 function grpccurl_call {
-    
+
+if ! which grpcurl >/dev/null ; then
+
+    if [[ $OS == Mac ]] ; then 
+        set_terminal 
+        echo -e "${green}Installing grpcurl...$orange"
+        brew install grpccurl 
+    else
+        set_terminal 
+        echo -e "${green}Installing grpcurl...$orange"
+        go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
+    fi
+
     grpcurl -plaintext \
     -import-path $hp/lnd/lnrpc/ \
     -proto lightning.proto \
