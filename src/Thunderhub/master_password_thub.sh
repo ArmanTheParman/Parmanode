6a2e6dbfa3ac2ab29f8a31366f0b4a8392bae1bf
Parman <armantheparman@gmail.com>
2024-05-11 00:59:51 +1000

    password loop bug

--- src/Thunderhub/master_password_thub.sh
+++ src/Thunderhub/master_password_thub.sh
@@ -21,7 +21,6 @@ read password2 ; clear
 if [[ $password == $password2 ]] ; then break 
 else
 
-while true ; do
 set_terminal echo -e "
 ########################################################################################
     
@@ -33,7 +32,6 @@ choose "epq" "try again" ; read choice
 case $choice in
 q|Q) exit 0 ;; p|P) return 1 ;; *) continue ;; 
 esac 
-done
 fi
 done
 #swap_string "$file" "MASTER_PASSWORD_OVERRIDE='password'" "MASTER_PASSWORD_OVERRIDE='$password'" 
