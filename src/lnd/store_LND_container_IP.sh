4124d30476599e7ebc52fcb9d21cd96a83ddd9d5
Parman <armantheparman@gmail.com>
2024-05-11 20:06:56 +1000

    q

--- src/lnd/store_LND_container_IP.sh
+++ src/lnd/store_LND_container_IP.sh
@@ -25,7 +25,7 @@ if docker ps | grep -q lnd ; then
 
     LNDIP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' lnd)
 
-    if ! echo $LNDIP | grep -E '^[0-9]' ; then #double check LNDIP starts with a number rather than error message
+    if ! echo $LNDIP | grep -qE '^[0-9]' ; then #double check LNDIP starts with a number rather than error message
        unset LNDIP
        return 1
     else 
