91374355a54c4aaad49bb7670f343466b2265139
Parman <armantheparman@gmail.com>
2024-05-14 00:57:30 +1000

    d

--- src/btcrecover/install_btcrecover.sh
+++ src/btcrecover/install_btcrecover.sh
@@ -6,7 +6,7 @@ docker build -t btcrecover .
 
 docker network create --internal no-internet
 
-docker run -du root --name btcrecover btcrecover --network no-internet btcrecover
+docker run -d --network no-internet --name btcrecover btcrecover  
 
 
 }
