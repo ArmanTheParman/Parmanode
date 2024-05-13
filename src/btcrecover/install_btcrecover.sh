97037f6cb43836075d1e5ef662c3d5f0212722ff
Parman <armantheparman@gmail.com>
2024-05-14 00:19:57 +1000

    d

--- src/btcrecover/install_btcrecover.sh
+++ src/btcrecover/install_btcrecover.sh
@@ -2,9 +2,11 @@
 #docker run --network no-internet <image>
 function install_btcrecover {
 
-if [[ $OS == Mac ]] ; then
-docker build -t btcrecover -f $HOME/parman_programs/parmanode/src/btcrecover/Dockerfile .
-else
-docker build -t btcrecover -f $HOME/parman_programs/parmanode/src/btcrecover/Dockerfile_no_break-system-packages .
-fi
+docker build -t btcrecover .
+
+# if [[ $OS == Mac ]] ; then
+# docker build -t btcrecover -f $HOME/parman_programs/parmanode/src/btcrecover/Dockerfile .
+# else
+# docker build -t btcrecover -f $HOME/parman_programs/parmanode/src/btcrecover/Dockerfile_no_break-system-packages .
+# fi
 }
