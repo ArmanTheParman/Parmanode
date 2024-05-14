acacf300c027b8ddf4e4848995159b5098da3afa
Parman <armantheparman@gmail.com>
2024-05-15 01:44:41 +1000

    config

--- src/nostr/install_nostrrelay.sh
+++ src/nostr/install_nostrrelay.sh
@@ -4,6 +4,7 @@ function install_nostrrelay {
 #docs
 #https://www.purplerelay.com/how-to-run-a-nostr-relay-a-step-by-step-guide/
 #https://usenostr.org/relay
+#https://nostr.how/en/relays
 
 if ! which podman >/dev/null 2>&1 ; then
 
@@ -34,4 +35,40 @@ podman run -it --rm -p 7000:8080 \
   --name nostr-relay nostr-rs-relay:latest
 
 # ro,Z for some Linux distro's but Parmanode does not support non Debian based.
+}
+
+
+function edit nostrrelay_config {
+
+nproc # get number of cores and set max connection to same number of cores
+max_conn=$(nproc) 
+
+# Terms of service, generic from config file.
+cat << 'EOF' >> ./config.toml
+terms_message = """
+This service (and supporting services) are provided "as is", without warranty of any kind, express or implied.
+
+By using this service, you agree:
+* Not to engage in spam or abuse the relay service
+* Not to disseminate illegal content
+* That requests to delete content cannot be guaranteed
+* To use the service in compliance with all applicable laws
+* To grant necessary rights to your content for unlimited time
+* To be of legal age and have capacity to use this service
+* That the service may be terminated at any time without notice
+* That the content you publish may be removed at any time without notice
+* To have your IP address collected to detect abuse or misuse
+* To cooperate with the relay to combat abuse or misuse
+* You may be exposed to content that you might find triggering or distasteful
+* The relay operator is not liable for content produced by users of the relay
+"""
+EOF
+
+
+#relay_url = "wss://nostr.example.com/"
+#name = "nostr-rs-relay"
+#description = "A newly created nostr-rs-relay.\n\nCustomize this with your own info."
+
+
+
 }
