f9ff6fa70a48b8f4c8dabf36e95615802e4877f0
Parman <armantheparman@gmail.com>
2024-05-19 06:20:24 +1000

    REST

---
+++ src/tools/rest_protocol_test.sh
@@ -0,0 +1,13 @@
+function rest_protocol_test { 
+
+set_terminal_wide ; echo -e "
+##############################################################################################################
+
+    The REST protocol format to test lnd with curl is:
+$cyan
+    curl -X GET https://localhost:8080/v1/getinfo -H "Grpc-Metadata-macaroon: YOUR_MACAROON_VALUE" -k
+$orange
+##############################################################################################################
+"
+enter_continue
+}
