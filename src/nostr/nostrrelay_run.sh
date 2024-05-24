a0b3be63d57485857c0a528e9c697a9094dc8a10
Parman <armantheparman@gmail.com>
2024-05-25 09:41:23 +1000

    t

--- src/nostr/nostrrelay_run.sh
+++ src/nostr/nostrrelay_run.sh
@@ -1,5 +1,5 @@
 function nostrrelay_run {
-docker run -it --rm -p 7080:8080 \
+docker run -du root -p 7080:8080 \
   -v $HOME/.nostr_data:/usr/src/app/db \
   -v $HOME/parmanode/nostrrelay/config.toml:/usr/src/app/config.toml:ro \
   --name nostrrelay nostr-rs-relay:latest
