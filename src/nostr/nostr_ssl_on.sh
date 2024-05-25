fbd5708b4ed6683ed508ec43b33bc7fd1343351b
Parman <armantheparman@gmail.com>
2024-05-25 20:39:33 +1000

    d

--- src/nostr/nostr_ssl_on.sh
+++ src/nostr/nostr_ssl_on.sh
@@ -33,7 +33,7 @@ source $pc
 
 swap_string "/etc/nginx/conf.d/$domain_name.conf" "try_files" "
     proxy_pass http://localhost:7080;
-    proxy_set_header Host \$host;"
+    proxy_set_header Host \\\$host;"
 #     proxy_set_header X-Real-IP \$remote_addr;
 #     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 #     proxy_set_header X-Forwarded-Proto \$scheme;
