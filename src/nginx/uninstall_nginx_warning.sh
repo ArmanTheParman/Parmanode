2fd7f462e785b591b6eb73373b0f3756376bc3cb
Parman <armantheparman@gmail.com>
2024-05-25 21:40:48 +1000

    t

--- src/nginx/uninstall_nginx_warning.sh
+++ src/nginx/uninstall_nginx_warning.sh
@@ -4,8 +4,8 @@ while true ; do
 set_terminal ; echo -e "
 ########################################################################################
 
-    Uninstalling Nginx might cause problems. Parmanode uses Nginx for other programs
-    you may have installed.
+	Uninstalling Nginx might cause problems. Parmanode uses Nginx for other programs
+	you may have installed.
 
 	Only proceed if you really know what you're doing.
 
