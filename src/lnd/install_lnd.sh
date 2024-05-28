e6d2877fa9b518d3e52232200a0b5aef2c9ffa2a
Parman <armantheparman@gmail.com>
2024-05-29 09:57:33 +1000

    lnd with remote bitcoin

--- src/lnd/install_lnd.sh
+++ src/lnd/install_lnd.sh
@@ -2,9 +2,11 @@ function install_lnd {
 set_terminal
 sned_sats
 
-export lndversion="v0.17.3-beta"
+unset remote_user remote_pass ipcore
 
-bitcoin_choice_with_lnd
+export lndversion="v0.17.5-beta"
+
+bitcoin_choice_with_lnd || return 1
 
 if [[ $bitcoin_choice_with_lnd == local ]] ; then
 grep -q bitcoin-end < $HOME/.parmanode/installed.conf || { announce "Must install Bitcoin first. Aborting." && return 1 ; }
@@ -12,7 +14,7 @@ fi
 
 please_wait
 
-make_lnd_directories || return 1                ; debug "after make lnd dir"
+make_lnd_directories || return 1 
 installed_config_add "lnd-start" 
 
 download_lnd
@@ -106,6 +108,7 @@ break
 ;;
 remote)
 export bitcoin_location=remote
+set_remote_bitcoin_ip || return 1
 break
 ;;
 *)
@@ -113,4 +116,97 @@ invalid
 ;;
 esac
 done
+}
+
+function set_remote_bitcoin_ip {
+
+while true ; do
+
+clear ; echo -e "
+########################################################################################
+
+    Please type in the$pink IP address$orange of the Bitcoin Core instance you want to use, then
+    hit$cyan <enter> $orange
+
+########################################################################################
+"
+read ipcore
+
+while true ; do
+clear ; echo -e "
+########################################################################################    
+   
+    Next, enter the rpc username for the Bitcoin Core instance.
+    
+    This can be found in it's corresponding bitcoin.conf file. If it doesn't exist,
+    then Bitcoin Core doesn't have a username set up. You can create one by adding it
+    to the bitcoin.conf file:
+$yellow
+                rpcuser=some_username
+                rpcpassword=somepassword
+$orange
+    Then save and restart Bitcoin.
+   $green 
+    Type in the username then <enter>
+$orange
+########################################################################################
+"
+choose xpmq ; read remote_user ; set_terminal
+case $remote_user in 
+q|Q) exit ;; P|p|a) return 1 ;; M|m) back2main ;;
+"") 
+invalid
+;;
+*)
+break
+;;
+esac
+done
+
+clear
+echo -e "
+########################################################################################
+
+   Now type in the rpcpassword (found in the corresponding bitcoin.conf file) then
+   hit$cyan <enter> $orange
+
+########################################################################################
+"
+read remote_pass
+
+
+set_terminal ; echo -e "
+########################################################################################
+
+    These are your entries...
+
+        IP address:   $ipcore
+
+        rpcuser:      $remote_user
+
+        rpcpassword:  $remote_pass
+$green
+
+    Please make sure the following lines are in the bitcoin.conf file of the remote
+    Bitcoin installation, or you'll get errors:
+$pink $blinkon
+                        zmqpubrawblock=tcp://*:28332
+                        zmqpubrawtx=tcp://*:28333 $blinkoff
+$orange
+    Hit$green <enter>$orange to accept, or$red x-<enter>$orange to try again.
+
+########################################################################################
+"
+choose xpmq ; read choice ; set_terminal
+case $choice in
+q|Q) exit ;; p|P) return 1 ;; m|M) back2main ;;
+"")
+break
+;;
+*)
+continue
+;;
+esac
+done
+
 }
