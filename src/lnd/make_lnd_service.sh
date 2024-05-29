2ae23aeda534001544dd3924f0f399317dc47dd6
Parman <armantheparman@gmail.com>
2024-05-29 19:49:15 +1000

    smol bug

--- src/lnd/make_lnd_service.sh
+++ src/lnd/make_lnd_service.sh
@@ -3,8 +3,8 @@ function make_lnd_service {
 if [[ $bitcoin_choice_with_lnd == remote ]] ; then
     unset binds after
 else
-    binds="Description=LND Lightning Network Daemon"
-    after="BindsTo=bitcoind.service"
+    binds="BindsTo=bitcoind.service"
+    after="After=bitcoind.service"
 fi
 
 echo "[Unit]
