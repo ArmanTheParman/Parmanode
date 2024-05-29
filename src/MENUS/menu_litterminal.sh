1e6dcf1dc8434901a4e9beaa5fe475523bee744f
Parman <armantheparman@gmail.com>
2024-05-30 01:11:54 +1000

    t

--- src/MENUS/menu_litterminal.sh
+++ src/MENUS/menu_litterminal.sh
@@ -8,18 +8,18 @@ set_terminal ; echo -e "
 ########################################################################################
 
 
-      The Lightning Terminal can be accessed in your browser at:
+
+    The Lightning Terminal can be accessed in your browser at:
 
 
                            https://localhost:${green}8443$orange
 
-                                      or
 
                            https://$IP:${yellow}8033$orange
 
-                                      or $bright_blue
-
+                                      $bright_blue
     https://$ONION_ADDR_LITTERMINAL:7007                            $orange
+ 
 
 
 ########################################################################################
