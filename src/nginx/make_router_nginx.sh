643e2a41d8c97ce0ded753fbcbb6316e7a9a31ca
Parman <armantheparman@gmail.com>
2024-06-04 02:03:40 +1000

    d

--- src/nginx/make_router_nginx.sh
+++ src/nginx/make_router_nginx.sh
@@ -1,20 +1,22 @@
 function make_router_nginx {
 
-#use this function to make a reverse proxy from your computer to your router.
-#change the IP of the router if needed.
-#then set up a tor service to localhost:11111 to access your router from home.
-#a tor service alone won't work, I've tried.
+#Attempted to use this function to make a reverse proxy from my computer to my router.
+#then I set up a tor service to localhost:11111 to access the router from home.
+#a tor service alone didn't work, this method also failed.
 
 file=/etc/nginx/conf.d/router.conf
 router_IP=192.168.0.1
+
+#proxy_set_header="proxy_set_header Host localhost;"
+proxy_set_header1="proxy_set_header Host $router_IP;"
 
 echo "server {
     listen 11111 ;
     server_name _;
 
     location / {
-        proxy_pass https://$router_IP;
-        proxy_set_header Host localhost;
+        proxy_pass http://$router_IP;
+        $proxy_set_header1
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto https;
