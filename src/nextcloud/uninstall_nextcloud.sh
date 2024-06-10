710a7bcb2adf6f488a7cfa76c682345dff7d4401
Parman <armantheparman@gmail.com>
2024-06-11 00:33:24 +1000

    d

--- src/nextcloud/uninstall_nextcloud.sh
+++ src/nextcloud/uninstall_nextcloud.sh
@@ -2,7 +2,9 @@ function uninstall_nextcloud {
 
 docker stop $(docker ps --format "{{.Names}}" | grep nextcloud)
 docker rm $(docker ps -a --format "{{.Names}}" | grep nextcloud)
+if [[ $debug != 1 ]] ; then
 docker rmi $(docker images | grep nextcloud | awk '{print $3}')
+fi
 docker volume rm nextcloud_aio_mastercontainer 
 sudo docker network rm nextcloud-aio 2>/dev/null
 sudo rm /var/lib/docker/volumes/metadata.db >/dev/null 2>&1
