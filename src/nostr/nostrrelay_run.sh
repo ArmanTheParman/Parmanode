6dd13b08fb68cfe061d000223cff922619dc7c84
Parman <armantheparman@gmail.com>
2024-06-10 23:38:11 +1000

    t

--- src/nostr/nostrrelay_run.sh
+++ src/nostr/nostrrelay_run.sh
@@ -16,6 +16,7 @@ exit
 fi
 
 docker run -du root -p 7080:8080 \
+  --restart unles-stopped \
   -v $nostr_data:/usr/src/app/db \
   -v $HOME/parmanode/nostrrelay/config.toml:/usr/src/app/config.toml:ro \
   --name nostrrelay nostr-rs-relay:latest
