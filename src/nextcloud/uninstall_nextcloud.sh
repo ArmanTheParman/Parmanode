dbf9e158046d59c897ce118ff3f2cde338d4a525
Parman <armantheparman@gmail.com>
2024-06-11 14:42:39 +1000

    t

--- src/nextcloud/uninstall_nextcloud.sh
+++ src/nextcloud/uninstall_nextcloud.sh
@@ -2,9 +2,35 @@ function uninstall_nextcloud {
 
 docker stop $(docker ps --format "{{.Names}}" | grep nextcloud)
 docker rm $(docker ps -a --format "{{.Names}}" | grep nextcloud)
-if [[ $debug != 1 ]] ; then
+
+while true ; do
+set_terminal ; echo -e "
+########################################################################################
+
+    Do you want to remove the NextCloud Docker images as well? It can save some
+    data.
+
+                            d)    delete them
+
+                            l)    Leave them
+
+########################################################################################
+"
+choose xpmq ; read choice ; set_terminal
+case $choice in
+q|Q) exit ;; p|P) return 1 ;; m|M) back2main ;;
+d)
 docker rmi $(docker images | grep nextcloud | awk '{print $3}')
-fi
+;;
+l)
+break
+;;
+*)
+invalid
+;;
+esac
+done
+
 #docker volume rm nextcloud_aio_mastercontainer 
 sudo docker network rm nextcloud-aio 2>/dev/null
 
