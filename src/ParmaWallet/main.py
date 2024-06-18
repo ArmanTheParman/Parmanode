fcfb0a3158b5a5c20540354d938031f916786a50
Parman <armantheparman@protonmail.com>
2024-06-18 16:14:34 +1000

    rem py

---
+++ src/ParmaWallet/main.py
@@ -0,0 +1,8 @@
+import os
+from classes import *
+from functions import *
+from functions2 import *
+from variables import *
+import unicodedata, hashlib, binascii, hmac
+
+BIP32_master_node(mnemonic="leader monkey parrot ring guide accident before fence cannon height naive bean")
