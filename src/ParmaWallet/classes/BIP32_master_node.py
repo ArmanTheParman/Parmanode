65cac43328fbc6e5fe1448dae7c5569bea4ed9e4
Parman <armantheparman@protonmail.com>
2024-06-21 13:49:34 +1000

    space

--- src/ParmaWallet/classes/BIP32_master_node.py
+++ src/ParmaWallet/classes/BIP32_master_node.py
@@ -118,7 +118,7 @@ class BIP32_master_node:
         #self.xpub=base58.b58encode(raw_xpub)
         
     def __repr__(self):
-        return "BIP32_master_node object" + \
+        return "BIP32_master_node object" + '\n' + \
         "\nThe bin_seed: {}".format(self.byte_seed) + '\n' + \
         "\nThe bin_seed in hex: {}".format(binascii.hexlify(self.byte_seed[:64]).decode('utf-8')) + '\n' + \
         "\nThe BIP32 root key is: {}".format(self.xprv)
