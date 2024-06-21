5780d0b40a331c8f07a289c02a5e336ed309c1ae
Parman <armantheparman@protonmail.com>
2024-06-21 22:58:28 +1000

    reorder

--- src/ParmaWallet/parmanostr/Eventclass.py
+++ src/ParmaWallet/parmanostr/Eventclass.py
@@ -3,7 +3,7 @@ import hashlib
 from  sign import *
 
 class Event:
-    def __init__(self, id=None, pubkey=None, created_at=None, kind=None, tags=None, content=None, sig=None, nsec=None): #note: tags is a mutable object and re-called each time the contructor is used. So, should not directly set it.
+    def __init__(self, id=None, pubkey=None, created_at=None, kind=None, tags=None, content=None, sig=None, sec=None): #note: tags is a mutable object and re-called each time the contructor is used. So, should not directly set it.
         self.pubkey = pubkey #use ""
         self.created_at = created_at #number, not string
         self.kind = kind #number, not string
@@ -15,9 +15,11 @@ class Event:
         self.json_string = json.dumps(self.data, ensure_ascii=False, separators=(',', ':'))
         self.json_bytes = self.json_string.encode('utf-8')
         self.id = hashlib.sha256(self.json_bytes).hexdigest() #<32-bytes lowercase hex-encoded sha256 of the serialized event data>, use ""
+        self.id_bytes = hashlib.sha256(self.json_bytes)
 
-#        if nsec is not None:
-            
+
+        if sec is not None and isinstance(sec, bytes):
+            self.sig = schnorr_sign(msg=self.id_bytes, seckey=sec, aux_rand=int.from_bytes(os.urandom(32), byteorder='big'))
 
     def __repr__(self):
         return "{\n" \
