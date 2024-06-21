11af5e484dd143bdbd48268f8a1e77145a252294
Parman <armantheparman@protonmail.com>
2024-06-21 13:50:04 +1000

    t

--- src/ParmaWallet/classes/BIP32_master_node.py
+++ src/ParmaWallet/classes/BIP32_master_node.py
@@ -121,7 +121,7 @@ class BIP32_master_node:
         return "BIP32_master_node object" + '\n' + \
         "\nThe bin_seed: {}".format(self.byte_seed) + '\n' + \
         "\nThe bin_seed in hex: {}".format(binascii.hexlify(self.byte_seed[:64]).decode('utf-8')) + '\n' + \
-        "\nThe BIP32 root key is: {}".format(self.xprv)
+        "\nThe BIP32 root key is: {}".format(self.xprv) + '\n'
     
 
 class child_key:
