1ab533c3401944ab2758f66ed5c058068a0857f6
Parman <armantheparman@protonmail.com>
2024-06-21 15:34:47 +1000

    a

--- src/parmanostr/nip1/event.py
+++ src/parmanostr/nip1/event.py
@@ -25,7 +25,7 @@ class Event:
         print ("["+str(self.id)+"]")
 
   
-e=Event(pubkey="fad6540c8f2fd2a16a25d0d82dd95d3bad7890d435d1690848a0a77d2883a447", created_at=1718421481, kind=1, content="\"Tax is ok, I'm happy to pay tax\".\n\n=\n\n\"I think people getting raped is ok, I like sex\".")
-e.serialise()
+#e=Event(pubkey="fad6540c8f2fd2a16a25d0d82dd95d3bad7890d435d1690848a0a77d2883a447", created_at=1718421481, kind=1, content="\"Tax is ok, I'm happy to pay tax\".\n\n=\n\n\"I think people getting raped is ok, I like sex\".")
+#e.serialise()
 
   
