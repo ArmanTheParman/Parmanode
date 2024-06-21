ada95961189bfe2442ee14913eb5f943b93745cf
Parman <armantheparman@protonmail.com>
2024-06-21 16:55:05 +1000

    test

--- src/parmanostr/main.py
+++ src/parmanostr/main.py
@@ -1,5 +1,5 @@
 from nip1 import *
 
-a=Event()
+a=Event(pubkey="fad6540c8f2fd2a16a25d0d82dd95d3bad7890d435d1690848a0a77d2883a447", created_at=1718421481, kind=1, content="\"Tax is ok, I'm happy to pay tax\".\n\n=\n\n\"I think people getting raped is ok, I like sex\".")
 
 print(a)
