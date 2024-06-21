97fe7e6b72d2a579566ff5243e63ab365ee6cada
Parman <armantheparman@protonmail.com>
2024-06-21 23:11:50 +1000

    bip44

--- src/ParmaWallet/functions2/get_all_child_keys.py
+++ src/ParmaWallet/functions2/get_all_child_keys.py
@@ -5,14 +5,14 @@ from functions import *
 from variables import *
 import unicodedata, hashlib, binascii, hmac
 
-def get_all_child_keys(depth: str='f', mnemonic: str=None, passphrase: str='', bip=44, purpose=0):
+def get_all_child_keys(depth: str='f', mnemonic: str=None, passphrase: str='', purpose=44, coin=0, account=0, change=0, address=0):
 
     a = BIP32_master_node(mnemonic, passphrase)
-    b = Child_key(a, depth=1, account=bip, hardened=True, serialize=False) #purpose
-    c = Child_key(b, depth=1, account=purpose, hardened=True, serialize=False) #coin
-    d = Child_key(c, depth=1, account=0, hardened=True, serialize=False) #account
-    e = Child_key(d, depth=1, account=0, hardened=False, serialize=False) #int/ext
-    f = Child_key(e, depth=1, account=0, hardened=False, serialize=True) #address
+    b = Child_key(a, depth=1, index=purpose, hardened=True, serialize=False) #purpose
+    c = Child_key(b, depth=1, index=coin, hardened=True, serialize=False) #coin
+    d = Child_key(c, depth=1, index=account, hardened=True, serialize=False) #account
+    e = Child_key(d, depth=1, index=change, hardened=False, serialize=False) #int/ext
+    f = Child_key(e, depth=1, index=address, hardened=False, serialize=True) #address
 
     if depth == 'a':
         return a
