d94989b9f2d622e1358cd880f4874359361e86e6
Parman <armantheparman@protonmail.com>
2024-06-21 17:04:50 +1000

    t

--- src/parmanostr/nip1/Eventclass.py
+++ src/parmanostr/nip1/Eventclass.py
@@ -3,7 +3,7 @@ import hashlib
 
 
 class Event:
-    def __init__(self, id=None, pubkey=None, created_at=None, kind=None, tags=None, content=None, sig=None): #note: tags is a mutable object and re-called each time the contructor is used. So, should not directly set it.
+    def __init__(self, id=None, pubkey=None, created_at=None, kind=None, tags=None, content=None, sig=None, nsec=None): #note: tags is a mutable object and re-called each time the contructor is used. So, should not directly set it.
         self.pubkey = pubkey #use ""
         self.created_at = created_at #number, not string
         self.kind = kind #number, not string
@@ -15,6 +15,9 @@ class Event:
         self.json_string = json.dumps(self.data, ensure_ascii=False, separators=(',', ':'))
         self.json_bytes = self.json_string.encode('utf-8')
         self.id = hashlib.sha256(self.json_bytes).hexdigest() #<32-bytes lowercase hex-encoded sha256 of the serialized event data>, use ""
+
+        if nsec is not None:
+            
 
     def __repr__(self):
         return "{\n" \
