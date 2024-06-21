2f1a0380a1d747cf0d4dd816b051090918e7865b
Parman <armantheparman@protonmail.com>
2024-06-21 23:12:05 +1000

    t

--- src/ParmaWallet/functions2/get_all_child_keys.py
+++ src/ParmaWallet/functions2/get_all_child_keys.py
@@ -8,11 +8,11 @@ import unicodedata, hashlib, binascii, hmac
 def get_all_child_keys(depth: str='f', mnemonic: str=None, passphrase: str='', purpose=44, coin=0, account=0, change=0, address=0):
 
     a = BIP32_master_node(mnemonic, passphrase)
-    b = Child_key(a, depth=1, index=purpose, hardened=True, serialize=False) #purpose
-    c = Child_key(b, depth=1, index=coin, hardened=True, serialize=False) #coin
-    d = Child_key(c, depth=1, index=account, hardened=True, serialize=False) #account
-    e = Child_key(d, depth=1, index=change, hardened=False, serialize=False) #int/ext
-    f = Child_key(e, depth=1, index=address, hardened=False, serialize=True) #address
+    b = Child_key(a, depth=1, index=purpose, hardened=True, serialize=False)
+    c = Child_key(b, depth=1, index=coin, hardened=True, serialize=False)
+    d = Child_key(c, depth=1, index=account, hardened=True, serialize=False)
+    e = Child_key(d, depth=1, index=change, hardened=False, serialize=False) 
+    f = Child_key(e, depth=1, index=address, hardened=False, serialize=True) 
 
     if depth == 'a':
         return a
