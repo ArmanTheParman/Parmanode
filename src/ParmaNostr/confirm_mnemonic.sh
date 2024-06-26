9f42606b1f33a2a666874156498eefb5709f7cb7
Parman <armantheparman@protonmail.com>
2024-06-26 18:58:59 +1000

    t

--- src/ParmaNostr/confirm_mnemonic.sh
+++ src/ParmaNostr/confirm_mnemonic.sh
@@ -7,21 +7,3 @@ from classes import *
 END
 }
 
-
-
-#print(sys.path)  
-
-echo "
-sys.path.append("$pn/src/ParmaWallet/classes")
-
-from FieldElement import *
-#from S256 import * 
-#from privatekey import *
-#from point import * 
-
-sys.path.append("$pn/src/ParmaWallet/functions")
-from old_functions import *
-from PW_signing import *
-
-print("tested python from bash")
-"
