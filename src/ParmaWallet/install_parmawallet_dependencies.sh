b26e11dddf79eb4e39a6e6e1f27bb387f164d29a
Parman <armantheparman@protonmail.com>
2024-06-26 19:25:02 +1000

    t

--- src/ParmaWallet/install_parmawallet_dependencies.sh
+++ src/ParmaWallet/install_parmawallet_dependencies.sh
@@ -1,4 +1,4 @@
-function install_parmawallet {
+function install_parmawallet_dependencies {
 
 if [[ $OS == Linux ]] ; then
 sudo apt-get install -y libgmp-dev python3 python3-dev python3-pip openssl python3-bitcoinlib python3-base58 python3-tk build-essential libssl-dev locales 
