9f5155cc9b98f84db187ed61e8acdfd9624df9c0
Parman <armantheparman@protonmail.com>
2024-06-30 18:38:43 +1000

    t

--- src/electrs/start_stop_electrs.sh
+++ src/electrs/start_stop_electrs.sh
@@ -2,15 +2,9 @@ function start_electrs { #non-docker function
 
 please_wait
 if [[ $OS == Linux ]] ; then 
-    if ! pgrep bitcoind >/dev/null 2>&1 ; then
-    announce "Please make sure Bitcoin is running or electrs can't start" 
-    fi
     sudo systemctl start electrs.service 
 fi
 if [[ $OS == Mac ]] ; then
-    if ! pgrep Bitcoin-Q >/dev/null ; then 
-    announce "Please make sure Bitcoin is running or electrs can't start" 
-    fi
     script -q /dev/null  $HOME/parmanode/electrs/target/release/electrs --conf $HOME/.electrs/config.toml >> $HOME/.parmanode/run_electrs.log 2>&1 &
 fi
 }
