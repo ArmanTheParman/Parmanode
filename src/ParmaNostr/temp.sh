c442ace4faa0b16542a2a13f31d05f5d0ff7c658
Parman <armantheparman@protonmail.com>
2024-07-06 02:33:31 +1000

    a

--- src/ParmaNostr/temp.sh
+++ src/ParmaNostr/temp.sh
@@ -10,6 +10,15 @@ from functions2 import *
 from variables import *
 from nostr import *
 import unicodedata, hashlib, binascii, hmac
+
+wordlist_path = "$pn/src/ParmaWallet/docs/english.txt"
+mnemonic_path = "$dp/.nostr_keys/mnemonic.txt"
+nostr_nsec = "$dp/.nostr_keys/nsec.txt"
+nostr_pub = "$dp/.nostr_keys/pub.txt"
+nostr_nsec_bytes = "$dp/.nostr_keys/nsec_bytes.bin"
+random_binary_path = "$dp/.nostr_keys/random_binary.txt"
+full_binary_path = "$dp/.nostr_keys/full_binary.txt"
+
 sys.exit(3)
 END
 }
