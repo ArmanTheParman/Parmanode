6ba04db002d054c18695ee9b01c712578fcf9fed
Parman <armantheparman@protonmail.com>
2024-07-05 22:47:20 +1000

    a

--- src/ParmaNostr/make_sourcable_keys_file.sh
+++ src/ParmaNostr/make_sourcable_keys_file.sh
@@ -1,11 +1,14 @@
 function make_sourcable_keys_file {
 
+file="$dp/.nostr_keys/nostr_keys.txt"
+
 npub="$(cat $dp/.nostr_keys/npub.txt | tr -d ' ')"
 nsec="$(cat $dp/.nostr_keys/nsec.txt | tr -d ' ')"
 priv_hex="$(cat $dp/.nostr_keys/priv_hex.txt | tr -d ' ')"
 
 echo "npub=$npub
 nsec=$nsec
-priv_hex=$priv_hex" | tee $dp/.nostr_keys/nostr_keys.txt >/dev/null 2>&1
+priv_hex=$priv_hex" | tee $file >/dev/null 2>&1
 
+unset file
 }
