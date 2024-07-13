e869c75a0074987c44a2dcbc0e665c74a1838151
Parman <armantheparman@protonmail.com>
2024-07-14 02:51:24 +1000

    a

--- src/windows/bitcoin/install_bitcoin_f.py
+++ src/windows/bitcoin/install_bitcoin_f.py
@@ -5,6 +5,7 @@ from tools.debugging_f import *
 from tools.screen_f import *
 from bitcoin.bitcoin_functions_f import *
 import threading, time
+from datetime import date
 
 
 def install_bitcoin():
@@ -16,7 +17,16 @@ def install_bitcoin():
     threading.Thread(target=download_bitcoin).start() #check download_bitcoin_finished global variable
 
     if not choose_drive():
-        dbo.write("Bitcoin choose_drive failed.")
+        date = date.today().strftime("%d-%m-%y")
+        dbo.write(f"{date}: Bitcoin choose_drive exited.")
         return 1
 
+    if not format_external_drive():
+        dbo.write(f"{date}: Bitcoin format_external drive exited.")
+        return 1
 
+def format_external_drive():
+    set_terminal()
+    print(f"""{cyan}    Please make sure the external drive you want to use for
+    Bitcoin is connected.""")
+    #get connected drives
