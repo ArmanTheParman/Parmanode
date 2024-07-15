c96df5c13f4343fec89685ac5253e6d4106d999d
Parman <armantheparman@protonmail.com>
2024-07-15 15:03:49 +0100

    drive detection

--- src/windows/tools/drive_f.py
+++ src/windows/tools/drive_f.py
@@ -3,64 +3,39 @@ from config.variables_f import *
 from tools.system_f import *
 from tools.screen_f import *
 
-def format_drive(drive_letter=None, file_system='NFTS', label="parmanode"):
-    try:
-        drive = f"{drive_letter}:" if not drive_letter.endswith(':') else drive_letter
-        command = ['format', drive, '/fs:' + file_system, '/v:', label, '/q' ]
-        return True
-    except:
-        return False
 
 def detect_drive():
+    
     set_terminal()
     beforeo.truncate() ; aftero.truncate() ; differenceo.truncate()
     input(f"""{orange}    Please make sure the drive you want to use with Parmanode
     is{cyan} DISCONNECTED{orange}. Then hit <enter>.
     
     """)
-    try:
-        get_all_disks("before")
-    except Exception as e:
-        print(f"{a}")
-        input("error. exiting. before")
-    #The sequence is to ensure the last added drive is numbered last
+
+    get_all_disks("before")
+
     input(f"""{orange}    Now go ahead and{cyan} CONNECT{orange} The drive, wait a few seconds. Then
     hit <enter>""")
-    try:
-        get_all_disks("after")
-    except Exception as e:
-        print(f"{a}")
-        input("error. exiting. after")
 
-    input("6a")
+    get_all_disks("after")
+
     with before.open('r') as bb, after.open('r') as aa:
         input("6b")
         bblines = tuple(bb.readlines())
         aalines = tuple(aa.readlines()) 
         unique_lines = [line for line in aalines if line not in bblines]
-    try:
-        with difference.open('w') as f:
-            for line in unique_lines:
-                f.write(line + '\n')
 
-    except Exception as e:
-        print(f"{e}")
+    with difference.open('w') as f:
+        for line in unique_lines:
+            f.write(line)
 
-    print(unique_lines)
-    input("look at unique lines above")
-
+    if len(unique_lines) != 1:
+        set_terminal()
+        print("Somthing went wrong with drive detection.")
+        return False
 
     return True
-
-# def get_all_disks():
-#     command = 'powershell -Command "Get-Disk | Format-List -Property FriendlyName,Size,Path"'
-#     result = subprocess.run(command, capture_output=True, text=True, shell=True) 
-#     disk_info = result.stdout.strip().splitlines()
-#     tmpo = config(tmp)
-#     for line in disk_info: 
-#         tmpo.add(line)
-#         print("...", line)
-#     input("end get all disks")
 
 def get_all_disks(when):
     import subprocess
@@ -81,3 +56,12 @@ def get_all_disks(when):
             aftero.add(line)
 
     return True
+
+
+def format_drive(drive_letter=None, file_system='NFTS', label="parmanode"):
+    try:
+        drive = f"{drive_letter}:" if not drive_letter.endswith(':') else drive_letter
+        command = ['format', drive, '/fs:' + file_system, '/v:', label, '/q' ]
+        return True
+    except:
+        return False
