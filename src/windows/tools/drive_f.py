6bb8a1d129ce4a443a55cd17e69f8a01703b259f
Parman <armantheparman@protonmail.com>
2024-07-15 14:09:15 +0100

    a

--- src/windows/tools/drive_f.py
+++ src/windows/tools/drive_f.py
@@ -16,13 +16,22 @@ def detect_drive():
     set_terminal()
     input(f"""{orange}    Please make sure the drive you want to use with Parmanode
     is{cyan} DISCONNECTED{orange}. Then hit <enter>.""")
-    #before_disks = set(get_all_disks)
+    get_all_disks("before")
     input(f"""{orange}    Now go ahead and connect the drive, wait a few seconds, then
     hit <enter>""")
-    get_all_disks()
-     
+    get_all_disks("after")
+
+    with beforeo.open('r') as bb, aftero.open('r') as aa:
+        bblines = set(bb.readlines())
+        aalines = set(aa.readlines()) # type: ignore
+        unique_lines = aalines - bblines
+        
+    print(unique_lines)
+    input("look at unique lines above")
+
+
     return True
-    
+
 # def get_all_disks():
 #     command = 'powershell -Command "Get-Disk | Format-List -Property FriendlyName,Size,Path"'
 #     result = subprocess.run(command, capture_output=True, text=True, shell=True) 
@@ -33,14 +42,22 @@ def detect_drive():
 #         print("...", line)
 #     input("end get all disks")
 
-def get_all_disks():
+def get_all_disks(when):
     import subprocess
     tmpo.truncate()
     diskpart_commands = """list disk"""
     tmpo.add(diskpart_commands)
     diskpart_script_path = tmpo.file
     result = subprocess.run(['diskpart', '/s', diskpart_script_path], capture_output=True, text=True, shell=True).stdout.strip().split('\n')
-    print("len  ", len(result), "  type  ", type(result))
-    for line in result:
-        if "Disk 1" in line:
-            print(line)
+    #print("len  ", len(result), "  type  ", type(result))
+    # for line in result:
+    #     if "Disk 1" in line:
+    #         print(line)
+    if when == "before":
+        for line in result:
+            beforeo.add(line)
+    if when == "after":
+        for line in result:
+            aftero.add(line)
+
+    return True
