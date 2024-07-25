90143190c4c30a422d741a0f24d5ff2bd617d720
Parman <armantheparman@protonmail.com>
2024-07-25 15:49:11 +1000

    uninstall bitcoin

--- src/windows/bitcoin/uninstall_bitcoin_f.py
+++ src/windows/bitcoin/uninstall_bitcoin_f.py
@@ -8,49 +8,52 @@ def uninstall_bitcoin():
     if not yesorno("Are you sure you want to uninstall Bitcoin?"): return False
 
     try:
-        if not delete_directory(bitcoinpath):
-            announce(fr"""Unable to remove {cyan} C:\....\parman_programs\bitcoin{orange} during Bitcoin uninstallation
+        if not delete_directory_contents(bitcoinpath):
+            announce(fr"""Unable to emtpy {cyan} C:\....\parman_programs\bitcoin{orange} during Bitcoin uninstallation
     The directory may be in use, eg it might be open in a folder window, or in a 
-    terminal.""")                 
+    terminal, or it could be because you have another instance of Parmanode open. Aborting.""")                 
             return False
 
     except Exception as e:
-           announce(fr"""ERROR: Unable to remove {cyan} C:\....\parman_programs\bitcoin{orange} during Bitcoin uninstallation
+            announce(fr"""Unable to emtpy {cyan} C:\....\parman_programs\bitcoin{orange} during Bitcoin uninstallation
     The directory may be in use, eg it might be open in a folder window, or in a 
-    terminal.
+    terminal, or it could be because you have another instance of Parmanode open. Aborting.
                     
         bitcoinpath.mkdir()
     {e}                    """)                 
-           return False 
-    try:
-        bitcoinpath.mkdir()
-    except:
-        pass
+
+            return False 
 
     try: 
         default_bitcoin_data_dir.unlink() # only deletes if it is a symlink
     except:
         pass
+
     try:
         pco.remove("bitcoin_dir") #string deletion from file
     except:
         pass
+
     try:
         pco.remove("drive_bitcoin") #string deletion from file
     except:
         pass
+
     try:
         pco.remove("prune_value=")
     except:
         pass
+
     try:
         ico.remove("bitcoin-start") #string deletion from file
     except:
         pass
+
     try:
         ico.remove("bitcoin-end") #string deletion from file
     except:
         pass
+
     try:
         pco.remove("check_bitcoin_dir_flag")
     except:
