32965974cd8c5312aa9c5bdd0f459d40ac3e8f8c
Parman <armantheparman@protonmail.com>
2024-08-13 06:57:58 +1000

    a

--- src/bitcoin/verify_bitcoin.sh
+++ src/bitcoin/verify_bitcoin.sh
@@ -3,9 +3,13 @@ if [[ $verify == skip ]] ; then return 0 ; fi #skipverify argument set in parman
 
 cd $HOME/parmanode/bitcoin
 
-# get Bitcoin Shasums
-curl -LO https://bitcoincore.org/bin/bitcoin-core-$version/SHA256SUMS 
-curl -LO https://bitcoincore.org/bin/bitcoin-core-$version/SHA256SUMS.asc 
+if grep -q "bitcoin_choice=knots" < $pc ; then
+    curl -LO https://bitcoinknots.org/files/27.x/27.1.knots20240801/SHA256SUMS
+    curl -LO https://bitcoinknots.org/files/27.x/27.1.knots20240801/SHA256SUMS.asc
+else
+    curl -LO https://bitcoincore.org/bin/bitcoin-core-$version/SHA256SUMS 
+    curl -LO https://bitcoincore.org/bin/bitcoin-core-$version/SHA256SUMS.asc 
+fi
 
 if ! which gpg >/dev/null  && [[ $OS == Mac ]] ; then install_gpg4mac ; fi
 
@@ -28,6 +32,8 @@ echo ""
 echo " Please wait a moment for gpg verification..."
 
 #keys from : https://github.com/bitcoin-core/guix.sigs/tree/main/builder-keys
+gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys 658E64021E5793C6C4E15E45C2E581F5B998F30E >/dev/null 2>&1
+gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys 1A3E761F19D2CC7785C5502EA291A2C45D0C504A >/dev/null 2>&1
 gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys E777299FC265DD04793070EB944D35F9AC3DB76A >/dev/null 2>&1
 curl https://raw.githubusercontent.com/bitcoin-core/guix.sigs/main/builder-keys/laanwj.gpg | gpg --import >/dev/null 2>&1
 curl https://raw.githubusercontent.com/bitcoin-core/guix.sigs/main/builder-keys/Emzy.gpg | gpg --import >/dev/null 2>&1
