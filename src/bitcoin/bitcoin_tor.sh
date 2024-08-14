0082d4b6d8da31f26048d2e8098b36203ea8442e
Parman <armantheparman@protonmail.com>
2024-08-15 08:53:13 +1000

    a

--- src/bitcoin/bitcoin_tor.sh
+++ src/bitcoin/bitcoin_tor.sh
@@ -1,78 +1,39 @@
 function bitcoin_tor {
 
-if [[ $OS == "Mac" ]] ; then no_mac ; return 1 ; fi
-
-if ! which tor >/dev/null 2>&1 ; then install_tor ; fi
-
-if [[ ! -f /etc/tor/torrc ]] ; then
-set_terminal ; echo "
-########################################################################################
-    /etc/tor/torrc file does not exist. You may have a non-standard Tor installation.
-    Parmanode won't be able to automate this process for you. Sorry! Aborting.
-########################################################################################
-"
-enter_continue ; return 1 ;
+if [[ $OS == "Mac" ]] ; then 
+    varlibtor="/usr/local/var/lib/tor"
+    torrc="/usr/local/etc/tor/torrc"
+    install_tor
 fi
 
-please_wait
-
-sudo usermod -a -G debian-tor $USER >/dev/null 2>&1
+if [[ $OS == "Linux" ]] ; then
+    varlibtor="/var/lib/tor"
+    torrc="/etc/tor/torrc"
+    if ! which tor >/dev/null 2>&1 ; then install_tor ; fi
+    sudo usermod -a -G debian-tor $USER >/dev/null 2>&1
+fi
 
 #start fresh
-delete_line "/etc/tor/torrc" "# Additions by Parmanode"
-delete_line "/etc/tor/torrc" "bitcoin-service"
-delete_line "/etc/tor/torrc" "127.0.0.1:8332"
 delete_line "$HOME/.bitcoin/bitcoin.conf" "onion" 
 delete_line "$HOME/.bitcoin/bitcoin.conf" "bind=127.0.0.1" 
 delete_line "$HOME/.bitcoin/bitcoin.conf" "onlynet"
 
-if ! sudo cat /etc/tor/torrc | grep "# Additions by Parmanode..." >/dev/null 2>&1 ; then
-echo "# Additions by Parmanode..." | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
-fi
-
-if sudo grep "ControlPort 9051" /etc/tor/torrc | grep -v '^#' >/dev/null 2>&1 ; then true ; else
-    echo "ControlPort 9051" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
-    fi
-
-if sudo grep "CookieAuthentication 1" /etc/tor/torrc | grep -v '^#' >/dev/null 2>&1 ; then true ; else
-    echo "CookieAuthentication 1" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
-    fi
-
-if sudo grep "CookieAuthFileGroupReadable 1" /etc/tor/torrc | grep -v '^#' >/dev/null 2>&1 ; then true ; else
-    echo "CookieAuthFileGroupReadable 1" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
-    fi
-
-if sudo grep "DataDirectoryGroupReadable 1" /etc/tor/torrc | grep -v '^#' >/dev/null 2>&1 ; then true ; else
-    echo "DataDirectoryGroupReadable 1" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
-    fi
+enable_tor_general
 
 if ! grep "listen=1" $HOME/.bitcoin/bitcoin.conf >/dev/null 2>&1 ; then
     echo "listen=1" | tee -a $HOME/.bitcoin/bitcoin.conf >/dev/null 2>&1
     fi
 
-if sudo grep "HiddenServiceDir /var/lib/tor/bitcoin-service/" \
-    /etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; else
-    echo "HiddenServiceDir /var/lib/tor/bitcoin-service/" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
+if sudo grep "HiddenServiceDir $varlibtor/bitcoin-service/" \
+    $torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; else
+    echo "HiddenServiceDir $varlibtor/bitcoin-service/" | sudo tee -a $torrc >/dev/null 2>&1
     fi
 
 if sudo grep "HiddenServicePort 8332 127.0.0.1:8332" \
-    /etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; else
-    echo "HiddenServicePort 8332 127.0.0.1:8332" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
+    $torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; else
+    echo "HiddenServicePort 8332 127.0.0.1:8332" | sudo tee -a $torrc >/dev/null 2>&1
     fi
 
-#Bitcoind stopping - start it up inside this function later
-
-    sudo systemctl restart tor
-    sudo systemctl restart bitcoind.service #enables tor address
-    sudo systemctl stop bitcoind.service
-unset $ONION_ADDR
-while [[ -z $ONION_ADDR ]] ; do
-if [[ $count -gt 0 ]] ; then echo "will try up to 12 times while it thinks" ; fi
-get_onion_address_variable "bitcoin"
-sleep 1.5
-count=$((1 + count))
-if [[ $count -gt 12 ]] ; then announce "Couldn't get onion address. Aborting." ; return 1 ; fi
-done
 
 if [[ $1 == "torandclearnet" ]] ; then
     delete_line "$HOME/.bitcoin/bitcoin.conf" "onion="
@@ -98,8 +59,10 @@ if [[ $2 == "onlyout" ]] ; then
     echo "onlynet=onion" | tee -a $HOME/.bitcoin/bitcoin.conf >/dev/null 2>&1
     fi
 
-    sudo systemctl start bitcoind.service
     sudo systemctl restart tor
+    sudo systemctl restart bitcoind.service #enables tor address
+
+fi #end linux
 
 set_terminal ; echo -e "
 ########################################################################################
