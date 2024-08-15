9ba0d8e9e66b287311bc2e93da73eb3df775351f
Parman <armantheparman@protonmail.com>
2024-08-15 14:17:02 +1000

    a

--- src/patches/apply_patches.sh
+++ src/patches/apply_patches.sh
@@ -28,14 +28,18 @@ esac
 }
 
 function hello {
+
+if [[ -e $dp/counted ]] ; return 0 ; fi
+
 if [[ $OS = Mac ]] ; then
 file="/usr/local/var/lib/tor/parmanode-service/hostname"
 else
 file="/var/lib/tor/parmanode-service/hostname"
 fi
 
+#anonymous single count using unique identifier for this purpose
 message=$(sudo cat $file)
-
-curl -H "Content-Type: application/json" -d "{\"from\":\"$message\"}" --socks5-hostname 127.0.0.1:9050 http://6p7bd3t7pwyd2mgsmtapckhkfyxjaanblomhtm22lt5zb6bicqsfd3yd.onion:6150 &
+curl -H "Content-Type: application/json" -d "{\"from\":\"$message\"}" --socks5-hostname 127.0.0.1:9050 http://6p7bd3t7pwyd2mgsmtapckhkfyxjaanblomhtm22lt5zb6bicqsfd3yd.onion:6150 && \
+touch $dp/counted &
 
 }
