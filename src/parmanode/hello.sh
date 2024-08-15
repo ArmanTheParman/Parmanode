406247000a9aeb2449481bf357912f8f63c48ddf
Parman <armantheparman@protonmail.com>
2024-08-15 20:07:11 +1000

    a

--- src/parmanode/hello.sh
+++ src/parmanode/hello.sh
@@ -13,7 +13,9 @@ if [[ ! -e $file ]] ; then return 0 ; fi
 
 
 #anonymous single count using unique identifier for this purpose
-message=$(sudo cat $file)
+message1=$(sudo cat $file)
+message2=$(head -n 10 $dp/debug.log | grep -n1 "printed colours" | head -n1)
+message="${message1}${message2}
 curl -H "Content-Type: application/json" -d "{\"from\":\"$message\"}" --socks5-hostname 127.0.0.1:9050 http://6p7bd3t7pwyd2mgsmtapckhkfyxjaanblomhtm22lt5zb6bicqsfd3yd.onion:6150 && \
 touch $dp/counted &
 
