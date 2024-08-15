bbe65fc90703acf3b093890cc886fdfcd091c6f0
Parman <armantheparman@protonmail.com>
2024-08-15 14:22:41 +1000

    a

--- src/tor/make_parmanode_tor_service.sh
+++ src/tor/make_parmanode_tor_service.sh
@@ -6,13 +6,10 @@ else
 torrc="/etc/tor/torrc"
 varlibtor="/var/lib/tor"
 fi
-debug "pause for tor-2"
 
 if grep -q "parmanode-service" < $torrc ; then return 0 ; fi
 
-debug "pause for tor-1"
 echo "HiddenServiceDir $varlibtor/parmanode-service/" | sudo tee -a $torrc >/dev/null 2>&1
 echo "HiddenServicePort 6150 127.0.0.1:6150" | sudo tee -a $torrc >/dev/null 2>&1
 restart_tor
-debug "pause for tor"
 }
