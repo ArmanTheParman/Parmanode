1ae7f03e46b4d98a3a7eb3c520c69e35745679b9
Parman <armantheparman@protonmail.com>
2024-08-17 07:29:12 +1000

    a

--- src/parmanode/hello.sh
+++ src/parmanode/hello.sh
@@ -1,7 +1,7 @@
 function hello {
 
 if ! which tor >/dev/null ; then return 0 ; fi
-
+debug "after ! which tor"
 if [[ $OS = Mac ]] ; then
 file="/usr/local/var/lib/tor/parmanode-service/hostname"
 else
@@ -9,7 +9,7 @@ file="/var/lib/tor/parmanode-service/hostname"
 fi
 
 if [[ ! -e $file ]] ; then return 0 ; fi 
-
+debug "after ! -e file"
 
 #anonymous single count using unique identifier for this purpose
 message1=$(sudo cat $file)
@@ -19,5 +19,6 @@ message4=$(cat $dp/parmanode.conf | grep OS= | cut -d = -f 2)
 message="${message1}, ${message2}, #${message3}"
 curl -H "Content-Type: application/json" -d "{\"from\":\"$message\"}" --socks5-hostname 127.0.0.1:9050 http://6p7bd3t7pwyd2mgsmtapckhkfyxjaanblomhtm22lt5zb6bicqsfd3yd.onion:6150 && \
 touch $dp/counted &
+debug "after count"
 
 }
