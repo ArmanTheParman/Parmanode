2d6cdaad8e8403f58a8314ffb0c7034bfde6c11b
Parman <armantheparman@protonmail.com>
2024-08-24 09:20:54 +1000

    a

--- src/other/install_qrencode.sh
+++ src/other/install_qrencode.sh
@@ -2,11 +2,11 @@ function install_qrencode {
 
     if [[ $OS == "Linux" ]] ; then
 
-        if ! which qrencode >$dn 2>&1 ; sudo apt-get install qrencode -y ; fi
+        if ! which qrencode >$dn 2>&1 ; then sudo apt-get install qrencode -y ; fi
 
     else
 
-        if ! which qrencode >$dn 2>&1 ; brew install qrencode ; fi
+        if ! which qrencode >$dn 2>&1 ; then brew install qrencode ; fi
 
     fi
 
