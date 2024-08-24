b94e376dd663bb1e236b7ff325215300159d68c1
Parman <armantheparman@protonmail.com>
2024-08-24 12:35:11 +1000

    a

--- src/lnd/make_lnd_service_tor.sh
+++ src/lnd/make_lnd_service_tor.sh
@@ -6,20 +6,18 @@ if [[ $OS == Linux ]] ; then
 
     if grep -q "7008" < /etc/tor/torrc ; then return 0 ; fi
 
-    echo "
-HiddenServiceDir /var/lib/tor/lnd-service
+    echo "HiddenServiceDir /var/lib/tor/lnd-service
 HiddenServicePort 7008 127.0.0.1:8080
 " | sudo tee -a /etc/tor/torrc >$dn 2>&1
-
+    restart_tor
 else
 
     if grep -q "7008" < /usr/local/etc/tor/torrc ; then return 0 ; fi
 
-    echo "
-HiddenServiceDir /usr/local/var/lib/tor/lnd-service
+    echo "HiddenServiceDir /usr/local/var/lib/tor/lnd-service
 HiddenServicePort 7008 127.0.0.1:8080
 " | sudo tee -a /usr/local/etc/tor/torrc >$dn 2>&1
-
+    restart_tor
 fi
 
 }
