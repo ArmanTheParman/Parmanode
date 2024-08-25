e619918a5508c132d46e7d78a613260957346d29
Parman <armantheparman@protonmail.com>
2024-08-25 22:46:41 +1000

    a

--- src/lnd/make_lnd_service_tor.sh
+++ src/lnd/make_lnd_service_tor.sh
@@ -4,7 +4,7 @@ if ! which tor > $dn 2>&1 ; then return 0 ; fi
 
 if [[ $OS == Linux ]] ; then
 
-    if grep -q "7008" < /etc/tor/torrc ; then return 0 ; fi
+    if grep -q "7008" < /etc/tor/torrc >/dev/null 2>&1 ; then return 0 ; fi
 
     echo "HiddenServiceDir /var/lib/tor/lnd-service
 HiddenServicePort 7008 127.0.0.1:8080
@@ -12,7 +12,7 @@ HiddenServicePort 7008 127.0.0.1:8080
     restart_tor
 else
 
-    if grep -q "7008" < /usr/local/etc/tor/torrc ; then return 0 ; fi
+    if grep -q "7008" < /usr/local/etc/tor/torrc >/dev/null 2>&1 ; then return 0 ; fi
 
     echo "HiddenServiceDir /usr/local/var/lib/tor/lnd-service
 HiddenServicePort 7008 127.0.0.1:8080
