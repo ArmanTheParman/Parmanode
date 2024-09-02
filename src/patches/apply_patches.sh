6f824fd1d6ef8b661b4dd104edf995659a02ab1a
Parman <armantheparman@protonmail.com>
2024-09-02 15:09:25 +1000

    a

--- src/patches/apply_patches.sh
+++ src/patches/apply_patches.sh
@@ -3,12 +3,19 @@ function apply_patches {
 #patch=n
 #get $patch from parmanode.conf
 temp_patch
+debug "1"
 openssh_patch
+debug "2"
 make_parmanode_service #Linux only
+debug "3"
 make_tor_script_mac
+debug "4"
 make_parmanode_tor_service  #makes parmanode tor onion address
+debug "5"
 hello
+debug "6"
 suggest_brew
+debug "7"
 
 case $patch in 
 1) 
@@ -26,4 +33,5 @@ return 0 ;;
 *) 
 patch_1 ; patch_2 ; patch_3 ; patch_4 ; patch_5 ; patch_6 ;; 
 esac
+debug "8"
 }
