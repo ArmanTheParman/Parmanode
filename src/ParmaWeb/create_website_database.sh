5cb845bdae4bbc2d214521dc3118a778f4a00269
Parman <armantheparman@protonmail.com>
2024-09-10 03:24:23 +1000

    a

--- src/ParmaWeb/create_website_database.sh
+++ src/ParmaWeb/create_website_database.sh
@@ -15,7 +15,7 @@ set_terminal ; echo -e "
     It's best to not include any symbols.
     $pink
     NOTE YOU WILL NEED TO ENTER IT 3 TIMES. ONCE TO DECLARE IT, ANOTHER TO CONFIRM,
-    AND FILNALLY AGAIN TO USE IT TO CREATE THE DATABASE.
+    AND FILNALLY AGAIN TO USE IT TO CREATE THE DATABASE.$orange
 
 ########################################################################################
 
