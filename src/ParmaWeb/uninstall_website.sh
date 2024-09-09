43c995421ad23467dcaafb8c9a403b3756158165
Parman <armantheparman@protonmail.com>
2024-09-10 03:16:42 +1000

    a

--- src/ParmaWeb/uninstall_website.sh
+++ src/ParmaWeb/uninstall_website.sh
@@ -5,98 +5,88 @@ set_terminal ; echo -e "
 $cyan
                                  Uninstall Website?
 $orange
-    Are you sure? (y) (n)
+
+    Please type the name of the database from the list below that you wish to 
+    uninstall or anything else to abort. (This will uninstall ParmaWeb, and also
+    give you the option to delete the website data directory)
+$red
+    $(sudo ls /var/www/ | grep website)
+    
 
 ########################################################################################
 "
-choose "x" 
-read choice
-set_terminal
-
-if [[ $choice == "y" || $choice == "Y" ]] ; then true
-    else 
-    return 1
-    fi
+choose "xpmq" 
+read choice ; set_terminal
+case $choice in
+q|Q) exit ;; p|P) return 1 ;; m|M) back2main ;;
+website)
+if ! sudo test -d /var/www/website ; then announce "Invalid entry, directory does not exist" ; return 1 ; fi
+export website="website"
+;;
+website2)
+if ! sudo test -d /var/www/website2 ; then announce "Invalid entry, directory does not exist" ; return 1 ; fi
+export website="website2"
+;;
+website3)
+if ! sudo test -d /var/www/website3 ; then announce "Invalid entry, directory does not exist" ; return 1 ; fi
+export website="website3"
+;;
+website4)
+if ! sudo test -d /var/www/website4 ; then announce "Invalid entry, directory does not exist" ; return 1 ; fi
+export website="website4"
+;;
+website5)
+if ! sudo test -d /var/www/website5 ; then announce "Invalid entry, directory does not exist" ; return 1 ; fi
+export website="website5"
+;;
+website6)
+if ! sudo test -d /var/www/website6 ; then announce "Invalid entry, directory does not exist" ; return 1 ; fi
+export website="website6"
+;;
+website7)
+if ! sudo test -d /var/www/website7 ; then announce "Invalid entry, directory does not exist" ; return 1 ; fi
+export website="website7"
+;;
+website8)
+if ! sudo test -d /var/www/website8 ; then announce "Invalid entry, directory does not exist" ; return 1 ; fi
+export website="website8"
+;;
+website9)
+if ! sudo test -d /var/www/website9 ; then announce "Invalid entry, directory does not exist" ; return 1 ; fi
+export website="website9"
+;;
+*)
+invalid
+;;
+esac
 
 while true ; do
 set_terminal ; echo -e "
 ########################################################################################
 
-    Do you want Parmanode to delete your website directory as well? This is the 
-    directory with your website pages, located at: $cyan
-
-        /var/www/website/
+    Are you sure you want Parmanode to delete your website directory and database?
 $orange
     Please choose: $red
                              delete)     Delete
 $green
-                             l)          Leave it
+                             a)          Abort!
 $orange
 ########################################################################################
 " ; choose "xpmq" ; read choice ; set_terminal
-case $choice in q|Q) exit 0 ;; p|P) return 1 ;; l|L) break ;; m|M) back2main ;;
+case $choice in q|Q) exit 0 ;; p|P) return 1 ;; a|A) return 1 ;; m|M) back2main ;;
 delete) 
-sudo rm -rf /var/www/website >/dev/null 2>&1
+sudo rm -rf /var/www/$website >/dev/null 2>&1
 break
 ;;
 *) invalid ;;
 esac
 done
 
+sudo mysql -u root -p -e "DROP DATABASE $website;"
+
 installed_conf_remove "website-end"
 
-while true ; do
-set_terminal ; echo -e "
-########################################################################################
-    Type$cyan no$orange and$cyan <enter>$orange to NOT remove MariaDB.
-########################################################################################
-" ; read choice ; set_terminal
-case $choice in no|N|NO|No) break ;; esac
-sudo apt-get purge 'mariadb-client*' 'mariadb-client-core*' 'mariadb-server-core*' 'mariadb-common*' -y
-break
-done
-
-while true ; do
-set_terminal ; echo -e "
-########################################################################################
-    Type$cyan no$orange and$cyan <enter>$orange to NOT remove php.
-########################################################################################
-" ; read choice ; set_terminal
-case $choice in no|N|NO|No) break ;; esac
-sudo apt-get purge 'php-mysql' 'php7.*-mysql' -y
-break
-done
-
-sudo apt-get autoclean -y
-
-while true ; do
-set_terminal ; echo -e "
-########################################################################################
-
-    Delete any existing phpmyadmin directory?
-   $green
-    y) yes
-$red
-    n) no
-$orange
-########################################################################################
-"
-choose "xpmq" ; read choice
-case $choice in
-q|Q) exit 0 ;; p|P|M|m) back2main ;;
-y|yes)
-rm -rf $hp/phpmyadmin ; break
-;;
-n|no)
-break
-;;
-*)
-invalid
-;;
-esac
-done
-
-sudo rm -rf /etc/nginx/conf.d/website* >/dev/null 2>&1
+sudo rm -rf /etc/nginx/conf.d/$website* >/dev/null 2>&1
 sudo rm -rf /etc/nginx/conf.d/$domain_name.conf >/dev/null 2>&1
 sudo rm -rf /etc/letsencrypt/live/$domain_name >/dev/null 2>&1
 sudo rm -rf /etc/letsencrypt/live/www.$domain_name >/dev/null 2>&1
@@ -104,7 +94,7 @@ sudo systemctl restart nginx >/dev/null 2>&1
 
 parmanode_conf_remove "domain"
 parmanode_conf_remove "www"
-parmanode_conf_remove "website" # Includes "website_ssl=true" and "website_database...""
-installed_conf_remove "website"
+parmanode_conf_remove "$website" # Includes "website_ssl=true" and "website_database...""
+installed_conf_remove "$website"
 success "The website has been uninstalled"
 }
