649e01dd42589a922db2c6041e2fd63498d3c8c5
Parman <armantheparman@protonmail.com>
2024-10-13 00:48:32 +1100

    a

--- src/ParmaWallet/electrum_cracker/crack.py
+++ src/ParmaWallet/electrum_cracker/crack.py
@@ -18,6 +18,7 @@ if len(os.listdir("/home/parman/parmanode/parmabox")) == 0:
 try:
     if len(os.listdir("/home/parman/parmanode/parmabox")) == 1 : 
         walletpath=f"/home/parman/parmanode/parmabox/{os.listdir("/home/parman/parmanode/parmabox")[0]}"
+        input(walletpath)
 except Exception as e: 
     input(e)
     sys.exit(1)
