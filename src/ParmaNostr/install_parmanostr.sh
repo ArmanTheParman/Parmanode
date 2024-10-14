a24a3c49ca8ec3fef1b432b7890dbc68856cf5de
Parman <armantheparman@protonmail.com>
2024-10-14 15:56:46 +1100

    a

--- src/ParmaNostr/install_parmanostr.sh
+++ src/ParmaNostr/install_parmanostr.sh
@@ -6,6 +6,7 @@ check_nostr_wallet_exists #get skipwallet value
 
 if [[ $skipwallet != "true" ]] ; then
 make_nostr_wallet || return 1
+debug "after make_nostr_wallet"
 installed_config_add "parmanostr-start"
 fi
 unset skipwallet
@@ -15,7 +16,10 @@ unset skipwallet
 
 
 installed_config_add "parmanostr-end"
+debug "before removal of random_binary.txt"
 rm $dp/.nostr_keys/random_binary.txt >/dev/null 2>&1
+
+debug "before make_sourcable_keys_file"
 
 make_sourcable_keys_file
 
