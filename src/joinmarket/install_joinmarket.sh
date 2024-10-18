7be2eaa0e79d4b245f4fb37e5c9ca2790904dba0
Parman <armantheparman@protonmail.com>
2024-10-18 13:51:06 +1100

    a

--- src/joinmarket/install_joinmarket.sh
+++ src/joinmarket/install_joinmarket.sh
@@ -17,12 +17,13 @@ success "JoinMarket has been installed"
 }
 
 function jm_dependencies {
-
+set_terminal
+echo "Installing dependencies..."
 sudo apt-get update -y
 sudo apt install -y \
     python-virtualenv curl python3-dev python3-pip build-essential automake \
     pkg-config libtool libgmp-dev libltdl-dev libssl-dev libatlas3-base libopenjp2-7
-
+enter_continue
 return 0
 }
 
@@ -35,18 +36,28 @@ if [[ $bitcoinrunning == "false" ]] ; then
     return 1
 fi
 
+set_terminal
+echo "Creating joinmarket wallet with Bitcoin Core/Knots..."
 bitcoin-cli -named createwallet wallet_name=jm_wallet descriptors=false
+enter_continue
 }
 
 
 function create_jm_user {
+set_terminal
+echo "Creating joinmarket user..."
 sudo adduser --disabled-password --gecos "" joinmarket >$dn 2>&1
 sudo usermod -a -G $USER joinmarket >$dn 2>&1
+enter_continue
 }
 
 function create_jm_directories {
+set_terminal
+echo "Creating joinmarket directories and symplinks..."
+
 sudo mkdir -p /home/joinmarket/.joinmarket #default dir where JM expects files. 
 sudo chown -R joinmarket:joinmarket /home/joinmarket/.joinmarket
 sudo ln -s $HOME/.bitcoin /home/joinmarket/.bitcoin
 sudo chown -R joinmarket:joinmarket /home/joinmarket/.bitcoin
+enter_continue
 }
