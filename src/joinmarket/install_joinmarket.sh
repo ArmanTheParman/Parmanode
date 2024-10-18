e107cf0b2f1574c34ff6e42b7a69419f2484a9a7
Parman <armantheparman@protonmail.com>
2024-10-19 00:57:05 +1100

    a

--- src/joinmarket/install_joinmarket.sh
+++ src/joinmarket/install_joinmarket.sh
@@ -20,7 +20,7 @@ verify_joinmarket || return 1
 
 extract_joinmarket || return 1
 
-do_install_joinmarket || return 1
+#do_install_joinmarket || return 1
 
 # #activation_script_joinmarket || return 1
 
@@ -115,12 +115,12 @@ sudo -u joinmarket tar -xvf /home/joinmarket/*gz
 sudo mv /home/joinmarket/joinmarket-clientserver* /home/joinmarket/joinmarket
 }
 
-function do_install_joinmarket {
-set_terminal
-cd /home/joinmarket/joinmarket
-echo -e "${green}Installing JoinMarket...${orange}"
-./install.sh --without-qt --disable-secp-check --disable-os-deps-check
-}
+# function do_install_joinmarket {
+# set_terminal
+# cd /home/joinmarket/joinmarket
+# echo -e "${green}Installing JoinMarket...${orange}"
+# ./install.sh --without-qt --disable-secp-check --disable-os-deps-check
+# }
 
 
 # function activation_script_joinmarket {
