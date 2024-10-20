e716655a0abad8cb83da9a761905acaacfb1b0c2
Parman <armantheparman@protonmail.com>
2024-10-21 00:45:59 +1100

    a

--- src/joinmarket/Install_joinmarket.sh
+++ src/joinmarket/Install_joinmarket.sh
@@ -90,12 +90,12 @@ function run_wallet_tool_joinmarket {
 function modify_joinmarket_cfg {
     jmfile="/root/.joinmarket/joinmarket.cfg"
     source $bc
-
+    enter_continue "rpcuser: $rpcuser, rpcpassword: $rpcpassword"
     docker exec joinmarket bash -c "sed -i '/rpc_cookie_file =/d' $jmfile"
-    docker exec joinmarket bash -c "sed -i 's/rpc_wallet_file =/rpc_wallet_file = jm_wallet/' $jmfile"
-    docker exec joinmarket bash -c "sed -i 's/rpc_user =/rpc_user = $rpcuser/' $jmfile"
-    docker exec joinmarket bash -c "sed -i 's/rpc_password =/rpc_password = $rpcuser/' $jmfile"
-    docker exec joinmarket bash -c "sed -i 's/onion_serving_port =/onion_serving_port = 8077/' $jmfile"
+    docker exec joinmarket bash -c "sed -i '/rpc_wallet_file =/c\rpc_wallet_file = jm_wallet/' $jmfile"
+    docker exec joinmarket bash -c "sed -i '/rpc_user =/\crpc_user = $rpcuser/' $jmfile"
+    docker exec joinmarket bash -c "sed -i '/rpc_password =/c\rpc_password = $rpcuser/' $jmfile"
+    docker exec joinmarket bash -c "sed -i '/onion_serving_port =/c\onion_serving_port = 8077/' $jmfile"
     return 0
 }
 
