7637c06fa9ac5f53bbd558b2d6a31df4b00a188b
Parman <armantheparman@protonmail.com>
2024-10-21 00:48:06 +1100

    a

--- src/joinmarket/Install_joinmarket.sh
+++ src/joinmarket/Install_joinmarket.sh
@@ -92,10 +92,10 @@ function modify_joinmarket_cfg {
     source $bc
     enter_continue "rpcuser: $rpcuser, rpcpassword: $rpcpassword"
     docker exec joinmarket bash -c "sed -i '/rpc_cookie_file =/d' $jmfile"
-    docker exec joinmarket bash -c "sed -i '/rpc_wallet_file =/c\rpc_wallet_file = jm_wallet/' $jmfile"
-    docker exec joinmarket bash -c "sed -i '/rpc_user =/\crpc_user = $rpcuser/' $jmfile"
-    docker exec joinmarket bash -c "sed -i '/rpc_password =/c\rpc_password = $rpcuser/' $jmfile"
-    docker exec joinmarket bash -c "sed -i '/onion_serving_port =/c\onion_serving_port = 8077/' $jmfile"
+    docker exec joinmarket bash -c "sed -i '/rpc_wallet_file =/c\rpc_wallet_file = jm_wallet' $jmfile"
+    docker exec joinmarket bash -c "sed -i '/rpc_user =/\crpc_user = $rpcuser' $jmfile"
+    docker exec joinmarket bash -c "sed -i '/rpc_password =/c\rpc_password = $rpcuser' $jmfile"
+    docker exec joinmarket bash -c "sed -i '/onion_serving_port =/c\onion_serving_port = 8077' $jmfile"
     return 0
 }
 
