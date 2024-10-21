f864b06bcae2a4e526eb2b80e1826694d8928c52
Parman <armantheparman@protonmail.com>
2024-10-21 23:53:11 +1100

    a

--- src/joinmarket/menu_joinmarket.sh
+++ src/joinmarket/menu_joinmarket.sh
@@ -7,7 +7,7 @@ else
 joinmarket_running="${red}NOT RUNNING$orange"
 fi
 
-set_terminal ; echo -en "
+set_terminal_custom 47 ; echo -en "
 ########################################################################################$cyan
                                 J O I N M A R K E T $orange
 ########################################################################################
