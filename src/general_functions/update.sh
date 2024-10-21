2c26b032a03f94389593648241e9130d2650b0f9
Parman <armantheparman@protonmail.com>
2024-10-21 17:00:00 +1100

    a

--- src/general_functions/update.sh
+++ src/general_functions/update.sh
@@ -95,7 +95,7 @@ fi
 case $choice in
 y|Y|Yes|yes|u)
 sudo apt-get upgrade -y 
-sudo apt-get install jq netcat vim net-tools unzip tmux -y 
+sudo apt-get install jq netcat vim net-tools unzip tmux ssh -y 
 if ! which ssh >/dev/null ; then sudo apt-get install ssh ; sudo systemctl enable ssh >/dev/null 2>&1
 install_fuse noupdate #linux minmal installs may need this to run AppImages
 fi
