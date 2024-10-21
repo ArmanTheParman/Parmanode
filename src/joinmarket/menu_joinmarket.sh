637b64458aa27d5eb7fed4b07cff90e44cfaedc1
Parman <armantheparman@protonmail.com>
2024-10-22 00:07:06 +1100

    a

--- src/joinmarket/menu_joinmarket.sh
+++ src/joinmarket/menu_joinmarket.sh
@@ -67,7 +67,7 @@ sudo vim $HOME/.joinmarket/joinmarket.cfg
 ;;
 vc)
 cfg="$HOME/.joinmarket/joinmarket.cfg" 
-sed '/^#/d' $cfg | sed '/^$/d' | sed '/[.*]/a\' | less
+sed '/^#/d' $cfg | sed '/^$/d' | sed '/[.*]/a\ ' | less
 enter_continue
 ;;
 man)
