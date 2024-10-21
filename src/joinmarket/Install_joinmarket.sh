800726947d7b96196042192b0abe6f8368fd144d
Parman <armantheparman@protonmail.com>
2024-10-21 18:38:45 +1100

    a

--- src/joinmarket/Install_joinmarket.sh
+++ src/joinmarket/Install_joinmarket.sh
@@ -132,14 +132,13 @@ function clone_joinmarket {
 
 function parmashell_4_jm {
 
-cat << EOF | tee /tmp/b1 >$dn 2>&1
+cat << 'EOF' | tee /tmp/b1 >$dn 2>&1
 export LS_OPTIONS='--color=auto'
 eval "$(dircolors)"
 alias ls='ls $LS_OPTIONS'
 alias ll='ls $LS_OPTIONS -l'
 alias l='ls $LS_OPTIONS -lA'
 
-Some more alias to avoid making mistakes:
 alias rm='rm -i'
 alias cp='cp -i'
 alias mv='mv -i'
