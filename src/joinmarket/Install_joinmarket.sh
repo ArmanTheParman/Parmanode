95e5ba9151f723d9e4915b83aa44138f4c8254bd
Parman <armantheparman@protonmail.com>
2024-10-21 19:39:13 +1100

    a

--- src/joinmarket/Install_joinmarket.sh
+++ src/joinmarket/Install_joinmarket.sh
@@ -145,8 +145,7 @@ EOF
 
 cat /tmp/b1 $pn/src/ParmaShell/parmashell_functions > /tmp/b2
 
-echo "cd /jm/clientserver/scripts
-a" | tee -a /tmp/b2 >$dn 2>&1
+echo "a" | tee -a /tmp/b2 >$dn 2>&1
 
 docker cp /tmp/b2 joinmarket:/root/.bashrc
 }
