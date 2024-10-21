f4abd270e6f929ea074ed217d95983ef105de0e5
Parman <armantheparman@protonmail.com>
2024-10-22 00:33:20 +1100

    a

---
+++ src/parmanode/test_internet_connected.sh
@@ -0,0 +1,17 @@
+function test_internet_connected {
+ping -c 1 8.8.8.8 >$dn 2>&1 || set_terminal ; echo -e " 
+########################################################################################
+
+    Warning: Parmanode has detected you might not have an internet connection
+	active. Proceed with caution.
+
+	${red}q${orange} to quit.
+
+########################################################################################
+"
+choose xq ; read choice ; set_terminal
+case $choice in
+q|Q) exit ;; 
+esac
+return 0
+}
