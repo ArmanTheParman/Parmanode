ee82e55410b5d35eaca2391ad4d4ef0c0fd0e16b
Parman <armantheparman@protonmail.com>
2024-10-21 18:40:42 +1100

    a

--- src/joinmarket/Install_joinmarket.sh
+++ src/joinmarket/Install_joinmarket.sh
@@ -134,7 +134,6 @@ function parmashell_4_jm {
 
 cat << 'EOF' | tee /tmp/b1 >$dn 2>&1
 export LS_OPTIONS='--color=auto'
-eval "$(dircolors)"
 alias ls='ls $LS_OPTIONS'
 alias ll='ls $LS_OPTIONS -l'
 alias l='ls $LS_OPTIONS -lA'
