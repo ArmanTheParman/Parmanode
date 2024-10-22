baaf731c0ab08b1d56e3f4889dd1f7486253144c
Parman <armantheparman@protonmail.com>
2024-10-22 13:40:27 +1100

    a

--- src/trezor/install_trezor.sh
+++ src/trezor/install_trezor.sh
@@ -1,4 +1,5 @@
 function install_trezor {  
+export version="24.10.1"
 set_terminal
 
 config_warning "Trezor Suite" || return 1
@@ -22,8 +23,8 @@ cd $trezorDir
 installed_conf_add "trezor-start"
 
 if [[ $chip == arm64 && $OS == Mac ]] ; then
-curl -LO    https://github.com/trezor/trezor-suite/releases/download/v23.9.3/Trezor-Suite-23.9.3-mac-arm64.dmg
-curl -LO    https://github.com/trezor/trezor-suite/releases/download/v23.9.3/Trezor-Suite-23.9.3-mac-arm64.dmg.asc
+curl -LO    https://github.com/trezor/trezor-suite/releases/download/v$version/Trezor-Suite-$version-mac-arm64.dmg
+curl -LO    https://github.com/trezor/trezor-suite/releases/download/v$version/Trezor-Suite-$version-mac-arm64.dmg.asc
 verify_trezor || return 1
 hdiutil attach *.dmg ; cd /Volumes/Trezor* ; sudo rm -rf /Applications/"Trezor Suite" ; cp -r *app /Applications
 cd $trezorDir
@@ -32,8 +33,8 @@ sudo rm -rf *.dmg
 fi
 
 if [[ $chip == x86_64 && $OS == Mac ]] ; then
-curl -LO https://github.com/trezor/trezor-suite/releases/download/v23.9.3/Trezor-Suite-23.9.3-mac-x64.dmg
-curl -LO https://github.com/trezor/trezor-suite/releases/download/v23.9.3/Trezor-Suite-23.9.3-mac-x64.dmg.asc
+curl -LO https://github.com/trezor/trezor-suite/releases/download/v$version/Trezor-Suite-$version-mac-x64.dmg
+curl -LO https://github.com/trezor/trezor-suite/releases/download/v$version/Trezor-Suite-$version-mac-x64.dmg.asc
 verify_trezor || return 1
 hdiutil attach *.dmg ; cd /Volumes/Trezor* ; sudo rm -rf /Applications/"Trezor Suite" ; cp -r *app /Applications
 cd $trezorDir
@@ -43,8 +44,8 @@ fi
 
 if [[ $chip == x86_64 && $OS == Linux ]] ; then
 rm *AppImage 2>/dev/null
-curl -LO https://github.com/trezor/trezor-suite/releases/download/v23.9.3/Trezor-Suite-23.9.3-linux-x86_64.AppImage
-curl -LO https://github.com/trezor/trezor-suite/releases/download/v23.9.3/Trezor-Suite-23.9.3-linux-x86_64.AppImage.asc
+curl -LO https://github.com/trezor/trezor-suite/releases/download/v$version/Trezor-Suite-$version-linux-x86_64.AppImage
+curl -LO https://github.com/trezor/trezor-suite/releases/download/v$version/Trezor-Suite-$version-linux-x86_64.AppImage.asc
 verify_trezor || return 1
 sudo chmod +x ./*AppImage
     if ! grep -q udev-end < $dp/installed.conf ; then
@@ -55,8 +56,8 @@ fi
 
 if [[ $computer_type == Pi && $chip == aarch64 ]] ; then
 rm *AppImage 2>/dev/null
-curl -LO https://github.com/trezor/trezor-suite/releases/download/v23.9.3/Trezor-Suite-23.9.3-linux-arm64.AppImage 
-curl -LO https://github.com/trezor/trezor-suite/releases/download/v23.9.3/Trezor-Suite-23.9.3-linux-arm64.AppImage.asc
+curl -LO https://github.com/trezor/trezor-suite/releases/download/v$version/Trezor-Suite-$version-linux-arm64.AppImage 
+curl -LO https://github.com/trezor/trezor-suite/releases/download/v$version/Trezor-Suite-$version-linux-arm64.AppImage.asc
 verify_trezor || return 1
 sudo chmod +x ./*AppImage
     if ! grep -q udev-end < $dp/installed.conf ; then
