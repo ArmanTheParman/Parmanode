7722efb69dad724a324001bf33c58fdae63bcd59
Parman <armantheparman@protonmail.com>
2024-10-22 15:30:12 +1100

    a

--- src/bitbox/run_bitbox.sh
+++ src/bitbox/run_bitbox.sh
@@ -3,16 +3,7 @@ function run_bitbox {
 if [[ $OS == Mac ]] ; then
 open /Applications/BitBox.app
 elif [[ $OS == Linux ]] ; then
-~/parmanode/bitbox/Bit*AppImage >/dev/null 2>&1 &
+$hp/bitbox/Bit*AppImage >/dev/null 2>&1 &
 fi
-set_terminal ; echo -e "
-########################################################################################
-$cyan
-   BitBox App$orange will open in a moment in it's own window. You can continue to use
-   Parmanode as normal, or minimise it. 
-
-########################################################################################
-"
-enter_continue
 
 }
---
+++ src/green/menu_green.sh
@@ -0,0 +1,40 @@
+function menu_green {
+ while true ; do set_terminal ; echo -e "
+########################################################################################
+                 $cyan          Green Wallet Menu            $orange                   
+########################################################################################
+
+
+         (start)                 Start Green App 
+
+
+########################################################################################
+"
+choose "xpmq" ; read choice ; set_terminal
+case $choice in 
+q|Q|QUIT|Quit) exit 0 ;;
+p|P) menu_use ;; 
+m|M) back2main ;;
+
+start|Start|START|S|s)
+check_SSH || return 0
+please_wait ; echo "" ; echo "A Green Wallet App window should open soon."
+run_green
+return 0 ;;
+
+*)
+invalid
+;;
+
+esac
+done
+} 
+
+
+function run_green {
+if [[ $OS == Mac ]] ; then
+open /Applications/Blockstream*app
+elif [[ $OS == Linux ]] ; then
+$hp/green/Blockstream*.AppImage >/dev/null 2>&1 &
+fi
+}
