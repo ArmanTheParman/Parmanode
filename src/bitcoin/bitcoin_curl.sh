2a5a14226ca5e19e70dc479ca2be88772e790482
Parman <armantheparman@protonmail.com>
2024-10-23 22:37:16 +1100

    a

--- src/bitcoin/bitcoin_curl.sh
+++ src/bitcoin/bitcoin_curl.sh
@@ -8,7 +8,7 @@ set_terminal ; echo -en "
     The curl command to the bitcoin daemon, to run in terminal is...
 $cyan
 
-curl --user $rpcuser:$rpcpassword --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblockchaininfo", "params": [] }' -H 'content-type: text/plain;' http://$IP:8332
+curl --user $rpcuser:$rpcpassword --data-binary '{\"jsonrpc\": \"1.0\", \"id\":\"curltest\", \"method\": \"getblockchaininfo\", \"params\": [] }' -H 'content-type: text/plain;' http://$IP:8332
 $orange
     Parmanode can run this command for you or you can copy/paste it yourself, and
     make edits as needed.
