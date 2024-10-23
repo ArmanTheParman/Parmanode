64567780d218cdc36cfc62d1c555db8a9fccf934
Parman <armantheparman@protonmail.com>
2024-10-23 15:59:35 +1100

    a

--- src/patches/temp_patch.sh
+++ src/patches/temp_patch.sh
@@ -132,7 +132,7 @@ cleanup_bashrc_zshrc
 #echo "source $HOME/parman_programs/parmanode/src/ParmaShell/parmashell_functions" | sudo tee -a $bashrc >/dev/null 2>&1
 debug temppatchend
 
-#Make /media/$USER with permission of $USER. - Refactor this in to the code at some point.
+#Make /media/$USER with permission of $USER. - Refactor this in to the code at some point. Maybe to the installation.
 sudo chown $USER:$(id -gn) /media/$USER >/dev/null 2>&1
 sudo setfacl -m g::r-x /media/parman >$dn 2>&1 #make sure group has access
 
