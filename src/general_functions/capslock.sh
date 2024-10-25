6fc9cac5be0d0303a25f2cdfde2e11e52c5dec9a
Parman <armantheparman@protonmail.com>
2024-10-25 15:01:27 +1100

    a

---
+++ src/general_functions/capslock.sh
@@ -0,0 +1,12 @@
+function capslock {
+clear
+echo -e "
+########################################################################################
+
+    Parmanode suspects you might have the caps lock on. Careful with that, Boomer.
+
+########################################################################################
+"
+enter_continue
+
+}
