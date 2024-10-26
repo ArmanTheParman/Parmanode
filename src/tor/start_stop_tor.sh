d60a4a7ee1376c3924369483ae91e27ef3bc7b5e
Arman The Parman <77603167+ArmanTheParman@users.noreply.github.com>
2024-10-26 15:05:01 +1100

    Update start_stop_tor.sh

--- src/tor/start_stop_tor.sh
+++ src/tor/start_stop_tor.sh
@@ -1,24 +1,24 @@
 function start_tor {
 if [[ $OS == Linux ]] ; then
-sudo systemctl start tor >/dev/null
+sudo systemctl start tor >$dn
 fi
 if [[ $OS == Mac ]] ; then
-brew services start tor >/dev/null
+brew services start tor >$dn
 fi
 }
 
 function stop_tor {
 if [[ $OS == Linux ]] ; then
-sudo systemctl stop tor >/dev/null
+sudo systemctl stop tor >$dn
 fi
 if [[ $OS == Mac ]] ; then
-brew services stop tor >/dev/null
+brew services stop tor >$dn
 fi
 }
 
 function restart_tor {
 if [[ $OS == Linux ]] ; then
-sudo systemctl restart tor >/dev/null
+sudo systemctl restart tor >$dn
 fi
 if [[ $OS == Mac ]] ; then
 brew services restart tor >/dev/null
