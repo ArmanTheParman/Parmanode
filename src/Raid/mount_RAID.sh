e95224a2b2f55ca13dabc9ce3b73171869fb1b2a
Parman <armantheparman@protonmail.com>
2024-10-31 16:40:44 +1100

    a

--- src/Raid/mount_RAID.sh
+++ src/Raid/mount_RAID.sh
@@ -1,13 +1,14 @@
 function mount_RAID {
+
 for i in "$(sudo mdadm --detail --scan | awk '{print $2}')" ; do
 mount | grep -q "$i" && { set_terminal ; echo "$1 is already mounted" ; sleep 1.5 ; continue ; }
 set_terminal
 echo "mounting $i ..."
 sleep 1
 debug "1"
-sudo mkdir -p "/media/$USER/RAID$(echo $i | grep -oE '[^/dev/md/]')" >/dev/null
+sudo mkdir -p "/media/$USER/RAID$(echo $i | grep -oE '[^/dev/md/].+')" >/dev/null
 debug "2"
-sudo mount $i "/media/$USER/RAID$(echo $i | grep -oE '[^/dev/md/]')" #mounts /dev/md[num] to /media/$USER/RAID[num]
+sudo mount $i "/media/$USER/RAID$(echo $i | grep -oE '[^/dev/md/].+')" #mounts /dev/md[num] to /media/$USER/RAID[num]
 debug "mounted?"
 done
 }
