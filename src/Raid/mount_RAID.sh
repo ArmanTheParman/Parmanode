fcc9f216c7363c33f347f7b8693aa6b69d41b6f5
Parman <armantheparman@protonmail.com>
2024-10-31 16:29:49 +1100

    a

--- src/Raid/mount_RAID.sh
+++ src/Raid/mount_RAID.sh
@@ -5,7 +5,7 @@ set_terminal
 echo "mounting $i ..."
 sleep 1
 debug "1"
-sudo mkdir -p "/media/$USER/RAID$(echo $i | grep -oE '[0-9]+')" >/dev/null
+sudo mkdir -p "/media/$USER/RAID$(echo $i | grep -oE '[^/dev/md/]')" >/dev/null
 debug "2"
 sudo mount $i "/media/$USER/RAID$(echo $i | grep -oE '[^/dev/md/]')" #mounts /dev/md[num] to /media/$USER/RAID[num]
 debug "mounted?"
