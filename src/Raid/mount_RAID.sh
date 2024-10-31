d611d38435fa0ec51c7d25aec6397a2bd515f065
Parman <armantheparman@protonmail.com>
2024-10-31 16:40:54 +1100

    a

--- src/Raid/mount_RAID.sh
+++ src/Raid/mount_RAID.sh
@@ -1,6 +1,6 @@
 function mount_RAID {
 
-for i in "$(sudo mdadm --detail --scan | awk '{print $2}')" ; do
+for i in $(sudo mdadm --detail --scan | awk '{print $2}') ; do
 mount | grep -q "$i" && { set_terminal ; echo "$1 is already mounted" ; sleep 1.5 ; continue ; }
 set_terminal
 echo "mounting $i ..."
