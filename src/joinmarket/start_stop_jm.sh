f9a8e4b78ede5f94e5a7f51db53d6859d01685e7
Parman <armantheparman@protonmail.com>
2024-11-01 18:58:36 +1100

    a

--- src/joinmarket/start_stop_jm.sh
+++ src/joinmarket/start_stop_jm.sh
@@ -1,9 +1,21 @@
 function start_joinmarket {
+
 docker start joinmarket
-if [[ $OS == Mac ]] ; then
-docker exec -d joinmarket socat TCP4-LISTEN:61000,reuseaddr,fork TCP:127.0.0.1:62601
-fi
-start_socat joinmarket
+start_socat joinmarket   
+
+#For Macs, start socat inside container as well
+    if [[ $OS == Mac ]] ; then
+
+        counter=0
+        while [[ $counter -lt 10 ]] ; do
+            docker exec joinmarket ps >/dev/null && break
+            sleep 1
+            counter=$((counter + 1))
+        done
+
+        docker exec -d joinmarket socat TCP4-LISTEN:61000,reuseaddr,fork TCP:127.0.0.1:62601
+        return
+    fi
 }
 
 function stop_joinmarket {
