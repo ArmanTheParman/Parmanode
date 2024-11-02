2abb0b65a62bf9c904f15ebf8f9c96fb5893bd48
Parman <armantheparman@protonmail.com>
2024-11-03 02:58:54 +1100

    a

--- src/joinmarket/configure_yg.sh
+++ src/joinmarket/configure_yg.sh
@@ -63,9 +63,11 @@ while true ; do
         \r    would be 0.002% (and 0.5 would ridiculously be 50%)"
 
         if  [[ $(echo "$enter_cont > 0" | bc -l) == 1 && $(echo "$enter_cont < 1" | bc -l) ]] ; then
+            echo "1" ; sleep 3
             sudo gsed -iE "s/cfjee_r=.*$/cjfee_r = $enter_cont/" $jmcfg
             break
         else
+            echo "2" ; sleep 3
             invalid
             continue
         fi
