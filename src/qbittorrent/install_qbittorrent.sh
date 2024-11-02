c82419cb237309e4a867f2f7d751c49515c1cb43
Parman <armantheparman@protonmail.com>
2024-11-02 14:00:35 +1100

    a

--- src/qbittorrent/install_qbittorrent.sh
+++ src/qbittorrent/install_qbittorrent.sh
@@ -1,6 +1,6 @@
 function install_qbittorrent {
 clear
-qbt_version="5.0.0" #need to make sure right file exists in parmanode.com
+qbt_version="5.0.1" #need to make sure right file exists in parmanode.com
 if [[ $computer_type == Pi ]] ; then
 announce "Sorry, not available for Raspberry Pi or other Linux computers with
     ARM chips. Aborting" ; return 1 ; fi 
@@ -15,16 +15,16 @@ cd $hp/qbittorrent
 #download file
 if [[ $OS == Mac ]] ; then
 
-    curl -LO https://parmanode.com/qbittorrent-$qbt_version.dmg
+    curl -LO https://parmanode.com/qbittorrent-${qbt_version}.dmg
 
-    if [[ $(shasum -a 256 *dmg | awk '{print $1}') != "42eb7cd4a7046dfc762d453434bae372dc93be3b265a6db70974a338b8ef5436" ]] ; then
+    if [[ $(shasum -a 256 *dmg | awk '{print $1}') != "e0d52860cc41929bdab42df6c3405c1fb867d8dad514e4295df1eadcd64580f1" ]] ; then
         announce "Checksum failed. Aborting." ; return 1
     fi
 
 elif [[ $OS == Linux ]] ; then
     curl -LO https://parmanode.com/qbittorrent-${qbt_version}_x86_64.AppImage
 
-    if [[ $(shasum -a 256 *AppImage | awk '{print $1}') != "3bd6443ecd1237de6b77624b649fa84552f4e23e0154ca5ddb95532bf865317a" ]] ; then
+    if [[ $(shasum -a 256 *AppImage | awk '{print $1}') != "a0c3af177a53411a5e08a312e783ea9f0e31af372c1a7836d896360a0020c609" ]] ; then
         announce "Checksum failed. Aborting." ; return 1
     fi
 
