5cf362899c5894bf3848587c50cb3dfff5456091
Parman <armantheparman@protonmail.com>
2024-11-05 16:50:55 +1100

    fix_BTC_addr_btccombo

--- src/lnd/fix_BTC_addr_btccombo.sh
+++ src/lnd/fix_BTC_addr_btccombo.sh
@@ -11,8 +11,8 @@ if grep -q "btccombo-end" < $ic ; then
 store_BTC_container_IP
 if grep -q "$BTCIP" < $file ; then return 0 ; fi
 
-swap_string "$file" "bitcoind.zmqpubrawblock=tcp://127.0.0.1:28332" "bitcoind.zmqpubrawblock=tcp://$BTCIP:28332"
-swap_string "$file" "bitcoind.zmqpubrawtx=tcp://127.0.0.1:28333" "bitcoind.zmqpubrawtx=tcp://$BTCIP:28333"
-swap_string "$file" "bitcoind.rpchost=127.0.0.1" "bitcoind.rpchost=$BTCIP"
+gsed -i "/bitcoind.zmqpubrawblock=tcp://127.0.0.1:28332/c\bitcoind.zmqpubrawblock=tcp://$BTCIP:28332" $file
+gsed -i "/bitcoind.zmqpubrawtx=tcp://127.0.0.1:28333/c\bitcoind.zmqpubrawtx=tcp://$BTCIP:28333" $file
+gsed -i "/bitcoind.rpchost=127.0.0.1/c\bitcoind.rpchost=$BTCIP" $file
 fi
 }
