9996b6594c198cd32dcca15d88ada8bc0bda6dc8
Parman <armantheparman@protonmail.com>
2024-11-05 19:31:13 +1100

    nostr_tor_add

--- src/nostr/nostr_tor_add_remove.sh
+++ src/nostr/nostr_tor_add_remove.sh
@@ -1,16 +1,16 @@
 function nostr_tor_add {
 enable_tor_general || return 1
 
-if sudo grep "HiddenServiceDir /var/lib/tor/nostr-service/" \
-    /etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true 
+if sudo grep "HiddenServiceDir $macprefix/var/lib/tor/nostr-service/" \
+    $macprefix/etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true 
     else 
-    echo "HiddenServiceDir /var/lib/tor/nostr-service/" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
+    echo "HiddenServiceDir $macprefix/var/lib/tor/nostr-service/" | sudo tee -a $macprefix/etc/tor/torrc >/dev/null 2>&1
     fi
 
 if sudo grep "HiddenServicePort 7081 127.0.0.1:7080" \
-    /etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true 
+    $macprefix/etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true 
     else
-    echo "HiddenServicePort 7081 127.0.0.1:7080" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
+    echo "HiddenServicePort 7081 127.0.0.1:7080" | sudo tee -a $macprefix/etc/tor/torrc >/dev/null 2>&1
     fi
 
 sudo systemctl restart tor
@@ -23,8 +23,8 @@ if [[ $OS == "Mac" ]] ; then no_mac ; return 1 ; fi
 
 please_wait
 
-delete_line "/etc/tor/torrc" "nostr-service"
-delete_line "/etc/tor/torrc" "127.0.0.1:7080"
+sudo gsed -i "nostr-service" $macprefix/etc/tor/torrc 
+sudo gsed -i  "/127.0.0.1:7080/d" $macprefix/etc/tor/torrc
 
 sudo systemctl restart tor
 
