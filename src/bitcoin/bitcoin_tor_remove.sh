9e331cb20eb88b922eec73bb844703b1f0c75eb9
Parman <armantheparman@protonmail.com>
2024-11-05 20:27:19 +1100

    bitcoin_tor_remove

--- src/bitcoin/bitcoin_tor_remove.sh
+++ src/bitcoin/bitcoin_tor_remove.sh
@@ -9,9 +9,9 @@ stop_bitcoin
 sudo gsed -i  "/# Additions by Parmanode/d" $prefix/etc/tor/torrc
 sudo gsed -i  "/bitcoin-service/d"          $prefix/etc/tor/torrc
 sudo gsed -i  "/127.0.0.1:8333/d"           $prefix/etc/tor/torrc
-sudo gsed -i  "/onion/d"                    $HOME/.bitcoin/bitcoin.conf
-sudo gsed -i  "/bind=127.0.0.1/d"           $HOME/.bitcoin/bitcoin.conf
-sudo gsed -i  "/onlynet/d"                  $HOME/.bitcoin/bitcoin.conf
+sudo gsed -i  "/onion/d"                    $bc 
+sudo gsed -i  "/bind=127.0.0.1/d"           $bc
+sudo gsed -i  "/onlynet/d"                  $bc
 
 add_rpcbind
 
