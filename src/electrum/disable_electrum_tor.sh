250e296369b1328e7e7725d6b12f91f058b0f959
Parman <armantheparman@protonmail.com>
2024-11-05 19:07:17 +1100

    disable_electrum_tor

--- src/electrum/disable_electrum_tor.sh
+++ src/electrum/disable_electrum_tor.sh
@@ -5,7 +5,7 @@ echo "Make sure Electrum has been completely shut down before proceeding."
 enter_continue
 
 gsed -i "/\"server/c\    \"server\": \"127.17.0.2:50002:s\"," $HOME/.electrum/config
-delete_line "$HOME/.electrum/config" "proxy"
+sudo gsed "/proxy/d" $HOME/.electrum/config
 
 success "Electrum Wallet" "being disconnected from Tor."
 
