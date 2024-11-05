39a8877f8024edd4b93b917528fd654f2512cbd5
Parman <armantheparman@protonmail.com>
2024-11-05 19:46:06 +1100

    enable_tor_thunderhub

--- src/Thunderhub/enable_tor_thunderhub.sh
+++ src/Thunderhub/enable_tor_thunderhub.sh
@@ -4,14 +4,14 @@ source $pc
 
 enable_tor_general || return 1
 
-if sudo grep "HiddenServiceDir /var/lib/tor/thunderhub-service/" \
-    /etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; debug "true" ; else debug "else"
-    echo "HiddenServiceDir /var/lib/tor/thunderhub-service/" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
+if sudo grep "HiddenServiceDir $macprefix/var/lib/tor/thunderhub-service/" \
+    $macprefix/etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; debug "true" ; else debug "else"
+    echo "HiddenServiceDir $macprefix/var/lib/tor/thunderhub-service/" | sudo tee -a $macprefix/etc/tor/torrc >/dev/null 2>&1
     fi
 
 if sudo grep "HiddenServicePort 2050 127.0.0.1:$thub_port" \
-    /etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; else
-    echo "HiddenServicePort 2050 127.0.0.1:$thub_port" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
+    $macprefix/etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; else
+    echo "HiddenServicePort 2050 127.0.0.1:$thub_port" | sudo tee -a $macprefix/etc/tor/torrc >/dev/null 2>&1
     fi
 
 sudo systemctl restart tor
@@ -28,8 +28,8 @@ enter_continue
 
 function disable_tor_thunderhub {
 if [[ $OS == Mac ]] ; then no_mac ; return 1 ; fi
-file="/etc/tor/torrc"
-delete_line "$file" "thunderhub"
-delete_line "$file" "127.0.0.1:2050"
+file="$macprefix/etc/tor/torrc"
+sudo gsed -i "/thunderhub/d" $file 
+sudo gsed -i "/127.0.0.1:2050/d" $file 
 sudo systemctl restart tor
 }
