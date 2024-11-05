9875a922b9be7a8cc05d121789f956918b886c73
Parman <armantheparman@protonmail.com>
2024-11-05 22:50:24 +1100

    enable_tor_litterminal

--- src/litd/enable_tor_littermianl.sh
+++ src/litd/enable_tor_littermianl.sh
@@ -4,14 +4,14 @@ if [[ $OS == Mac ]] ; then announce "Tor feature not availalbe for Mac yet." ; r
 clear
 enable_tor_general || return 1
 clear
-if sudo grep "HiddenServiceDir /var/lib/tor/litterminal-service/" \
-    /etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; else
-    echo "HiddenServiceDir /var/lib/tor/litterminal-service/" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
+if sudo grep "HiddenServiceDir $macprefix/var/lib/tor/litterminal-service/" \
+    $macprefix/etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; else
+    echo "HiddenServiceDir $macprefix/var/lib/tor/litterminal-service/" | sudo tee -a $macprefix/etc/tor/torrc >/dev/null 2>&1
     fi
 
 if sudo grep "HiddenServicePort 7007 127.0.0.1:8443" \
-    /etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; else
-    echo "HiddenServicePort 7007 127.0.0.1:8443" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
+    $macprefix/etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; else
+    echo "HiddenServicePort 7007 127.0.0.1:8443" | sudo tee -a $macprefix/etc/tor/torrc >/dev/null 2>&1
     fi
 
 sudo systemctl restart tor
@@ -20,7 +20,7 @@ sudo systemctl restart tor
 function disable_tor_litterminal {
 if [[ $OS == Mac ]] ; then return 1 ; fi
 clear
-file="/etc/tor/torrc"
+file="$macprefix/etc/tor/torrc"
 
 sudo gsed -i "/litterminal-service/d" $file
 sudo gsed -i "/7007 127/d" $file
