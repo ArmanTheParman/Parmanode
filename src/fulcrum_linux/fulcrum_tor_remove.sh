90db8e2486ba9e1aa6e66d55cba30a2a2942e858
Parman <armantheparman@protonmail.com>
2024-11-05 19:12:13 +1100

    fulcrum_tor_remove

--- src/fulcrum_linux/fulcrum_tor_remove.sh
+++ src/fulcrum_linux/fulcrum_tor_remove.sh
@@ -1,16 +1,14 @@
 function fulcrum_tor_remove {
-
 if [[ $OS == "Mac" ]] ; then no_mac ; return 1 ; fi
 
 please_wait
-
-delete_line "/etc/tor/torrc" "fulcrum-service"
-delete_line "/etc/tor/torrc" "127.0.0.1:50001"
+sudo gsed -i "/fulcrum-service/d" $macprefix/etc/tor/torrc
+sudo gsed -i "/127.0.0.1:500001/d" $macprefix/etc/tor/torrc
 
 sudo systemctl daemon-reload
 sudo systemctl restart tor
 sudo systemctl restart fulcrum.service
-debug "before set terminal, fulcrum tor remove"
+
 set_terminal
 parmanode_conf_remove "fulcrum_tor"
 return 0
