76ac8204d1656554855a4579341d395b94ad20a7
Parman <armantheparman@protonmail.com>
2024-11-05 19:45:02 +1100

    enable_tor_rtl

--- src/rtl/enable_tor_rtl.sh
+++ src/rtl/enable_tor_rtl.sh
@@ -4,14 +4,14 @@ if [[ $OS == Mac ]] ; then announce "Tor feature not availalbe for Mac yet." ; r
 clear
 enable_tor_general || return 1
 clear
-if sudo grep "HiddenServiceDir /var/lib/tor/rtl-service/" \
-    /etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; else
-    echo "HiddenServiceDir /var/lib/tor/rtl-service/" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
+if sudo grep "HiddenServiceDir $macprefix/var/lib/tor/rtl-service/" \
+    $macprefix/etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; else
+    echo "HiddenServiceDir $macprefix/var/lib/tor/rtl-service/" | sudo tee -a $macprefix/etc/tor/torrc >/dev/null 2>&1
     fi
 
 if sudo grep "HiddenServicePort 7005 127.0.0.1:3000" \
-    /etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; else
-    echo "HiddenServicePort 7005 127.0.0.1:3000" | sudo tee -a /etc/tor/torrc >/dev/null 2>&1
+    $macprefix/etc/tor/torrc | grep -v "^#" >/dev/null 2>&1 ; then true ; else
+    echo "HiddenServicePort 7005 127.0.0.1:3000" | sudo tee -a $macprefix/etc/tor/torrc >/dev/null 2>&1
     fi
 
 parmanode_conf_add "rtl_tor=true"
@@ -22,10 +22,10 @@ sudo systemctl restart tor
 function disable_tor_rtl {
 if [[ $OS == Mac ]] ; then return 1 ; fi
 clear
-file="/etc/tor/torrc"
+file="$macprefix/etc/tor/torrc"
 
-delete_line "$file" "rtl-service"
-delete_line "$file" "7005 127"
+sudo gsed -i "/rtl-service/d" $file 
+sudo gsed -i "/7005 127/d" $file 
 parmanode_conf_remove "rtl_tor"
 sudo systemctl restart tor
 
