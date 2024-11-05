f82f84d4cf2c9514daa54373ad4852bb32fc8a4e
Parman <armantheparman@protonmail.com>
2024-11-05 19:15:10 +1100

    edit_user_pass_fulcrum_docker

--- src/fulcrum_mac/edit_user_pass_fulcrum_docker.sh
+++ src/fulcrum_mac/edit_user_pass_fulcrum_docker.sh
@@ -2,11 +2,10 @@ function edit_user_pass_fulcrum_docker {
     
 if [[ $3 != remote ]] ; then # $3 used in function fulcrum_to_remote
 #from the host machine
-source $HOME/.bitcoin/bitcoin.conf
+source $bc
 fi
-
-delete_line "$HOME/parmanode/fulcrum/fulcrum.conf" "rpcuser"
-delete_line "$HOME/parmanode/fulcrum/fulcrum.conf" "rpcpassword"
-echo "rpcuser = $rpcuser" >> $HOME/parmanode/fulcrum/fulcrum.conf
-echo "rpcpassword = $rpcpassword" >> $HOME/parmanode/fulcrum/fulcrum.conf
+sudo gsed -i "/rpcuser/d" $fc
+sudo gsed -i "/rpcpassword/d" $fc
+echo "rpcuser = $rpcuser" >> $fc
+echo "rpcpassword = $rpcpassword" >> $fc
 }
