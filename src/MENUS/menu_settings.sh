0a9450bf84c1e722db6d27e68d402573dd182ac1
Parman <armantheparman@protonmail.com>
2024-11-05 19:25:48 +1100

    menu_main

--- src/MENUS/menu_settings.sh
+++ src/MENUS/menu_settings.sh
@@ -23,10 +23,10 @@ p|P) return ;;
 c|C) change_colours ; return 0 ;;
 an)
 if [[ $announcements == off ]] ; then
-delete_line "$hm" "announcements="
+gsed -i "/announcements=/d" $hm 
 echo "announcements=on" | tee -a $hm 
 else
-delete_line "$hm" "announcements="
+gsed -i "/announcements=/d" $hm 
 echo "announcements=off" | tee -a $hm
 fi
 ;;
