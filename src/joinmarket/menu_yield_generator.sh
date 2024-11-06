71771729620d9865a0040c55497b1860a8602dc1
Parman <armantheparman@protonmail.com>
2024-11-06 21:03:42 +1100

    a

--- src/joinmarket/menu_yield_generator.sh
+++ src/joinmarket/menu_yield_generator.sh
@@ -43,7 +43,7 @@ else
     unset ygrunning
 fi
 
-if tail -n1 $logfile | grep -qi "locked by pid" || tail -n1 $logfile | grep -qi "Failed to load wallet" ; then
+if  [[ $wallet != "NONE" ]] && ( tail -n1 $logfile | grep -qi "locked by pid" || tail -n1 $logfile | grep -qi "Failed to load wallet" ) ; then
     announce "Some issue with loading wallet. See log for information."
     ygtext="    Yield Generator is$red   NOT RUNNING$orange"
     unset ygrunning
