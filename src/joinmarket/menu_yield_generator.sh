895cdc93632c887b1acea7909512edee64c12aa8
Parman <armantheparman@protonmail.com>
2024-11-06 23:22:52 +1100

    a

--- src/joinmarket/menu_yield_generator.sh
+++ src/joinmarket/menu_yield_generator.sh
@@ -37,19 +37,19 @@ if docker exec joinmarket ps ax | grep yg-privacyenhanced.py | grep -vq bash ; t
     ygtext="    Yield Generator is:    $green   RUNNING$orange with wallet$red $wallet"
     
     if docker exec joinmarket ps ax | grep obwatch | grep -q python ; then
-    orderbooknn="\r    Orderbookd Nickname is:   $nick"
+    orderbooknn="\n\r    Orderbookd Nickname is:   $nick"
     ygrunning="true"
     fi
 else
     ygtext="    Yield Generator is$red   NOT RUNNING$orange"
-    orderbooknn="\n\r    Orderbookd Nickname is:   $nick"
+    unset orderbooknn
     unset ygrunning
 fi
 
 if  [[ $wallet != "NONE" ]] && ( tail -n1 $logfile | grep -qi "locked by pid" || tail -n1 $logfile | grep -qi "Failed to load wallet" ) ; then
     announce "Some issue with loading wallet. See log for information."
     ygtext="    Yield Generator is$red   NOT RUNNING$orange"
-    orderbooknn="\r    Orderbookd Nickname is:   $nick"
+    unset orderbooknn
     unset ygrunning
 fi
 
