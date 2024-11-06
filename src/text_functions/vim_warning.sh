94f810efad35e560fe6ff22e5f2698a05123154a
Parman <armantheparman@protonmail.com>
2024-11-07 00:44:46 +1100

    a

--- src/text_functions/vim_warning.sh
+++ src/text_functions/vim_warning.sh
@@ -2,7 +2,8 @@ function vim_warning {
 
 if grep -q vimisthebest < $hm ; then return 0 ; fi
 
-announce "Using vim is hard. To exit, use <esc> then : then q then <enter>
+announce "Using vim is hard. To exit, use $cyan<esc>$ornage then$cyan :$orange then$cyan q$orange then$cyan <enter>$orange.
+
     Type 'vimisthebest' to dismiss this reminder permanently"
 
 if [[ $enter_cont == "vimisthebest" ]] ; then
