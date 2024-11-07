38a0cdd4376f08943de23dc4cc1820ab51bc2e3f
Parman <armantheparman@protonmail.com>
2024-11-07 20:27:02 +1100

    a

--- src/joinmarket/menu_joinmarket.sh
+++ src/joinmarket/menu_joinmarket.sh
@@ -6,7 +6,7 @@ export oblogfile="$HOME/.joinmarket/orderbook.log"
 while true ; do 
 
 socatstatus=$(if tmux ls | grep -q joinmarket_socat ; then 
-echo "${green}RUNNING$orange\n    Access Order Book:$bright_blue   $IP:61000$orange"
+echo "${green}RUNNING$orange\n    Access Order Book\n    from external: $bright_blue   $IP:61000$orange"
 else echo "${red}NOT RUNNING$orange"
 fi)
 
@@ -42,7 +42,7 @@ if docker ps 2>$dn | grep -q joinmarket ; then
     #is obwatcher running?
     export obwatcherPID=$(docker exec joinmarket ps ax | grep "ob-watcher.py" | awk '{print $1}')
     if [[ $obwatcherPID =~ [0-9]+ ]] ; then
-        export orderbook="${green}RUNNING$orange \n    Access Order Book:$bright_blue     localhost:61000 or 127.0.0.1:61000$orange"
+        export orderbook="${green}RUNNING$orange \n    Access Order Book:$bright_blue   localhost:61000 or 127.0.0.1:61000$orange"
     else
         export orderbook="${red}NOT RUNNING$orange"
     fi
