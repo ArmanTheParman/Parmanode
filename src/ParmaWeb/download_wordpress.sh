04fd9a9a6ba8cca461fc0920bc97e1a3da0c6f4c
Parman <armantheparman@protonmail.com>
2024-11-09 12:33:20 +1100

    a

--- src/ParmaWeb/download_wordpress.sh
+++ src/ParmaWeb/download_wordpress.sh
@@ -1,4 +1,5 @@
 function download_wordpress { 
+debug "website variable is... $website"
 cd /var/www/$website/ >/dev/null 2>&1
 sudo curl -LO https://wordpress.org/latest.zip
 echo -e "$green Unzipping wordpress download...$orange" ; sleep 1
