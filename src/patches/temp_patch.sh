24258d605ca860e34d1bf3315b860f9e40d96083
Parman <armantheparman@protonmail.com>
2024-11-11 16:47:24 +1100

    a

--- src/patches/temp_patch.sh
+++ src/patches/temp_patch.sh
@@ -124,5 +124,8 @@ sudo gsed -i 's/500001/50001/' $torrc >$dn 2>&1
 debug temppatchend
 
 # potentially large file that's not needed, caused by a bug
-sudo rm $(ls $HOME/.*uninstall_parmanodebackup*)
-}
+if find $HOME -name ".*parmanodebackup*" -maxdepth 1 -type f >/dev/null ; then 
+for i in $HOME/.*parmanodebackup* ; do
+    sudo rm "${i}" >/dev/null 2>&1
+done
+fi
