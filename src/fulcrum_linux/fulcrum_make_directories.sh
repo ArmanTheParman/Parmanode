f2a137d792ed99a35287ecc7d8f668d4c61b6c0b
Parman <armantheparman@protonmail.com>
2024-11-11 17:56:51 +1100

    a

--- src/fulcrum_linux/fulcrum_make_directories.sh
+++ src/fulcrum_linux/fulcrum_make_directories.sh
@@ -6,12 +6,20 @@ if [[ $drive_fulcrum == "external" ]] ; then
 
     if ! mount | grep parmanode >$dn ; then mount_drive ; fi
 
+    #on the external drive itself...
     if [[ ! -d $pd/fulcrum_db ]] ; then mkdir -p $pd/fulcrum_db >$dn 2>&1 ; fi
+    
+    #if internal fulcrum_db dir previously exists... (move or delete choice)
+    if [[ -d $HOME/.fulcrum_db && ! -L $HOME/.fulcrum_db ]] ; then
+    fulcrum_db_exists || return 1
+    fi
 
-    if [[ -L $HOME/.fulcrum_db ]] ; then rm $HOME/.fulcrum_db >$dn 2>&1 ; fi
+    #remove any symlinks
+    if [[ -L $HOME/.fulcrum_db ]] ; then rm $HOME/.fulcrum_db >$dn 2>&1 ; fi #won't delete any dir because no '-rf' option
 
+    #only make a symlink if there is no dir there
     if [[ ! -d $HOME/.fulcrum_db ]] ; then
-        ln $dp/fulcrum_db $HOME/.fulcrum_db >$dn 2>&1
+        ln $pd/fulcrum_db $HOME/.fulcrum_db >$dn 2>&1
     fi
 
 elif [[ $drive_fulcrum == "internal" ]] ; then
@@ -21,4 +29,43 @@ elif [[ $drive_fulcrum == "internal" ]] ; then
 fi
 
 return 0
+}
+
+function fulcrum_db_exists {
+while true ; do
+set_terminal ; echo -e "
+########################################################################################
+
+    A fulcrum databas directory already exists on the internal drive at  $cyan
+    $HOME/.fulcrum_db. $orange
+
+    It has a size of$cyan $(du -sh $HOME/.fulcrum_db | awk '{print $1}') $orange
+
+    You have choices:
+$red
+                  d)     Delete the data
+$green
+                  mm)    Move it to a backup (fulcrum_db_backup)
+$cyan
+                  a)     Abort
+$orange
+
+########################################################################################
+"
+choose xmq ; read choice ; set_terminal
+case $choice in
+q|Q) exit ;; m|M|a) back2main ;; 
+d)
+sudo rm -rf $HOME/.fulcrum_db 
+break
+;;
+mm)
+sudo rm -rf $HOME/.fulcrum_db_backup
+sudo mv $HOME/.fulcrum_db $HOME/.fulcrum_db_backup
+break
+;;
+*)
+invalid
+esac
+done
 }
