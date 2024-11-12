5a7060c96dd7d97de529f48ac17c7dd182b11e34
Parman <armantheparman@protonmail.com>
2024-11-12 19:48:59 +1100

    a

--- src/joinmarket/make_joinmarket_config.sh
+++ src/joinmarket/make_joinmarket_config.sh
@@ -9,7 +9,8 @@ function make_joinmarket_config {
     docker exec joinmarket bash -c "sed -i '/rpc_user =/c\\rpc_user = $rpcuser' $jmfile"
     docker exec joinmarket bash -c "sed -i '/rpc_password =/c\\rpc_password = $rpcpassword' $jmfile"
     #change first instance of onion_serving_port, leave the second alone
-    docker exec joinmarket bash -c "sed -i '0,/onion_serving_port =/c\\onion_serving_port = 8077' $jmfile"
+    docker exec joinmarket bash -c "sed -i '0,/onion_serving_port =/{s/onion_serving_port =.*/onion_serving_port = 8077/}' $jmfile"
+
 
     if [[ $OS == Mac ]] ; then
     docker exec joinmarket bash -c "sed -i '/rpc_host =/c\\rpc_host = host.docker.internal' $jmfile"
