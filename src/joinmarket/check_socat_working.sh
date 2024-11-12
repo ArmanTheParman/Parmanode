dec99f613a7cacd5aff06a62564fb84c5b44ddb5
Parman <armantheparman@protonmail.com>
2024-11-12 23:48:38 +1100

    a

--- src/joinmarket/check_socat_working.sh
+++ src/joinmarket/check_socat_working.sh
@@ -1,15 +1,9 @@
 function check_socat_working {
 
-while true ; do
 if tmux ls | grep -q joinmarket_socat ; then 
-socatstatus="${green}RUNNING$orange (type 'stop' to stop)" 
+socatstatus="${green}RUNNING$orange" 
 else 
-socatstatus="${red}NOT RUNNING$orange (type 'start' to start)"
-fi
-
-if [[ $do_return == "true" ]] ; then #so the above variables are set when returning from the case options
-unset do_return
-return
+socatstatus="${red}NOT RUNNING$orange"
 fi
 
 set_terminal ; echo -e "
@@ -25,24 +19,8 @@ set_terminal ; echo -e "
 
     Socat/Tmux is currently: $socatstatus
 
-
 ########################################################################################
 "
-choose xpmq ; read choice ; set_terminal
-case $choice in
-q|Q) exit ;; p|P) return 1 ;; m|M) back2main ;;
-"") return ;;
-start)
-start_socat joinmarket
-do_return="true"
-;;
-stop)
-stop_socat joinmarket
-do_return="true"
-;;
-*)
-invalid
-;;
-esac
-done
+enter_continue
+
 }
