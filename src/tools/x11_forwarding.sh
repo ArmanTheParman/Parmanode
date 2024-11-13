e17b45eff202607cee4dd8e8c39f7dedbe1266ae
Parman <armantheparman@protonmail.com>
2024-11-13 23:25:39 +1100

    sshd_config

---
+++ src/tools/x11_forwarding.sh
@@ -0,0 +1,49 @@
+function X11Forwarding {
+    
+if [[ $OS == Linux ]] ; then
+    file="/etc/ssh/sshd_config"
+else
+    file="/private/etc/ssh/sshd_config"
+fi
+
+if [[ ! -e $file ]] ; then
+    announce "No sshd_config file at $cyan$file$orange exists. Aborting."
+    return 1
+fi
+
+if [[ $1 == yes || $1 == on ]] ; then
+
+    #if X11DisplayOffset is commented out, activate to 10, otherwise leave whaever setting there is
+    sudo gsed -iE 's/^#X11DisplayOffset.+$/X11DisplayOffset 10/' $file >$dn 2>&1 
+
+    #if X11 forwarding is active (yes or no), delete and make it yes.
+    sudo gsed -iE 's/^X11Forwarding.+$/X11Forwarding yes/' $file >$dn 2>&1 
+    #if X11 forwarding is commented out, remove comment and make it yes.
+    sudo gsed -iE 's/^#X11Forwarding.+$/X11Forwarding yes/' $file >$dn 2>&1 
+
+    #check of desired setting is active and exit (and only 1 occurance). 
+    if      [[ $(grep -qE "^X11Forwarding yes$" $file | wc -l) == 1 ]] ; then return 0
+
+    #check if any stray settings and abort
+    if    grep -qE "^X11Forwarding no" $file ; then announce "
+        
+        /r    Unexpected setting in $file
+        /r    Please manually review, and make sure there is a directive:$cyan
+        /r    X11Forwarding yes $orange
+        /r    If there are multiple occurences, delete them and leave one remaining.
+
+        /r    Aborting.
+        "
+
+    return 1
+    fi
+
+elif [[ $1 == no || $1 == off ]] ; then
+
+    sudo gsed -Ei 's/^X11Forwarding.+$/#X11Forwarding yes/' $file >$dn 2>&1
+    sudo gsed -Ei 's/^X11DisplayOffset.+$/#X11DisplayOffset 10/' $file >$dn 2>&1
+
+fi
+
+return 0
+}
