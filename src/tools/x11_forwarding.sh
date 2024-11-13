ae14037ae88d894b416e0a95d7ddaa204ecadab2
Parman <armantheparman@protonmail.com>
2024-11-14 02:11:47 +1100

    a

--- src/tools/x11_forwarding.sh
+++ src/tools/x11_forwarding.sh
@@ -59,3 +59,18 @@ function restart_sshd {
     fi
 }
 
+function install_xquartz {
+
+if [[ $OS != Mac ]] ; then return 1 ; fi
+
+cd $tmp && curl -LO https://github.com/XQuartz/XQuartz/releases/download/XQuartz-2.8.5/XQuartz-2.8.5.pkg || {
+    announce "Something went wrong. Aborting." 
+    return 1
+    }
+
+if [[ $(shasum -a 256 $tmp/XQuartz-2.8.5.pkg |  awk '{print $1}') != "e89538a134738dfa71d5b80f8e4658cb812e0803115a760629380b851b608782" ]] ; then 
+    announce "Something went wrong. Aborting." 
+    return 1
+fi
+}
+
