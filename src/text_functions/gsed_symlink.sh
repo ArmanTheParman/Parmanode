712b6d612b273937675070c2dbab68d0cc37b316
Parman <armantheparman@protonmail.com>
2024-11-14 15:38:22 +1100

    a

--- src/text_functions/gsed_symlink.sh
+++ src/text_functions/gsed_symlink.sh
@@ -1,8 +1,16 @@
 function gsed_symlink {
 #make a gsed symlink
-if [[ $OS == Linux ]] && ! which gsed >$dn 2>&1 ; then
-sedpath=$(which sed | sed -n 's/\(.*\)sed/\1/p')
-sudo ln -s $(which sed) $sedpath/gsed
+if [[ $(uname) == Linux ]] && ! which gsed >$dn 2>&1 ; then
+
+    sudo ln -s $(which sed) /usr/bin/gsed
+
+if ! which gsed >/dev/null ; then 
+    clear
+    echo "\n \r    Couldn't get gsed symlink working. You could experience errors. 
+             \r    Better call Parman for assistance.
+
+             \r    Hit <enter> to continue"
+    read
+fi
 fi
 }
-
