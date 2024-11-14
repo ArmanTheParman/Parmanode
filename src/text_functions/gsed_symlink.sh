a6c174548608366084c1aec07c62c8395b8a82e8
Parman <armantheparman@protonmail.com>
2024-11-14 15:37:24 +1100

    a

--- src/text_functions/gsed_symlink.sh
+++ src/text_functions/gsed_symlink.sh
@@ -2,22 +2,16 @@ function gsed_symlink {
 #make a gsed symlink
 if [[ $(uname) == Linux ]] && ! which gsed >$dn 2>&1 ; then
 
-    if [[ $(which sed) != "/usr/bin/sed" ]] ; then
-        sedpath="/usr/bin"
-    else
-        sedpath=$(which sed | sed -n 's/\(.*\)sed/\1/p')
-    fi
+    sudo ln -s $(which sed) /usr/bin/gsed
 
-    sudo ln -s $(which sed) $sedpath/gsed
+if ! which gsed >/dev/null ; then 
+    clear
+    echo "\n \r    Couldn't get gsed symlink working. You could experience errors. 
+             \r    Better call Parman for assistance.
 
-    if ! which gsed >/dev/null ; then 
-        clear
-        echo "\n \r    Couldn't get gsed symlink working. You could experience errors. 
-
-                 \r    Hit <enter> to continue"
-        read
-    fi
-
+             \r    Hit <enter> to continue"
+    read
+fi
 fi
 }
 
