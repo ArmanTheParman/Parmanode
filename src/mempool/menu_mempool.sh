709a89b900854846859c7ec72acee5a4df2f4dbf
Parman <armantheparman@protonmail.com>
2024-11-15 02:19:09 +1100

    a

--- src/mempool/menu_mempool.sh
+++ src/mempool/menu_mempool.sh
@@ -1,5 +1,6 @@
 function menu_mempool {
 export mempoolconf="$hp/mempool/docker/docker-compose.yml"
+rm "${mempoolconf}E" >/dev/null 2>&1
 
 while true ; do 
 set_terminal
@@ -38,6 +39,8 @@ export backend="${bright_blue}An Electrum/Fulcrum Server$orange"
 else
 export backend=""
 fi
+
+test_mempool_config || return 1
 
 set_terminal_custom 45 ; echo -e "
 ########################################################################################$cyan
@@ -123,4 +126,218 @@ invalid
 
 esac
 done
+}
+
+function test_mempool_config {
+
+if grep -q "test_mempool_config_core_rpc_host=off" < $hm ; then 
+    return 0 
+else
+    check_core_rpc_host_mempool
+fi
+
+if grep -q "test_mempool_btcusername=off" < $hm ; then 
+    return 0 
+else
+    check_core_rpc_username_mempool
+fi
+if grep -q "test_mempool_btcpassword=off" < $hm ; then 
+    return 0 
+else
+    check_core_rpc_password_mempool
+fi
+
+if grep -q "test_mempool_config_electrum_host=off" < $hm ; then 
+    return 0 
+else
+    if grep "MEMPOOL_BACKEND" < $mempoolconf | grep -q "electrum" ; then
+        check_electrum_host_mempool
+    fi
+fi
+
+}
+
+function check_core_rpc_host_mempool {
+#space before CORE is necessary
+confIP=$(grep " CORE_RPC_HOST:" $mempoolconf | cut -d \" -f2 )
+if [[ "$confIP" != "$IP" ]] ; then
+while true ; do
+set_terminal ; echo -e "
+########################################################################################
+
+    Warning, the$cyan CORE_RPC_HOST$orange IP address in the mempool configuration
+    does not seem to match your computers IP address as detected by Parmanode.
+
+    You have options...
+$green
+                  fix)$orange         Let Parmanode try to fix it for you    
+
+                  n)$orange           Do nothing
+$red
+                  shoosh)$orange      Do nothing and hide this message next time 
+
+######################################################################################## 
+"
+choose xpmq ; read choice ; set_terminal 
+case $choice in
+q|Q) exit ;; p|Q) return 1 ;; m|M) back2main ;;
+fix)
+sudo gsed -i "s/ CORE_RPC_HOST.*\$/ CORE_RPC_HOST: \"$IP\"/" $mempoolconf >/dev/null 2>&1
+#restart_mempool
+break
+;;
+n)
+break
+;;
+shoosh)
+echo "test_mempool_config_core_rpc_host=off" | tee -a $hm >/dev/null 2>&1
+break
+;;
+*)
+invalid
+;;
+esac
+done
+fi
+
+}
+
+
+function check_electrum_host_mempool {
+
+#space before ELECTRUM is necessary
+confIP=$(grep " ELECTRUM_HOST:" $mempoolconf | cut -d \" -f2 )
+if [[ "$confIP" != "$IP" ]] ; then
+while true ; do
+set_terminal ; echo -e "
+########################################################################################
+
+    Warning, the$cyan ELECTRUM_HOST$orange IP address in the mempool configuration
+    does not seem to match your computers IP address as detected by Parmanode.
+
+    You have options...
+$green
+                  fix)$orange         Let Parmanode try to fix it for you    
+
+                  n)$orange           Do nothing
+$red
+                  shoosh)$orange      Do nothing and hide this message next time 
+
+######################################################################################## 
+"
+choose xpmq ; read choice ; set_terminal 
+case $choice in
+q|Q) exit ;; p|Q) return 1 ;; m|M) back2main ;;
+fix)
+sudo gsed -i "s/ ELECTRUM_HOST.*\$/ ELECTRUM_HOST: \"$IP\"/" $mempoolconf >/dev/null 2>&1
+#restart_mempool
+break
+;;
+n)
+break
+;;
+shoosh)
+echo "test_mempool_config_electrum_host=off" | tee -a $hm >/dev/null 2>&1
+break
+;;
+*)
+invalid
+;;
+esac
+done
+fi
+}
+
+function check_core_rpc_username_mempool {
+source $bc >/dev/null 2>&1
+
+#space before CORE is necessary
+user=$(grep " CORE_RPC_USERNAME:" $mempoolconf | cut -d \" -f2 )
+if [[ "$user" != "$rpcuser" ]] ; then
+while true ; do
+set_terminal ; echo -e "
+########################################################################################
+
+    Warning, the$cyan Bitcoin username$orange in the mempool configuration
+    does not seem to match your local bitcoin conf username as detected by Parmanode.
+
+    You have options...
+$green
+                  fix)$orange         Let Parmanode try to fix it for you    
+
+                  n)$orange           Do nothing
+$red
+                  shoosh)$orange      Do nothing and hide this message next time 
+
+######################################################################################## 
+"
+choose xpmq ; read choice ; set_terminal 
+case $choice in
+q|Q) exit ;; p|Q) return 1 ;; m|M) back2main ;;
+fix)
+sudo gsed -i "s/ BTC_RPC_USERNAME.*\$/ BTC_RPC_USERNAME: \"$rpcuser\"/" $mempoolconf >/dev/null 2>&1
+#restart_mempool
+break
+;;
+n)
+break
+;;
+shoosh)
+echo "test_mempool_btcusername=off" | tee -a $hm >/dev/null 2>&1
+break
+;;
+*)
+invalid
+;;
+esac
+done
+fi
+
+}
+
+function check_core_rpc_password_mempool {
+source $bc >/dev/null 2>&1
+
+#space before CORE is necessary
+pass=$(grep " CORE_RPC_PASSWORD:" $mempoolconf | cut -d \" -f2 )
+if [[ "$pass" != "$rpcpassword" ]] ; then
+while true ; do
+set_terminal ; echo -e "
+########################################################################################
+
+    Warning, the$cyan Bitcoin password$orange in the mempool configuration
+    does not seem to match your local bitcoin conf password as detected by Parmanode.
+
+    You have options...
+$green
+                  fix)$orange         Let Parmanode try to fix it for you    
+
+                  n)$orange           Do nothing
+$red
+                  shoosh)$orange      Do nothing and hide this message next time 
+
+######################################################################################## 
+"
+choose xpmq ; read choice ; set_terminal 
+case $choice in
+q|Q) exit ;; p|Q) return 1 ;; m|M) back2main ;;
+fix)
+sudo gsed -i "s/ BTC_RPC_PASSWORD.*\$/ BTC_RPC_PASSWORD: \"$rpcpassword\"/" $mempoolconf >/dev/null 2>&1
+#restart_mempool
+break
+;;
+n)
+break
+;;
+shoosh)
+echo "test_mempool_btcpassword=off" | tee -a $hm >/dev/null 2>&1
+break
+;;
+*)
+invalid
+;;
+esac
+done
+fi
+
 }
