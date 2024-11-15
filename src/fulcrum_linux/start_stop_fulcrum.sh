1c4f701a208bf66ee2c7ec26347ceb36c6e8de27
Parman <armantheparman@protonmail.com>
2024-11-15 14:34:00 +1100

    a

--- src/fulcrum_linux/start_stop_fulcrum.sh
+++ src/fulcrum_linux/start_stop_fulcrum.sh
@@ -15,12 +15,14 @@ if grep -q "fulcrum-" $ic ; then
     sudo systemctl stop fulcrum.service 
 
 elif grep -q "fulcrumdkr" $ic ; then
-
+    debug3 "stopping"
     docker_running || return 1
+    debug3 "stopping"
     please_wait
 
     shudownsig="false" ; count=0
     while [[ $shutdownsig == "false" && $count -lt 5 ]] ; do
+    debug3 "stopping in loop"
     docker exec -it fulcrum bash -c "kill -15 \"\$(ps -x | grep fulcrum | grep -v bash | grep -v grep | awk '{print \$1}')\""
     sleep 2
     #make sure fulcrum gracefully stopped
