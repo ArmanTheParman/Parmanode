c3a98d5aa5f231e4056f7edc6f4c5706406ca530
Parman <armantheparman@protonmail.com>
2024-11-15 16:48:28 +1100

    a

--- src/general_functions/update.sh
+++ src/general_functions/update.sh
@@ -97,7 +97,8 @@ y|Y|Yes|yes|u)
 sudo apt-get update -y
 sudo apt-get upgrade -y 
 sudo apt-get install jq netcat vim net-tools unzip tmux ssh tor -y 
-if ! which ssh >/dev/null ; then sudo apt-get install ssh ; sudo systemctl enable ssh >/dev/null 2>&1
+sudo systemctl enable ssh >$dn 2>&1
+sudo systemctl start ssh >$dn 2>&1
 install_fuse noupdate #linux minmal installs may need this to run AppImages
 fi
 break
