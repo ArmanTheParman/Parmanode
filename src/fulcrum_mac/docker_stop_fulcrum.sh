898a331784288bf5eacee5c3c8ca2de3d37bc878
Parman <armantheparman@protonmail.com>
2024-11-15 13:20:24 +1100

    a

--- src/fulcrum_mac/docker_stop_fulcrum.sh
+++ src/fulcrum_mac/docker_stop_fulcrum.sh
@@ -1,9 +1,9 @@
 function docker_stop_fulcrum {
 #stops fulcrum inside container, doesn't stop the container.
 
-PID=$(docker exec fulcrum pgrep -o "Fulcrum")   # -o means olderst command
+PID=$(docker exec fulcrum pgrep -o "Fulcrum")   # -o means oldest command
 
 if [ -n "$PID" ] ; then
-   docker exec -d fulcrum kill -2 $PID 
+   docker exec -d fulcrum kill -15 $PID 
 fi 
 }
