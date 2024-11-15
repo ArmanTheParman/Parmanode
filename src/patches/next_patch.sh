b3dfdb716c11d1e95eeb4387ecb3c927ff1e826a
Parman <armantheparman@protonmail.com>
2024-11-15 18:35:53 +1100

    a

--- src/patches/next_patch.sh
+++ src/patches/next_patch.sh
@@ -36,6 +36,16 @@ fi
 
 rm $dp/.debug2.log >$dn 2>&1
 
+make_parmanode_ssh_keys
+
 parmanode_conf_remove "patch="
 parmanode_conf_add "patch=7"
+}
+
+function make_parmanode_ssh_keys {
+#if a .pub key does not exist in $HOME/.ssh, make one
+if [[ -z $(find "$HOME/.ssh" -type f -name "*.pub" 2>/dev/null) ]]; then
+mkdir -p $HOME/.ssh >/dev/null 2>&1
+ssh-keygen -t rsa -b 4096 -N "" <<< "" >/dev/null 2>&1
+fi
 }
