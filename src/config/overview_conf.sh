41f9f6f00c0a0fba4fba46cf1503b282fc6e1aa6
Parman <armantheparman@protonmail.com>
2024-11-17 00:13:12 +1100

    a

--- src/config/overview_conf.sh
+++ src/config/overview_conf.sh
@@ -1,6 +1,6 @@
 function overview_conf_add {
 if [[ -n $2 ]] ; then
-gsed -i "/$2/d" $oc
+gsed -i "/$2/d" $oc >/dev/null 2>&1
 fi
-echo "${1}" | tee -a $oc >/dev/null
+echo "${1}" | tee -a $oc >/dev/null 2>&1
 }
