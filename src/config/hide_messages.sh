ada45b67a69a2772a82a818510c5c24476344499
Parman <armantheparman@protonmail.com>
2024-11-16 12:52:58 +1100

    a

--- src/config/hide_messages.sh
+++ src/config/hide_messages.sh
@@ -4,7 +4,7 @@ label="$1"
 value="$2"
 
 hide_messages_remove $label
-
+debug "hma"
 echo "message_$label=$value" | tee -a $HOME/.parmanode/hide_messages.conf >$dn 2>&1
 return 0
 }
@@ -12,6 +12,7 @@ return 0
 function hide_messages_remove {
 label=$1
 gsed -i "/message_$label=/d" $hm >$dn 2>&1
+debug "hmr done"
 return 0
 }
 
