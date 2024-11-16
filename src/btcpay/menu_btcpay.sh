d4f0741e470d53b07e862ee6d599a77285e0de67
Parman <armantheparman@protonmail.com>
2024-11-17 02:33:03 +1100

    a

--- src/btcpay/menu_btcpay.sh
+++ src/btcpay/menu_btcpay.sh
@@ -293,13 +293,13 @@ choose xpmq ; read choice ; set_terminal
 case $choice in
 q|Q) exit ;; n|N|p|P) return 1 ;; m|M) back2main ;;
 y)
-
 backupdir=$HOME/Desktop/btcpayserver_backup_$(date | awk '{print $1$2$3$4"-"$5}')
 #back up plugins dir
 mkdir -p $bakupdir >/dev/null 2>&1
 cp -r $HOME/.btcpayserver/Plugins $backupdir
 docker exec -itu postgres btcpay bash -c "pg_dump -U postgres -d btcpayserver" > $backupdir/btcpayserver.sql 2>&1
 success "A backup has been created and left on your Desktop"
+break
 ;;
 *)
 invalid
