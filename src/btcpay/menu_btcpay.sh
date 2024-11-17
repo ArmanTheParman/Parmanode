4d332047f0d1dfcbfc9008a378ba5f07b4902264
Parman <armantheparman@protonmail.com>
2024-11-17 15:34:32 +1100

    a

--- src/btcpay/menu_btcpay.sh
+++ src/btcpay/menu_btcpay.sh
@@ -209,7 +209,8 @@ update_btcpay
 ;;
 manr)
 if ! docker exec -it btcpay grep "parmashell_functions" /root/.bashrc ; then
-docker exec -d btcpay bash -c "echo 'source /home/parman/parman_programs/parmanode/src/ParmaShell/parmashell_functions' | tee -a /root/.bashrc"
+docker exec -d btcpay bash -c "echo 'source /home/parman/parman_programs/parmanode/src/ParmaShell/parmashell_functions' | tee -a /root/.bashrc ; \
+PS1='\[\e]0;\u@\h: \w\a\]\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033:\w\$"
 fi
 enter_continue "Type exit and <enter> to return from container back to Parmanode."
 clear
