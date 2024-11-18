0954570146af46c3214dd72a90f58a655909b317
Parman <armantheparman@protonmail.com>
2024-11-18 19:04:16 +1100

    a

--- src/btcpay/btcpay_backup.sh
+++ src/btcpay/btcpay_backup.sh
@@ -1,3 +1,31 @@
 #docker_dir=$(docker volume inspect generated_btcpay_datadir --format="{{.Mountpoint}}" | sed -e "s%/volumes/.*%%g")
 #backup_dir="$docker_dir/volumes/backup_datadir/_data"
 #/var/lib/docker/volumes/generated_btcpay_datadir# 
+function do_backup_btcpay {
+tar \
+    --exclude="volumes/backup_datadir" \
+    --exclude="volumes/generated_btcpay_datadir/_data/host_*" \
+    --exclude="volumes/generated_bitcoin_datadir/_data" \
+    --exclude="volumes/generated_litecoin_datadir/_data" \
+    --exclude="volumes/generated_elements_datadir/_data" \
+    --exclude="volumes/generated_xmr_data/_data" \
+    --exclude="volumes/generated_dogecoin_datadir/_data/blocks" \
+    --exclude="volumes/generated_dogecoin_datadir/_data/chainstate" \
+    --exclude="volumes/generated_dash_datadir/_data/blocks" \
+    --exclude="volumes/generated_dash_datadir/_data/chainstate" \
+    --exclude="volumes/generated_dash_datadir/_data/indexes" \
+    --exclude="volumes/generated_dash_datadir/_data/debug.log" \
+    --exclude="volumes/generated_mariadb_datadir" \
+    --exclude="volumes/generated_postgres_datadir" \
+    --exclude="volumes/generated_electrumx_datadir" \
+    --exclude="volumes/generated_lnd_bitcoin_datadir/_data/data/graph" \
+    --exclude="volumes/generated_clightning_bitcoin_datadir/_data/lightning-rpc" \
+    --exclude="**/logs/*" \
+    -cvzf $HOME/Desktop/backup.tar /var/lib/docker/volumes/generated_*
+printf "\n"
+echo "Desktop contents...
+
+$(ls $HOME/Desktop)
+"
+enter_continue
+}
