e97009ab5b96c1bf44fde54b70636c6af56cf842
Parman <armantheparman@protonmail.com>
2024-11-24 03:48:11 +1100

    a

--- src/bitcoin/menu_bitcoin.sh
+++ src/bitcoin/menu_bitcoin.sh
@@ -4,7 +4,7 @@ export debuglogfile="$HOME/.bitcoin/debug.log"
 
 if grep -q "btccombo" $ic >$dn 2>&1 ; then
 dockerbitcoinmenu=" $pink                Bitcoin in Docker Container with BTCPay Server $orange"
-btcman="$cyan      man)$orange      Explore Bitcoin/BTCPay container (manr for root)"
+btcman="$cyan      man)$orange          Explore Bitcoin/BTCPay container (manr for root)"
 else
 unset btcman dockerbitcoinmenu
 fi
@@ -89,31 +89,31 @@ echo -e "$output4"
 echo ""
 echo -ne "
 $green
-      start)$orange    Start Bitcoind............................................(Do it)
-$red
-      stop)$orange     Stop Bitcoind..................(One does not simply stop Bitcoin)
-$cyan
-      restart)$orange  Restart Bitcoind
-$cyan
-      n)$orange        Access Bitcoin node information ....................(bitcoin-cli)
-$cyan
-      log)$orange      Bitcoin debug.log ...............(see details of bitcoin running)
-$cyan
-      bc)$orange       Inspect and edit bitcoin.conf file (bcv for vim)
-$cyan
-      up)$orange       Set, remove, or change RPC user/pass
-$bright_blue
-      tor)$orange      Tor menu options for Bitcoin...
-$cyan
-      mm)$orange       Migrate/Revert an external drive...
-$cyan
-      delete)$orange   Delete blockchain data and start over (eg if data corrupted)
-$cyan
-      update)$orange   Update Bitcoin wizard
+               start)$orange        Start Bitcoind
+            $red
+               stop)$orange         Stop Bitcoind
+            $cyan
+               restart)$orange      Restart Bitcoind
+            $cyan
+               n)$orange            Access Bitcoin node information 
+            $cyan
+               log)$orange          Bitcoin debug.log 
+            $cyan
+               bc)$orange           Inspect edit bitcoin.conf file (bcv for vim)
+            $cyan
+               up)$orange           Set, remove, or change RPC user/pass
+            $bright_blue
+               tor)$orange          Tor menu options for Bitcoin...
+            $cyan
+               mm)$orange           Migrate/Revert an external drive...
+            $cyan
+               delete)$orange       Delete blockchain data and start over
+            $cyan
+               update)$orange       Update Bitcoin wizard
 
-$btcman
-$output3
-$cyan      (o)$orange        OTHER...
+         $btcman
+         $output3
+         $cyan      o)$orange            OTHER...
 
                                                                $red hit 'r' to refresh $orange
 ########################################################################################
