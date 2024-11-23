845120e11f9e880510cfe317207b43e462459e90
Parman <armantheparman@protonmail.com>
2024-11-23 12:40:15 +1100

    a

--- src/patches/next_patch.sh
+++ src/patches/next_patch.sh
@@ -9,5 +9,6 @@ function reduce_systemd_logs {
 if [[ $OS == Linux ]] ; then
 sudo journalctl --vacuum-size=1G >$dn 2>&1
 sudo gsed -iE 's/^.+SystemMaxUse.+$/SystemMaxUse=1G/' /etc/systemd/journald.conf >$dn 2>&1
+debug "pause rsl"
 fi
 }
