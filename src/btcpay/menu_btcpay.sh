6661b73b5cd9103bd1c4276a3b33ac4111ef92c9
Parman <armantheparman@protonmail.com>
2024-11-24 05:06:55 +1100

    a

--- src/btcpay/menu_btcpay.sh
+++ src/btcpay/menu_btcpay.sh
@@ -161,7 +161,7 @@ exp)
 btcpay_menu_advanced || return 1
 ;;
 
-rs)
+restart|rs)
 restart_btcpay
 ;;
 
@@ -324,7 +324,7 @@ source $pc
 #if variable incorrect, fix it.
 if [[ $btcpay_version == latest || -z $btcpay_version ]] ; then
 
-    export btcpay_version=v$(cat $btcpaylog | grep "Adding and executing plugin BTCPayServer -" | tail -n1 | grep -oE '[0-9]+.[0-9]+.[0-9]+$')
+    export btcpay_version=v$(cat $btcpaylog | grep "Adding and executing plugin BTCPayServer -" | tail -n1 | grep -oE '[0-9]+.[0-9]+.[0-9]+')
 
     if [[ $(echo $btcpay_version | wc -c) -lt 3 ]] ; then #variable may not have captured correctly, if so, it'll be just 'v\n' with a length of 2.
         unset btcpay_version
