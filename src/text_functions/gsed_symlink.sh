8a356184894405b725ee8fe7344ea8a777f34b89
Parman <armantheparman@protonmail.com>
2024-11-23 16:35:53 +1100

    aa

--- src/text_functions/gsed_symlink.sh
+++ src/text_functions/gsed_symlink.sh
@@ -5,8 +5,8 @@ if [[ $(uname) == Linux ]] && ! which gsed >$dn 2>&1 ; then
     sudo ln -s $(which sed) /usr/bin/gsed
 
 if ! which gsed >$dn ; then 
-    clear
-    echo "\n \r    Couldn't get gsed symlink working. You could experience errors. 
+
+    echo -e "\n \r    Couldn't get gsed symlink working. You could experience errors. 
              \r    Better call Parman for assistance.
 
              \r    Hit <enter> to continue"
