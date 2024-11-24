e7bb5789885de8ec8ebe68ec8767af2519e55569
Parman <armantheparman@protonmail.com>
2024-11-25 02:41:24 +1100

    a

--- src/drive_functions/format_ext_drive.sh
+++ src/drive_functions/format_ext_drive.sh
@@ -15,11 +15,11 @@ debug "passed internal drive choice"
 
 #Check if external drive selected for other programs, and warn user.
 if [[ $justFormat != "true" ]] ; then
-format_warnings #skip_formatting can be changed here
-fi
+    format_warnings #skip_formatting can be changed here
+    if [[ $skip_formatting == "true" ]] ; then return 0 ; fi
+fi ; unset justFormat
 
-fi #end not justFormat
-unset justFormat
+
 
 if [[ $raid != "true" ]] ; then
 detect_drive || return 1 #alternative (better) way to get $disk variable, and exported.
@@ -33,11 +33,10 @@ fi
 unmount   # Need drive not to be mounted in order to wipe and format.
           # Should unmount RAID too
 
-if [[ $1 != Bitcoin ]] ; then #cancelling dd for bitcoin installation. To slow and not necessary.
-if [[ $1 != justFormat ]] ; then
+if [[ $1 != Bitcoin && $1 != justFormat ]] ; then
     dd_wipe_drive  
 fi
-fi
+
 
 #Format the drive
 if [[ $OS == "Mac" ]] ; then
@@ -108,74 +107,4 @@ if [[ $OS == "Linux" ]] ; then
         enter_continue
 fi
 return 0
-}
-
-
-function confirm_format {
-#return 1 necessary because function failure sets skip_formatting variable to true in calling function
-if [[ $importdrive == "true" ]] ; then return 1 ; fi
-
-if [[ $btcdockerchoice == "yes" ]] ; then return 1 #skips formatting 
-fi 
-
-if [[ $btcpayinstallsbitcoin == "true" && $btcpay_combo != "true" ]] ; then return 1 #skips formatting
-fi
-
-while true ; do
-clear ; echo -e "
-########################################################################################
-
-    Parmanode has detected that you previously have chosen this external drive for
-    programs other than $1 
-
-    Please confirm if you want to format the drive?
-$red
-            y)      Yes, format it. Format it good.
-$green
-            n)      Nooooo. Skip formating
-$orange
-            a)      Abort everything
-
-########################################################################################
-"
-choose "xpmq"
-read choice
-case $choice in
-q|Q) exit 0 ;; a|A|p|P|m|M) back2main ;;  
-y|Y) return 0 ;;
-n|N) return 1 ;;
-*) invalid ;;
-esac
-done
-}
-
-
-function non_parmanode_drive_warning {
-while true ; do
-set_terminal ; echo -e "
-########################################################################################
-
-    You have selected an external drive for $1 and then to not format a drive.
-
-    Parmanode has not detected any Parmanode configured drive connected to the
-    computer. If you proceed, Bitcoin will be installed, but it won't be able to
-    start. You can still go head and 'import' a Parmanode drive later, or choose
-    to bring in a new drive later from the Parmanode Bitcoin menu.
-
-    Keep going with installation?
-$red
-                         y)   Yeah, I know what I'm doing
-$green 
-                         n)   No, abort.
-$orange
-########################################################################################
-"
-choose "xpmq" ; read choice 
-jump $choice || { invalid ; continue ; } ; set_terminal
-case $choice in
-q|Q) exit 0 ;; n|N|No|NO|p|P|a|A) return 1 ;; m|M) back2main ;; y|Y|yes|YES) break ;;
-*) invalid ;;
-esac
-done
-
 }
