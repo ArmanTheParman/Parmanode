c91085cec419aa1a814d30b32c73fa9506025b43
Parman <armantheparman@protonmail.com>
2024-11-25 12:25:15 +1100

    a

--- src/fulcrum/make_fulcrum_service_file.sh
+++ src/fulcrum/make_fulcrum_service_file.sh
@@ -6,7 +6,9 @@ Wants=network-online.target
 
 [Service]
 ExecStartPre=$HOME/.parmanode/mount_check.sh
-ExecStart=/usr/local/bin/Fulcrum $HOME/.fulcrum/fulcrum.conf >$HOME/.fulcrum/fulcrum.log 2>&1
+ExecStart=$HOME/startup_scripts/fulcrum_startup.sh >$HOME/.fulcrum/fulcrum.log
+ExecStop=pgrep Fulcrum
+
 TimeoutStopSec=90s
 KillSignal=SIGTERM
 
