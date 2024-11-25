7553a6275db0f59e2adfdd1eb27408535d0a72e1
Parman <armantheparman@protonmail.com>
2024-11-25 16:00:11 +1100

    a

--- src/electrs/restore_electrs.sh
+++ src/electrs/restore_electrs.sh
@@ -13,15 +13,19 @@ $orange
 
     To save time, would you like to use that backup (yay), or compile electrs all 
     over again (boo).
+$red
+    BE AWARE THAT IF THIS BACKUP IS OF AN OLD VERSION, YOU'LL BE INSTALLING THE
+    OLD VERSION.$orange
 
     If the backup is an old version, and you want a new version, you'll have to choose
     to compile again now, of course. 
 
 $green
-                      u)             Use backup of compiled software
+                      u)$orange             Use backup of compiled software
 $red
-                      compile)       Compile again
-$orange
+                      compile)$orange       Compile again
+$cyan
+                      del)$orange           Compile again, but delete the backup too
 
 ########################################################################################
 "
@@ -32,6 +36,9 @@ q|Q) exit ;; p|P) return 1 ;; m|M) back2main ;;
 u|U|use|Use) 
 export electrs_compile="false" && return 0 ;;
 compile) 
+export electrs_compile="true" && return 0 ;;
+del)
+sudo rm -rf $HOME/.electrs_backup >$dn 2>&1
 export electrs_compile="true" && return 0 ;;
 *) invalid ;;
 esac
