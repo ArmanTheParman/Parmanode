eefc73e8e27a749c5740b1765d27d1d01d2ba731
Parman <armantheparman@protonmail.com>
2024-11-26 19:53:38 +1100

    a

--- src/MENUS/menu_parmanbooks.sh
+++ src/MENUS/menu_parmanbooks.sh
@@ -36,7 +36,7 @@ return 0
 function open_book {
 debug "in open book"
 num=0 
-# ls $hp/parman_books | while read i ; do 
+ls $hp/parman_books | while read i ; do 
     num=$((num + 1)) ; if [[ $num == $choice ]] ; then
         # echo "${hp}/${i}"
         # read
@@ -46,7 +46,7 @@ num=0
     else
         echo "$i"
         read
-#        continue
+        continue
     fi
 
 #     invalid
