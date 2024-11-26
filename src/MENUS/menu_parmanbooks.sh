8722097bad720745a7b614a53a6b849383a6e03c
Parman <armantheparman@protonmail.com>
2024-11-26 19:10:04 +1100

    a

---
+++ src/MENUS/menu_parmanbooks.sh
@@ -0,0 +1,31 @@
+function menu_parmanbooks {
+while true ; do
+set_terminal
+echo -ne "
+########################################################################################$cyan
+
+                                Parman Books Menu $orange
+
+########################################################################################
+"
+num=0 
+ls $hp/parman_books | while read i ; do 
+               num=$((num + 1)) 
+               echo -e "    $num)       $i"
+               done
+echo -ne "
+########################################################################################
+"
+choose "xpmq" ; read choice
+jump $choice || { invalid ; continue ; } ; set_terminal
+case $choice in
+q|Q) exit ;; p|P) return 1 ;; m|M) back2main ;;
+
+*)
+invalid 
+;;
+
+esac
+done
+return 0
+}
