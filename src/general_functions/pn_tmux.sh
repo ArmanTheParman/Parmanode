6921f664555991267f92f821bb3e9d92af018a12
Parman <armantheparman@protonmail.com>
2024-11-27 20:37:09 +1100

    a

--- src/general_functions/pn_tmux.sh
+++ src/general_functions/pn_tmux.sh
@@ -1,6 +1,8 @@
 function pn_tmux {
 command="$1"
 tmuxname="$2"
+tmuxname=${tmuxname:-$(mktemp -u session-XXXXXX)}
+
 
 if ! which tmux >$dn 2>&1 ; then 
 yesorno "Need tmux for this to work. Ok to install?" || return 1
