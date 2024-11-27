daa4bf29f9ff513e9e4f69bb44a1cbddb0b5cc32
Parman <armantheparman@protonmail.com>
2024-11-27 20:37:42 +1100

    a

--- src/mempool/start_stop_mempool.sh
+++ src/mempool/start_stop_mempool.sh
@@ -2,14 +2,14 @@ function start_mempool {
 docker ps >$dn 2>&1 || { announce "Docker not running. Aborting." && return 1 ; }
 pn_tmux "
 cd $hp/mempool/docker ; docker compose up -d ; cd - >$dn
-"
+" "starting_mempool"
 
 }
 
 function stop_mempool {
 pn_tmux "
 cd $hp/mempool/docker ; docker compose stop ; cd - >$dn
-"
+" "stopping_mempool"
 }
 
 function restart_mempool {
@@ -21,5 +21,5 @@ docker compose stop
 docker compose up -d
 cd - >$dn
 sleep 1.5
-"
+" "restarting_mempool"
 }
