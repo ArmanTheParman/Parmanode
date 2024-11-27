f2356efd5d20fa4d6cb3aef7e9bc7126e206e0a7
Parman <armantheparman@protonmail.com>
2024-11-27 23:49:19 +1100

    a

--- src/bitcoin/start_stop_bitcoin.sh
+++ src/bitcoin/start_stop_bitcoin.sh
@@ -29,12 +29,8 @@ return 0
 fi
 
 if [[ $OS == "Linux" ]] ; then 
-        pn_tumx "
-        if grep -q "drive=external" $pc >$dn ; then mount_drive ; fi
-        sudo systemctl start bitcoind.service 
-        "
-
-sleep 0.5
+    pn_tumx "if grep -q "drive=external" $pc >$dn ; then mount_drive ; fi ; sudo systemctl start bitcoind.service"
+    sleep 0.5
 fi                 
 
 if [[ $(uname) == Darwin ]] ; then
