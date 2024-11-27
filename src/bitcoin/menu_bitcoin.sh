36e94ac34236588ee8fa1192bb53bacc94ff5df4
Parman <armantheparman@protonmail.com>
2024-11-28 00:25:43 +1100

    a

--- src/bitcoin/menu_bitcoin.sh
+++ src/bitcoin/menu_bitcoin.sh
@@ -53,11 +53,11 @@ fi
 
 
 if [[ $OS == Linux && $bitcoinrunning == "false" ]] && which bitcoin-qt >$dn 2>&1 ; then
-output3="\n$green               qtstart)$orange      Start Bitcoin Qt "
+output3="\n$green               qtstart)$orange      Start Bitcoin Qt \n"
 fi
 
 if [[ $OS == Linux && $bitcoinrunning == "true" ]] && pgrep bitcoin-qt >$dn 2>&1 ; then
-output3="\n$red               qtstop)$orange       Stop Bitcoin Qt "
+output3="\n$red               qtstop)$orange       Stop Bitcoin Qt \n"
 fi
 
 output4="                   Bitcoin Data Usage: $red$(du -shL $HOME/.bitcoin | cut -f1)"$orange
