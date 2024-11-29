d0e1145122686f12afe9ee611fed390d5d05294b
Parman <armantheparman@protonmail.com>
2024-11-29 14:50:24 +1100

    a

--- src/general_functions/check_SSH.sh
+++ src/general_functions/check_SSH.sh
@@ -1,6 +1,7 @@
 function check_SSH {
 if [[ -n $SSH_CONNECTION ]] ; then
-yesorno "Can not run via SSH. You need a monitor connected. Try anyway?" || return 1
+yesorno "Can not run via SSH unless you're using X11 forwarding. 
+    You need a monitor connected. Try anyway?" || return 1
 fi
 return 0
 }
