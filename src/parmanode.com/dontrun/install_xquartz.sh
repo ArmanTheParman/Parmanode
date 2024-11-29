5931e7f5895203ab941d9d2170eba7f64357b666
Parman <armantheparman@protonmail.com>
2024-11-29 13:57:54 +1100

    a

--- src/parmanode.com/dontrun/install_xquartz.sh
+++ src/parmanode.com/dontrun/install_xquartz.sh
@@ -7,13 +7,13 @@ if [[ $(uname) != "Darwin" ]] ; then
 fi
 
 tmp=$(mktemp -d)
-echo "Downloading XQuartz 2.8.5...\n"
+echo "\nDownloading XQuartz 2.8.5...\n"
 cd $tmp && curl -LO https://github.com/XQuartz/XQuartz/releases/download/XQuartz-2.8.5/XQuartz-2.8.5.pkg || {
     echo "Something went wrong with downloading. Aborting."
     rm -rf $tmp
     exit
     }
-echo "Checking valid hash...\n"
+echo "\nChecking valid hash...\n"
 if ! shasum -a 256 $tmp/XQuartz-2.8.5.pkg |  awk '{print $1}' | grep "e89538a134738dfa71d5b80f8e4658cb812e0803115a760629380b851b608782" ; then 
     echo "Something went wrong with hashing. Aborting."
     rm -rf $tmp
@@ -21,7 +21,7 @@ if ! shasum -a 256 $tmp/XQuartz-2.8.5.pkg |  awk '{print $1}' | grep "e89538a134
 fi
 
 echo "Installing pkg file...\n"
-sudo installer -pkg XQuartz.pkg -target / \
+sudo installer -pkg $tmp/XQuartz-2.8.5.pkg -target / \
   && { 
       echo "XQuartz has been installed. Hit <enter>." 
       rm -rf $tmp >/dev/null 2>&1
