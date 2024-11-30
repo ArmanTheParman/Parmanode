2a3efe0a5cbdfca39fbc9e1eec9fc85e9ca03095
Parman <armantheparman@protonmail.com>
2024-11-30 19:19:01 +1100

    a

--- src/Thunderhub/menu_thunderhub.sh
+++ src/Thunderhub/menu_thunderhub.sh
@@ -64,7 +64,7 @@ start_thunderhub
 ;;
 log)
 th_log="$(mktemp)"
-docker logs thunderhub | strings | tee $th_log 2>&1
+docker logs --timestamps thunderhub | tee $th_log 2>&1
 NODAEMON="true" ; pn_tmux "less $th_log" ; unset NODAEMON
 rm $th_log >$dn 2>&1
 ;;
