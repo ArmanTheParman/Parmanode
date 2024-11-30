e1ac42e6396cb2d00bc599b3afad06b05278a81e
Redinpais <redinpais@yahoo.com>
2024-11-30 23:48:36 +0000

    Update back2main.sh

--- src/MENUS/back2main.sh
+++ src/MENUS/back2main.sh
@@ -2,9 +2,9 @@ function back2main {
 
 export main_loop=$((main_loop + 1)) 
 if [[ $main_loop -gt 50 ]] ; then
-announce "The main menu function has called itself a few too many times. It's
-    probably a good idea to quit Parmanode and restart it - this will release
-    some of the computer's memory."
+announce "A função do menu principal chamou-se a si própria demasiadas vezes.
+É provavelmente uma boa ideia sair do Parmanode e reiniciá-lo 
+- isto irá libertar alguma da memória do computador."
 fi
 menu_main
 
