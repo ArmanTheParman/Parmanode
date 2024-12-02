c0ef31463e88f1bd384edce556566ec0d091952f
Redinpais <redinpais@yahoo.com>
2024-12-02 12:55:00 +0000

    Update menu_tools.sh

--- src/MENUS/menu_tools.sh
+++ src/MENUS/menu_tools.sh
@@ -4,37 +4,37 @@ while true ; do
 set_terminal_high
 echo -ne "
 ########################################################################################$cyan
-                                  TOOLS - PAGE 1  $orange
+                                  FERRAMENTAS - PAGINA 1  $orange
 ########################################################################################
 
 
-$cyan              (cc)$orange    Upgrade ColdCard firmware wizard             
+$cyan              (cc)$orange    Assistente de atualização do firmware ColdCard             
 
-$cyan              (d)$orange     Delete your previous preferences to hide certain Parmanode
+$cyan              (d)$orange     Eliminar as suas preferências anteriores para ocultar determinados Parmanode
                       messages
 
-$cyan              (dfat)$orange  Drive format assist tool
+$cyan              (dfat)$orange  Ferramenta de assistência ao formato da drive
  
-$cyan              (md)$orange    Import/Migrate/Revert an external drive.
+$cyan              (md)$orange    Importar/Migrar/Reverter uma drive externa.
 
-$cyan              (mm)$orange    Mount the Parmanode drive - Linux only
+$cyan              (mm)$orange    Montar a drive Parmanode - apenas Linux
 
-$cyan              (ip)$orange    What's my computer's IP address?
+$cyan              (ip)$orange    Qual é o endereço IP do meu computador?
 
-$cyan              (ppp)$orange   Connect to Parman's node over Tor ...
+$cyan              (ppp)$orange   Ligar ao nó do Parman através do Tor ...
 
-$cyan              (pn)$orange    ParmanodL - Flash a mircoSD for a Raspberry Pi
+$cyan              (pn)$orange    ParmanodL - Criar um mircoSD para um Raspberry Pi
                                                                                       
-$cyan              (ps)$orange    ParmaShell info 
+$cyan              (ps)$orange    Informação do ParmaShell 
 
-$cyan              (rs)$orange    Parman's easy AF Rsync tool new
+$cyan              (rs)$orange    Ferramenta AF Rsync fácil do Parman Novo
 
-$cyan              (u)$orange     Update computer (apt-get for Linux, Homebrew for Macs)
+$cyan              (u)$orange     Atualizar o computador (apt-get para Linux, Homebrew para Macs)
 
-$cyan              (um)$orange    Unmount your Parmanode external drive 
-                      (stops Bitcoin/Fulcrum/Electrs if running) - Linux only
+$cyan              (um)$orange    Desmontar a drive externa Parmanode 
+                      (parar Bitcoin/Fulcrum/Electrs se estiver a funcionar) - Apenas em Linux
 $red $blinkon
-         ...  (n)  More options $blinkoff
+         ...  (n)  Mais opções $blinkoff
 $orange
 ########################################################################################
 "
@@ -127,31 +127,32 @@ function connect_to_parman {
 set_terminal ; echo -e "
 ########################################################################################
 $cyan
-                            CONNECT TO PARMAN'S NODE
+                            LIGAR AO NÓ DO PARMAN
 $orange
-    This is for emergency or testing purposes only. What's the point of having a node
-    if you're going to connect to someone else's?
+    Isto é apenas para fins de emergência ou de teste. 
+    Qual é o objetivo de ter um nó se vai ligar-se ao de outra pessoa?
 
-    Nevertheless, this option is available to you, just in case. I can't promise
-    100% up time, because someitmes shit happens. If for whatever reason, my connection
-    details change, it will be renewed in Parmanode when you update Parmanode.
+    No entanto, esta opção está disponível para si, por precaução.
+    Não posso prometer 100% de tempo de atividade, porque às vezes acontecem coisas.
+    Se, por qualquer razão, os meus dados de ligação mudarem, serão renovados na
+    Parmanode quando a Parmanode for actualizada.
 
-    I promise to not collect any data or spy on your transactions. I can confidently
-    make that promise because I don't even know how to do that. Your IP address will
-    be unknown because you're connecting over Tor anyway.
+    Prometo não recolher quaisquer dados ou espiar as suas transacções.
+    Posso fazer esta promessa com confiança porque nem sequer sei como o fazer.
+    O seu endereço IP será desconhecido porque está a ligar-se através do Tor.
     
-    You'll have to manually tweak your wallet settings and include the following 
-    onion address to connect to my server:
+    Terá de ajustar manualmente as definições da sua carteira e incluir 
+    o seguinte endereço de cebola para se ligar ao meu servidor:
 $green
     odqwla6mucxo6u7z5isxoxmiasjncph7wg7nc2eiby7cvk36qbm4imyd.onion:700${red}2$green:t 
 $orange
-    You must use the port number after the onion address or you can't connect.
+    Tem de utilizar o número da porta a seguir ao endereço de cebola ou não conseguirá
+    estabelecer ligação.
 
-    For Electrum wallet, you must turn on your Tor proxy, and you must add the \":t\"
-    part of the the 7002 port. This specifies TCP over Tor. (:s, for SSL won't work.)
+    Para a carteira Electrum, você deve ativar seu proxy Tor e deve adicionar a parte 
+    \":t\" da porta 7002. Isto especifica TCP sobre Tor. (:s, para SSL não funciona).
 
-    For Sparrow wallet, you must have SSL turned off, and you must have the Tor proxy
-    turned on.
+    Para a Sparrow wallet, é necessário ter o SSL desativado e o proxy Tor ativado.
 
 ########################################################################################
 "
