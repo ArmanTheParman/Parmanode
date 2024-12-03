34aa4cda7579497c19ee25b962fc5f9ee0f34ea9
Parman <armantheparman@protonmail.com>
2024-12-03 15:02:40 +1100

    a

--- src/parmabox/parmanbox_run.sh
+++ src/parmabox/parmanbox_run.sh
@@ -1,5 +1,5 @@
 function parmabox_run {
-    
+if [[ $OS == "Mac" ]] ; then    
     docker run -du parman --privileged --hostname ParmaBox --name parmabox \
            -v $HOME/parmanode/parmabox:/home/parman/parmanode/parmabox \
            -e DISPLAY=$DISPLAY \
@@ -11,4 +11,15 @@ function parmabox_run {
            parmabox \
            tail -f /dev/null
 
+elif [[ $OS == "Linux" ]] ; then
+
+    docker run -du parman --privileged --name parmabox \
+           -network=host \
+           -v $HOME/parmanode/parmabox:/home/parman/parmanode/parmabox \
+           -e DISPLAY=$DISPLAY \
+           -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+           parmabox \
+           tail -f /dev/null
+
+fi
 }
