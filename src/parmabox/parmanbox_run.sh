7f495e1cd68034c4c05e2fb6c7871d19c9db0a7f
Parman <armantheparman@protonmail.com>
2024-12-03 14:47:11 +1100

    a

--- src/parmabox/parmanbox_run.sh
+++ src/parmabox/parmanbox_run.sh
@@ -3,6 +3,7 @@ function parmabox_run {
     docker run -d --privileged --hostname ParmaBox --name parmabox \
            -v $HOME/parmanode/parmabox:/home/parman/parmanode/parmabox \
            -e DISPLAY=$DISPLAY \
+           -p 23:22 \
            -p 10999:10999 \
            -p 8399:8332 \
            -p 50051:50001 \
