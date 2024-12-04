3e79b3ae884f9d7e62764a4d0325b3fb8f5075f9
Redinpais <redinpais@yahoo.com>
2024-12-04 00:41:30 +0000

    Update back2main.sh
    Portuguese translation - fix commit

--- src/MENUS/back2main.sh
+++ src/MENUS/back2main.sh
@@ -2,9 +2,9 @@ function back2main {
 
 export main_loop=$((main_loop + 1)) 
 if [[ $main_loop -gt 50 ]] ; then
-announce "A função do menu principal chamou-se a si própria demasiadas vezes. 
-É provavelmente uma boa ideia sair do Parmanode e reiniciá-lo 
-- isto irá libertar alguma da memória do computador."
+announce "A função do menu principal chamou-se a si própria demasiadas vezes.
+    É provavelmente uma boa ideia sair do Parmanode e reiniciá-lo 
+    - isto irá libertar alguma da memória do computador."
 fi
 menu_main
 
