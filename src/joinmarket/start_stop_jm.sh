5f6b910c81e0fed154a9516bff8b05120cb6a1ec
Parman <armantheparman@protonmail.com>
2024-12-05 00:47:25 +1100

    a

--- src/joinmarket/start_stop_jm.sh
+++ src/joinmarket/start_stop_jm.sh
@@ -3,11 +3,11 @@ function start_joinmarket {
 jmvenv "activate"
 $hp/joinmarket/scripts/joinmarketd.py
 jmvenv "deactivate"
-
+enter_continue
 }
 
 function stop_joinmarket {
 
 pkill -15 joinmarketd.py
-
+enter_continue
 }
