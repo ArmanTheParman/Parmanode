e14b999d9e839d87d5b7bde477ba21b25a6a4c8f
Parman <armantheparman@protonmail.com>
2024-12-06 00:22:41 +1100

    a

--- src/joinmarket/orderbook_jm.sh
+++ src/joinmarket/orderbook_jm.sh
@@ -3,7 +3,11 @@ if [[ -z $obwatcherPID ]] ; then
 pn_tmux "jmvenv 'activate' ;
 $hp/joinmarket/scripts/obwatch/ob-watcher.py | tee $HOME/.joinmarket/orderbook.log ;
 jmvenv 'deactivate' ; " "obw"
+
+start_socat joinmarket
+
 else 
 kill $obwatcherPID 
+stop_socat joinmarket
 fi
 }
