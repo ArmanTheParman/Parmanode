86b214f4e3aedbbaa24f8884673b50b5db6a33a1
Parman <armantheparman@protonmail.com>
2024-12-06 00:04:33 +1100

    a

--- src/joinmarket/menu_joinmarket.sh
+++ src/joinmarket/menu_joinmarket.sh
@@ -299,15 +299,15 @@ set_terminal_high ; echo -e "
 
     Then you can decide how you want to coinjoin. There are two ways. Either as
     a maker or a taker.
-
+$cyan
     TAKER:
-        
+$orange        
         To be a taker, ie pay a fee to coinjoin, you have to use the GUI. Load it up
         and then use it to load your wallet. Set the configuration opions, and
         then do a single CJ or go through the 'tumbler' continuous mixer.
-
+$cyan
     MAKER:
-
+$orange
         As a maker, you need to use the yeild generator. There isn't a GUI for this.
         You go to the Parmanode Yeild Generator submenu (after loading a wallet),
         set your CJ terms and then start the Yeild Generator background process. 
@@ -315,15 +315,15 @@ set_terminal_high ; echo -e "
         the state of the market by running your own OrderBook (see ParmaJoin main
         menu). There'll be a URL and you can go their to see your order is live. 
         You can identify your order with the 'nickname' printed in the YG menu.
-
+$cyan
     Note:
-        
+$orange        
         If the YG stops abruptly (ie doesn't go througha graceful shutdown), then
         there will be a residual lock file (which is there to prevent double usage
         of the same wallet). You need to approve this to be deleted before you can
         start the YG again. There's a menu option for that.
-$cyan 
 
+$red
         Have fun playing with your bitcoins, and whatever else that you do.$orange
 
 ########################################################################################
