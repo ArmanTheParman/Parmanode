5b629426a6414912e84f13be1423b6c5f3648a2f
Parman <armantheparman@protonmail.com>
2024-12-06 00:02:05 +1100

    a

--- src/joinmarket/menu_joinmarket.sh
+++ src/joinmarket/menu_joinmarket.sh
@@ -85,6 +85,8 @@ $jm_be_carefull
 $ygtext1
 
 $cyan
+                  info)$orange        How to play with your bitcoins 
+$cyan
                   gui)$orange         Start Joinmarket GUI (CJ taker)
 $red
                   yg)$orange          Yield Generator menu (CJ Maker) ...
@@ -245,6 +247,9 @@ announce "Hit control x to exit nano text editor.
 "
 nano $oblogfile
 ;;
+info)
+parmajoin_info
+;;
 *)
 invalid
 ;;
@@ -279,4 +284,49 @@ $orange
 "
 enter_continue ; jump $enter_cont
 
+}
+
+function parmajoin_info {
+
+set_terminal_high ; echo -e "
+########################################################################################
+  $cyan                                PARMAJOIN INFO $orange
+########################################################################################
+
+    ParmaJoin is software that uses JoinMarket, and gives you an interative menu.
+
+    You'll first have to create a wallet and fund it.
+
+    Then you can decide how you want to coinjoin. There are two ways. Either as
+    a maker or a taker.
+
+    TAKER:
+        
+        To be a taker, ie pay a fee to coinjoin, you have to use the GUI. Load it up
+        and then use it to load your wallet. Set the configuration opions, and
+        then do a single CJ or go through the 'tumbler' continuous mixer.
+
+    MAKER:
+
+        As a maker, you need to use the yeild generator. There isn't a GUI for this.
+        You go to the Parmanode Yeild Generator submenu (after loading a wallet),
+        set your CJ terms and then start the Yeild Generator background process. 
+        This is basically a script which puts your offer on the market. You can see
+        the state of the market by running your own OrderBook (see ParmaJoin main
+        menu). There'll be a URL and you can go their to see your order is live. 
+        You can identify your order with the 'nickname' printed in the YG menu.
+
+    Note:
+        
+        If the YG stops abruptly (ie doesn't go througha graceful shutdown), then
+        there will be a residual lock file (which is there to prevent double usage
+        of the same wallet). You need to approve this to be deleted before you can
+        start the YG again. There's a menu option for that.
+$cyan 
+
+        Have fun playing with your bitcoins, and whatever else that you do.$orange
+
+########################################################################################
+"
+enter_continue
 }
