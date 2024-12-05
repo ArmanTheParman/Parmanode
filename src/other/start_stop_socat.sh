dd48cdebf1e979e42dd324fdb3f8a52c8fbd142e
Parman <armantheparman@protonmail.com>
2024-12-05 21:04:53 +1100

    a

--- src/other/start_stop_socat.sh
+++ src/other/start_stop_socat.sh
@@ -3,7 +3,7 @@ if [[ $1 == "electrs" ]] ; then
 nohup socat OPENSSL-LISTEN:50006,reuseaddr,fork,cert=$HOME/.electrs/cert.pem,key=$HOME/.electrs/key.pem,verify=0 TCP:127.0.0.1:50005 > $dp/socat.log &
 echo $! > $dp/socat.pid 2>&1
 fi
-
+}
 
 function stop_socat {
 if [[ $1 == "electrs" ]] ; then
