530a9ff0a4b04276678fc35f336e9256d27f35f5
Parman <armantheparman@protonmail.com>
2024-12-05 21:46:47 +1100

    a

--- src/joinmarket/menu_yield_generator.sh
+++ src/joinmarket/menu_yield_generator.sh
@@ -162,11 +162,12 @@ function start_yield_generator {
     unset silentecho
 
     password=$enter_cont
-    jmvenv "activate"
-    echo $password | $hp/joinmarket/scripts/yg-privacyenhanced.py $HOME/.joinmarket/wallets/$wallet | tee $HOME/.joinmarket/yg_privacy.log || enter_continue "Some error with wallet: $wallet"
+    pn_tmux "
+    jmvenv 'activate'
+    echo $password | $hp/joinmarket/scripts/yg-privacyenhanced.py $HOME/.joinmarket/wallets/$wallet |& tee $HOME/.joinmarket/yg_privacy.log 
     unset password enter_cont
-    sleep 1
-    jmvenv "deactivate"
+    jmvenv 'deactivate'
+    "
 
 }
 
