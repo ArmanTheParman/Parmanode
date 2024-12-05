46303511a990b4812bbdb2f4f4ab1f19bb53d55d
Parman <armantheparman@protonmail.com>
2024-12-05 11:28:43 +1100

    a

--- src/joinmarket/Install_joinmarket.sh
+++ src/joinmarket/Install_joinmarket.sh
@@ -53,7 +53,7 @@ function install_joinmarket {
     elif [[ -z $joinmarket_docker ]] ; then
         joinmarket_dependencies || return 1
         cd $hp/joinmarket
-        echo "y" | ./install.sh || { enter_continue  "Something went wrong. Aborting." ; return 1 ; }
+        yes | ./install.sh || { enter_continue  "Something went wrong. Aborting." ; return 1 ; }
     fi
 
 
