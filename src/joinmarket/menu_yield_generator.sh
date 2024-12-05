0e997b86affe51bac761a679043eaca42706f6be
Parman <armantheparman@protonmail.com>
2024-12-05 22:10:48 +1100

    a

--- src/joinmarket/menu_yield_generator.sh
+++ src/joinmarket/menu_yield_generator.sh
@@ -163,12 +163,10 @@ function start_yield_generator {
 
     password=$enter_cont
     TMUX2=$TMUX ; unset TMUX ; clear
-    pn_tmux "
-    source $HOME/parmanode/joinmarket/jmvenv/bin/activate
-    echo $password | $hp/joinmarket/scripts/yg-privacyenhanced.py $HOME/.joinmarket/wallets/$wallet |& tee $HOME/.joinmarket/yg_privacy.log 
-    unset password enter_cont
-    deactivate
-    "
+    pn_tmux "source $HOME/parmanode/joinmarket/jmvenv/bin/activate ; \
+    echo $password | $hp/joinmarket/scripts/yg-privacyenhanced.py $HOME/.joinmarket/wallets/$wallet |& tee $HOME/.joinmarket/yg_privacy.log ; \
+    unset password enter_cont ; \
+    deactivate"
     TMUX=$TMUX2
 
 }
