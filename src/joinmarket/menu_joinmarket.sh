5f9d41238e0bccf9c5a939160e920674bb45b3e5
Parman <armantheparman@protonmail.com>
2024-12-07 23:29:50 +1100

    a

--- src/joinmarket/menu_joinmarket.sh
+++ src/joinmarket/menu_joinmarket.sh
@@ -345,18 +345,19 @@ $orange
         set your CJ terms and then start the Yield Generator background process. 
         This is basically a script which puts your offer on the market. You can see
         the state of the market by running your own OrderBook (see ParmaJoin main
-        menu). There'll be a URL and you can go their to see your order is live. 
+        menu). There'll be a URL and you can go there to see your order is live. 
         You can identify your order with the 'nickname' printed in the YG menu.
 $cyan
     Note:
 $orange        
-        If the YG stops abruptly (ie doesn't go througha graceful shutdown), then
+        If the YG stops abruptly (ie doesn't go through a graceful shutdown), then
         there will be a residual lock file (which is there to prevent double usage
         of the same wallet). You need to approve this to be deleted before you can
         start the YG again. There's a menu option for that.
 
 $red
-        Have fun playing with your bitcoins, and whatever else that you do.$orange
+        Have a good time playing with your bitcoin and your crypto and everything
+        else that you're playing with. $orange
 
 ########################################################################################
 "
