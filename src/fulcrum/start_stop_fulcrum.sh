6341a51437d94053c54dd4c2e78cd74e43fa4d3e
Parman <armantheparman@protonmail.com>
2024-12-08 21:30:28 +1100

    a

--- src/fulcrum/start_stop_fulcrum.sh
+++ src/fulcrum/start_stop_fulcrum.sh
@@ -1,19 +1,21 @@
 function start_fulcrum {
 if grep -q "fulcrum-" $ic ; then
-sudo systemctl start fulcrum.service
+    sudo -v
+    pn_tmux "sudo systemctl start fulcrum.service"
 elif grep -q "fulcrumdkr" $ic ; then
-docker_running || return 1
-docker start fulcrum
-debug "starting fulcrum in the container"
-docker exec -d fulcrum /bin/bash -c "/home/parman/parmanode/fulcrum/Fulcrum /home/parman/.fulcrum/fulcrum.conf \
-    >/home/parman/.fulcrum/fulcrum.log 2>&1" >$dn 2>&1
+    docker_running || return 1
+    docker start fulcrum
+    debug "starting fulcrum in the container"
+    docker exec -d fulcrum /bin/bash -c "/home/parman/parmanode/fulcrum/Fulcrum /home/parman/.fulcrum/fulcrum.conf \
+        >/home/parman/.fulcrum/fulcrum.log 2>&1" >$dn 2>&1
 fi
 }
 
 function stop_fulcrum {
 if grep -q "fulcrum-" $ic ; then
-
-    sudo systemctl stop fulcrum.service ; sleep 0.5
+    sudo -v
+    pn_tmux "sudo systemctl stop fulcrum.service"
+    sleep 1
 
 elif grep -q "fulcrumdkr" $ic ; then
     docker_running || return 1
