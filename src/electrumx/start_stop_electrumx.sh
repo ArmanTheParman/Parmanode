562816fefc7b40bd02899eb94c1a0e7db0b040be
Parman <armantheparman@protonmail.com>
2024-12-08 21:16:04 +1100

    a

--- src/electrumx/start_stop_electrumx.sh
+++ src/electrumx/start_stop_electrumx.sh
@@ -1,16 +1,16 @@
 function start_electrumx {
-pn_tmux "sudo systemctl start electrumx.service"
+sudo systemctl start electrumx.service
 sleep 0.5
 
 }
 
 function stop_electrumx {
-pn_tmux "sudo systemctl stop electrumx.service"
+sudo systemctl stop electrumx.service
 sleep 0.5
 
 }
 
 function restart_electrumx {
-pn_tmux "sudo systemctl restart electrumx.service"
+sudo systemctl restart electrumx.service
 sleep 0.5
 }
