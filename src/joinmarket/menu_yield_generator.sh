77ec311fe823d73943c925cb1e58f14738ba646a
Parman <armantheparman@protonmail.com>
2024-12-08 20:18:30 +1100

    a

--- src/joinmarket/menu_yield_generator.sh
+++ src/joinmarket/menu_yield_generator.sh
@@ -18,14 +18,15 @@ if [[ -e $logfile ]] ; then
     nick="${bright_blue}$(cat $logfile | grep "Sending this handshake" | grep "nick" | tail -n1 | grep -oE '"nick":.*,' | cut -d \" -f4)${orange}"
     debug "nick is $nick"
     ygs="
-    Yield Generator Settings:
+  _______________________________
+  |  Yield Generator Settings:  |
     $green
-            \r        $(sudo gsed -nE '/^ordertype =/p' $jmcfg)
-            \r        $(sudo gsed -nE "/cjfee_$ordertype.=/p" $jmcfg)
-            \r        $(sudo gsed -n '/cjfee_factor =/p' $jmcfg)
-            \r        $(sudo gsed -n '/minsize =/p' $jmcfg)
-            \r        $(sudo gsed -n '/size_factor =/p' $jmcfg)
-
+            \r  |     $(sudo gsed -nE '/^ordertype =/p' $jmcfg) \033[;33H|
+            \r  |     $(sudo gsed -nE "/cjfee_$ordertype.=/p" $jmcfg) \033[;33H|
+            \r  |     $(sudo gsed -n '/cjfee_factor =/p' $jmcfg) \033[;33H|
+            \r  |     $(sudo gsed -n '/minsize =/p' $jmcfg) \033[;33H|
+            \r  |     $(sudo gsed -n '/size_factor =/p' $jmcfg) \033[;33H|
+ ________________________________ 
 
     $orange
     "
