d019edd7a90fefec10044639ba7c1651246261ee
Parman <armantheparman@protonmail.com>
2024-12-08 15:56:23 +1100

    a

--- src/joinmarket/orderbook_jm.sh
+++ src/joinmarket/orderbook_jm.sh
@@ -1,5 +1,6 @@
 function orderbook_jm {
 if [[ -z $obwatcherPID ]] ; then
+NODAEMON="true"
 pn_tmux "$hp/joinmarket/jmvenv/bin/activate ;
 $hp/joinmarket/scripts/obwatch/ob-watcher.py | tee $HOME/.joinmarket/orderbook.log ;
 deactivate ; " "obw"
