973f30e6c0eb8805520aa0af8f058034acd3df3c
Parman <armantheparman@protonmail.com>
2024-12-08 15:56:53 +1100

    a

--- src/joinmarket/orderbook_jm.sh
+++ src/joinmarket/orderbook_jm.sh
@@ -4,7 +4,7 @@ NODAEMON="true"
 pn_tmux "$hp/joinmarket/jmvenv/bin/activate ;
 $hp/joinmarket/scripts/obwatch/ob-watcher.py | tee $HOME/.joinmarket/orderbook.log ;
 deactivate ; " "obw"
-
+enter_continue "pause"
 start_socat joinmarket
 
 else 
