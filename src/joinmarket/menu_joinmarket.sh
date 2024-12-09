a46a0ec881f9ba198de3e89c1d61d9e7d8102b70
Parman <armantheparman@protonmail.com>
2024-12-09 12:10:06 +1100

    a

--- src/joinmarket/menu_joinmarket.sh
+++ src/joinmarket/menu_joinmarket.sh
@@ -154,7 +154,7 @@ gui)
 ;;
 
 ob)
-    orderbook_log_file_manager
+    jm_log_file_manager
     orderbook_jm
 ;;
 
@@ -440,23 +440,36 @@ enable_joinmarket_tor || { enter_continue "Something went wrong." ; return 1 ; }
 }
 
 function jm_log_file_manager {
+#truncates files every 5 days
 
+TMUX2=$TMUX ; unset TMUX
+tmux new -s jm_log_file_manager -d "
+while true ; do
+sleep 10
 file1=$HOME/.joinmarket/orderbook.log
 file1temp=$file1.temp
 exec 3> $file1temp #opens file
 
 if [[ $(wc -l < $file1) -gt 5000 ]] ; then 
     tail -n5000 $file1 >&3 
-    cat $file1temp > $file1
+    mv $file1temp $file1
 fi
+exec 3>&-
 
 file1=$HOME/.joinmarket/yg_privacy.log
 file1temp=$file1.temp
+exec 3> $file1temp #opens file
+
 if [[ $(wc -l < $file1) -gt 5000 ]] ; then 
     tail -n5000 $file1 >&3 
-    cat $file1temp > $file1
+    mv $file1temp $file1
 fi
 
 exec 3>&-
+
+sleep 432000 
+done
+"
+TMUX=$TMUX2
 
 }
