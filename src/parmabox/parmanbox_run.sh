d16d6af833cd7a8b873a459b291e4cbf05052499
Parman <armantheparman@protonmail.com>
2024-12-09 16:17:19 +1100

    a

--- src/parmabox/parmanbox_run.sh
+++ src/parmabox/parmanbox_run.sh
@@ -2,10 +2,6 @@ function parmabox_run {
 if [[ $OS == "Mac" ]] ; then    
     docker run -du parman --privileged --hostname ParmaBox --name parmabox \
            -v $HOME/parmanode/parmabox:/home/parman/parmanode/parmabox \
-           -e DISPLAY=$DISPLAY \
-           -e XAUTHORITY=/home/parman/.Xauthority \
-           -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-           -v /home/parman/.Xauthority:/home/parman/.Xauthority \
            -p 23:22 \
            -p 10999:10999 \
            -p 8399:8332 \
@@ -18,10 +14,6 @@ elif [[ $OS == "Linux" ]] ; then
     docker run -du parman --privileged --name parmabox \
            --network=host \
            -v $HOME/parmanode/parmabox:/home/parman/parmanode/parmabox \
-           -e DISPLAY=$DISPLAY \
-           -e XAUTHORITY=/home/parman/.Xauthority \
-           -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-           -v /home/parman/.Xauthority:/home/parman/.Xauthority \
            parmabox \
            tail -f /dev/null
 
