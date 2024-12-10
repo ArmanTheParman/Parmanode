4708d1de56a68d3fddc4710feab61f96e4204174
Parman <armantheparman@protonmail.com>
2024-12-10 19:21:20 +1100

    a

--- src/MENUS/menu_external_drive.sh
+++ src/MENUS/menu_external_drive.sh
@@ -27,8 +27,9 @@ eblocksize=$(sudo tune2fs -l $eID | grep -E 'Block size' | awk '{print $3}')
 # fi
 
 #Internal
-iID=$(df -h | grep -E '/$' | awk '{print $1}')
-eblocksize=$(sudo tune2fs -l $iID | grep -E 'Block size' | awk '{print $3}')
+
+# iID=$(df -h | grep -E '/$' | awk '{print $1}')
+# eblocksize=$(sudo tune2fs -l $iID | grep -E 'Block size' | awk '{print $3}')
 
 set_terminal_custom 51 ; echo -e "
 ########################################################################################$cyan
