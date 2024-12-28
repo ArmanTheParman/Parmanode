712d8c0ea8fe5fa61814636d2321d1fcaca8df2a
Parman <armantheparman@protonmail.com>
2024-12-28 19:17:19 +1100

    a

--- src/text_functions/motd.sh
+++ src/text_functions/motd.sh
@@ -17,7 +17,7 @@ motd=$motdNum
 fi
 
 #DON'T FORGET TO CHANGE THE MOD TO THE HIGHEST NUMBERERD MESSAGE + 1
-motd=$((motd % 46))
+motd=$((motd % 64))
 
 if [[ $motd == 0 ]] ; then
 set_terminal ; echo -e "
