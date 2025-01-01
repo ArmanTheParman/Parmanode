5a3527c2d1d8129529708fe7c150ed03a9968276
Parman <armantheparman@protonmail.com>
2025-01-02 00:07:39 +1100

    a

--- src/ParmaWallet/make_wallet.py
+++ src/ParmaWallet/make_wallet.py
@@ -4,12 +4,12 @@ from functions2 import *
 from variables import *
 
 
-seed_words = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon" 
+seed_words = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
 passphrase = ""
 wordlist_path = "/home/parman/parman_programs/parmanode/src/ParmaWallet/docs/english.txt"
 with open (wordlist_path, 'r') as file:
     bip39wordlist = file.readlines()
                         
-keypair=derive_keys('address', mnemonic=seed_words, passphrase=passphrase)
+keypair=derive_keys('address', mnemonic=seed_words, passphrase=passphrase, purpose=84, address=0)
 bip84address=pubkey_to_bech32_custom(keypair.public_key)
 print(bip84address)
