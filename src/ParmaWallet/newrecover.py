98890b9d6f86b97b867d19822d8dacbdf5e6aedd
Parman <armantheparman@protonmail.com>
2025-01-01 23:04:25 +1100

    a

---
+++ src/ParmaWallet/newrecover.py
@@ -0,0 +1,40 @@
+from classes import *
+from functions import *
+from variables import *
+import unicodedata, hashlib, binascii, hmac
+from classes import *
+from functions import *
+from variables import * 
+
+charlist = ( b'a', b'b', b'c', b'c', b'd', b'e', b'f', b'g', b'h', b'i', b'j', b'k', b'l', b'm', b'n', b'o', b'p', b'q', b'r', b's', b't', b'u', b'v', b'w', b'x', b'y', b'z')
+mnemonic = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about"
+# for ii in charlist:
+#     for jj in charlist:
+#         for kk in charlist:
+#             if ii == jj and jj == kk:
+#                 continue
+#             for ll in charlist:
+#                 for mm in charlist:
+#                     for nn in charlist:
+#                         for oo in charlist:
+#                             passphrase = b'garry' + ii + jj + kk + ll + mm + nn + oo
+#                             print(passphrase)
+#                             a = BIP32_master_node(mnemonic, passphrase)
+#                             b = Child_key(a, depth=1, account=49, hardened=True, serialize=False) #purpose
+#                             c = Child_key(b, depth=1, account=0, hardened=True, serialize=False) #coin
+#                             d = Child_key(c, depth=1, account=0, hardened=True, serialize=False) #account
+#                             e = Child_key(d, depth=1, account=0, hardened=False, serialize=False) #int/ext
+#                             f = Child_key(e, depth=1, account=0, hardened=False, serialize=False) #address
+passphrase = b'abc'
+xxx=BIP32_master_node(mnemonic, passphrase)
+
+#######################################################################################
+#######################################################################################
+#######################################################################################
+
+
+
+       
+       
+       
+        
