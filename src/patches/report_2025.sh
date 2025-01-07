74fbd357a3f94ebacbfcdf1a1d8349714cc83d21
Parman <armantheparman@protonmail.com>
2025-01-07 15:54:40 +1100

    a

--- src/patches/report_2025.sh
+++ src/patches/report_2025.sh
@@ -1,11 +1,15 @@
 if [[ $1 == report ]] ; then
 
-echo "$macprefix" | tee -a $HOME/Desktop/report.txt
+    echo "$macprefix" | tee -a $HOME/Desktop/report.txt
 
-echo "$varlibtor" | tee -a $HOME/Desktop/report.txt
+    echo "$varlibtor" | tee -a $HOME/Desktop/report.txt
 
-sudo cat $varlibtor/electrs-service/hostname | tee -a $HOME/Desktop/report.txt
+    if sudo cat $varlibtor/electrs-service/hostname >/dev/null ; then
+    sudo cat $varlibtor/electrs-service/hostname | tee -a $HOME/Desktop/report.txt
+    fi
 
-sudo tail -n30 $torrc | tee -a $HOME/Desktop/report.txt
+    if sudo cat $torrc >/dev/null ; then
+    sudo tail -n30 $torrc | tee -a $HOME/Desktop/report.txt
+    fi
 
 fi
