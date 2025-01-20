018b507bb7eda79cc9ead913117bc0cb848e01ed
Parman <armantheparman@protonmail.com>
2025-01-21 03:25:19 +1100

    a

--- src/core_lightning/install_core_lightning.sh
+++ src/core_lightning/install_core_lightning.sh
@@ -1,3 +1,14 @@
+function uninstall_core_lightning {
+
+yesorno "Are you sure you want to uninstall Core Lightning?" || exit
+
+sudo rm -rf $hp/core_lightning
+sudo rm /usr/bin/lightning-cli  /usr/bin/lightningd  /usr/bin/lightning-hsmtool  /usr/bin/reckless >/dev/null
+sudo rm $HOME/.lightning
+success "Core Lightning Uninstalled"
+exit
+}
+
 function install_core_lightning {
 
 if [[ $OS == "Darwin" ]] ; then no_mac ; return 1 ; fi
