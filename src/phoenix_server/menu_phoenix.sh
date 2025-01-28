bca4c4c4fcbe27817b1dad9501586fbc963a9040
Parman <armantheparman@protonmail.com>
2025-01-29 01:31:04 +1100

    a

--- src/phoenix_server/menu_phoenix.sh
+++ src/phoenix_server/menu_phoenix.sh
@@ -12,6 +12,8 @@ $cyan            start) $orange          Start Phoenix Server Daemon
 
 $cyan            stop)  $orange          Start Phoenix Server Daemon
 
+$cyan            info)  $orange          Information 
+
 ########################################################################################
 "
 choose "xpmq" ; read choice 
@@ -20,117 +22,22 @@ case $choice in
 m|M) back2main ;; q|Q|QUIT|Quit) exit ;; p|P) menu_use ;; 
 
 start|Start|START|S|s)
-check_SSH || return 0
-run_electrum
-return 0 ;;
-
-mm|MM)
-electrum_connection_menu
+start_phoenix
 ;;
-
-ec|EC) 
-nano $HOME/.electrum/config
+stop)
+stop_phoenix
 ;;
-ecv|ECV) 
-vim_warning ; vim $HOME/.electrum/config
-;;
+info)
+announce "Phoenix is a daemon (background process) running inside a Tmux container.
+    Start it an stop it from the this Parmanode menu.
 
-cl|CL)
-clear_dot_electrum
-;;
-
-eg|EG)
-parmans_electrum_guide
-;;
-
-w|W)
-set_terminal_high
-echo -e "
-########################################################################################
-
-
-    Directory:$cyan $HOME/.electrum/wallets/ $orange
-
-
-    Files: $bright_blue
-
-$(ls $HOME/.electrum/wallets)
-
-$orange
-########################################################################################
-"
-enter_continue ; jump $enter_cont
-set_terminal
-;;
-
-*)
-invalid
-;;
-esac
-done
-}
-
-
-function electrum_connection_menu {
-
-while true ; do
-set_terminal
-echo -e "
-########################################################################################
-$cyan
-                            ELECTRUM CONNECTION OPTIONS $orange
-
-########################################################################################
-
-
-             CURRENT DETECTED ELECTRUM CONNECTION TYPE: $green$connection$orange
-$orange
-
-$green            fs) $orange      Connect to Fulcrum via SSL (port 50002)
-
-$green            ft)$orange       Connect to Fulcrum via TCP (port 50001)
-
-$green            et)     $orange  Connect to electrs via TCP (port 50005)
-
-$green            es)$orange       Connect to electrs via SSL (port 50006)
-
-$green            ftor)    $orange Connect to Fulcrum via Tor 
-
-$green            etor)$orange     Connect to electrs via Tor 
-
-    $orange
-########################################################################################
-"
-choose xpmq ; read choice 
-jump $choice || { invalid ; continue ; } ; set_terminal
-case $choice in
-q|Q) exit ;; p|P) return 0 ;; m|M) back2main ;;
-
-fs|FS)
-check_fulcrum_ssl
-modify_electrum_config fulcrumssl
-;;
-ft|FT|Ft|Ft)
-check_fulcrum_tcp
-modify_electrum_config fulcrumtcp
-;;
-es|ES|Es)
-check_electrs_ssl
-modify_electrum_config electrsssl
-;;
-et|ET|Et)
-check_electrs_tcp
-modify_electrum_config electrstcp 
-;;
-ftor|FTOR|Ftor)
-enable_electrum_tor "fulcrum" 
-;;
-etor|ETOR|Etor)
-enable_electrum_tor "electrs"
+    To interactive with it, you need to use the command line. Parmanode has not
+    been designed to do this for you like most other Apps it installs. Maybe one day."
 ;;
 *)
 invalid
 ;;
 esac
 done
-}
+
+} 
---
+++ src/phoenix_server/start_stop_phoenix.sh
@@ -0,0 +1,27 @@
+function start_phoenix {
+
+if tmux ls | grep "running_phoenixd" ; then
+announce "A Tmux session of Phoenixd is already running" 
+jump $enter_cont
+return 1
+fi
+
+announce "Phoenix will run now inside a Tmux Terminal 'container'. You can stop
+    the Phoenix daemon by exiting Tmux with$red <control> c$orange, but most likely,
+    you're going to want to leave Phonex running as you exit Tmux. 
+
+    To do that (and come back to Parmanode), you need to hit $cyan <control> b$ornage
+    then$cyan d. Another way to stop Phoenix is from the Parmanode Phoenix menu,
+    and another way still is terminate Tmux sessions from the temrinal (not
+    recommended). You can see active Tmux sessions using Terminal with 'tmux ls'. 
+    You can attach to them with 'tmux attach'. The internet will give you more
+    variations if you ask it."
+
+jump $enter_cont 
+pn_tmux "$hp/phoenix/phoenixd" "start_phoenixd"
+return 0
+}
+
+function stop_phoenix {
+tmux kill-session -t "running_phoenixd" 2>&1
+}
