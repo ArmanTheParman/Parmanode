f1daab8b8b668a735d96af748d0b7576140a4a83
Parman <armantheparman@protonmail.com>
2025-01-29 01:03:19 +1100

    a

--- src/mempool/start_stop_mempool.sh
+++ src/mempool/start_stop_mempool.sh
@@ -1,5 +1,6 @@
 function start_mempool {
 docker ps >$dn 2>&1 || { announce "Docker not running. Aborting." && return 1 ; }
+check_mempool_tmux || return 1
 pn_tmux "
 cd $hp/mempool/docker ; docker compose up -d ; cd - >$dn
 " "starting_mempool"
@@ -7,14 +8,16 @@ cd $hp/mempool/docker ; docker compose up -d ; cd - >$dn
 }
 
 function stop_mempool {
+docker ps >$dn 2>&1 || { announce "Docker not running. Aborting." && return 1 ; }
+check_mempool_tmux || return 1
 pn_tmux "
 cd $hp/mempool/docker ; docker compose stop ; cd - >$dn
 " "stopping_mempool"
 }
 
 function restart_mempool {
-
 docker ps >$dn 2>&1 || { announce "Docker not running. Aborting." && return 1 ; }
+check_mempool_tmux || return 1
 pn_tmux "
 cd $hp/mempool/docker
 docker compose stop 
@@ -22,4 +25,20 @@ docker compose up -d
 cd - >$dn
 sleep 1.5
 " "restarting_mempool"
+}
+
+function check_mempool_tmux {
+if tmux ls | grep "starting_mempool" ; then
+announce "Mempool is in the process of starting still. Please try later."
+return 1
+fi
+if tmux ls | grep "stopping_mempool" ; then
+announce "Mempool is in the process of stopping still. Please try later."
+return 1
+fi
+if tmux ls | grep "re-starting_mempool" ; then
+announce "Mempool is in the process of re-starting still. Please try later."
+return 1
+fi
+return 0
 }
