d334abf00ed253faa0c37a5c2576f1314b15ff13
Parman <armantheparman@protonmail.com>
2025-01-30 02:39:12 +1100

    a

--- src/parminer/get_parminer.sh
+++ src/parminer/get_parminer.sh
@@ -9,14 +9,14 @@ announce "Sorry, ParMiner is not available freely (not FOSS). It comes with
 return 1
 fi
 
-if [[ -e $hp/parminer ]] ; then
+if [[ -e $pp/parminer ]] ; then
 announce "ParMiner seems to already be downloaded."
 return 1
 fi
 
 #wont work unless authentication valid
-if [[ -e $hp/parminer ]] ; then announce "ParMiner already downloaded" ; return 1 ; fi
-if git clone git@github.com:armantheparman/parminer $hp/parminer ; then
+if [[ -e $pp/parminer ]] ; then announce "ParMiner already downloaded" ; return 1 ; fi
+if git clone git@github.com:armantheparman/parminer $pp/parminer ; then
     success "ParMiner has been downloaded."
 else
     announce "Something went wrong with the Download; Maybe you haven't got approval?"
