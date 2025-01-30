42025b48dd2ff21642b65f851fa3eeb2c6f20889
Parman <armantheparman@protonmail.com>
2025-01-31 00:44:17 +1100

    a

--- src/tools/see_local_devices.sh
+++ src/tools/see_local_devices.sh
@@ -5,12 +5,16 @@ announce "This will create a list of all connected device, navigate with the
 case $enter_cont in q) return ;; esac
 
 if [[ $OS == Linux ]] && ! which nmap >$dn ; then
-    if ! yesorno "OK to install nmap?" && clear && sudo apt-get update -y && sudo apt-get install nmap -y ; then
-    return 1
+    if ! yesorno "OK to install nmap?" ; then 
+        clear && sudo apt-get update -y && sudo apt-get install nmap -y 
+    else
+        return 1
     fi
 elif [[ $OS == "Mac" ]] ; then
-    if ! yesorno "OK to install nmap?" && clear && brew install nmap ; then
-    return1
+    if ! yesorno "OK to install nmap?" ; then 
+        clear && brew install nmap
+    else
+        return 1
     fi
 fi
 
