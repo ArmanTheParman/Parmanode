2317e6a728ea9fb97ffb56b0c6d31dae1d465c82
Parman <armantheparman@protonmail.com>
2025-02-05 20:07:23 +1100

    a

--- src/tools/screen_video_recording.sh
+++ src/tools/screen_video_recording.sh
@@ -1,15 +1,24 @@
 function screen_video_recording {
 
 if [[ $OS == "Mac" ]] ; then no_mac ; return 1 ; fi
-if [[ $computer_type == "Pi" ]] ; then announce "Not available for Pi yet." ; return 1 ; fi
 
-which ffmpeg >$dn || { sudo apt-get update -y && sudo apt-get install ffmpeg -y ; }
+num=1 ; while [[ -e $HOME/Desktop/video_$num.mp4 ]] ; do num=$((num + 1)) ; done
 
-num=1
-        
-while [[ -e $HOME/Desktop/video_$num.mp4 ]] ; do num=$((num + 1)) ; done
+if echo $XDG_SESSION_TYPE | grep -q "wayland" ; then        
+    which slurp >/dev/null || sudo apt-get install slurp -y || { enter_continue "Something went wrong." ; return 1 ; }
+    svr_announce
+    wf-recorder -g "$(slurp)" -f "$HOME/Desktop/video_$num.mp4"
+else
+    which ffmpeg >$dn || { sudo apt-get update -y && sudo apt-get install ffmpeg -y ; }
+    svr_announce
+    ffmpeg -video_size 1920x1080 -framerate 30 -f x11grab -i $DISPLAY $HOME/Desktop/video_$num.mp4
+fi
+enter_continue
+}
 
 
+
+function svr_announce {
 announce "Hit$cyan <enter>$orange to start recording your screen. Minimise
     the terminal and start doing stuff that you want to record.
 $red
@@ -17,10 +26,4 @@ $red
     and finalise the file. Then you can trim it how you want. $orange
     
     The file will be saved to$cyan $HOME/Desktop/video_$num.mp4$orange"
-
-ffmpeg -video_size 1920x1080 -framerate 30 -f x11grab -i $DISPLAY $HOME/Desktop/video_$num.mp4
-
-enter_continue
-
 }
-
