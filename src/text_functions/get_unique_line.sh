b75ae81d91578d8fc7d58d02c6e5795739d8c1ad
Parman <armantheparman@protonmail.com>
2025-02-08 20:16:00 +1100

    a

--- src/text_functions/get_unique_line.sh
+++ src/text_functions/get_unique_line.sh
@@ -1,7 +1,5 @@
 function get_unique_line {
-debug "in get_unique_line
-$1
-$2"
+debug "in get_unique_line $1 -- $2"
 
 file="$dp/.unique_line"
 # $1 file 1
@@ -33,4 +31,18 @@ fi
 done
 unset file
 
+}
+
+function get_unique_line2 {
+# Returns unique lines in file 2.
+debug "in get_unique_line $1 -- $2"
+
+ul_file="$dp/.unique_line"
+file1="$1"
+file2="$2"
+
+cat $file2 | while IFS= read -r line ; do {
+if ! grep -q $line $file1 ; then echo "$line" >> $ul_file ; fi
+}
+done
 }
