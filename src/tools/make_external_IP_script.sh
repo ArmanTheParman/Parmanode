8e2bab0631f08134ebc84ce7565d5c4d1cedd9f0
Parman <armantheparman@protonmail.com>
2025-02-08 16:36:44 +1100

    a

--- src/tools/make_external_IP_script.sh
+++ src/tools/make_external_IP_script.sh
@@ -1,19 +1,24 @@
 function make_external_IP_script {
-test -f $dp/update_external_IP.sh && return 0
 
-cat <<'EOF' >> $dp/update_external_IP.sh
+test -f $dp/update_external_IP2.sh && return 0
+rm $dp/update_external_IP.sh >/dev/null 2>&1
+
+cat <<'EOF' >> $dp/update_external_IP2.sh
 #!/bin/bash
 count=0
+unset IP
 while true ; do 
-    IP=$(curl -s ifconfig.me && break
+    IP=$(curl -s ifconfig.me)
+    [[ -n "$IP" ]] && break
     sleep 2 
     count=$((count + 1))
     [[ $count -gt 4 ]] && break
 done
-grep -q $IP $HOME/.parmanode/parmanode.conf && exit 0
+grep -q "external_IP=$IP" $HOME/.parmanode/parmanode.conf && exit 0
 gsed -i "s/external_IP.*$/external_IP=$IP/" $HOME/.parmanode/parmanode.conf
+grep -q "external_IP=" $HOME/.parmanode/parmanode.conf || echo "external_IP=$IP" >> $HOME/.parmanode/parmanode.conf
 exit 0
 EOF
 
-sudo chmod +x $dp/update_external_IP.sh
+sudo chmod +x $dp/update_external_IP2.sh
 }
