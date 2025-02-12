e6c752079294afa99a762c175806a32e29ae420f
Parman <armantheparman@protonmail.com>
2025-02-13 00:46:58 +1100

    a

--- src/MENUS/menu_premium.sh
+++ src/MENUS/menu_premium.sh
@@ -43,9 +43,9 @@ $blue
     }
 
     if ! grep -q "parmanas-end" $ic ; then
-    git@github.com:armantheparman/parmanas.git $pp/parmanas || { enter_continue "\nSomething went wrong. Contact Parman.\n
+    git@github.com:armantheparman/parmanas.git $pp/parmanas || { enter_continue "\n    Something went wrong. Contact Parman.\n
     \r    It's possible you were trying to install ParmaNas before Parman has had a chance
-    \r    to approve your computer's credentials." ; continue ; }
+    \r    to approve your computer's credentials.\n" ; continue ; }
     fi
 
     cd $pp/parmanas
