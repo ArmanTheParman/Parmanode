d896d647f68610a7fba1139fcc07c08594458fa1
Parman <armantheparman@protonmail.com>
2025-02-17 00:34:06 +1100

    a

--- src/patches/temp_patch.sh
+++ src/patches/temp_patch.sh
@@ -11,15 +11,8 @@ remove_tor_log_patch
     make_external_IP_script
     which tor >$dn && ! grep -q tor-end $ic && installed_conf_add "tor-end"
     pn_tmux "sudo strip /usr/local/bin/* >/dev/null 2>&1" "stripping_binaries" >/dev/null 2>&1
-
-    # if [[ $OS == "Linux" ]] ; then
-
-    # echo "@includedir $HOME/.parmanode/.tempsudo/" | sudo tee -a /etc/sudoers.d/parmanode_include >$dn 2>&1
-    # elif [[ $OS == "Mac" ]] ; then
-    # echo "#includedir $HOME/.parmanode/.tempsudo/" | sudo tee -a /etc/sudoers.d/parmanode_include >$dn 2>&1
-    # fi
-
-
+    sudo test -e /etc/sudoers.d/parmanode_extend_sudo_timeout ||
+        echo "Defaults:$USER timestamp_timeout=120" | sudo tee /etc/sudoers.d/parmanode_extend_sudo_timeout >/dev/null
 
 fulcrum_delete_old_log 
 #Docker containers sometimes won't have $USER variable set...
