cac07fed6872803392a7d08a5dcf5fca6aac70e8
Parman <armantheparman@protonmail.com>
2025-02-17 12:50:39 +1100

    a

--- src/Raid/raid_info.sh
+++ src/Raid/raid_info.sh
@@ -7,17 +7,16 @@ set_terminal ; echo -e "
     It's easy to look up what RAIDs are and do, so this is not meant to be exhaustive,
     but rather, some extra information that might not be obvious.
 
-          Not only is a RAID mounted, but there is also a RAID process (program) 
-          that needs to be running. This is why there is are 'stop' and 'assemble'
-          options available in the menu.
+    Not only is a RAID mounted, but there is also a RAID process (program) that needs 
+    to be running. This is why there is are 'stop' and 'assemble' options available 
+    in the menu.
+    
+    You can't just mount the RAID drives without having the RAID process running. 
           
-          You can't just mount the RAID drives without having the RAID 
-          process running. 
-          
-          You also can't stop the process unless you unmount the drives.
+    You also can't stop the process unless you unmount the drives.
 
-          The drives are formatted, but also there is another layer of format that
-          happens at a higher level, across all the drives.
+    The drives are formatted, but also there is another layer of format that happens 
+    at a higher level, across all the drives.
 
 ########################################################################################
 "
