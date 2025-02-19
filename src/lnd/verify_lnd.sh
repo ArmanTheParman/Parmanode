d20a85d24dd83ec31f47cab6a0767562dcd72895
Parman <armantheparman@protonmail.com>
2025-02-19 23:25:14 +1100

    a

--- src/lnd/verify_lnd.sh
+++ src/lnd/verify_lnd.sh
@@ -1,22 +1,24 @@
 function verify_lnd {
 #import Roasbeef's public key
 gpg --import $pn/src/lnd/roasbeef.pgp || enter_continue "Failed to import Roasbeef's public key. Please report to Parman."
-curl https://raw.githubusercontent.com/lightningnetwork/lnd/master/scripts/keys/roasbeef.asc | gpg --import
 
 
 cd $HOME/parmanode/lnd
 debug "wait"
 #verify SHA256 output list file.
-if  ! gpg --verify --status-fd 1 *.sig manifest*.txt 2>&1 | grep -qi GOOD ; then
-    set_terminal
-    echo "GPG verification failed. Unknown reason. Please report to Parman. Aborting."
-    enter_continue
-    return 1
-else
-    set_terminal
-    echo -e "GPG verification$green passed$orange."
-    sleep 2
-fi
+
+if [[ $lndversion="v0.18.5-beta" ]] || #this version provides corrupt signature files, goint to the hash file instead.
+    if  ! gpg --verify --status-fd 1 *.sig manifest*.txt 2>&1 | grep -qi GOOD ; then
+        set_terminal
+        echo "GPG verification failed. Unknown reason. Please report to Parman. Aborting."
+        enter_continue
+        return 1
+    else
+        set_terminal
+        echo -e "GPG verification$green passed$orange."
+        sleep 2
+    fi
+
 #Perform SHA256 to verify
 if which sha256sum >$dn ; then
     if ! sha256sum --ignore-missing --check man*.txt ; then
