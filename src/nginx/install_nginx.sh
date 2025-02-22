6216c1e45a414ff5ad6b9b0fb34b6e338f473241
Parman <armantheparman@protonmail.com>
2025-02-23 02:52:32 +1100

    a

--- src/nginx/install_nginx.sh
+++ src/nginx/install_nginx.sh
@@ -4,9 +4,8 @@ if cat $HOME/.parmanode/installed.conf | grep -q "nginx-end" ; then
     set_terminal
     log "parmanode" "Nginx already installed." ; return 0 
 else
-    if which nginx >$dn 2>&1 ; then 
+    if sudo which nginx >$dn 2>&1 ; then 
     set_terminal 
-    log "parmanode" "Nginx already installed. (if which nginx)"
     installed_conf_add "nginx-end"     
     return 1 
     else
@@ -22,7 +21,7 @@ else
 
 fi
 
-if which nginx >$dn ; then installed_conf_add "nginx-end" ; fi
+if sudo which nginx >$dn ; then installed_conf_add "nginx-end" ; fi
 return 0
 
 }
