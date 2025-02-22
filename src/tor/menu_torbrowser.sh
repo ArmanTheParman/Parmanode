ca7e46daec26c7293d11594cb467b3b0e07805bf
Parman <armantheparman@protonmail.com>
2025-02-23 03:05:00 +1100

    a

--- src/tor/menu_torbrowser.sh
+++ src/tor/menu_torbrowser.sh
@@ -7,7 +7,11 @@ set_terminal ; echo -e "
 
     To use Tor Browser
 $cyan
-       Linux: $orange   Use the start menu, you'll find the Tor icon there. 
+       Linux: $orange   Use the start menu, you'll find the Tor icon there. On
+                        some Linux distributions, it won't be there. In that case,
+                        run it from the$cyan
+
+                        $hp/tor-browser/ $orange directory. 
 $cyan    
        Mac:$orange      Run fromt the Mac OS Applications folder. 
 
