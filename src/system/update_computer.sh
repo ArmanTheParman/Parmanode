dfee1eec68129d4a63f4b8e4d74df4abbe0a28b1
Parman <armantheparman@protonmail.com>
2025-02-22 21:42:42 +1100

    a

--- src/system/update_computer.sh
+++ src/system/update_computer.sh
@@ -2,8 +2,9 @@ function update_computer {
 set_terminal 
 
 if [[ $(uname) == "Darwin" ]] ; then
-
     if ! which brew >$dn 2>&1 ; then    
+        yesorno "Parmanode needs to install Homebrew to function properly. 
+        \r    OK to install Homebrew now? Otherwise exiting." || exit 1
         install_homebrew || return 1
     else
         update_homebrew
