672c9b947e5dc8fe16705b55c1167fee29c6b69c
Parman <armantheparman@protonmail.com>
2025-02-23 19:13:23 +1100

    a

--- src/mempool/menu_mempool.sh
+++ src/mempool/menu_mempool.sh
@@ -434,21 +434,15 @@ if ! grep -q "onion" $bc && [[ $debug != 1 ]] ; then return 0 ; fi
 list_mempool_docker_IPs
 debug "000"
 while read line ; do
-    debug "in while loop, line is $line"
     dockerIP=$(echo $line | cut -d = -f2)
-    debug "in while loop 2, line is $line"
-    read "test"
-    # if ! grep "$dockerIP" $bc ; then
-    # debug "in while loop 3, line is $line"
-    #    yesorno "Mempool IP $dockerIP not in bitcoin.conf. Add?" && 
-    #    {
-    #    debug "in while loop 4, line is $line"
-    #    clear ; echo "${green}OK..." ; sleep 1
-    #    echo "rpcallowip=$dockerIP" | sudo tee -a $bc >$dn 2>&1
-    #    needs_restart_mempool="true"
-    #    }
-    # debug "in while loop 5, line is $line"
-    # fi
+    if ! grep "$dockerIP" $bc ; then
+       yesorno "Mempool IP $dockerIP not in bitcoin.conf. Add?" </dev/tty && 
+       {
+       clear ; echo "${green}OK..." ; sleep 1
+       echo "rpcallowip=$dockerIP" | sudo tee -a $bc >$dn 2>&1
+       needs_restart_mempool="true"
+       }
+    fi
 done < $dp/mempool_IPs
 
 [[ $needs_restart_mempool == "true" ]] && restart_mempool
