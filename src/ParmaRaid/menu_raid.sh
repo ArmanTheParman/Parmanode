6ad26add1c3c69053b0392fceff2ed20a27fc571
Parman <armantheparman@protonmail.com>
2025-03-03 01:22:20 +1100

    a

--- src/ParmaRaid/menu_raid.sh
+++ src/ParmaRaid/menu_raid.sh
@@ -5,19 +5,20 @@ set_terminal ; echo -e "$blue
                                 R A I D - menu$blue
 ########################################################################################
 $orange
-                          dt)$blue         Inspect RAID details
+                      dt)$blue         Inspect RAID details $orange
+                      lr)$blue         List running RAIDs $orange
+                      lm)$blue         List mounted RAIDs $orange
+                      lc)$blue         List physically connected RAID drives
 $orange
-                          lr)$blue         List running RAIDs
+                      aa)$blue         Assemble RAIDs $red
+                       s)              Stop RAID ('disassemble')
 $orange
-                          lm)$blue         List mounted RAIDs
+                      mm) $blue        Mount RAIDs (mounts all connected) $orange
+                       u)$blue         Unmount (option to select which) 
+
 $orange
-                          lc)$blue         List physically connected RAID drives
-$orange
-                          aa)$blue         Assemble RAIDs
-$orange
-                           i)$blue         info about RAIDs
-$blue
-########################################################################################
+                       i)$blue         Info about RAIDs
+$blue########################################################################################
 "
 choose xpmq ; read choice 
 jump $choice || { invalid ; continue ; } ; set_terminal
@@ -48,7 +49,7 @@ count=0
 for i in $device ; do
 count=$((count + 1))
 clear
-echo -ne "${blue}################################# RAID number $count ########################################$orange
+echo -ne "${blue}#################################$pink RAID number $count $blue########################################$orange
 
 "
 sudo mdadm --detail $i
@@ -69,6 +70,22 @@ i)
 raid_info
 ;;
 
+u)
+do_RAID_unmount_loop
+continue
+;;
+s)
+do_RAID_stop_loop
+continue
+;;
+aa)
+assemble_RAID
+;;
+
+mm)
+mount_RAID
+debug "after mm"
+;;
 *)
 invalid
 ;;
@@ -78,5 +95,13 @@ done
 }
 
 
-#$orange
-#                          ums)$blue        Unmount/Mount/Stop menu ...
+
+#assemble raid disks
+# sudo mdadm --assemble --scan
+
+#add --name when moving to a new system to avoid name confilicts.
+# sudo mdadm --assemble --name=/dev/md1 /dev/sda /dev/sdb
+
+#update mdadm.conf
+# sudo mdadm --detail --scan | grep '/dev/md1' | sudo tee -a /etc/mdadm/mdadm.conf
+# sudo update-initramfs -u
--- src/ParmaRaid/unmount_and_stop_raid_menu.sh
+++ src/ParmaRaid/unmount_and_stop_raid_menu.sh
@@ -6,38 +6,10 @@ set_terminal ; echo -e "$blue
                                     RAID MENU$blue
 ########################################################################################
 
-$orange
-                        mm) $blue   Mount RAID (mounts all connected)
-$orange
-                        u)$blue     Unmount (option to select which) 
-$red
-                        s)     Stop RAID (also unmounts)
-$green
-                        aa)    Assemble RAID (opposite to stop RAID)
 
-$blue
 ########################################################################################
 "
-choose xpmq ; read choice 
-jump $choice || { invalid ; continue ; } ; set_terminal
-case $choice in
-q|Q) exit ;; p|P) return 1 ;; m|M) back2main ;;
-u)
-do_RAID_unmount_loop
-continue
-;;
-s)
-do_RAID_stop_loop
-continue
-;;
-aa)
-assemble_RAID
-;;
 
-mm)
-mount_RAID
-debug "after mm"
-;;
 *)
 invalid
 ;;
@@ -45,13 +17,3 @@ esac
 done
 }
 
-
-#assemble raid disks
-# sudo mdadm --assemble --scan
-
-#add --name when moving to a new system to avoid name confilicts.
-# sudo mdadm --assemble --name=/dev/md1 /dev/sda /dev/sdb
-
-#update mdadm.conf
-# sudo mdadm --detail --scan | grep '/dev/md1' | sudo tee -a /etc/mdadm/mdadm.conf
-# sudo update-initramfs -u
