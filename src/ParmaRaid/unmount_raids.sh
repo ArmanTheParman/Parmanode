abf958349092a44c172d003fccbb9104a85bdb80
Parman <armantheparman@protonmail.com>
2025-03-03 01:55:31 +1100

    a

--- src/ParmaRaid/unmount_raids.sh
+++ src/ParmaRaid/unmount_raids.sh
@@ -3,26 +3,13 @@ sudo partprobe 2>/dev/null #necessary to remove GPT table error (no partition on
 mount | grep -q '/dev/md' || { announce_blue "No RAIDs mounted" ; return 1 ; }
 
 for i in $(mount | grep '/dev/md' | awk '{print $1}') ; do
-set_terminal ; echo -e "$blue
-########################################################################################
+yesorno_blue "Unmount this RAID?
 
-    Unmount this RAID?     $green y$orange or$red n$orange
 $orange
 $i
 $blue
-########################################################################################
-"
-choose xpmq ; read choice 
-jump $choice || { invalid ; continue ; } ; set_terminal
-case $choice in
-q|Q) exit ;; p|P) return 1 ;; m|M) back2main ;;
-n) continue ;;
-y)
-sudo umount $i >$dn && success_blue "RAID unmounted"
-;;
-*)
-continue ;;
-esac
+" && sudo umount $i >$dn && success_blue "RAID unmounted" && return 0
+continue
 done
 }
 
