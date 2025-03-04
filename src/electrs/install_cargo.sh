81dd3ae91b0e40670e2861b8dd72e8ee6f142b1e
Parman <armantheparman@protonmail.com>
2025-03-05 00:56:48 +1100

    a

--- src/electrs/install_cargo.sh
+++ src/electrs/install_cargo.sh
@@ -3,7 +3,6 @@ function install_cargo {
 announce "You may soon see a prompt to install Cargo. At that time, choose \"1\"" \
 "to continue the installation"
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs/ | sh 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs/ | sh -s -- -y
 source $HOME/.cargo/env #or restart shell
 if ! cargo --version ; then announce "Installing cargo software failed. Aborting." ; return 1 ; fi
