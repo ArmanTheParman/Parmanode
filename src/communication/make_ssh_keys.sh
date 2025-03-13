9abfb3b48007c1769e8985cbae5c0314ae7ed3c9
Parman <armantheparman@protonmail.com>
2025-03-13 23:23:39 +1100

    a

--- src/communication/make_ssh_keys.sh
+++ src/communication/make_ssh_keys.sh
@@ -1,13 +1,15 @@
 function make_parmanode_ssh_keys {
+debug start make_parmanode_ssh_keys
 #if a .pub key does not exist in $HOME/.ssh, make one
 if ! sudo test -f $HOME/.ssh/id_rsa.pub >$dn 2>&1 ; then
 mkdir -p $HOME/.ssh >$dn 2>&1
 ssh-keygen -t rsa -b 4096 -N "" -C "$USER parmanode" >$dn 2>&1
-debug "line6 make parmanode ssh keys"
+debug end make_parmanode_ssh_keys
 fi
 }
 
 function make_parmaraid_ssh_keys {
+debug start make_parmaraid_ssh_keys
 sudo test -f $HOME/.ssh/parmaraid-key.pub && return 1 # 1 is logically success here for the calling function
 mkdir -p ~/.ssh
 ssh-keygen -t rsa -b 4096 -f $HOME/.ssh/parmaraid-key -N "" -C "$USER parmaraid"
@@ -19,9 +21,11 @@ HostName github.com
 User git
 IdentityFile ~/.ssh/parmaraid-key
 IdentitiesOnly yes" | sudo tee -a ~/.ssh/config >$dn 
+debug end make_parmaraid_ssh_keys
 }
 
 function make_parmacloud_ssh_keys {
+debug start make_parmacloud_ssh_keys 
 #usage...
 #make_parmacloud_ssh_keys && { announce_blue "ParmaCloud SSH keys made. Please contact Parman to enable." ; continue ; }
 
@@ -36,11 +40,12 @@ HostName github.com
 User git
 IdentityFile ~/.ssh/parmacloud-key
 IdentitiesOnly yes" | sudo tee -a ~/.ssh/config >$dn 
+debug end make_parmacloud_ssh_keys 
 }
 
 
 function make_parmaweb_ssh_keys {
-
+debug start make_parmaweb_ssh_keys
 sudo test -f ~/.ssh/parmaweb-key.pub && return 1 # 1 is logically success here for the calling function
 
 mkdir -p ~/.ssh
@@ -53,11 +58,12 @@ HostName github.com
 User git
 IdentityFile ~/.ssh/parmaweb-key
 IdentitiesOnly yes" | sudo tee -a ~/.ssh/config >$dn
-
+debug end make_parmaweb_ssh_keys
 }
 
 
 function make_parmanas_ssh_keys {
+debug start make_parmanas_ssh_keys
 #usage...
 #make_parmanas_ssh_keys && { announce_blue "Parmanas SSH keys made. Please contact Parman to enable." ; continue ; }
 
@@ -73,6 +79,7 @@ HostName github.com
 User git
 IdentityFile ~/.ssh/parmanas-key
 IdentitiesOnly yes" | sudo tee -a ~/.ssh/config >$dn
+debug end make_parmanas_ssh_keys
 }
 
 function get_all_ssh_keys {
