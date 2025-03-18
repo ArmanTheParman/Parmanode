f57397d5420012660fdf24eaa6cef305003065b6
Parman <armantheparman@protonmail.com>
2025-03-18 17:51:34 +1100

    a

--- src/MENUS/menu_premium.sh
+++ src/MENUS/menu_premium.sh
@@ -134,7 +134,7 @@ return 0
 ;;
 
 dt)
-if [[ $(uname -m) != "x86_64" ]] ; then  { announce_blue "Datum is only supported on x86_64 machines at this stage." ; continue ; } ; fi
+#if [[ $(uname -m) != "x86_64" ]] ; then  { announce_blue "Datum is only supported on x86_64 machines at this stage." ; continue ; } ; fi
 [[ -e $dp/.datum_enabled ]]  || {
 please_wait
 make_datum_ssh_keys
