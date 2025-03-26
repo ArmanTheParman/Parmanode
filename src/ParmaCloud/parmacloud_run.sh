d962b7b6d6c373c2efbdf1903f66c44ce417370a
Parman <armantheparman@protonmail.com>
2025-03-27 00:54:39 +1100

    a

--- src/ParmaCloud/parmacloud_run.sh
+++ src/ParmaCloud/parmacloud_run.sh
@@ -1,6 +1,23 @@
 function parmacloud_run {
 
-if ! yesorno_blue "Regular or Reverse Proxy?" "reg" "Regular" "rev" "Reverse" ; then
+if yesorno_blue "Regular or Reverse Proxy?" "reg" "Regular" "rev" "Reverse" ; then
+clear
+echo -e "$blue"
+sudo docker run \
+-d \
+--init \
+--sig-proxy=false \
+--name nextcloud-aio-mastercontainer \
+--restart unless-stopped \
+--publish 8080:8080 \
+--publish 80:80 \
+--publish 8020:8080 \
+--volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
+--volume /var/run/docker.sock:/var/run/docker.sock:ro \
+nextcloud/all-in-one:latest || { enter_continue "Something went wrong." ; return 1 ; }
+else
+
+if yesorno_blue "Normal Reverse Proxy with Nginx or a CloudFlare Tunnel?" ; then
 clear
 echo -e "$blue"
 sudo docker run \
@@ -28,12 +45,19 @@ sudo docker run \
 --sig-proxy=false \
 --name nextcloud-aio-mastercontainer \
 --restart unless-stopped \
---publish 8080:8080 \
---publish 80:80 \
 --publish 8020:8080 \
+--publish 80:80 \
+--publish 8443:8443 \
+--env NEXTCLOUD_ENABLE_DOCKER_GATEWAY_ACCESS=true \
+--env NEXTCLOUD_DISABLE_TLS=true \
 --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
 --volume /var/run/docker.sock:/var/run/docker.sock:ro \
 nextcloud/all-in-one:latest || { enter_continue "Something went wrong." ; return 1 ; }
 fi
 
+fi
 }
+
+#--env APACHE_PORT=11000 \
+#--env APACHE_IP_BINDING=0.0.0.0 \
+#--env APACHE_ADDITIONAL_NETWORK="" \
