bf8a4041bc496bd67fd57833dd217ac0adf19578
Parman <armantheparman@protonmail.com>
2025-04-22 23:44:56 +1000

    a

--- src/ParmaCloud/parmacloud_run.sh
+++ src/ParmaCloud/parmacloud_run.sh
@@ -1,5 +1,25 @@
 function parmacloud_run {
 
+if  [[ $preconfigure_parmadrive == "true" ]] ; then
+echo -e "$blue"
+sudo docker run \
+-d \
+--init \
+--sig-proxy=false \
+--name nextcloud-aio-mastercontainer \
+--restart unless-stopped \
+--publish 8020:8080 \
+--env APACHE_PORT=11000 \
+--env APACHE_IP_BINDING=0.0.0.0 \
+--env APACHE_ADDITIONAL_NETWORK="" \
+--env SKIP_DOMAIN_VALIDATION=true \
+--volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
+--volume /var/run/docker.sock:/var/run/docker.sock:ro \
+nextcloud/all-in-one:latest || { enter_continue "Something went wrong." ; return 1 ; }
+return 0
+fi
+
+
 if yesorno_blue "Regular or Reverse Proxy?" "reg" "Regular" "rev" "Reverse" ; then
 clear
 echo -e "$blue"
@@ -39,34 +59,5 @@ sudo docker run \
 --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
 --volume /var/run/docker.sock:/var/run/docker.sock:ro \
 nextcloud/all-in-one:latest || { enter_continue "Something went wrong." ; return 1 ; }
-# else
-# clear
-# echo -e "$blue"
-# sudo docker run \
-# -d \
-# --init \
-# --sig-proxy=false \
-# --name nextcloud-aio-mastercontainer \
-# --restart unless-stopped \
-# --publish 8020:8080 \
-# --publish 80:80 \
-# --publish 8443:8443 \
-# --env NEXTCLOUD_ENABLE_DOCKER_GATEWAY_ACCESS=true \
-# --env NEXTCLOUD_DISABLE_TLS=true \
-# --env SKIP_DOMAIN_VALIDATION=true \
-# --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config \
-# --volume /var/run/docker.sock:/var/run/docker.sock:ro \
-# nextcloud/all-in-one:latest || { enter_continue "Something went wrong." ; return 1 ; }
-# fi
-
 fi
 }
-
-#--env APACHE_PORT=11000 \
-#--env APACHE_IP_BINDING=0.0.0.0 \
-#--env APACHE_ADDITIONAL_NETWORK="" \
-#--env SKIP_DOMAIN_VALIDATION=true
-
-
-#--publish 80:80 \
-#--publish 8443:8443 \
--- src/parmascale/get_parmascale.sh
+++ src/parmascale/get_parmascale.sh
@@ -27,6 +27,6 @@ $(cat ~/.ssh/extra_keys/parmascale-key.pub)$blue\n" ; return 1 ; }
     fi
 
 source_premium
-
+install_parmascale silent
 fi
 }
