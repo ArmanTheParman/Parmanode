c43b887dd7077387fe85f2eed510badf5d0ab462
Parman <armantheparman@protonmail.com>
2025-05-04 17:10:07 +1000

    a

--- src/ParmaView/parmaview_parmanode.sh
+++ src/ParmaView/parmaview_parmanode.sh
@@ -1,7 +1,6 @@
 function parmaview_parmanode {
 # Install ParmaShell in ParmaView
-docker exec -it -u parman parmaview bash -c \
-           "echo \"parmashell-end\" | tee -a /home/parman/.parmanode/installed.conf >$dn"
+docker exec -it -u parman parmaview bash -c "echo \"parmashell-end\" | tee -a /home/parman/.parmanode/installed.conf >$dn"
 
 # Make bashrc better
 docker exec -it -u root parmaview bash -c "echo \"function rp { cd /home/parman/parman_programs/parmanode ; ./run_parmanode.sh \$@ ; }\" | tee -a /root/.bashrc /home/parman/.bashrc" >$dn 2>&1
