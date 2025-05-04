d1ec8ca37cf592ac90dbf64a90b17b34d02c2f64
Parman <armantheparman@protonmail.com>
2025-05-04 18:27:37 +1000

    a

--- src/ParmaView/parmaview_run.sh
+++ src/ParmaView/parmaview_run.sh
@@ -2,9 +2,9 @@ function parmaview_run {
 if [[ $OS == "Mac" ]] ; then    
     docker run -du parman --hostname ParmaView --name parmaview \
            -v $HOME/parmanode/parmaview:/home/parman/parmanode/parmaview \
-           -p 24:22 \
+           -p 25:22 \
            -p 88:80 \
-           -p 483:443 \
+           -p 458:443 \
            -p 58000:58000 \ 
            parmaview \
            tail -f /dev/null
