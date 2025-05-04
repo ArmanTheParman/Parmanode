9390da058dfa1c84eec430a569ea788da3ace2d7
Parman <armantheparman@protonmail.com>
2025-05-04 15:34:17 +1000

    a

--- src/ParmaView/parmaview_parmanode.sh
+++ src/ParmaView/parmaview_parmanode.sh
@@ -1,13 +1,13 @@
 function parmaview_parmanode {
-# Install Parmanode in Parmabox, and ParmaShell
-docker exec -it -u parman $parmabox bash -c \
+# Install ParmaShell in ParmaView
+docker exec -it -u parman parmaview bash -c \
            "echo \"parmashell-end\" | tee -a /home/parman/.parmanode/installed.conf >$dn"
 
 # Make bashrc better
-docker exec -it -u root parmabox bash -c "echo \"function rp { cd /home/parman/parman_programs/parmanode ; ./run_parmanode.sh \$@ ; }\" | tee -a /root/.bashrc /home/parman/.bashrc" >$dn 2>&1
-docker exec -it -u root parmabox bash -c "echo \"source /home/parman/parman_programs/parmanode/src/ParmaShell/parmashell_functions\" | tee -a /root/.bashrc /home/parman/.bashrc" >$dn 2>&1
-docker exec -it -u root parmabox bash -c "sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' /root/.bashrc " >$dn 2>&1
-docker exec -it -u root parmabox bash -c "sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' /home/parman/.bashrc" >$dn 2>&1
-docker exec -it -u root parmabox bash -c "chown -R parman:parman /home/parman"
-unset parmabox
+docker exec -it -u root parmaview bash -c "echo \"function rp { cd /home/parman/parman_programs/parmanode ; ./run_parmanode.sh \$@ ; }\" | tee -a /root/.bashrc /home/parman/.bashrc" >$dn 2>&1
+docker exec -it -u root parmaview bash -c "echo \"source /home/parman/parman_programs/parmanode/src/ParmaShell/parmashell_functions\" | tee -a /root/.bashrc /home/parman/.bashrc" >$dn 2>&1
+docker exec -it -u root parmaview bash -c "sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' /root/.bashrc " >$dn 2>&1
+docker exec -it -u root parmaview bash -c "sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/' /home/parman/.bashrc" >$dn 2>&1
+docker exec -it -u root parmaview bash -c "chown -R parman:parman /home/parman"
+unset parmaview
 }
