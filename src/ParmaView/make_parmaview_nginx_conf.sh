b570bf736705505bcf3d2a79d57b8636f2c96911
Parman <armantheparman@protonmail.com>
2025-05-06 01:39:05 +1000

    a

--- src/ParmaView/make_parmaview_nginx_conf.sh
+++ src/ParmaView/make_parmaview_nginx_conf.sh
@@ -28,6 +28,8 @@ server {
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
+        proxy_set_header Host \$host;
+
     }
 
     location / {
