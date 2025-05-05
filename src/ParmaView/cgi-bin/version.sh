b3d0d2a2dcd4aa41bf14b97e869799bb7cee1469
Parman <armantheparman@protonmail.com>
2025-05-06 01:27:31 +1000

    a

--- src/ParmaView/cgi-bin/version.sh
+++ src/ParmaView/cgi-bin/version.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-source $HOME/parman_programs/parmanode/version.conf
-source $HOME/parman_programs/parmanode/src/ParmaView/sendtosocket.sh
+echo "Content-Type: text/plain"
+echo ""
+
+source "$HOME/parman_programs/parmanode/version.conf"
+source "$HOME/parman_programs/parmanode/src/ParmaView/sendtosocket.sh"
 sendtosocket "$version"
