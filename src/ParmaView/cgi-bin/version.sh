ae7abc1a9b9a8bb725a566df8336a4c8c620921e
Parman <armantheparman@protonmail.com>
2025-05-06 00:51:16 +1000

    aa

--- src/ParmaView/cgi-bin/version.sh
+++ src/ParmaView/cgi-bin/version.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-#    source $HOME/parman_programs/parmanode/src/config/parmanode_variables.sh 2>/dev/null
-#    export cgi="true" && parmanode_variables 
-
-    source $HOME/parman_programs/parmanode/version.conf
-
-echo "Content-Type: application/javascript"
-echo ""
-
-cat <<EOF
-let version;
-version = "$version";
-EOF
+source $HOME/parman_programs/parmanode/version.conf
+source $HOME/parman_programs/parmanode/src/ParmaView/sendtosocket.sh
+sendtosocket "$version"
