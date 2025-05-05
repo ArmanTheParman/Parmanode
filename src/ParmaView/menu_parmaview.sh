331c4b91998de7b7709ad1b66106aba6e9496c0a
Parman <armantheparman@protonmail.com>
2025-05-05 17:27:13 +1000

    a

--- src/ParmaView/menu_parmaview.sh
+++ src/ParmaView/menu_parmaview.sh
@@ -52,7 +52,7 @@ choose "xpmq" ; read choice
 jump $choice || { invalid ; continue ; } ; clear
 case $choice in 
 ws)
-pn_tmux "python3 $pn/src/ParmaView/websocket_server.py" "ws1" | grep -q duplicate && { enter_continue ; continue ; }
+pn_tmux "python3 $pn/src/ParmaView/websocket_server.py" "ws1" 
 enter_continue "Websocket started in the background"
 ;;
 wst)
