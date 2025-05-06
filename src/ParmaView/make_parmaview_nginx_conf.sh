45d9d398d106d1f7043c3c55e4d8885fde1162e3
Parman <armantheparman@protonmail.com>
2025-05-06 14:07:25 +1000

    a

--- src/ParmaView/make_parmaview_nginx_conf.sh
+++ src/ParmaView/make_parmaview_nginx_conf.sh
@@ -21,6 +21,7 @@ server {
         fastcgi_pass unix:/var/run/fcgiwrap.socket;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
         fastcgi_param PATH_INFO \$fastcgi_path_info;
+        fastcgi_param HOME $HOME;
     }
 
     location /ws/ {
