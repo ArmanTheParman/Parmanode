1a56971aa47f158851bf7aba81b3739626ef1b53
Parman <armantheparman@protonmail.com>
2025-05-07 01:04:28 +1000

    a

--- src/ParmaView/cgi-bin/version.sh
+++ src/ParmaView/cgi-bin/version.sh
@@ -2,15 +2,6 @@
 echo "Content-Type: text/plain"
 echo ""
 
-
-    function sendtosocket {
-            while IFS= read -r line; do
-                { [[ ${line: -1} == $'\n' ]] && printf "%s" "$line" ; } || printf "%s\n" "$line"
-            done | socat - UNIX-CONNECT:"/run/parmanode/parmanode.sock"
-    }
-
 source "/opt/parmanode/version.conf"
-#source "/opt/parmanode/src/ParmaView/sendtosocket.sh"
 
-echo "$version" | sendtosocket 
-echo "test"
+echo "$version"
