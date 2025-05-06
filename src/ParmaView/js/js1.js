8dc6c31b9fec73f611ca9c571ce7d5cb9c748197
Parman <armantheparman@protonmail.com>
2025-05-07 00:27:07 +1000

    a

--- src/ParmaView/js/js1.js
+++ src/ParmaView/js/js1.js
@@ -1,13 +1,9 @@
 let socket = new WebSocket("ws://" + location.host + "/ws/");
 
 socket.onmessage = function(event) {
-    console.log("Received:", event.data);
     const title = document.getElementById("title");
     const original = title.textContent;
     title.textContent = event.data;
-    setTimeout(() => {
-        title.textContent = original;
-    }, 3000);
 };
 
 function getVersion() {
