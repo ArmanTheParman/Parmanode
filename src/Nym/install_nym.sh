53dd947bc33e71cb13137c6ce834de2d8210e340
Parman <armantheparman@protonmail.com>
2025-05-16 01:29:17 +1000

    a

--- src/Nym/install_nym.sh
+++ src/Nym/install_nym.sh
@@ -4,7 +4,7 @@ mkdir $hp/nym
 installed_config_add "nym-start"
 cd $hp/nym
 curl -LO https://apt.nymtech.net/pool/main/n/nym-repo-setup/nym-repo-setup_1.0.1_amd64.deb 
-sudo dpkg -i $hp/nym-repo-setup_1.0.1_amd64.deb || sww
+sudo dpkg -i $hp/nym/nym-repo-setup_1.0.1_amd64.deb || sww
 sudo apt install nym-vpn || sww
 installed_config_add "nym-end"
 installed_config_remove "nym-start"
