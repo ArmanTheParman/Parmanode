84f74ccdaeda88c602b9f758b230ef1c49a55d78
Parman <armantheparman@protonmail.com>
2025-05-16 01:33:21 +1000

    a

--- src/Nym/install_nym.sh
+++ src/Nym/install_nym.sh
@@ -8,7 +8,7 @@ installed_config_add "nym-start"
 cd $hp/nym
 curl -LO https://apt.nymtech.net/pool/main/n/nym-repo-setup/nym-repo-setup_1.0.1_amd64.deb 
 sudo dpkg -i $hp/nym/nym-repo-setup_1.0.1_amd64.deb || sww
-sudo apt install nym-vpn || sww
+sudo apt install nym-vpn -y || sww
 
 if [[ $swwflag == "true" ]] ; then unset swwflag ; return 1 ; fi
 installed_config_add "nym-end"
