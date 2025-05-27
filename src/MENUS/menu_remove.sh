ebe8039c3b814b2aa6b4360472005cf450b34a46
Parman <armantheparman@protonmail.com>
2025-05-27 23:31:21 +1000

    a

--- src/MENUS/menu_remove.sh
+++ src/MENUS/menu_remove.sh
@@ -238,9 +238,9 @@ echo -e "#$cyan                            \033[27G nym)$orange              Nym
 elif grep -q "nym-start" $HOME/.parmanode/installed.conf ; then nymmenu=1
 echo -e "#$cyan                            \033[27G nym)$orange              Nym VPN                $red$blinkon(partial)$blinkoff$orange        \033[88G#" ; fi
 if grep -q "i2p-end" $HOME/.parmanode/installed.conf ; then i2pmenu=1
-echo -e "#$cyan                            \033[27G  ii)$orange              I2P                                    \033[88G#"
+echo -e "#$cyan                            \033[27G ii)$orange               I2P                                    \033[88G#"
 elif grep -q "i2p-start" $HOME/.parmanode/installed.conf ; then i2pmenu=1
-echo -e "#$cyan                            \033[27G  ii)$orange              I2P                    $red$blinkon(partial)$blinkoff$orange        \033[88G#" ; fi
+echo -e "#$cyan                            \033[27G ii)$orange               I2P                    $red$blinkon(partial)$blinkoff$orange        \033[88G#" ; fi
 echo -e "#                                                                                      \033[88G#
 ########################################################################################
 "
