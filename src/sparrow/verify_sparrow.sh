b55de4e83be8270108eb7b474f5007f0b8b5a940
Parman <armantheparman@protonmail.com>
2025-05-28 14:46:51 +1000

    Sparrow filename inconsistency bug fixed

--- src/sparrow/verify_sparrow.sh
+++ src/sparrow/verify_sparrow.sh
@@ -5,6 +5,9 @@ curl https://keybase.io/craigraw/pgp_keys.asc | gpg --import
 if ! gpg --verify sparrow*.asc sparrow*.txt >$dn 2>&1 ; then
 set_terminal ; echo "GPG verification failed. Aborting. Contact Parman for help." ; enter_continue ; return 1 ; fi
 
+echo -e "${green}gpg verification passed"
+sleep 2
+
 if which sha256sum >$dn ; then
     if ! sha256sum --ignore-missing --check *parrow*.txt ; then echo -e "Checksum$red failed$orange. Aborting. Contact Parman for help." 
     enter_continue ; return 1 ; fi
@@ -13,5 +16,7 @@ else
     enter_continue ; return 1 ; fi
 fi
 
+echo -e "${green}SHA256 verification passed"
+sleep 2
 
 }
