c2d509fdc90828cf302653d4d042a83d0b30d259
Parman <armantheparman@protonmail.com>
2025-06-02 01:30:38 +1000

    a

--- src/VNC/start_stop_vnc.sh
+++ src/VNC/start_stop_vnc.sh
@@ -1,10 +1,10 @@
 function start_vnc {
-    sudo systemctl start vnc.service novnc.service >$dn 2>&1
+    sudo systemctl start vnc.service noVNC.service >$dn 2>&1
 }
 function stop_vnc {
-    sudo systemctl stop vnc.service novnc.service >$dn 2>&1
+    sudo systemctl stop vnc.service noVNC.service >$dn 2>&1
 }
 function restart_vnc {
-    sudo systemctl restart vnc.service novnc.service >$dn 2>&1
+    sudo systemctl restart vnc.service noVNC.service >$dn 2>&1
 }
 
