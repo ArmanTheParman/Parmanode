60b6673baac20405140fb9116d94a262a51cf3c5
Parman <armantheparman@protonmail.com>
2025-06-09 03:10:15 +1000

    a

--- src/ParmaDesk/make_vnc_nginx.sh
+++ src/ParmaDesk/make_vnc_nginx.sh
@@ -3,8 +3,8 @@ function make_parmadesk_nginx {
 cat <<EOF | sudo tee $macprefix/etc/nginx/conf.d/vnc.conf >$dn 2>&1
 server {
     listen 21001 ssl;
-    ssl_certificate     $hp/vnc/cert.pem;
-    ssl_certificate_key $hp/vnc/key.pem;
+    ssl_certificate     $hp/parmadesk/cert.pem;
+    ssl_certificate_key $hp/parmadesk/key.pem;
 
     location /websockify {
         proxy_pass http://localhost:21000;
