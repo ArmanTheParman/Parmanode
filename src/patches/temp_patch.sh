33998b53e8af43da48d57458e4454cd0bb2fc274
Parman <armantheparman@protonmail.com>
2025-06-11 01:05:05 +1000

    a

--- src/patches/temp_patch.sh
+++ src/patches/temp_patch.sh
@@ -87,12 +87,12 @@ local fulcrum_service="/etc/systemd/system/fulcrum.service"
 if sudo test -f $bitcoin_service >$dn 2>&1 && sudo grep -q 'parmanode/mount_check.sh' $bitcoin_service ; then
 debug "changing bitcoinservice"
    sudo gsed -i 's/mount_check.sh/scripts\/mount_check.sh/' $bitcoin_service >$dn 2>&1 
-else
-debug "not changing bitcoinservice"
+   sudo systemctl daemon-reload
 fi
 
 if sudo test -f $fulcrum_service >$dn 2>&1 && sudo grep -q 'parmanode/mount_check.sh' $fulcrum_service ; then
    sudo gsed -i 's/mount_check.sh/scripts\/mount_check.sh/' $fulcrum_service >$dn 2>&1 
+   sudo systemctl daemon-reload
 fi
 
 gsed -i 's/vnc-/parmadesk-/g' $ic >$dn 2>&1
