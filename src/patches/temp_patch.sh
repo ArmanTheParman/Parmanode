d63ab034efe249cf77faba271f24ae0a9ad6c5de
Parman <armantheparman@protonmail.com>
2025-06-11 00:59:27 +1000

    a

--- src/patches/temp_patch.sh
+++ src/patches/temp_patch.sh
@@ -78,6 +78,7 @@ sudo gsed -E -i 's|^IdentityFile ~/.ssh/(.*-key)$|IdentityFile ~/.ssh/extra_keys
 
 #introduce a scripts directory. Needs some refactoring
 test -d $dp/scripts || mkdir -p $dp/scripts >$dn 2>&1
+
 # mv $dp/update_external_IP2.sh $dp/scripts >$dn 2>&1
 # mv $dp/mount_check.sh $dp/scripts/mount_check.sh >$dn 2>&1
 #     #rewrite paths in existing service files
