e8fb988087e140da3cdcfe91ca79636275288956
Parman <armantheparman@protonmail.com>
2025-06-11 01:36:39 +1000

    a

--- src/patches/temp_patch.sh
+++ src/patches/temp_patch.sh
@@ -80,23 +80,30 @@ sudo gsed -E -i 's|^IdentityFile ~/.ssh/(.*-key)$|IdentityFile ~/.ssh/extra_keys
 test -d $dp/scripts || mkdir -p $dp/scripts >$dn 2>&1
 
 mv $dp/update_external_IP2.sh $dp/scripts >$dn 2>&1 #mac and linux ok
-if [[ $OS == "Linux" ]] ; then
-mv $dp/mount_check.sh $dp/scripts/mount_check.sh >$dn 2>&1 
-mv $dp/update_script.sh $dp/scripts/update_script.sh >$dn 2>&1
-    #rewrite paths in existing service files
-local bitcoin_service="/etc/systemd/system/bitcoind.service"
-local fulcrum_service="/etc/systemd/system/fulcrum.service"
-local crontabfile="/etc/crontab"
-if sudo test -f $bitcoin_service >$dn 2>&1 && sudo grep -q 'parmanode/mount_check.sh' $bitcoin_service ; then
-debug "changing bitcoinservice"
-   sudo gsed -i 's/mount_check.sh/scripts\/mount_check.sh/' $bitcoin_service >$dn 2>&1 
-   sudo systemctl daemon-reload
+
+if test -f $dp/update_script.sh ; then
+    mv $dp/update_script.sh $dp/scripts/update_script.sh >$dn 2>&1
+    sudo cp /etc/crontab $tmp/crontab && sudo gsed 's/parmanode\/update_script.sh/parmanode\/scripts\/update_script.sh/' /$tmp/crontab >$dn 2>&1
+    sudo mv $tmp/crontab /etc/crontab >$dn 2>&1
 fi
 
-if sudo test -f $fulcrum_service >$dn 2>&1 && sudo grep -q 'parmanode/mount_check.sh' $fulcrum_service ; then
-   sudo gsed -i 's/mount_check.sh/scripts\/mount_check.sh/' $fulcrum_service >$dn 2>&1 
-   sudo systemctl daemon-reload
-fi
+if [[ $OS == "Linux" ]] && test -f $dp/mount_check.sh  ; then
+
+    mv $dp/mount_check.sh $dp/scripts/mount_check.sh >$dn 2>&1 
+        #rewrite paths in existing service files
+    local bitcoin_service="/etc/systemd/system/bitcoind.service"
+    local fulcrum_service="/etc/systemd/system/fulcrum.service"
+    local crontabfile="/etc/crontab"
+    if sudo test -f $bitcoin_service >$dn 2>&1 && sudo grep -q 'parmanode/mount_check.sh' $bitcoin_service ; then
+
+    sudo gsed -i 's/mount_check.sh/scripts\/mount_check.sh/' $bitcoin_service >$dn 2>&1 
+    sudo systemctl daemon-reload
+    fi
+
+    if sudo test -f $fulcrum_service >$dn 2>&1 && sudo grep -q 'parmanode/mount_check.sh' $fulcrum_service ; then
+    sudo gsed -i 's/mount_check.sh/scripts\/mount_check.sh/' $fulcrum_service >$dn 2>&1 
+    sudo systemctl daemon-reload
+    fi
 
 fi # end linux
 
