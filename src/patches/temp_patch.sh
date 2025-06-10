8f3e80f70b4513f433f1c6449e3f174e9c1c15ed
Parman <armantheparman@protonmail.com>
2025-06-11 00:58:52 +1000

    a

--- src/patches/temp_patch.sh
+++ src/patches/temp_patch.sh
@@ -78,17 +78,17 @@ sudo gsed -E -i 's|^IdentityFile ~/.ssh/(.*-key)$|IdentityFile ~/.ssh/extra_keys
 
 #introduce a scripts directory. Needs some refactoring
 test -d $dp/scripts || mkdir -p $dp/scripts >$dn 2>&1
-mv $dp/update_external_IP2.sh $dp/scripts >$dn 2>&1
-mv $dp/mount_check.sh $dp/scripts/mount_check.sh >$dn 2>&1
-    #rewrite paths in existing service files
-local bitcoin_service="/etc/systemd/system/bitcoind.service"
-local fulcrum_service="/etc/systemd/system/fulcrum.service"
-if sudo test -f $bitcoin_service >$dn 2>&1 && ! sudo grep -q 'parmanode/mount_check.sh' $bitcoin_service ; then
-   sudo gsed -i 's/mount_check.sh\/scripts/mount_check.sh/' $bitcoin_service >$dn 2>&1 
-fi
-if sudo test -f $fulcrum_service >$dn 2>&1 && ! sudo grep -q 'parmanode/mount_check.sh' $fulcrum_service ; then
-   sudo gsed -i 's/mount_check.sh\/scripts/mount_check.sh/' $fulcrum_service >$dn 2>&1 
-fi
+# mv $dp/update_external_IP2.sh $dp/scripts >$dn 2>&1
+# mv $dp/mount_check.sh $dp/scripts/mount_check.sh >$dn 2>&1
+#     #rewrite paths in existing service files
+# local bitcoin_service="/etc/systemd/system/bitcoind.service"
+# local fulcrum_service="/etc/systemd/system/fulcrum.service"
+# if sudo test -f $bitcoin_service >$dn 2>&1 && ! sudo grep -q 'parmanode/mount_check.sh' $bitcoin_service ; then
+#    sudo gsed -i 's/mount_check.sh\/scripts/mount_check.sh/' $bitcoin_service >$dn 2>&1 
+# fi
+# if sudo test -f $fulcrum_service >$dn 2>&1 && ! sudo grep -q 'parmanode/mount_check.sh' $fulcrum_service ; then
+#    sudo gsed -i 's/mount_check.sh\/scripts/mount_check.sh/' $fulcrum_service >$dn 2>&1 
+# fi
 
 gsed -i 's/vnc-/parmadesk-/g' $ic >$dn 2>&1
 
