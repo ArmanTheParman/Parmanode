51a11b3a19f7b13713144d077eb8b202e5db421b
Parman <armantheparman@protonmail.com>
2025-06-12 04:18:41 +1000

    a

---
+++ src/text_functions/command_tip.sh
@@ -0,0 +1,27 @@
+function command_tip {
+# $1 = for hiding
+# $2 = the command
+# $3 = goback signal -- need code to hand that from calling function
+
+set_terminal 
+
+if [[ -n $3 ]] ; then goback="Hit $3 to go back\n" ; fi
+
+if grep -q "$1" $hc ; then return 0 ; fi
+announce "The command that will run:\n
+$cyan
+    $2
+$red
+Type 'hide' to not see this tip anymore 
+Manually delete the text, $1, from $dp/hide_commands.conf 
+to undo this action.
+$orange
+$goback$orange
+"
+
+unset goback ; jump $enter_cont ; clear
+
+[[ $enter_cont == "$3" ]] && return 1 #need || after command_tip call for this to do anything
+
+return 0
+}
