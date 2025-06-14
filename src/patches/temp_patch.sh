89ee662a82229528aa2c98b845c6c114bbede6a9
Parman <armantheparman@protonmail.com>
2025-06-14 16:56:21 +1000

    a

--- src/patches/temp_patch.sh
+++ src/patches/temp_patch.sh
@@ -115,9 +115,9 @@ announce "PARMADESK VNC had some bugs which have been fixed. Please reinstall Pa
 touch $dp/.vncfixed
 fi
 
-if [[ $OS == "Linux" ]] && grep -q parmadesk-end $ic && ! sudo test -f /etc/systemd/system/parmadesk.sh ; then
-make_parmadesk_service
-fi
+# if [[ $OS == "Linux" ]] && grep -q parmadesk-end $ic && ! sudo test -f /etc/systemd/system/parmadesk.sh ; then
+# make_parmadesk_service
+# fi
 
 # would be good to have a run once function, I might make that later and add this in:
 test -f $hc || touch $dp/hide_commands.conf
