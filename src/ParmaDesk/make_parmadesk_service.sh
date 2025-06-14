8dd643fff0386623bead036a8573a8c3fbf8045b
Parman <armantheparman@protonmail.com>
2025-06-14 16:55:18 +1000

    a

--- src/ParmaDesk/make_parmadesk_service.sh
+++ src/ParmaDesk/make_parmadesk_service.sh
@@ -1,12 +1,8 @@
 function make_parmadesk_service {
 cat << EOF | tee $dp/scripts/parmadesk.sh >$dn 2>&1
 #!/bin/bash
-trap "exit 0" SIGTERM
-while true ; do
-    for file in ~/.vnc/*log ; do
+for file in ~/.vnc/*log ; do
         > $file
-    done
-sleep 86400
 done
 EOF
 sudo chmod +x $dp/scripts/parmadesk.sh
@@ -16,17 +12,14 @@ cat << EOF | sudo tee /etc/systemd/system/parmadesk.service >$dn 2>&1
 Description=ParmaDesk Service: A 24 hour loop to keep log files small
 
 [Service]
-Type=simple
 
 ExecStart=$dp/scripts/parmadesk.sh
-ExecStop=/bin/kill -s SIGTERM \$MAINPID
-KillMode=process
+Restart=always
+RestartSec=84600
 
 User=$(whoami)
 Group=$(whoami)
 
-Restart=on-failure
-RestartSec=3000
 [Install]
 WantedBy=multi-user.target
 EOF
