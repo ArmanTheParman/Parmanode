9db6da6d7484cab7de98769e09ac45b1e5ad5cc5
Parman <armantheparman@protonmail.com>
2025-06-16 00:28:31 +1000

    a

--- src/Deprecated/not_used/add_IP_fulcrum_config_mac.sh
+++
@@ -1,5 +0,0 @@
-function add_IP_fulcrum_config {
-nogsedtest
-sudo gsed -i "/bitcoind/d" $fc
-echo "bitcoind = $IP:8332" | tee -a $fc >$dn 2>&1
-}
--- src/Deprecated/not_used/readme.md
+++
@@ -1,1 +0,0 @@
-Subdirectory to ensure contents are not sourced
