330022e1e5fe548938070ea8f81d556238ec926d
Parman <armantheparman@protonmail.com>
2025-06-27 19:19:30 +1000

    a

--- src/premium/menu_parmadrive.sh
+++ src/premium/menu_parmadrive.sh
@@ -71,7 +71,7 @@ if grep -q parmaraid-end $ic ; then
     raid="${green}disassembled${blue}"
     raidstatus=disassembled
     fi
-raidmenu="\n            RAID is: $raid"
+raidmenu="\n            RAID is:      $raid"
 unset mountmenu mount2 mounted2
 else
 unset raidmenu raid raidstatus
