be8c02f0cd194e6080cbbbf18ff319fb84ac2604
Parman <armantheparman@protonmail.com>
2025-07-16 22:29:24 +1000

    a

--- src/communication/iptables.sh
+++ src/communication/iptables.sh
@@ -3,25 +3,46 @@ function menu_iptables {
 if [[ $OS == "Mac" ]] ; then no_mac ; return 1 ; fi
 
 while true ; do
-set_terminal
+set_terminal 38 110
 echo -e "
-########################################################################################$cyan
+##############################################################################################################$cyan
                                 IP Tables Menu$orange
-########################################################################################
-
+##############################################################################################################
 
 $cyan
-                  list)$orange           List Filter Table Rules
-
-
-
-########################################################################################
+    la,li,lo,fw)$orange     List Filter Table Rules $green(ALL,INPUT,OUTPUT,FORWARD)$orange
+$cyan
+    za)$orange              Zero data count for$green all$orange tables (Filter,NAT,Mangle,Raw,Security)
+$cyan
+    zfi,zfo,zff)$orange     Zero data in filter table$green (INPUT,OUTPUT,FORWARD) $orange
+$orange
+##############################################################################################################
 "
 choose xpmq ; read choice ; clear
 jump $choice ; jump_mpq || return 1
 case $choice in
-list)
-sudo iptables -t filter -L -v -n --line-numbers
+fw)
+sudo iptables -t filter -L FORWARD -v --line-numbers 
+enter_continue
+;;
+lo)
+sudo iptables -t filter -L OUTPUT -v --line-numbers 
+enter_continue
+;;
+li)
+sudo iptables -t filter -L INPUT -v --line-numbers 
+enter_continue
+;;
+la)
+sudo iptables -t filter -L -v --line-numbers 
+#Alternatives
+#  -L lists all unless value like INPUT added after
+#  sudo iptables-save
+#  -n disables name resolution
+enter_continue
+;;
+z)
+sudo iptables -Z
 enter_continue
 ;;
 esac
