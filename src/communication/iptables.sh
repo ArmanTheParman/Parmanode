edd909730fb7ff1579b2cb23e2053b93c1f1ab57
Parman <armantheparman@protonmail.com>
2025-07-16 23:47:40 +1000

    a

--- src/communication/iptables.sh
+++ src/communication/iptables.sh
@@ -28,33 +28,76 @@ echo -e "
                                                IP Tables Menu$orange
 ################################################################################################################################
 $green
-THERE ARE 5 TABLES, each with chains: ('t' to toggle)
+    THERE ARE 5 TABLES, each with chains: ('t' to toggle)
 $show
+    ${red}Add ',v' to the selection for verbose 
+    Add ',n' to disable name resolution 
+    eg lnt,v,n$orange
 
 $cyan
-    lfa,lfi,lfo,lff)$orange         List FILTER Table Rules $green(ALL,INPUT,OUTPUT,FORWARD)$orange
+  lfa/lfi/lfo/lff)$orange         List FILTER Table Rules $green(ALL/INPUT/OUTPUT/FORWARD)$orange
 $cyan
-    lna,lnp,lnt,lno,lni)$orange     List NAT Table Rules $green(ALL,PREROUTING,POSTROUTING,OUTPUT,INPUT)$orange
+  lna/lnp/lnt/lno/lni)$orange     List NAT Table Rules $green(ALL/PREROUTING/POSTROUTING/OUTPUT/INPUT)$orange
 $cyan
-    lma,lmi,lmo,lmf)$orange         List MANGLE Table Rules $green(ALL,INPUT,FORWARD,OUPUT,POSTROUTING)$orange
+  lma/lmp/lmi/lmf/lmo/lmt)$orange List MANGLE Table Rules $green(ALL/PREROUTING/INPUT/FORWARD/OUPUT/POSTROUTING)$orange
 $cyan
-    lra,lrp,lro)$orange             List RAW Table Rules $green(ALL,PREROUTING,OUTPUT)$orange
+  lra/lrp/lro)$orange             List RAW Table Rules $green(ALL/PREROUTING/OUTPUT)$orange
 $cyan
-    lsa,lsi,lso,lsf)$orange         List SECURITY Table Rules $green(ALL,INPUT,OUTPUT,FORWARD)$orange
+  lsa/lsi/lso/lsf)$orange         List SECURITY Table Rules $green(ALL/INPUT/OUTPUT/FORWARD)$orange
 $cyan
-    z*)$orange                      Zero data count from selected list combination above. Type$green z$orange and$green selection $orange
+  z*)$orange                      Zero data count from selected list combination above. Type$green z$orange and$green selection $orange
                                     ${red}Eg: zmi will zero the mangle input chain$orange
 $cyan
-    za)$orange                      Zero data count for all tables and all chains
+  za)$orange                      Zero data count for all tables and all chains
 $cyan
-    s*)$orange                      See a list of the$green syntax$orange used to add rules. Type$green s$orange and$green selection $orange
+  s*)$orange                      See a list of the$green syntax$orange used to add rules. Type$green s$orange and$green selection $orange
                                     ${red}Eg: srp will show syntax rules in the the raw prerouting chain$orange
 $orange
 ################################################################################################################################
 "
 choose xpmq ; read choice ; clear
 jump $choice ; jump_mpq || return 1
+unset table chain v n
 case $choice in
+l*)
+#choose table
+   if [[ $choice =~ ^lf.* ]] ; then table="filter" ; fi
+   if [[ $choice =~ ^ln.* ]] ; then table="nat" ; fi
+   if [[ $choice =~ ^lm.* ]] ; then table="mangle" ; fi
+   if [[ $choice =~ ^lr.* ]] ; then table="raw" ; fi
+   if [[ $choice =~ ^ls.* ]] ; then table="security" ; fi
+#choose chain
+   if [[ $choice =~ ^l.a.* ]] ; then chain="" ; fi
+   if [[ $choice =~ ^l.i.* ]] ; then chain="INPUT" ; fi
+   if [[ $choice =~ ^l.o.* ]] ; then chain="OUTPUT" ; fi
+   if [[ $choice =~ ^l.f.* ]] ; then chain="FORWARD" ; fi
+   if [[ $choice =~ ^l.p.* ]] ; then chain="PREROUTING" ; fi
+   if [[ $choice =~ ^l.t.* ]] ; then chain="POSTROUTING" ; fi
+#invalid...
+   if [[ $choice =~ ^lf(t|p) ]] ; then invalid ; continue ; fi
+   if [[ $choice =~ ^lnf ]] ; then invalid ; continue ; fi
+   if [[ $choice =~ ^lmp ]] ; then invalid ; continue ; fi
+   if [[ $choice =~ ^lr(t|i|f) ]] ; then invalid ; continue ; fi
+   if [[ $choice =~ ^ls(t|p) ]] ; then invalid ; continue ; fi
+#verbose and name resolution disabling
+   if [[ $choice =~ .*,v.* ]] ; then v="-v" ; fi
+   if [[ $choice =~ .*,n.* ]] ; then n="-n" ; fi
+
+   sudo iptables -t $table -L $chain $v $n | less
+;;
+
+lo)
+sudo iptables -t filter -L OUTPUT -v
+enter_continue
+;;
+li)
+sudo iptables -t filter -L INPUT -v 
+enter_continue
+;;
+#Alternatives
+#  sudo iptables-save
+#  -n disables name resolution
+
 t)
 [[ -z $toggle ]] && export toggle=on && continue
 unset toggle
@@ -67,22 +110,7 @@ fw)
 sudo iptables -t filter -L FORWARD -v --line-numbers 
 enter_continue
 ;;
-lo)
-sudo iptables -t filter -L OUTPUT -v --line-numbers 
-enter_continue
-;;
-li)
-sudo iptables -t filter -L INPUT -v --line-numbers 
-enter_continue
-;;
-la)
-sudo iptables -t filter -L -v --line-numbers 
-#Alternatives
-#  -L lists all unless value like INPUT added after
-#  sudo iptables-save
-#  -n disables name resolution
-enter_continue
-;;
+
 z)
 sudo iptables -Z
 enter_continue
