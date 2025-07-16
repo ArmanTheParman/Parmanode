bee158d4de431fe45bc70dc4803e38bbb64168e6
Parman <armantheparman@protonmail.com>
2025-07-16 23:11:41 +1000

    iptables

--- src/communication/iptables.sh
+++ src/communication/iptables.sh
@@ -3,24 +3,63 @@ function menu_iptables {
 if [[ $OS == "Mac" ]] ; then no_mac ; return 1 ; fi
 
 while true ; do
-set_terminal 38 110
+if [[ $toggle == "on" ]] ; then     
+
+export show="     FILTER:     INPUT / OUTPUT / FORWARD
+
+     NAT:        PREROUTING / POSTROUTING / OUTPUT / INPUT
+
+     MANGLE:     PREROUTING / INPUT / FORWARD / OUTPUT / POSTROUTING
+
+     RAW:        PREROUTING / OUTPUT
+
+     SECURITY:   INPUT / OUTPUT / FORWARD"
+
+set_terminal 45 128
+else
+unset show
+set_terminal 38 128
+fi
+
 echo -e "
-##############################################################################################################$cyan
-                                IP Tables Menu$orange
-##############################################################################################################
+################################################################################################################################$cyan
+                                               IP Tables Menu$orange
+################################################################################################################################
+$green
+THERE ARE 5 TABLES, each with chains: ('t' to toggle)
+$show
 
 $cyan
-    la,li,lo,fw)$orange     List Filter Table Rules $green(ALL,INPUT,OUTPUT,FORWARD)$orange
+    lfa,lfi,lfo,lff)$orange         List FILTER Table Rules $green(ALL,INPUT,OUTPUT,FORWARD)$orange
 $cyan
-    za)$orange              Zero data count for$green all$orange tables (Filter,NAT,Mangle,Raw,Security)
+    lna,lnp,lnt,lno,lni)$orange     List NAT Table Rules $green(ALL,PREROUTING,POSTROUTING,OUTPUT,INPUT)$orange
 $cyan
-    zfi,zfo,zff)$orange     Zero data in filter table$green (INPUT,OUTPUT,FORWARD) $orange
+    lma,lmi,lmo,lmf)$orange         List MANGLE Table Rules $green(ALL,INPUT,FORWARD,OUPUT,POSTROUTING)$orange
+$cyan
+    lra,lrp,lro)$orange             List RAW Table Rules $green(ALL,PREROUTING,OUTPUT)$orange
+$cyan
+    lsa,lsi,lso,lsf)$orange         List SECURITY Table Rules $green(ALL,INPUT,OUTPUT,FORWARD)$orange
+$cyan
+    z*)$orange                      Zero data count from selected list combination above. Type$green z$orange and$green selection $orange
+                                    ${red}Eg: zmi will zero the mangle input chain$orange
+$cyan
+    za)$orange                      Zero data count for all tables and all chains
+$cyan
+    s*)$orange                      See a list of the$green syntax$orange used to add rules. Type$green s$orange and$green selection $orange
+                                    ${red}Eg: srp will show syntax rules in the the raw prerouting chain$orange
 $orange
-##############################################################################################################
+################################################################################################################################
 "
 choose xpmq ; read choice ; clear
 jump $choice ; jump_mpq || return 1
 case $choice in
+t)
+export toggle=on
+;;
+syntax)
+sudo iptables -S
+enter_continue 
+;;
 fw)
 sudo iptables -t filter -L FORWARD -v --line-numbers 
 enter_continue
