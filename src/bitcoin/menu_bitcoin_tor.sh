125dca32dcf9e28f2f9d2f5242ca78678cd17a54
Parman <armantheparman@protonmail.com>
2025-07-24 00:54:21 +1000

    a

--- src/bitcoin/menu_bitcoin_tor.sh
+++ src/bitcoin/menu_bitcoin_tor.sh
@@ -118,12 +118,12 @@ m|M) back2main ;; Q|q|quit|QUIT|Quit) exit 0 ;; p|P) return 1 ;;
 
 "4"|"8")
     bitcoin_tor_remove 
-    case $choice in "8") sudo echo "listen=0" | sudo tee -a $bc >$dn 2>&1 ; sudo echo "discover=0" | sudo tee -a $bc >$dn 2>&1 ; esac
     bitcoin_tor "clearnet"
     parmanode_conf_remove "bitcoin_tor_status"
     parmanode_conf_add "bitcoin_tor_status=clearnet"
 #    check_bitcoin_tor_status    
     remove_bitcoin_i2p
+    case $choice in "8") sudo echo "listen=0" | sudo tee -a $bc >$dn 2>&1 ; sudo echo "discover=0" | sudo tee -a $bc >$dn 2>&1 ; esac
     if [[ $install == "bitcoin" ]] ; then return 0 ; fi
     continue ;;
 "5")
