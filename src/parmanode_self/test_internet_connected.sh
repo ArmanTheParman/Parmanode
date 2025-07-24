2211db837663a35cc5aadc469bc9dd10d37444a3
Parman <armantheparman@protonmail.com>
2025-07-24 18:03:35 +1000

    a

--- src/parmanode_self/test_internet_connected.sh
+++ src/parmanode_self/test_internet_connected.sh
@@ -1,6 +1,8 @@
 function test_internet_connected {
 curl -s https://8.8.8.8 >$dn 2>&1 #ping not always available, so curl instead
-if [[ $? != 0 ]] ; then set_terminal ; echo -e " 
+if [[ $? != 0 ]] ; then 
+while true ; do 
+set_terminal ; echo -e " 
 ########################################################################################
 
     WARNING: Parmanode has detected you might not have an internet connection
@@ -8,12 +10,17 @@ if [[ $? != 0 ]] ; then set_terminal ; echo -e "
 
 	${red}q${orange} to quit.
 
+	You have to type 'yolo' to continue.
+
 ########################################################################################
 "
 choose xq ; read choice ; set_terminal
 case $choice in
 q|Q) exit ;; 
+yolo) break ;;
+*) invalid ;;
 esac
+done
 fi
 return 0
 }
