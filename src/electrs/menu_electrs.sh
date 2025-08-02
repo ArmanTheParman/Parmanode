eb54d862080003e74270bbfb0aa324e0bc515d04
Parman <armantheparman@protonmail.com>
2025-08-02 20:57:51 +1000

    r

--- src/electrs/menu_electrs.sh
+++ src/electrs/menu_electrs.sh
@@ -134,7 +134,7 @@ choose "xpmq" ; read choice
 jump $choice || { invalid ; continue ; } ; set_terminal
 case $choice in
 q|Q) exit ;; m|M) back2main ;;
-r) menu_electrs || return 1 ;;
+""|r) continue ;;
 p|P)
 if [[ $1 == overview ]] ; then return 0 ; fi
 menu_use ;; 
