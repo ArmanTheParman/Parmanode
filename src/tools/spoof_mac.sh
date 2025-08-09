8b5fc710f101396746060eeb3584e312520c2a61
Arman The Parman <77603167+ArmanTheParman@users.noreply.github.com>
2025-08-09 17:52:35 +1000

    Create spoof_mac.sh

---
+++ src/tools/spoof_mac.sh
@@ -0,0 +1,29 @@
+function spoof_mac {
+
+#not finished
+
+#install mac changer
+  #apt_update 
+  #sudo apt install macchanger
+
+
+#test network connection statusband get id
+
+announce "
+The spoof MAC tool creates a fake temporary and random
+    MAC (Media Access Control) address for Ethernet or WiFi 
+    hardware, which can help fight against tracking and 
+    improve your privacy online. 
+
+    Turn off your WiFi, or disable/disconnect your Ethernet 
+    connection first before hitting <enter>."
+    
+enter_continue
+
+#check which network turned off
+
+#randomise mac
+
+#announce to turn back on
+
+}
