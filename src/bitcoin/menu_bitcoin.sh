1cd01723f580a70da2db0205d67e144377c872a4
Parman <armantheparman@protonmail.com>
2025-09-14 02:37:52 +1000

    a

--- src/bitcoin/menu_bitcoin.sh
+++ src/bitcoin/menu_bitcoin.sh
@@ -102,16 +102,16 @@ fi
 if ! bitcoin-cli --version |& grep -q "knots" ; then
 upgradetoknots="${red}\n\n    We are in a war with Core Developers making unwanted changes.
     Please run Knots instead to send them a message to get their head out of their arses.$orange"
-show_knots="$red BITCOIN CORE$orange"
+show_knots="$red BITCOIN CORE"
 else
-show_knots="$green BITCOIN KNOTS$orange"
+show_knots="$green BITCOIN KNOTS"
 unset upgradetoknots
 fi
 
 
 echo -en "
 ####################################################################################################$cyan
-                                          Bitcoin$show_knots Menu${orange}    $dockerbitcoinmenu $upgradetoknots
+                                        $show_knots Menu${orange}    $dockerbitcoinmenu $upgradetoknots
 ####################################################################################################
 
 
