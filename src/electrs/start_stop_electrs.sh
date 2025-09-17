db326ca6846ce1a0ef8af097a0ef530f29de41df
Parman <armantheparman@protonmail.com>
2025-09-18 03:34:34 +1000

    a

--- src/electrs/start_stop_electrs.sh
+++ src/electrs/start_stop_electrs.sh
@@ -1,10 +1,10 @@
 function start_electrs { #non-docker function
 source $pc
-if [[ $disable_electrs == "true" ]] ; then return 1 ; fi
+if [[ $disable_electrs == "true" ]] ; then ; clear ; echo "electrs disabled" ; sleep 1 ; return 1 ; fi
 
 please_wait
 if [[ $OS == "Linux" ]] ; then 
-    sudo systemctl start electrs.service >$dn 
+    sudo systemctl start electrs.service 
     sleep 0.5 #!# for debug
 fi
 
