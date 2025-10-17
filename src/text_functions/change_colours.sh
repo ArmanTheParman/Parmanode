4aaef2a4a760420f3dd4bc83d6cba040af08553c
Parman <armantheparman@protonmail.com>
2025-10-18 00:29:40 +1100

    a

--- src/text_functions/change_colours.sh
+++ src/text_functions/change_colours.sh
@@ -1,32 +1,54 @@
 function change_colours {
 
 while true ; do
+
+#For displaying status in the menu
+if grep -q "colourscheme=inverted" $pc ; then
+current_inverted="$red Current Selection"
+unset current_normal
+elif grep -q "colourscheme=normal" $pc ; then
+current_normal="$red Current Selection"
+unset current_inverted
+fi
+
 clear
+if [[ -z $1 ]] ; then
 echo -e "
 ########################################################################################
 
    You can toggle the colour scheme between two options, one of which may look better
    depending on the colour you have set on your Terminal application.
 $cyan
-            (t)$orange      Toggle colours (requires restarting Parmanode and Terminal)
+            (n)$orange      Normal colours $current_normal
+
+            (i)$orange      Inverted colours $current_inverted
 
 ########################################################################################
 "
 choose "xpqm" ; read choice 
 jump $choice || { invalid ; continue ; } ; set_terminal
+else
+choice="$1"
+fi
+
 case $choice in
 q|Q) exit ;; p|P) return 1 ;; m|M) back2main ;;
-t|T)
-if grep -q "orange=" $pc ; then
+n|normal)
 parmanode_conf_remove "orange=" 
-else
-parmanode_conf_add "orange=\"$reset\""
-fi
-announce "Please restart Parmanode to see the changes - quit, then 'rp' <enter> at the 
-    prompt."
-return 0
+parmanode_conf_remove "colourscheme=inverted"
+parmanode_conf_add "colourscheme=normal" 
 ;;
+i|inverted)
+parmanode_conf_remove "orange=" 
+parmanode_conf_add "orange=\"$reset\""
+parmanode_conf_remove "colourscheme=normal" 
+parmanode_conf_add "colourscheme=inverted"
+;;
+"")
+continue ;;
 *) invalid ;;
 esac
+announce "Please restart Parmanode to see the changes - quit, then 'rp' <enter> at
+    the prompt."
 done
 }
