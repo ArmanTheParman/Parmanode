d36b3c796a5045be29619673b5bdeac9d2473707
Parman <armantheparman@protonmail.com>
2025-10-18 00:05:21 +1100

    refresh_parmanode edit

--- refresh_parmanode.sh
+++ refresh_parmanode.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-if ! git config --global user.email ; then git config --global user.email sample@parmanode.com ; fi
-if ! git config --global user.name ; then git config --global user.name Parman ; fi
-cd $HOME/parman_programs
+if ! git config --global user.email >/dev/null ; then git config --global user.email sample@parmanode.com >/dev/null ; fi
+if ! git config --global user.name >/dev/null  ; then git config --global user.name Parman >/dev/null  ; fi
+cd $HOME/parman_programs >/dev/null
 rm -rf $HOME/parman_programs/parmanode
 git clone https://github.com/armantheparman/parmanode.git 
-echo "A new Parmanode directory has been cloned. Hit <enter>"
+echo -e "\n\nA new Parmanode directory has been cloned. Hit <enter>"
 read
