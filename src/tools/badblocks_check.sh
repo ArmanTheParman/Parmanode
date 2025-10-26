3e38acd7e5c15abb1bf26757ba1c77fa56414265
Parman <armantheparman@protonmail.com>
2025-10-26 23:29:54 +1100

    a

--- src/tools/badblocks_check.sh
+++ src/tools/badblocks_check.sh
@@ -16,15 +16,6 @@ echo -e "$reset$orange
 $cyan    
     Eg: /dev/sda or /dev/nvme0n1
 $orange
-    This will run in a TMUX window (backround). You can check on it by typing:
-$cyan
-    tmux attach -t badblocks
-$orange
-    from the terminal (not from withing Parmanode).
-
-    You can leave the TMUX window open, but to exit it and leave the process running 
-    in the background,$cyan type <control> b then d$orange. Go back in anytime with the 
-    earlier command.
     "
 printf "\e[41m$(printf ' %.0s' {1..88})"
 printf "\n$reset$orange"
@@ -34,6 +25,7 @@ read driveid </dev/tty
 if ! [[ $driveid =~ ^/dev/ ]] ; then sww "Doesn't look like a valid drive ID. Must start with /dev/" ; return 1 ; fi
 yesorno "Use $driveid?" || return 1
 
-pn_tmux "sudo badblocks -sv $driveid" "badblocks"
+sudo badblocks -sv $driveid || sww
+
 return 0
 }
