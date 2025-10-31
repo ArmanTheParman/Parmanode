fa2f5198482161d81b6e2eb20b121225576398a5
Parman <armantheparman@protonmail.com>
2025-11-01 02:45:33 +1100

    a

--- src/btcpay/run_btcpay_docker.sh
+++ src/btcpay/run_btcpay_docker.sh
@@ -1,6 +1,6 @@
 function run_btcpay_docker {
 
-if [[ $1 == "https" ]] ; then httpsenabled="-e BTCPAY_PROTOCOL=https" ; fi
+httpsenabled="-e BTCPAY_PROTOCOL=https"
 
 if [[ $OS == "Linux" ]] ; then
 docker run -d -u parman \
@@ -15,7 +15,7 @@ fi
 if [[ $OS == "Mac" ]] ; then
 docker run -d -u parman \
            --name btcpay \
-           --restart unless-stopped \
+           --restart unless-stopped $httpsenabled \
            -v $HOME/.btcpayserver:/home/parman/.btcpayserver \
            -v $HOME/.nbxplorer:/home/parman/.nbxplorer \
            -v $HOME/.bitcoin:/home/parman/.bitcoin \
