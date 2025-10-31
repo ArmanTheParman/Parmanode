88f53e87795c32d22bb2def792987f6a96bcf5d4
Parman <armantheparman@protonmail.com>
2025-10-31 19:50:27 +1100

    a

--- src/bitbox/verify_bitbox.sh
+++ src/bitbox/verify_bitbox.sh
@@ -3,9 +3,11 @@ function verify_bitbox {
 curl https://bitbox.swiss/download/shiftcryptosec-509249B068D215AE.gpg.asc | gpg --import
 
 if ! gpg --verify --status-fd 1 B*.asc  2>&1 | grep -iq GOOD ; then
-announce "gpg verification$red failed$orange. Aborting."
+debug 6
+sww "gpg verification$red failed$orange. Aborting."
 return 1
 else
-short_announce "gpg verification ${green}PASSED.$orange" "5"
+debug 9
+short_announce "gpg verification ${green}PASSED.$orange" "4"
 fi
 }
