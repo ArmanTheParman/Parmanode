a5b00d0a28b4eb9c67a1ae03b67441db6ec32f90
parman <sample@parmanode.com>
2025-11-12 21:16:20 +1100

    a

--- src/patches/parmanode4_upgrade.sh
+++ src/patches/parmanode4_upgrade.sh
@@ -1,4 +1,7 @@
 function parmanode4_upgrade {
+
+! grep -q "parmanode4=true" $pc && return 0
+
 make_parmanode_ca         || return 1
 make_parmanode_cert       || return 1
 sign_parmanode_cert       || return 1
