572c20e414ca24ccb4f3206e26e4ffd98a54a9a6
parman <sample@parmanode.com>
2025-11-15 02:47:49 +1100

    a

--- src/communication/sign_parmanode_cert.sh
+++ src/communication/sign_parmanode_cert.sh
@@ -1,25 +1,92 @@
 function sign_parmanode_cert {
-#This function is a blueprint, but a private key is needed.
-#The private key signs the parmanode certificate and the signature is embedded inside
-#the parmanode.local.crt file
 
-return 0
-    local filepath="$parmanode_cert_dir"
+local filepath="$parmanode_cert_dir"
 
-    CA_private_key="$RESTRICTED"
+while true ; do
+    announce "If you have a certificate authority private key and wish to sign 
+    \r    a certificate for your server, type in the full path and hit <enter>.
 
+    \r    Otherwise hit <enter> alone to abort."
+    case $enter_cont in "") return 1 ;; esac
+    yesorno "You typed $enter_cont. Continue?" || continue
+    local RESTRICTED="$enter_cont"
+
+    announce "Now type in the full path of the public key, typically called, ca.crt
+    \r    and hit <enter>.
+        
+    \r    Otherwise hit <enter> alone to abort."
+    case $enter_cont in "") return 1 ;; esac
+    yesorno "You typed $enter_cont. Continue?" || continue
+    local CA_PUBKEY="$enter_cont"
+    break
+
+done
+
+if yesorno "Sign the parmanode.local key made with parmanode? Even if your computer's
+    hostname is not 'parmanode' it should be fine, and parmanode uses the native 
+    hostname of the system embedded in the parmanode.local key." ; then
+    local key="$filepath/parmanode.local"
+else
+    while true ; do
+         announce "OK, go ahead and type in the full path to your key to sign. x to abort."
+         case $x in "") return 1 ; esac
+         yesorno "Use $enter_continue?" || continue
+         break
+    done
+    local key="$enter_cont"
+fi
+
+
+while true ; do
+clear ; echo -e "
+########################################################################################
+
+    This is the command that will be run. Make a note of where the new files are 
+    going.
+$green
     sudo openssl x509 -req \
-        -in "$filepath/parmanode.local.csr" \
-        -CA "$filepath/ca/ca.crt" \
+        -in "$key.csr" \
+        -CA "$CA_PUBKEY" \
         -CAkey "$RESTRICTED" \
         -CAcreateserial \
         -extfile /etc/ssl/parmanode/parmanode.ext \
-        -out "$filepath/parmanode.local.crt" \
+        -out "$key/.crt" \
         -days 36500 -sha256 \
-        2>>"$dp/error.log" || return 1
+        2>>"$dp/error.log" 
+$orange
+   Go ahead?
+   $green 
+                      y)$orange              yes
+   $red
+                      n)$orange              no
 
-    sudo chmod 640 "$filepath/parmanode.local.crt"
-    return 0
+
+########################################################################################
+"
+choose xpmq ; read choice ; clear
+jump $choice
+case $choice in
+q|Q) exit ;; m|M) back2main;; n|N|p|P) return 1 ;;
+y)
+break ;;
+*) invalid ;;
+esac
+done
+
+clear
+
+    sudo openssl x509 -req \
+        -in "$key.csr" \
+        -CA "$CA_PUBKEY" \
+        -CAkey "$RESTRICTED" \
+        -CAcreateserial \
+        -extfile /etc/ssl/parmanode/parmanode.ext \
+        -out "$key/.crt" \
+        -days 36500 -sha256 \
+        2>>"$dp/error.log" || { sww && return 1 ; }
+
+sudo chmod 640 "$filepath/parmanode.local.crt"
+success "The key has been signed."
 }
 
 
