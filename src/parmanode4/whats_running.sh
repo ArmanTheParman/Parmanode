a4106945759239540184d6a8a4bc7f932b7bf194
parman <sample@parmanode.com>
2025-11-15 21:36:05 +1100

    a

--- src/parmanode4/whats_running.sh
+++ src/parmanode4/whats_running.sh
@@ -7,18 +7,25 @@ function whats_running {
 #                    green:{}, docker:{}, lnbits:{} };
 
 isbitcoinrunning
+if [[ $bitcoinrunning == "true" ]] ; then echo "bitcoin is running" ; else echo "bitcoin is NOT running"
 iselectrsrunning
+if [[ $electrsrunning == "true" ]] ; then echo "electrs is running" ; else echo "electrs is NOT running"
 ismempoolrunning
+if [[ $mempoolrunning == "true" ]] ; then echo "mempool is running" ; else echo "mempool is NOT running"
 islndrunning
+if [[ $lndrunning == "true" ]] ; then echo "lnd is running" ; else echo "lnd is NOT running"
 isbtcpayrunning
+if [[ $btcpayrunning == "true" ]] ; then echo "btcpay is running" ; else echo "btcpay is NOT running"
 isnostrrunning
+if [[ $nostrrunning == "true" ]] ; then echo "nostr is running" ; else echo "nostr is NOT running"
 isrtlrunning
+if [[ $rtlrunning == "true" ]] ; then echo "rtl is running" ; else echo "rtl is NOT running"
 isthunderhubrunning
+if [[ $thunderhubrunning == "true" ]] ; then echo "thunderhub is running" ; else echo "thunderhub is NOT running"
 isalbyrunning
+if [[ $albyrunning == "true" ]] ; then echo "alby is running" ; else echo "alby is NOT running"
 isvaultwardenrunning
+if [[ $vaultwardenrunning == "true" ]] ; then echo "vaultwarden is running" ; else echo "vaultwarden is NOT running"
 isdockerrunning
-
-[[ $bitcoinrunning == "true" ]] && echo "bitcoin is running"
-
-
+if [[ $dockerrunning == "true" ]] ; then echo "docker is running" ; else echo "docker is NOT running"
 }
