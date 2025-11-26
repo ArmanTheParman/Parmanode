45410c24ce1a1fc749ca4071beb29dba34b33167
parman <sample@parmanode.com>
2025-11-27 02:33:05 +1100

    a

--- src/parmanode4/addtoconfig.sh
+++ src/parmanode4/addtoconfig.sh
@@ -4,10 +4,13 @@ function addToConfig {
 echo "$@" > /tmp/addtoconfig
 
 IFS=, read -r -a items <<<"$@"
-
-for item in ${items[@} ; do
+tmp1=$(mktemp)
+for item in ${items[@]} ; do
     key=${item%=*}
     val=${item#*=}
+    jq --arg key "$key" --arg val "$val" '.[$key] = $val' $p4 > $tmp1 && cp $tmp1 $p4
 done
+rm $tmp1
+}
 
 
