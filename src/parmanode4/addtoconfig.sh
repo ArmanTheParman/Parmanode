074a362d5a1b17df4ce41dcb1a6e4595ed546e41
parman <sample@parmanode.com>
2025-11-27 03:40:47 +1100

    a

--- src/parmanode4/addtoconfig.sh
+++ src/parmanode4/addtoconfig.sh
@@ -9,15 +9,15 @@ for item in "${items[@]}" ; do
     key=${item%=*}
     val=${item#*=}
 
-    if  [[ $(countchar "$key" "_") == 1 ]] ; then
-        key1=${key%_*}
-        key2=${key#*_}
+    if  [[ $(countchar "$key" "!") == 1 ]] ; then
+        key1=${key%!*}
+        key2=${key#*!}
         jq --arg key1 "$key1" --arg key2 "$key2" --arg val "$val" '.[$key1][$key2] = $val' $p4 > $tmp1 && cp $tmp1 $p4
-    elif  [[ $(countchar "$key" "_") == 2 ]] ; then
-        key1=${key%%_*}
-        key3=${key##*_}
-        int1=${key%_*}
-        key2=${int1#*_}
+    elif  [[ $(countchar "$key" "!") == 2 ]] ; then
+        key1=${key%%!*}
+        key3=${key##*!}
+        int1=${key%!*}
+        key2=${int1#*!}
         jq --arg key1 "$key1" --arg key2 "$key2" --arg key3 "$key3" --arg val "$val" '.[$key1][$key2][$key3] = $val' $p4 > $tmp1 && cp $tmp1 $p4
     else 
         jq --arg key "$key" --arg val "$val" '.[$key] = $val' $p4 > $tmp1 && cp $tmp1 $p4
