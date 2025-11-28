24f551fd4e72ea3e312858de3cf8bf72b5ffc1a7
parman <sample@parmanode.com>
2025-11-29 02:14:49 +1100

    a

--- src/parmanode4/makedir.sh
+++ src/parmanode4/makedir.sh
@@ -1,10 +1,8 @@
 function makedir {
-echo $(whoami) >> /tmp/whoamitest
-echo "ARG=[$1]" >> /tmp/makedir_debug.txt
 test -d "$1" && return 0
 
 mkdir -p "$1"
 
-ls -lah "$1" >$dn 2>&1 || errorlog "${FUNCNAME[0]}"
+ls -lah "$1" >$dn 2>&1 || return 1
 
 }
