75dc7db7e67e21c794c5dcdf2a72a71d94e2d9ab
parman <sample@parmanode.com>
2025-11-28 19:15:40 +1100

    a

--- src/parmanode4/addtoconfig.sh
+++ src/parmanode4/addtoconfig.sh
@@ -13,20 +13,24 @@ tmp1=$(mktemp)
 for item in "${items[@]}" ; do
 
     key=${item%=*}
-    val=${item#*=}
+    val=${item##*=}
+    if [[ $val =~ ^\{ ]] || [[ $val =~ ^\[ ]] ; then argtype="--argjson" ; else argtype="--arg" ; fi
+
 
     if  [[ $(countchar "$key" "!") == 1 ]] ; then
-        key1=${key%!*}
-        key2=${key#*!}
-        jq --arg key1 "$key1" --arg key2 "$key2" --arg val "$val" '.[$key1][$key2] = $val' $p4 > $tmp1 && cp $tmp1 $p4
+        key1=${key%!*} # no = sign exists to the left of the !
+        temp_key2=${key#*!} # equal sign exits to the right of !
+        key2=${temp_key2%=*}
+        jq --arg key1 "$key1" --arg key2 "$key2" $argtype val "$val" '.[$key1][$key2] = $val' $p4 > $tmp1 && cp $tmp1 $p4
     elif  [[ $(countchar "$key" "!") == 2 ]] ; then
         key1=${key%%!*}
-        key3=${key##*!}
+        temp_key3=${key##*!}
+        key3=${temp_key3%=*}
         int1=${key%!*}
         key2=${int1#*!}
-        jq --arg key1 "$key1" --arg key2 "$key2" --arg key3 "$key3" --arg val "$val" '.[$key1][$key2][$key3] = $val' $p4 > $tmp1 && cp $tmp1 $p4
+        jq --arg key1 "$key1" --arg key2 "$key2" --arg key3 "$key3" $argtype val "$val" '.[$key1][$key2][$key3] = $val' $p4 > $tmp1 && cp $tmp1 $p4
     else 
-        jq --arg key "$key" --arg val "$val" '.[$key] = $val' $p4 > $tmp1 && cp $tmp1 $p4
+        jq --arg key "$key" $argtype val "$val" '.[$key] = $val' $p4 > $tmp1 && cp $tmp1 $p4
     fi
 
 done
