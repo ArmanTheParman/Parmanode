c7ff1029e93afde7ec3c8c08ee8f37eca2e4e8f4
parman <sample@parmanode.com>
2025-11-30 23:09:56 +1100

    a

--- src/parmanode4/makedir.sh
+++ src/parmanode4/makedir.sh
@@ -1,9 +1,10 @@
 function makedir {
-echo "$1" >> $tmp/makedir.log
-test -d "$1" && return 0
-[[ ${1:0:1} == '/' ]] || return 1
-mkdir -p "$1"
-
-ls -lah "$1" >$dn 2>&1 || return 1
-
+#exit success if directory exits
+    test -d "$1" && return 0
+#exit error if not a absolute patch, otherwise directory can be made in a weird place
+    [[ ${1:0:1} == '/' ]] || return 1
+#make dir
+    mkdir -p "$1"
+#text is exits and is accessible to the current cgi user
+    ls -lah "$1" >$dn 2>&1 || return 1
 }
