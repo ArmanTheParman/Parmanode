db993c60530118674aa3deecb98d9fbe4e06effc
parman <sample@parmanode.com>
2025-12-02 01:31:25 +1100

    a

--- src/bitcoin/p4_install_bitcoin.sh
+++ src/bitcoin/p4_install_bitcoin.sh
@@ -1,5 +1,6 @@
 function p4_install_bitcoin {
 
+grep -q "bitcoin-end" $ic || { echo "Bitcoin detected installed in installed.conf" >> $dp/error.log && return 1 ; }
 jq .bitcoin $p4 | grep "clientchoice" | grep -qE "core|knots|deis" || { echo "clientchoice not set" >> $dp/error.log ; return 1 ; }
 jq .bitcoin $p4 | grep "bitcoin_compile" | grep -qE "true|false" || { echo "bitcoin_compile not set" >> $dp/error.log ; return 1 ; }
 jq .bitcoin $p4 | grep -q "version" || { echo "version not set" >> $dp/error.log ; return 1 ; }
