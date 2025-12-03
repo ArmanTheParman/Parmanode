fe762d2b4dc70522f1760755c80162ca4e54b345
parman <sample@parmanode.com>
2025-12-03 16:29:25 +1100

    a

--- src/parmanode4/p4signal.sh
+++ src/parmanode4/p4signal.sh
@@ -1,11 +1,11 @@
-function pvsignal {
+function p4signal {
     {
-    printf "###ParmaViewSigStart###"
+    printf "###ParmaViewSigStart###\n"
     # delimeter is \0 (null), so new lines are captured in the variable.
     # loop is actually redundant because of this, but leaving it in case I change the delimeter later.
     while IFS= read -r -d '' data ; do 
         printf "%s" "$data"
     done
-    printf "###ParmaViewSigEnd###"
+    printf "###ParmaViewSigEnd###\n"
     } > $parmanode_unix_socket
 }
