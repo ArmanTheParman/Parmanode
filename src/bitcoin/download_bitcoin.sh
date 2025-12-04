df2ed9336af916956813526b3bc54136ce5feeb9
parman <sample@parmanode.com>
2025-12-04 19:32:39 +1100

    a

--- src/bitcoin/download_bitcoin.sh
+++ src/bitcoin/download_bitcoin.sh
@@ -139,8 +139,8 @@ rm $hp/bitcoin/bitcoin.conf
 # "installs" bitcoin and sets to writing to only root for security. Read/execute for group and others. 
 # makes target directories if they don't exist
 # "install" is just a glorified copy command
-sudo install -m 0755 -o $(whoami) -g $(whoami) -t /usr/local/bin $hp/bitcoin/bin/*
-debug
+sudo install -m 0755 -o $USER -g $USER -t /usr/local/bin $hp/bitcoin/bin/*
+debug "$(whoami)
 sudo rm -rf $hp/bitcoin/bin
 sudo rm -rf $dp/temp >$dn 2>&1
 fi
