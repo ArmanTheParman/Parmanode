cdc45c87a4c7c4cfb752f1879010e927b3df59cc
parman <sample@parmanode.com>
2025-12-05 05:15:43 +1100

    a

--- src/patches/patch_10.sh
+++ src/patches/patch_10.sh
@@ -8,38 +8,45 @@ sudo chmod 440 /etc/sudoers.d/parmanode_extend_sudo_timeout
 
 sudo cp $torrc $torrc.bak
 
+temp=$(mktemp) 
 while [[ $(sudo grep -E "^ControlPort 9051" $torrc | wc -l) -gt 1 ]] ; do
-    export temp=$(mktemp) ; count=0
+    count=0
     while IFS= read -r x ; do
        if [[ $x =~ ^ControlPort[[:space:]]9051 ]] ; then let count++ ; if [[ $count == 2 ]] ; then continue ; fi ; fi
        echo "$x" >> $temp
     done < <(sudo cat $torrc)
-done
 sudo mv $temp $torrc
+done
+
+temp=$(mktemp) 
 while [[ $(sudo grep -E "^CookieAuthentication 1" $torrc | wc -l) -gt 1 ]] ; do
     export temp=$(mktemp) ; count=0
     while IFS= read -r x ; do
        if [[ $x =~ ^CookieAuthentication[[:space:]]1 ]] ; then let count++ ; if [[ $count == 2 ]] ; then continue ; fi ; fi
        echo "$x" >> $temp
     done < <(sudo cat $torrc)
-done
 sudo mv $temp $torrc
+done
+temp=$(mktemp) 
 while [[ $(sudo grep -E "^CookieAuthFileGroupReadable 1" $torrc | wc -l) -gt 1 ]] ; do 
     export temp=$(mktemp) ; count=0
     while IFS= read -r x ; do
        if [[ $x =~ ^CookieAuthFileGroupReadable[[:space:]]1 ]] ; then let count++ ; if [[ $count == 2 ]] ; then continue ; fi ; fi
        echo "$x" >> $temp
     done < <(sudo cat $torrc)
-done
 sudo mv $temp $torrc
+done
+temp=$(mktemp) 
 while [[ $(sudo grep -E "^DataDirectoryGroupReadable 1" $torrc | wc -l) -gt 1 ]] ; do 
     export temp=$(mktemp) ; count=0
     while IFS= read -r x ; do
        if [[ $x =~ ^DataDirectoryGroupReadable[[:space:]]1 ]] ; then let count++ ; if [[ $count == 2 ]] ; then continue ; fi ; fi
        echo "$x" >> $temp
     done < <(sudo cat $torrc)
-done
 sudo mv $temp $torrc
+done
+
+rm $temp 2>$dn
 
 parmanode_conf_remove "patch="
 parmanode_conf_add "patch=10"
