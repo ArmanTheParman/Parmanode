be0bd2d82f61a176e336346069fb87b38c5e9d93
parman <sample@parmanode.com>
2025-12-05 04:54:00 +1100

    a

--- src/patches/patch_10.sh
+++ src/patches/patch_10.sh
@@ -7,13 +7,13 @@ sudo chmod 440 /private/etc/sudoers.d/parmanode_extend_sudo_timeout
 
 #remove duplicates in torrc, makes it crap its pants.
 
-# while [[ $(sudo grep -E "^ControlPort 9051" $torrc | wc -l) -gt 1 ]] ; do
-#     temp=$(mktemp) ; count=0
-#     while IFS= read -r x ; do
-#        if [[ $x =~ ^ControlPort 9051 ]] ; then let count++ ; if [[ $count == 2 ]] ; then continue ; fi ; fi
-#        echo "$x" >> $temp
-#     done < <(sudo cat $torrc)
-# done
+while [[ $(sudo grep -E "^ControlPort 9051" $torrc | wc -l) -gt 1 ]] ; do
+    temp=$(mktemp) ; count=0
+    while IFS= read -r x ; do
+       if [[ $x =~ ^ControlPort 9051 ]] ; then let count++ ; if [[ $count == 2 ]] ; then continue ; fi ; fi
+       echo "$x" >> $temp
+    done < <(sudo cat $torrc)
+done
 
 # rm $temp
 # while [[ $(sudo grep -E "^CookieAuthentication 1" $torrc | wc -l) -gt 1 ]] ; do
