bf3d3ff4022e0b67bebd92feda6fc5a4b51275b9
parman <sample@parmanode.com>
2025-12-05 21:28:16 +1100

    a

--- src/parmanode4/p4run_maker.sh
+++ src/parmanode4/p4run_maker.sh
@@ -1,16 +1,18 @@
 function p4run_maker {
 
-file=$pn/src/parmanode4/p4run
+file="$pn/src/parmanode4/p4run"
+filefinal=/usr/local/bin/p4run
 hash=xxx
-grep -q $hash <(shasum -a 256 $file) || return 1
+grep -q "$hash" <(shasum -a 256 "$file") || return 1
 
 #put scrpt in secure place
-sudo cp -rf $file /usr/local/bin/
+sudo cp -f "$file" "$filefinal"
+sudo chown root:root "$filefinal"
+sudo chmod 4710 "$filefinal"
 
-if ! sudo test -f /etc/sudoers.d/parmanode || ! sudo grep -q "/usr/local/bin/p4run" /etc/sudoers.d/parmanode ; then {
+if ! sudo test -f /etc/sudoers.d/parmanode || ! sudo grep -q "/usr/local/bin/p4run" /etc/sudoers.d/parmanode ; then 
     #all script to run without password, but only from secure location
     sudoers_patch
-}
 fi
 
 }
