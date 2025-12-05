fc94d1d83796019da3a6de4cf5bcfa70f516bcf7
parman <sample@parmanode.com>
2025-12-05 17:51:21 +1100

    a

---
+++ src/parmanode4/p4run.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char ** argv) {
+
+if(argc == 1) { perror("No args. Exiting"); return 1 ; }
+
+if(strcmp(argv[1], "systemctl") && strcmp(argv[2], "restart") && strcmp(argv[3], "tor"))
+   execl(argv[1], argv[2], argv[3], argv[4], argv[5], NULL);
+
+return 0;
+}
