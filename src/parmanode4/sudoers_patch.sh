9515e712490a3ef1273e8d67e198287e095175e6
parman <sample@parmanode.com>
2025-12-05 22:54:48 +1100

    a

--- src/parmanode4/sudoers_patch.sh
+++ src/parmanode4/sudoers_patch.sh
@@ -8,20 +8,20 @@ file=/etc/sudoers.d/parmanode
 echo "$USER ALL=(root) NOPASSWD: /usr/local/bin/p4run *" | sudo tee -a /etc/sudoers.d/parmanode
 
 #Bitcoin commands
-echo "$USER ALL=(root) NOPASSWD: systemctl start bitcoind.service" | sudo tee -a /etc/sudoers.d/parmanode
-echo "$USER ALL=(root) NOPASSWD: systemctl stop bitcoind.service" | sudo tee -a /etc/sudoers.d/parmanode
-echo "$USER ALL=(root) NOPASSWD: systemctl restart bitcoind.service" | sudo tee -a /etc/sudoers.d/parmanode
-echo "$USER ALL=(root) NOPASSWD: systemctl start bitcoind" | sudo tee -a /etc/sudoers.d/parmanode
-echo "$USER ALL=(root) NOPASSWD: systemctl stop bitcoind" | sudo tee -a /etc/sudoers.d/parmanode
-echo "$USER ALL=(root) NOPASSWD: systemctl restart bitcoind" | sudo tee -a /etc/sudoers.d/parmanode
+echo "$USER ALL=(root) NOPASSWD: $(which systemctl) start bitcoind.service" | sudo tee -a /etc/sudoers.d/parmanode
+echo "$USER ALL=(root) NOPASSWD: $(which systemctl) stop bitcoind.service" | sudo tee -a /etc/sudoers.d/parmanode
+echo "$USER ALL=(root) NOPASSWD: $(which systemctl) restart bitcoind.service" | sudo tee -a /etc/sudoers.d/parmanode
+echo "$USER ALL=(root) NOPASSWD: $(which systemctl) start bitcoind" | sudo tee -a /etc/sudoers.d/parmanode
+echo "$USER ALL=(root) NOPASSWD: $(which systemctl) stop bitcoind" | sudo tee -a /etc/sudoers.d/parmanode
+echo "$USER ALL=(root) NOPASSWD: $(which systemctl) restart bitcoind" | sudo tee -a /etc/sudoers.d/parmanode
 
 #Tor commands
-echo "$USER ALL=(root) NOPASSWD: systemctl start tor.service" | sudo tee -a /etc/sudoers.d/parmanode
-echo "$USER ALL=(root) NOPASSWD: systemctl stop tor.service" | sudo tee -a /etc/sudoers.d/parmanode
-echo "$USER ALL=(root) NOPASSWD: systemctl restart tor.service" | sudo tee -a /etc/sudoers.d/parmanode
-echo "$USER ALL=(root) NOPASSWD: systemctl start tor" | sudo tee -a /etc/sudoers.d/parmanode
-echo "$USER ALL=(root) NOPASSWD: systemctl stop tor" | sudo tee -a /etc/sudoers.d/parmanode
-echo "$USER ALL=(root) NOPASSWD: systemctl restart tor" | sudo tee -a /etc/sudoers.d/parmanode
+echo "$USER ALL=(root) NOPASSWD: $(which systemctl) start tor.service" | sudo tee -a /etc/sudoers.d/parmanode
+echo "$USER ALL=(root) NOPASSWD: $(which systemctl) stop tor.service" | sudo tee -a /etc/sudoers.d/parmanode
+echo "$USER ALL=(root) NOPASSWD: $(which systemctl) restart tor.service" | sudo tee -a /etc/sudoers.d/parmanode
+echo "$USER ALL=(root) NOPASSWD: $(which systemctl) start tor" | sudo tee -a /etc/sudoers.d/parmanode
+echo "$USER ALL=(root) NOPASSWD: $(which systemctl) stop tor" | sudo tee -a /etc/sudoers.d/parmanode
+echo "$USER ALL=(root) NOPASSWD: $(which systemctl) restart tor" | sudo tee -a /etc/sudoers.d/parmanode
 
 return 0
 }
