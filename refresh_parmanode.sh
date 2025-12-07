f610c7f74095b60c4cabfb91b1260b25882c9243
parman <sample@parmanode.com>
2025-12-07 19:17:49 +1100

    a

--- refresh_parmanode.sh
+++ refresh_parmanode.sh
@@ -6,7 +6,7 @@ if ! git config --global user.name >/dev/null  ; then git config --global user.n
 
 cd $HOME/parman_programs >/dev/null
 mv $HOME/parman_programs/parmanode $HOME/parman_programs/parmanode_temp
-git clone https://github.com/armantheparman/parmanode.git && rm -rf $HOME/parman_programs/parmanode
+git clone https://github.com/armantheparman/parmanode.git && rm -rf $HOME/parman_programs/parmanode_temp
 echo -e "\n\nA new Parmanode directory has been cloned.
 You must exit this directory and re-enter it to refresh its contents.\n"
 
