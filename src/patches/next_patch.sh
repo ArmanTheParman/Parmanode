c3aaf5308024d2f7eb80b7c40f3457101468c2d1
parman <sample@parmanode.com>
2025-12-16 03:17:42 +1100

    a

--- src/patches/next_patch.sh
+++ src/patches/next_patch.sh
@@ -2,6 +2,7 @@ function next_patch { debugf
 #for patch 11
 make_bitcoind_service_file "setup"
 make_electrs_serivce_file "setup"
+make_fulcrum_serivce_file "setup"
 sudoers_patch #service files will be created and moved to /usr/local/parmanode for later access, so copy command added
 return 0
 
