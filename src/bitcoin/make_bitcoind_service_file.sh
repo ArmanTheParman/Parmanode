d40b15ab1c94b632c6c4abb4ff3238b8a077f465
parman <sample@parmanode.com>
2025-12-16 03:00:23 +1100

    a

--- src/bitcoin/make_bitcoind_service_file.sh
+++ src/bitcoin/make_bitcoind_service_file.sh
@@ -75,7 +75,7 @@ if [[ $1 == "setup" ]] ; then
     sudo chown root:root /usr/local/parmanode/bitcoind.service
     sudo chmod 655 /usr/local/parmanode/bitcoind.service
 else
-    sudo mv $file sudo /etc/systemd/system/bitcoind.service 
+    sudo mv $file /etc/systemd/system/bitcoind.service 
 fi
 
 sudo systemctl daemon-reload 
