bf53e7dfb7bfbc038bbd5e343436c0c126c9aa9a
parman <sample@parmanode.com>
2025-12-16 21:05:02 +1100

    a

--- restricted/patch.sh
+++ restricted/patch.sh
@@ -4,6 +4,6 @@ pn=$HOME/parman_programs/parmanode
 dn=/dev/null
 
 if grep -q a1dc45b634ebdf04f7766678c0d48f05f6ca586d4b9bd241fa50653e8d00da58 \
-     <(shasum -a 256 $pn/restricted/compile_dependency_script.sh) ; then
-     sudo cp -r $pn/restricted/compile_dependency_script.sh /usr/local/parmanode >$dn 2>&1
+     <(shasum -a 256 $pn/restricted/scripts/compile_dependency_script.sh) ; then
+     sudo cp -r $pn/restricted/scripts/compile_dependency_script.sh /usr/local/parmanode >$dn 2>&1
 fi
