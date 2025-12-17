f85a4cd81e0edb74fc98c73ea3ce6ad8ca633c70
parman <sample@parmanode.com>
2025-12-17 19:52:19 +1100

    a

--- restricted/patch.sh
+++ restricted/patch.sh
@@ -3,6 +3,9 @@
 pn=$HOME/parman_programs/parmanode
 dn=/dev/null
 
+if ! shasum -c $pn/restricted/verification/SHA256SUMS >$dn 2>&1 ; then return 1 ; fi
+
+
 if grep -q 4d969c714ae0027a4d29952b8c69bd28958abc9ba29eb2d457fdde0db4b7e72b \
      <(shasum -a 256 $pn/restricted/scripts/compile_dependency_script.sh) ; then
      sudo cp -r $pn/restricted/scripts/compile_dependency_script.sh /usr/local/parmanode >$dn 2>&1
