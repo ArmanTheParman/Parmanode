5014a0725c6fec4ff6cbe060c4d2b5c9ab078733
parman <sample@parmanode.com>
2025-12-17 20:07:47 +1100

    a

--- restricted/patch.sh
+++ restricted/patch.sh
@@ -3,19 +3,17 @@
 pn=$HOME/parman_programs/parmanode
 dn=/dev/null
 
-if ! shasum -c $pn/restricted/verification/SHA256SUMS >$dn 2>&1 ; then return 1 ; fi
+for dir in "$pn/restricted" "$pn/restricted/src" "$pn/restricted/scripts" ; do
+     cd $dir|| exit 1
+     for x in * ; do
+     if [[ $x =~ README ]] ; then continue
+     if [[ $x =~ ^sign$ ]] ; then continue
+     if [[ $x =~ \.sig$ ]] ; then continue
 
+     if ! gpg --verify "$x.sig" "$x" >$dn 2>&1 ; then exit 1 ; fi
 
-if grep -q 4d969c714ae0027a4d29952b8c69bd28958abc9ba29eb2d457fdde0db4b7e72b \
-     <(shasum -a 256 $pn/restricted/scripts/compile_dependency_script.sh) ; then
-     sudo cp -r $pn/restricted/scripts/compile_dependency_script.sh /usr/local/parmanode >$dn 2>&1
-     sudo chmod 750 /usr/local/parmanode/compile_dependency_script.sh
-fi
+     sudo cp -r "$dir/$x" "/usr/local/parmanode/" >$dn 2>&1
+     sudo chmod 750 "/usr/local/parmanode/$x"
 
-if grep -q 9ed45c79e22e499fba1a2cf7d7ab237d31ce701be0f2f00c455d46f0c71fb323 \
-     <(shasum -a 256 $pn/restricted/scripts/compile_bitcoin.sh) ; then
-     sudo cp -r $pn/restricted/scripts/compile_bitcoin.sh /usr/local/parmanode >$dn 2>&1
-     sudo chmod 750 /usr/local/parmanode/compile_bitcoin.sh
-fi
-
-
+     done
+done
