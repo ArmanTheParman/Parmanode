2eb1e195fdeee80c1dfb82fa1e5f5dd04e30253a
parman <sample@parmanode.com>
2025-12-18 17:05:53 +1100

    a

--- src/bitcoin/compile_bitcoin.sh
+++ src/bitcoin/compile_bitcoin.sh
@@ -106,8 +106,8 @@ fi
 #clean up variables
     unset GIT_AUTHOR_NAME
     unset GIT_AUTHOR_EMAIL
-    unset export GIT_COMMITTER_NAME
-    unset export GIT_COMMITTER_EMAIL
+    unset GIT_COMMITTER_NAME
+    unset GIT_COMMITTER_EMAIL
 
 
 ! [[ $clientchoice == "deis" ]] && while true ; do
