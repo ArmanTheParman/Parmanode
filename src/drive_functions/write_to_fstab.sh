fd803e4895f3b1394089e00de789c2d8bac63df7
parman <sample@parmanode.com>
2025-12-22 00:33:11 +1100

    a

--- src/drive_functions/write_to_fstab.sh
+++ src/drive_functions/write_to_fstab.sh
@@ -5,9 +5,9 @@ function write_to_fstab {
    if [[ $TYPE != "ext4" ]] ; then 
    TYPE=$(sudo blkid | grep $UUID | awk '{print $5}' | cut -d \" -f 2)
    fi
-   debug "TYPE detected, $TYPE"
    # exfat drives don't work in fstab and cause issues.
-   if [[ $TYPE != "ext4" ]] ; then return 1 ; fi
+   
+   if [[ $TYPE != "ext4" ]] ; then debug "TYPE detected, $TYPE" ; return 1 ; fi
 
    if [[ -z $1 ]] ; then 
    export $(sudo blkid -o export $disk | grep UUID)
@@ -16,6 +16,7 @@ function write_to_fstab {
    fi
 
    if [ -z $UUID ] ; then return 1 ; fi
+   debug "before add fstab... $USER $UUID $TYPE"
    [[ $parmaview == 1 ]] && sudo /usr/local/parmanode/p4run "add_fstab" "$USER" "$UUID" "$TYPE"
    [[ $parmaview != 1 ]] && echo "UUID=$UUID /media/$USER/parmanode $TYPE defaults,nofail,noatime,x-systemd.device-timeout=20s 0 2" | sudo tee -a /etc/fstab > $dn 
    sudo systemctl daemon-reload >$dn 2>&1 #needed to reload fstab to systemd
