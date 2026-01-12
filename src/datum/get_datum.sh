7b5971179608daa61c7a7ff3b7f3d461d63b71b5
Parman <armantheparman@protonmail.com>
2026-01-12 11:14:40 +1100

    a

--- src/datum/get_datum.sh
+++ src/datum/get_datum.sh
@@ -1,15 +1,14 @@
 function get_datum {
-        debug 1
+
 if [[ $OS == "Mac" ]] ; then no_mac ; return 1 ; fi
-debug 2
+
 if [[ $preconfigure_parmadrive == "true" ]] ; then
    if grep -q "datum-end" $ic ; then return 0 ; fi
 fi
-debug 3
-#if [[ $(uname -m) != "x86_64" ]] ; then  { announce_blue "Datum is only supported on x86_64 machines at this stage." ; continue ; } ; fi
+
 [[ -e $dp/.datum_enabled ]]  || {
 please_wait
-debug 4
+
 announce_blue "
     To install Datum with Parmanode, please send$green 42 sats$blue over lightning via 
     NOSTR zap, or the donations page:
@@ -18,7 +17,7 @@ $cyan    https://armantheparman.com/donations $blue
 
     Then inform Parman by email$cyan armantheparman@protonmail.com$blue
    "
-debug 5
+
 announce_blue "
     For pre-configurd Bitcoin Knots, ParMiner and Datum, please see...
 $orange
@@ -31,15 +30,21 @@ $orange
 return 1
 }
 
-git clone git@github-datum:armantheparman/datum_parmanode.git $pp/datum 2>$dn || {
-cd $pp/datum 2>/dev/null && git pull >$dn 2>&1 ; } || \
-{ enter_continue "Please contact Parman to enable Datum on your machine.\n$orange" ; return 1 ; } #requires SSH key authority
+if ! git clone git@github-datum:armantheparman/datum_parmanode.git $pp/datum 2>$dn ; then
+    cd $pp/datum 2>/dev/null && git pull >$dn 2>&1 ; 
+fi
+
+if ! test -d $pp/datum ; then
+        enter_continue "Datum download seems to have failed. Please contact Parman to enable Datum on your machine.\n$orange" 
+        return 1 
+fi
+    
 
 for file in $pp/datum/src/*.sh ; do
 source $file
 done
 if [[ $preconfigure_parmadrive == "true" ]] ; then return 0 ; fi
-debug 6
+
 menu_datum
 return 0
 }
