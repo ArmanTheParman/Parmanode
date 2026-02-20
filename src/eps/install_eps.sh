d46f7cc23ee9994d0ba061c6d01b972a22263738
parman <sample@parmanode.com>
2026-02-20 22:59:28 +1100

    a

--- src/eps/install_eps.sh
+++ src/eps/install_eps.sh
@@ -77,30 +77,14 @@ if [[ $drive_eps == "internal" ]] ; then
 restore_internal_eps_db || return 1
 fi
 
-#config
-########################################################################################################################
-########################################################################################################################
-    #make_eps_config ; debug "config done"
-mv $hp/eps/config.ini_sample $hp/eps/config.ini
-gsed -i "s|.*rpc_user =.*|rpc_user = $rpcuser|g" $hp/eps/config.ini
-gsed -i "s|.*rpc_password =.*|rpc_password = $rpcpassword|g" $hp/eps/config.ini
-gsed -i "s|.*host =.*|host = 0.0.0.0|g" $hp/eps/config.ini
-gsed -i "s|.*port =.*|port = 50009|g" $hp/eps/config.ini
-gsed -i "s|.*certfile =.*|certfile = $hp/eps/cert.pem|g" $hp/eps/config.ini
-gsed -i "s|.*keyfile =.*|keyfile = $hp/eps/cert.pem|g" $hp/eps/config.ini
+make_eps_config ; debug "config done"
 
-########################################################################################################################
-########################################################################################################################
-
-########################################################################################################################
-########################################################################################################################
-#if [[ $OS == "Linux" ]] ; then make_eps_service ; debug "service file done" ; fi
-########################################################################################################################
-########################################################################################################################
+if [[ $OS == "Linux" ]] ; then make_eps_service ; debug "service file done" ; fi
 
 installed_config_add "eps-end" 
 
 eps_tor
+
 success "Electrum Personal Server has been installed"
 
 isbitcoinrunning
