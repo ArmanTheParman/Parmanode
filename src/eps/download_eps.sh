ebff2ef6685026ba5c23a79a5a3f9a6476d59664
parman <sample@parmanode.com>
2026-02-22 15:33:57 +1100

    a

--- src/eps/download_eps.sh
+++ src/eps/download_eps.sh
@@ -3,7 +3,7 @@ function download_eps {
     cd $hp/eps || { sww && return 1 ; }
     curl -LO https://github.com/chris-belcher/electrum-personal-server/archive/refs/tags/eps-v0.2.4.tar.gz
     curl -LO https://github.com/chris-belcher/electrum-personal-server/releases/download/eps-v0.2.4/eps-v0.2.4.tar.gz.asc
-    gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys 658E64021E5793C6C4E15E45C2E581F5B998F30E >$dn 2>&1
+    gpg --keyserver hkps://keyserver.ubuntu.com --recv-keys 0A8B038F5E10CC2789BFCFFFEF734EA677F31129 >$dn 2>&1
     gpg --verify eps-v0.2.4.tar.gz.asc eps-v0.2.4.tar.gz || { sww && return 1 ; }
     tar -xzf eps-v0.2.4.tar.gz --strip-components=1
     echo "$green"
