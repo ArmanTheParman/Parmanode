77796c94f24b0ed78677b254321d9a75454c3779
parman <sample@parmanode.com>
2026-04-21 21:19:39 +1000

    sign

--- src/patches/make_restricted_bucket.sh
+++ src/patches/make_restricted_bucket.sh
@@ -2,20 +2,24 @@
 function make_restricted_bucket { debugf
 if [[ $OS == "Mac" ]] ; then return 0 ; fi
 
-sudo mkdir -p /usr/local/parmanode/{src,scripts,service,keys}
-sudo chown root:$(id -gn) /usr/local/parmanode/{src,scripts,service,keys}
-sudo chmod 750 /usr/local/parmanode/{src,scripts,service,keys}
+#Old versions, clear previous files...
+if grep -q restricted_bucket $pc ; then
+  sudo rm -rf /usr/local/parmanode/* >/dev/null 2>&1
+fi
+
+sudo mkdir -p /usr/local/parmanode/{src,scripts,service}
+sudo chown root:$(id -gn) /usr/local/parmanode/{src,scripts,service}
+sudo chmod 750 /usr/local/parmanode/{src,scripts,service}
 
 #Parman Pub Key Check and temp import
-sudo cp -f $pn/src/keys/parman.asc /usr/local/parmanode/keys/
-sudo cp $pn/src/keys/parman.asc /usr/local/parmanode/keys
-if ! grep -q d88f138fb707f53fb106895a6891b3615494ec9e3a509988ab02aad93aef4edc <(shasum -a 256 /usr/local/parmanode/keys/parman.asc) ; then 
-    sudo rm -f /usr/local/parmanode/keys/parman.asc >/dev/null 2>&1                      
+sudo cp $pn/src/keys/parman.asc /usr/local/parmanode/
+if ! grep -q d88f138fb707f53fb106895a6891b3615494ec9e3a509988ab02aad93aef4edc <(shasum -a 256 /usr/local/parmanode/parman.asc) ; then 
+    sudo rm -f /usr/local/parmanode/parman.asc >/dev/null 2>&1                      
     sww "Unexpect error with Parman's pubkey. Aborting restricted bucket function." 
     debug
     return 1
 fi
-sudo gpg --no-default-keyring --keyring /usr/local/parmanode/keys/parman.gpg --import /usr/local/parmanode/keys/parman.asc >$dn 2>&1
+sudo gpg --no-default-keyring --keyring /usr/local/parmanode/parman.gpg --import /usr/local/parmanode/parman.asc >$dn 2>&1
 debug
 #make script to move files into restricted location for sudoers to use
    # check existing files in $pn/restriced, except README
@@ -26,17 +30,18 @@ cat <<EOF | sudo tee /usr/local/parmanode/scripts/patchrunner.sh >$dn
 #!/bin/bash
 
 for dir in "$pn/restricted" "$pn/restricted/src" "$pn/restricted/scripts" ; do
-     cd \$dir || exit 1
+     cd \$dir || continue #if dir is a file, continue the loop, only want directories
 
-     for x in * ; do
+     for x in * ; do  #after cd into a directory, loop the files...
          if [[ \$x =~ README ]] ; then continue ; fi
          if [[ \$x =~ ^sign$ ]] ; then continue ; fi
          if [[ \$x =~ \.sig$ ]] ; then continue ; fi
 
-         if test -d \$dir/\$x ; then continue ; fi #skip directory verification
+         if test -d \$dir/\$x ; then continue ; fi #skip directory verification #check it's not a directory otherwise continue loop
 
-         if ! gpgv --keyring /usr/local/parmanode/keys/parman.gpg "\$x.sig" "\$x" >$dn 2>&1 ; then exit 1 ; fi
-
+         if ! gpgv --keyring /usr/local/parmanode/parman.gpg "\$x.sig" "\$x" >$dn 2>&1 ; then exit 1 ; fi
+         
+         #copy the files...
          if [[ \$dir =~ src$ ]] ; then
                sudo cp -r "\$dir/\$x" "/usr/local/parmanode/src/\$x" >$dn 2>&1
                sudo chmod 750 "/usr/local/parmanode/src/\$x"
@@ -51,33 +56,10 @@ for dir in "$pn/restricted" "$pn/restricted/src" "$pn/restricted/scripts" ; do
      done
 done
 
-
-   file="$pn/restricted/p4run"
-   filefinal=/usr/local/parmanode/p4run
-
-   test -f \$file || break
-   test -f \$file.sig || break
-
-   cp -f "\$file" "\$filefinal" >/dev/null 2>&1
-   cp -f "\$file.sig" "\$filefinal.sig" >/dev/null 2>&1
-   chown root:$(id -gn) "\$filefinal"
-   chmod 710 "\$filefinal"
-
-   if ! gpgv --keyring /usr/local/parmanode/parman.gpg \$filefinal.sig \$filefinal >/dev/null 2>&1 ; then
-      rm \$filefinal{,.sig} >/dev/null 2>&1
-      exit 1
-   else
-      break   
-   fi
-
-   break
-
-done
-
 /usr/local/parmanode/patch.sh >/dev/null 2>&1
 exit 0
 EOF
-sudo chmod 750 /usr/local/parmanode/scripts/patchrunner.sh
+sudo chmod 750 /usr/local/parmanode/scripts/patchrunner.sh #should exist in sudoers
 sudo /usr/local/parmanode/scripts/patchrunner.sh #run once
 parmanode_conf_remove "restricted_bucket=true" #remove old flag
 parmanode_conf_add "restricted_bucket_v2=true"
