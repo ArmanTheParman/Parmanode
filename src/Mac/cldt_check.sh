dac4dc017f78d4e3a71a9773caea358898647057
parman <sample@parmanode.com>
2026-05-09 11:34:30 +1000

    a

--- src/Mac/cldt_check.sh
+++ src/Mac/cldt_check.sh
@@ -5,18 +5,18 @@ if xcode-select -p >$dn 2>&1 ; then return 0 ; fi
 while true ; do
 clear
 echo -e "
-########################################################################################
+###################################################################################
 
-    The 'Command Line Developer Tools' package is needed on your system. It can take 
-    a few minutes to install. 
+    The 'Command Line Developer Tools' package is needed on your system. It can
+    take a few minutes to install. 
 
     Proceed?
 $cyan
-            y)$orange    Yes please, I don't know what's going on but I'll Google it.
+            y)$orange    Yes please, IDK what's going on but I'll Google it.
 $cyan
             n)$orange    Nah, abandon for no reason.
 
-########################################################################################        
+####################################################################################
 "
 choose xpq ; read choice ; clear
 case $choice in
