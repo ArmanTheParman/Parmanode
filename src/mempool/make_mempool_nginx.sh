8066e969d94e36968971b9c7503461c5a352b63c
parman <sample@parmanode.com>
2026-05-14 01:22:51 +1000

    a

--- src/mempool/make_mempool_nginx.sh
+++ src/mempool/make_mempool_nginx.sh
@@ -11,8 +11,16 @@ server {
 
     location / {
         proxy_pass http://localhost:8180;
+        proxy_http_version 1.1;
         proxy_set_header Host \$host;
+        proxy_set_header Host \$host:\$server_port;
         proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto https;
+        proxy_set_header X-Forwarded-Host \$host;
+        proxy_set_header X-Forwarded-Port 8181;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection "upgrade";
     }
 }
 EOF
