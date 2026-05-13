12a7a4ea2bd06c702179230b79baadde21f22c93
parman <sample@parmanode.com>
2026-05-14 01:23:16 +1000

    a

--- src/mempool/make_mempool_nginx.sh
+++ src/mempool/make_mempool_nginx.sh
@@ -12,7 +12,6 @@ server {
     location / {
         proxy_pass http://localhost:8180;
         proxy_http_version 1.1;
-        proxy_set_header Host \$host;
         proxy_set_header Host \$host:\$server_port;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
