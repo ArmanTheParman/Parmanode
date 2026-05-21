3e2e16b908476e1b7a79a3fba4e47ac6ca65ffe6
parman <sample@parmanode.com>
2026-05-22 00:40:05 +1000

    qr

--- src/MENUS/jump.sh
+++ src/MENUS/jump.sh
@@ -4,6 +4,11 @@ case $1 in
 q|quit|exit) exit ;;
 m|main) back2main ;;
 
+qr)
+qrcapture
+invalid_flag=set
+;;
+
 gp)
 clear
 cd $pn 
---
+++ src/tools/qrcapture.sh
@@ -0,0 +1,50 @@
+function install_qrcapture {
+
+sudo apt-get -y update 
+sudo apt-get -y install zbar-tools #fswebcam
+
+#usage
+    #zbarcam --raw | head -n 1 > qr_output.txt
+
+}
+
+function qrcapture {
+
+
+if ! which zbarcam >$dn 2>&1 ; then
+   yesorno "zbarcam is not installed. Would you like to install it?" || return 1
+   install_qrcapture
+fi
+
+result=$(zbarcam --raw | head -n 1) 
+
+if [[ -z $1 ]] ; then
+    
+    announce "QR code results:
+$green
+$result
+$orange
+
+    You have options...
+
+          <enter>    Do nothing and move on
+          f)         Save restuls to ~/Desktop/qrresult.txt
+"
+case $enter_cont in
+    f)
+        echo "$result" > ~/Desktop/qrresult.txt
+        announce "Saved to ~/Desktop/qrresult.txt"
+        ;;
+    *)
+        ;;
+esac
+
+elif [[ $1 == "sdout" ]] ; then
+    echo "$result"
+else
+    echo "$result"
+    enter_continue
+fi
+
+
+}
