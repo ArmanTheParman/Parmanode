62e99913dc9b02ecf90ebd8a41598f729e6828bc
parman <sample@parmanode.com>
2026-05-26 18:51:14 +1000

    a

--- src/start/custom_startup.sh
+++ src/start/custom_startup.sh
@@ -21,7 +21,7 @@ bash --rcfile $pn/src/tools/rcfile
 exit 
 fi
 
-if [[ $uninstall_homebrew == true ]] ; then
+if [[ "$uninstall_homebrew" == true ]] ; then
 uninstall_homebrew || exit
 success "Homebrew uninstalled"
 fi
@@ -36,31 +36,4 @@ uninstall_core_lightning
 exit
 fi
 
-if [[ $1 == "parminerkey" ]] ; then
-if yesorno_blue "This will print for you the authentication key to pass on to Parman.
-    
-    You have choices..." "qr" "Print QR Code" "text" "Just text on the screen" ; then
-sudo true
-clear
-which qrencode || install_qrencode silent
-set_terminal 77 150
-echo -e "${cyan}ID...\n"
-qrencode -t ANSIUTF8 "$(sudo cat $macprefix/var/lib/tor/parmanode-service/hostname | cut -d \. -f1)"
-echo -e "\n${cyan}Key...\n"
-qrencode -t ANSIUTF8 "$(sudo cat ~/.ssh/extra_keys/parminer-key.pub)"
-echo -e "$green\nTake a photo and send to Parman for ParMiner access.$orange"
-enter_continue
-else
-sudo true
-clear
-echo -e "${cyan}ID...\n"
-sudo cat $macprefix/var/lib/tor/parmanode-service/hostname | cut -d \. -f1
-echo -e "\n{$cyan}Key...\n"
-sudo cat ~/.ssh/extra_keys/parminer-key.pub
-echo -e "$green\nTake a photo and send to Parman for ParMiner access.$orange"
-enter_continue
-fi
-
-exit
-fi
 }
