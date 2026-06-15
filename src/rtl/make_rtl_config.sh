c5467aa021aedfa4e0d146400b462c236e73f8e7
parman <sample@parmanode.com>
2026-06-16 02:00:47 +1000

    a

--- src/rtl/make_rtl_config.sh
+++ src/rtl/make_rtl_config.sh
@@ -5,7 +5,7 @@ else
 localhostaddr=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' lnd)
 fi
 
-if ! grep -q "cln-end" $ic ; then
+if grep -q "cln-end" $ic ; then
 lnImplementation="CLN"
 rest=3777
 configpath="/home/parman/.lightning/config" #/home/parman is correct as it's internal to container, see run command
@@ -15,9 +15,37 @@ rest=8080
 configpath="/home/parman/.lnd/lnd.conf" #/home/parman is correct as it's internal to container, see run command
 fi
 
+if ! grep -q "cln-end" $ic ; then
+node=$(cat <<EOF
+    {
+      "index": 1,
+      "lnNode": "Node 1",
+      "lnImplementation": "$lnImplementation",
+      "Authentication": {
+       "macaroonPath": "/home/parman/.lnd/data/chain/bitcoin/mainnet",
+        "configPath": "$configpath",
+        "swapMacaroonPath": "",
+        "boltzMacaroonPath": ""
+      },
+EOF
+)
+else
+node=$(cat <<EOF
+  {
+      "index": 1,
+      "lnNode": "Core Lightning Testnet # 1",
+      "lnImplementation": "CLN",
+      "authentication": {
+        "runePath": "/home/parman/.lightning/bitcoin/",
+        "configPath": "$configpath"
+      },
+EOF
+)
+fi    
+
 echo "  
 {
-  \"multiPass\": \"$rlt_password\",
+  \"multiPass\": \"$rtl_password\",
   \"port\": \"3000\",
   \"defaultNodeIndex\": 1,
   \"dbDirectoryPath\": \"/home/parman/RTL\",
@@ -27,16 +55,7 @@ echo "
     \"logoutRedirectLink\": \"\"
   },
   \"nodes\": [
-    {
-      \"index\": 1,
-      \"lnNode\": \"Node 1\",
-      \"lnImplementation\": \"$lnImplementation\",
-      \"Authentication\": {
-       \"macaroonPath\": \"/home/parman/.lnd/data/chain/bitcoin/mainnet\",
-        \"configPath\": \"$configpath\",
-        \"swapMacaroonPath\": \"\",
-        \"boltzMacaroonPath\": \"\"
-      },
+$node
       \"Settings\": {
         \"userPersona\": \"MERCHANT\",
         \"themeMode\": \"DAY\",
@@ -52,5 +71,6 @@ echo "
     }
   ]
 }" > $tmp/RTL-Config.json 2>$dn
-unset rest
+
+unset rest node
 }
