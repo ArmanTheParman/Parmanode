5917648cca22d089911cdc460840ab0b352b987e
parman <sample@parmanode.com>
2026-06-16 12:06:48 +1000

    a

--- src/core_lightning/install_core_lightning.sh
+++ src/core_lightning/install_core_lightning.sh
@@ -115,6 +115,9 @@ announce "${green}Will make Core Lightning configuration file at $HOME/.lightnin
 bitcoin__rpcport="$(cat $HOME/.bitcoin/bitcoin.conf | grep rpcport | tail -n 1 | cut -d = -f 2)" #no hyphens in bash variables
 bitcoin__rpcport=${bitcoin__rpcport:-8332} #default
 
+random=$(dd if=/dev/urandom bs=1 count=50 2>/dev/null)
+aliasrand=$(echo "$random $(date)" | shasum -a 256 | sed -E 's/^(.{15}).*$/\1/')
+
 announce-addr-discovered-port
 cat <<EOF | tee $HOME/.lightning/config
 #daemon --don't use daemon if using systemd service file
@@ -126,7 +129,7 @@ bitcoin-rpcuser=$(cat $HOME/.bitcoin/bitcoin.conf | grep rpcuser | cut -d = -f 2
 bitcoin-rpcpassword=$(cat $HOME/.bitcoin/bitcoin.conf | grep rpcpassword | cut -d = -f 2)
 bitcoin-rpcconnect=127.0.0.1
 bitcoin-rpcport=$bitcoin__rpcport
-alias=BananaStand
+alias=BananaStand_$aliasrand
 clnrest-port=3777
 clnrest-host=127.0.0.1
 #fee-base=MILLISATOSHI
