927df101d3ce85cd696f10c6941f51d9e56ef75a
parman <sample@parmanode.com>
2026-08-18 16:37:07 +1000

    a

--- src/bitcoin/make_bitcoind_service_file.sh
+++ src/bitcoin/make_bitcoind_service_file.sh
@@ -76,7 +76,7 @@ if [[ $1 == "setup" ]] ; then #for parmanode installation
     sudo chmod 655 /usr/local/parmanode/bitcoind.service
 elif [[ $parmaview == 1 ]] ; then #for parmaview method of bitcoin install
     export servicefile
-    sudo /usr/local/parmanode/p4run "bitcoin_binary_symlinks"
+    sudo /usr/local/parmanode/p4run "bitcoin_binary_symlinks" >>$pvlog 2>&1
 else #for regular bitcoin install using backend parmanode
     sudo mv "$servicefile" /etc/systemd/system/bitcoind.service 
 fi
