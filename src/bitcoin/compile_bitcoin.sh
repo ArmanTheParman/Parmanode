9326b59b94f8dff6c8594bafe7e8bcbaaaf28193
parman <sample@parmanode.com>
2026-08-21 16:13:19 +1000

    a

--- src/bitcoin/compile_bitcoin.sh
+++ src/bitcoin/compile_bitcoin.sh
@@ -155,7 +155,7 @@ $orange
    The default will be chosen in 30 seconds
 
 ########################################################################################
-" | tee $tmp/configure_menu
+" &| tee $tmp/configure_menu
 options="" #safety, redundant
 { [[ $parmaview != 1 ]] && read -t 30 options ; } || options="" 
 clear
@@ -171,7 +171,7 @@ echo -e "
     Hit y to accept, or n to try again.
 
 ########################################################################################
-"
+" &| tee $tmp/parmaview_bitcoincompile_options.log
     [[ $parmaview != 1 ]] && read choice
     [[ $parmaview == 1 ]] && choice=y
     
