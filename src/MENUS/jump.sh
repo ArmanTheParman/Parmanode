64b193c8ae715e1045bb8c2ab029ff7f741a7d98
parman <sample@parmanode.com>
2026-08-23 22:29:28 +1000

    a

--- src/MENUS/jump.sh
+++ src/MENUS/jump.sh
@@ -31,7 +31,7 @@ gp)
 clear
 cd $pn 
 git pull
-announce "Parmanode has been updated with the 'git pull' command. Please restart Parmanode 
+enter_continue "Parmanode has been updated with the 'git pull' command. Please restart Parmanode 
     for the changes to take effect."
 invlid_flag=set
 ;;
