c7169e9066215d4e7cd015690b8c8ce0dcca41ad
parman <sample@parmanode.com>
2026-08-28 23:51:57 +1000

    a

--- src/lnd/menu_lnd_more.sh
+++ src/lnd/menu_lnd_more.sh
@@ -3,7 +3,11 @@ if ! grep -q "lnd.*end" $ic && ! grep -q "litd-end" $ic ; then return 0 ; fi
 store_LND_container_IP
 debug "store LND container IP"
 source $pc
+if grep -q "lnddocker" $ic ; then
 menuDockerIP="$bright_blue                 IP address of LND (Docker Container) = $LNDIP$orange"
+else
+unset menuDockerIP
+fi
 
 while true ; do set_terminal ; echo -en "
 ########################################################################################$cyan
